0eb3c346c6e4e057becbb1ba6f5a2aa95c82e1d4
vithushan <vithushan19@gmail.com>
2020-12-26 02:11:12 -0500

    default practice questions

--- pages/practice/[slug].js
+++ pages/practice/[slug].js
@@ -17,22 +17,32 @@ const Quiz = () => {
     }
   };
 
+  var choices = [
+    {text: "A"},
+    {text: "B"},
+    {text: "C"},
+    {text: "D"},
+  ]
+  if (data != undefined) {
+    choices = data[index].choices
+  }
+
   return (
     <div>
       <div className="bg-pink-500 p-4 m-4">Quiz {47 + index} / 50</div>
       <div className="p-4 m-4 bg-purple-500">{data[index].question}</div>
       <div className="grid grid-cols-1 lg:grid-cols-2">
         <div onClick={onClick} className="p-4 m-4 bg-red-500">
-          {data[index].choices[0].text}
+          {choices[0].text}
         </div>
         <div onClick={onClick} className="p-4 m-4 bg-green-500">
-          {data[index].choices[1].text}
+          {choices[1].text}
         </div>
         <div onClick={onClick} className="p-4 m-4 bg-blue-500">
-          {data[index].choices[2].text}
+          {choices[2].text}
         </div>
         <div onClick={onClick} className="p-4 m-4 bg-yellow-500">
-          {data[index].choices[3].text}
+          {choices[3].text}
         </div>
       </div>
     </div>
