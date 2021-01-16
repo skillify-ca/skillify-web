053ad830e2b9da8321a07e77ee98c3d41c038bed
vithushan <vithushan19@gmail.com>
2021-01-16 13:48:58 -0500

    create join and leave buttons

--- client/pages/dash.js
+++ client/pages/dash.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   useSubscription,
   gql,
+  useMutation,
 } from "@apollo/client";
-import { WebSocketLink } from 'apollo-link-ws'
-import Race from "./../components/Race"
+import { WebSocketLink } from "apollo-link-ws";
+import Race from "./../components/Race";
 
 const link = process.browser
   ? new WebSocketLink({
-    uri: `ws://localhost:4000/`,
+      uri: `ws://localhost:4000/`,
       options: {
         reconnect: true,
       },
@@ -34,6 +35,18 @@ const GET_MESSAGES = gql`
   }
 `;
 
+const ADD_USER = gql`
+  mutation($user: String!, $content: String!) {
+    postMessage(user: $user, content: $content)
+  }
+`;
+
+const DELETE_USER = gql`
+  mutation($id: ID!) {
+    deleteMessage(id: $id)
+  }
+`;
+
 const Messages = ({ user }) => {
   const { data } = useSubscription(GET_MESSAGES);
   if (!data) {
@@ -42,11 +55,41 @@ const Messages = ({ user }) => {
   return JSON.stringify(data);
 };
 
+const JoinButton = () => {
+  const [addUser] = useMutation(ADD_USER);
+  const onClick = () => {
+    console.log("Join");
+    addUser({
+      variables: { user: "Player", content: "0" },
+    });
+  };
+  return (
+    <div>
+      <button onClick={onClick}>Join Now</button>
+    </div>
+  );
+};
+
+const LeaveButton = () => {
+  const [deleteUser] = useMutation(DELETE_USER);
+  const onClick = () => {
+    console.log("Leave");
+    deleteUser({ variables: { id: 0 } });
+  };
+  return (
+    <div>
+      <button onClick={onClick}>Leave Now</button>
+    </div>
+  );
+};
+
 const Dash = () => {
   return (
     <ApolloProvider client={client}>
       <div>
         <Race />
+        <JoinButton />
+        <LeaveButton />
         <Messages user="Vithushan" />
       </div>
     </ApolloProvider>
