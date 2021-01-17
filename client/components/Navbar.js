34f6832d066d200507049bef5fe57436b3ac6f3b
vithushan <vithushan19@gmail.com>
2021-01-17 17:24:04 -0500

    add classroom comp

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -44,9 +44,7 @@ export default function Navbar(props) {
         >
           {isAuthenticated && (
             <div>
-              <img src={user.picture} alt={user.name} />
               <h2>{user.name}</h2>
-              <p>{user.email}</p>
             </div>
           )}
         </a>
---
+++ client/pages/classroom.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import Navbar from "./../components/Navbar";
+
+const Classroom = () => {
+  return (
+    <div>
+      <Navbar/>
+      Classroom
+
+      Join Game
+      Create Game
+      </div>
+  );
+};
+
+export default Classroom;
