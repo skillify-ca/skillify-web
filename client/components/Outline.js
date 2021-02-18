5aeb2ca88e831b5239a7c229754e6749ef9862b3
vithushan19 <vithushan19@gmail.com>
2021-02-17 20:55:12 -0500

    Move Grade Nine out of home page

--- client/components/Outline.js
+++ client/components/Outline.js
@@ -13,47 +13,17 @@ export default function Outline(props) {
   const data = apiData["outline"];
   return (
     <div>
-      <div className="text-xl text-center p-4">Grade 9 Math (MPM1D1)</div>
+      <div className="text-xl text-center p-4">Nulakam</div>
       <div className="grid grid-cols-2 gap-4">
         <Link href="/games/TicTacToe">
           <div className="bg-purple-500 text-center">
             Tic Tac Toe
           </div>
         </Link>
-        <Link href="/strand/number-sense-and-algebra">
-          <div className="gap-0 divide-y-2 divide-red-800 text-center">
-            <div className="p-16 bg-red-500 text-center">
-              <p>{data[0]}</p>
-            </div>
-            <div className="p-4 bg-red-500">
-              <ProgressBar value={50} color={"red"} />
-            </div>
-          </div>
-        </Link>
-        <Link href="/strand/linear-relations">
-          <div className="gap-0 divide-y-2 divide-green-800">
-            <div className="p-16 bg-green-500 text-center">
-              <p>{data[1]}</p>
-            </div>
-            <div className="p-4 bg-green-500">
-              <ProgressBar value={33} color={"green"} />
-            </div>
-          </div>
-        </Link>
-        <Link href="/strand/analytic-geometry">
-          <div className="gap-0 divide-y-2 divide-blue-800">
-            <div className="p-16 bg-blue-500 text-center">
-              <p>{data[2]}</p>
-            </div>
-            <div className="p-4 bg-blue-500">
-              <ProgressBar value={100} color={"blue"} />
-            </div>
-          </div>
-        </Link>
-        <Link href="/strand/measurement-geometry">
+        <Link href="/grade-nine">
           <div className="gap-0 divide-y-2 divide-yellow-800">
             <div className="p-16 bg-yellow-500 text-center">
-              <p>{data[3]}</p>
+              Grade 9 Math (MPM1D1)
             </div>
             <div className="p-4 bg-yellow-500">
               <ProgressBar value={0} color={"yellow"} />
