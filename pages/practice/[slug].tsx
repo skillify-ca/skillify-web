7467c29af02c91f80466108d46063b88c206e702
vithushan19 <vithushan19@gmail.com>
2021-03-11 19:23:19 -0500

    Update [slug].tsx

--- pages/practice/[slug].tsx
+++ pages/practice/[slug].tsx
@@ -75,6 +75,7 @@ const Quiz = ({ slug }) => {
       <form onSubmit={submitGuess}>
         <button
           type="submit"
+          onClick={submitGuess}
           className="group relative w-3/4 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           Submit
