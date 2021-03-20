850674839a94b68481433fcf32039a847d2b0da5
vithushan19 <vithushan19@gmail.com>
2021-03-20 12:12:03 -0400

    Add replay button

--- pages/practice/[slug].tsx
+++ pages/practice/[slug].tsx
@@ -111,9 +111,16 @@ const Quiz = ({ slug }) => {
           <p className="text-2xl">Speed </p>1 minute 24 seconds
           <p className="text-2xl">Accuracy</p>
           {Math.round((100 * correctGuesses) / length)}%
+          <button
+            type="submit"
+            className="group relative w-3/4 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            onClick={onCloseGameOver}
+          >
+            Replay
+          </button>
           <Link href="/">
             <button className="group relative w-3/4 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              OK
+              Home
             </button>
           </Link>
         </div>
