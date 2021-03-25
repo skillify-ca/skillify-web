f3a29196803ddafa1a864e17687405ef4bbb0fb5
vithushan <vithushan19@gmail.com>
2021-03-24 21:58:27 -0400

    fix

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,15 +1,8 @@
-import { Col, Row, Tabs } from "antd";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import React from "react";
-import SkillList from "../components/SkillList";
-
-const { TabPane } = Tabs;
-
-function callback(key) {
-  console.log(key);
-}
 
 export default function Profile(props) {
   
-    return <div className="flex"></div>;
+    return <div className="flex">
+      hey
+    </div>;
 }
