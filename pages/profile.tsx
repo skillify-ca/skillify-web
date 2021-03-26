e545ddbb9336bcd46619dac77dad3c871c7d41e5
vithushan <vithushan19@gmail.com>
2021-03-26 09:22:27 -0400

    add dummuy badge

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Badge from "../components/Badge";
 
 export default function Profile(props) {
-  
-    return <div className="flex">
-      hey
-    </div>;
+  return (
+    <div className="flex">
+      
+      <li>
+        <ul>- got it</ul>
+        <ul>- got it</ul>
+        <ul>- not yet</ul>
+        <ul>
+          <Badge text={"addition"} />
+        </ul>
+      </li>
+    </div>
+  );
 }
