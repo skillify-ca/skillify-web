5d70ffdd0f35a86103d9e9704933563133b56b79
vithushan <vithushan19@gmail.com>
2021-03-29 21:48:14 -0400

    fix profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -5,66 +5,54 @@ import initializeApollo from "../lib/apollo";
 import { gql, useQuery } from "@apollo/client";
 import { FETCH_USERS } from "../graphql/fetchUsers";
 import { FETCH_FLASHCARD_GUESSES } from "../graphql/fetchFlashcardGuesses";
+import { FETCH_USER_SKILLS } from "../graphql/fetchUserSkills";
 
 export default function Profile(props) {
   const guesses = useQuery(FETCH_FLASHCARD_GUESSES);
-  const [session] = useSession();
-  const skills = [
-    { title: "Numbers", image: "images/skills/counting.png", mastered: true },
-    { title: "Addition", image: "images/skills/addition.png", mastered: false },
-  ];
+  const userSkillsData = useQuery(FETCH_USER_SKILLS);
+  let skills = [];
 
-  const lockedSkills = [
-    { title: "Subtraction", image: "images/skills/lock.png" },
-    { title: "Multiplication", image: "images/skills/lock.png" },
-    { title: "Division", image: "images/skills/lock.png" },
-    { title: "Mixed Operations", image: "images/skills/lock.png" },
-    { title: "Fractions", image: "images/skills/lock.png" },
-    { title: "Geometry", image: "images/skills/lock.png" },
-    { title: "Variables", image: "images/skills/lock.png" },
-    { title: "Estimation", image: "images/skills/lock.png" },
-    { title: "Logic", image: "images/skills/lock.png" },
-    { title: "Patterns", image: "images/skills/lock.png" },
-    { title: "Money", image: "images/skills/lock.png" },
-    { title: "Time", image: "images/skills/lock.png" },
-    { title: "Data", image: "images/skills/lock.png" },
-    { title: "Decimals", image: "images/skills/lock.png" },
-    { title: "Stats", image: "images/skills/lock.png" },
-  ];
+  if (userSkillsData.data) {
+    skills = userSkillsData.data.user_skills;
+  }
+
+  const [session] = useSession();
 
   const users = useQuery(FETCH_USERS);
+  console.log(skills)
   return (
     <div className="flex flex-col">
       <Navbar />
       <ul>
-        {guesses.data && guesses.data.flashcard_guesses.map((it) => (
-          <li key={it.guessId}>GUESS {it.question} {it.guess}</li>
-        ))}
-        {skills.map((it) => (
-          <li key={it.title}>
-            <div className="gap-4 flex bg-gradient-to-b from-purple-400 via-purple-500 to-purple-500 p-2 m-4 items-center justify-between text-center rounded-xl">
-              <div className="flex gap-4 h-8 items-center p-2">
-                <img src={it.image} alt="skill image" className="w-8" />
-                <p>{it.title}</p>
+        {skills
+          .filter((it) => it.locked == false)
+          .map((it) => (
+            <li key={it.skill.title}>
+              <div className="gap-4 flex bg-gradient-to-b from-purple-400 via-purple-500 to-purple-500 p-2 m-4 items-center justify-between text-center rounded-xl">
+                <div className="flex gap-4 h-8 items-center p-2">
+                  <img src={it.skill.image} alt="skill image" className="w-8" />
+                  <p>{it.skill.title}</p>
+                </div>
+                <img
+                  src={
+                    it.stars == 3 ? "images/checkmark.png" : "images/progress.png"
+                  }
+                  alt="skill image"
+                  className="w-8"
+                />
               </div>
-              <img
-                src={
-                  it.mastered ? "images/checkmark.png" : "images/progress.png"
-                }
-                alt="skill image"
-                className="w-8"
-              />
-            </div>
-          </li>
-        ))}
-        {lockedSkills.map((it) => (
-          <li key={it.title}>
-            <div className="gap-4 flex bg-gray-400 p-2 m-4 items-center text-center rounded-xl">
-              <img src={it.image} alt="skill image" className="w-8" />
-              <p>{it.title}</p>
-            </div>
-          </li>
-        ))}
+            </li>
+          ))}
+        {skills
+          .filter((it) => it.locked == true)
+          .map((it) => (
+            <li key={it.skill.title}>
+              <div className="gap-4 flex bg-gray-400 p-2 m-4 items-center text-center rounded-xl">
+                <img src="images/skills/lock.png" alt="skill image" className="w-8" />
+                <p>{it.skill.title}</p>
+              </div>
+            </li>
+          ))}
       </ul>
     </div>
   );
