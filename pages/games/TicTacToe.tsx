f6cdeafb066ebbb450218bcaba1438b059b1eb4c
Harrishan Loganathan <harrishanlogan@gmail.com>
2021-05-04 16:21:29 -0400

    fix smth
    booom

--- pages/games/TicTacToe.tsx
+++ pages/games/TicTacToe.tsx
@@ -2,14 +2,12 @@ import React, { useState } from "react";
 import GameBoard from "../../components/ticTacToe/GameBoard";
 import { Container } from "../../components/ticTacToe/Container";
 import TeacherControls from "../../components/ticTacToe/TeacherControls";
-import { HarriButton } from "../../components/stories/harributton";
 
 const TicTacToe = () => {
   return (
     <div>
       <div className="p-4">
         <GameBoard />
-<HarriButton label='label'/>
       </div>
     </div>
   );
