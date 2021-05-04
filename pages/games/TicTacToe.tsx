878908fdad4d6997e5001ba07f756b0d85bf16c3
Harrishan Loganathan <harrishanlogan@gmail.com>
2021-05-04 16:15:07 -0400

    Commit Name button
    - Added own button

--- pages/games/TicTacToe.tsx
+++ pages/games/TicTacToe.tsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import GameBoard from "../../components/ticTacToe/GameBoard";
 import { Container } from "../../components/ticTacToe/Container";
 import TeacherControls from "../../components/ticTacToe/TeacherControls";
+import { HarriButton } from "../../components/stories/harributton";
 
 const TicTacToe = () => {
   return (
     <div>
       <div className="p-4">
         <GameBoard />
+<HarriButton label='label'/>
       </div>
     </div>
   );
