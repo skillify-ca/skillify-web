b524913a7ce61005b16ccf292a632bcbb6542395
LavaBoy202 <lavannithi2@gmail.com>
2021-05-04 13:32:58 -0400

    Update WordProblem.tsx

--- components/stories/WordProblem.tsx
+++ components/stories/WordProblem.tsx
@@ -17,7 +17,7 @@ export const WordProblem: React.FC<WordProblemProp> = ({
       <p className="align-left">Hayley has a chest of coins. Inside, there are 256 gold coins and 920 silver coins. how many coins are in the chest?</p>
       </div>
       <div className="text-2xl flex flex-wrap">
-           <input type="number" className="appearance-none relative block px-3 py-2 text-center border rounded-md shadow-md focus:outline-none focus:ring-indigo-500 bg-blue-100 focus:z-10 sm:text-sm w-1/5"
+           <input type="number" className="appearance-none relative block px-3 py-2 text-center border rounded-md shadow-md focus:outline-none focus:ring-indigo-500 bg-blue-100 focus:z-10 text-sm sm:text-lg sm:w-1/5 w-full"
         placeholder="Enter Answer">
         </input>
         <p className="ml-3 text-yellow-400">coins</p>
