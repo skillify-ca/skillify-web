b55404604a040722b93798f3b2f3efd4aa9939f2
Harrishan Loganathan <harrishanlogan@gmail.com>
2021-05-06 13:48:48 -0400

    added true or false component to storybook
    true or false no comment changes

--- components/stories/TrueorFalse.tsx
+++ components/stories/TrueorFalse.tsx
@@ -14,38 +14,8 @@ export const TrueorFalse: React.FC<TrueorFalseProp> = ({
     return (
         <div className="items-center" > 
       <p className = "text-5xl text-center">{question}</p>
-      <button className="items-center">True</button>
+      <button >True</button>
       <button>False</button>    
         </div>
     )
   }
-/* trial code
- export const TrueorFalse: React.FC<TrueorFalseProp> = ({
-    question,
-    ...props
-  }) => {
-    const [answer, setAnswer] = useState();
-    const parse = () => {
-        const part = question.split(" ");
-        return {
-          first: part[0],
-          operator: part[1],
-          second: part[2],
-          sol: part[4],
-        };
-      }
-      var num1 = parseInt(parse().first)
-      var num2 = parseInt(parse().second)
-      var ans = parseInt(parse().sol)
-      switch(parse().operator) {
-          case '+': if (num1 + num2 == ans) {
-              answer = 
-          }
-      }
-      return (
-          <div className = "text-5xl flex flex-wrap"> 
-        <p className = "text-center">question</p>
-          </div>
-      )
-
-      }; */
