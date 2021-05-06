0607036842cbc0108faeac9978f68da5ef8e74ec
vithushan19 <vithushan19@gmail.com>
2021-05-06 16:55:54 -0400

    Move text input inside the vertical equation component

--- components/stories/QuestionSet.tsx
+++ components/stories/QuestionSet.tsx
@@ -29,7 +29,7 @@ const QuestionSet = ({
 
     if (questionData[index].questionType === QuestionType.VERTICAL_EQUATION) {
       return (
-        <VerticalEquation question={questionData[index].text} operator={questionData[index].operator} />
+        <VerticalEquation question={questionData[index].text} operator={questionData[index].operator} submitGuess={submitGuess} />
       );
     } else if (questionData[index].questionType == QuestionType.BINARY_WORD_PROBLEM) {
       return <WordProblem question={questionData[index].text} />
@@ -38,12 +38,7 @@ const QuestionSet = ({
     return <p className="text-6xl">{questionData[index].text}</p>;
   };
 
-  const handleKeypress = (e) => {
-    //it triggers by pressing the enter key
-    if (e.charCode === 13) {
-      submitGuess(e);
-    }
-  };
+
 
   return (
     <div className="flex flex-col justify-center items-center bg-gray-200 gap-8 pb-24">
@@ -54,24 +49,6 @@ const QuestionSet = ({
         </p>
       </div>
       <Card size="large">{questionData[index] && questionComponent()}</Card>
-      <input
-        id="guess"
-        type="number"
-        autoComplete="off"
-        value={guess}
-        onChange={(e) => setGuess(e.target.value)}
-        className="appearance-none relative block px-3 py-4 text-center border rounded-md shadow-md focus:outline-none focus:ring-indigo-500 bg-blue-100 focus:z-10 sm:text-sm"
-        placeholder="Enter Answer"
-        ref={inputElement}
-        onKeyPress={handleKeypress}
-      />
-
-      <Button
-        onClick={submitGuess}
-        label="Submit"
-        backgroundColor="blue"
-        textColor="white"
-      />
     </div>
   );
 };
--- components/stories/VerticalEquation.tsx
+++ components/stories/VerticalEquation.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "./Button";
 
 export interface VerticalEquationProp {
   question?: string;
   operator: string;
+  submitGuess: (e) => void;
 }
 
 /**
@@ -11,8 +13,16 @@ export interface VerticalEquationProp {
 export const VerticalEquation: React.FC<VerticalEquationProp> = ({
   question,
   operator,
+  submitGuess,
   ...props
 }) => {
+  const [guess, setGuess] = useState('');
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.charCode === 13) {
+      submitGuess(e);
+    }
+  };
   const parse = () => {
     const parts = question.split(" ");
     return {
@@ -21,12 +31,32 @@ export const VerticalEquation: React.FC<VerticalEquationProp> = ({
     };
   };
   return (
-    <div className="text-8xl flex flex-col flex-end items-end border-b-8 border-blue-900">
-      <p className="align-right">{parse().first}</p>
-      <div className="flex">
-        <p>{operator}</p>
-        <p>{parse().second}</p>
+    <div className="flex flex-col gap-4">
+      <div className="text-8xl flex flex-col flex-end items-end border-b-8 border-blue-900">
+        <p className="align-right">{parse().first}</p>
+        <div className="flex">
+          <p>{operator}</p>
+          <p>{parse().second}</p>
+        </div>
+
       </div>
+      <input
+        id="guess"
+        type="number"
+        autoComplete="off"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value)}
+        className="appearance-none relative block px-3 py-4 text-center border rounded-md shadow-md focus:outline-none focus:ring-indigo-500 bg-blue-100 focus:z-10 sm:text-sm"
+        placeholder="Enter Answer"
+        onKeyPress={handleKeypress}
+      />
+
+      <Button
+        onClick={submitGuess}
+        label="Submit"
+        backgroundColor="blue"
+        textColor="white"
+      />
     </div>
   );
 };
