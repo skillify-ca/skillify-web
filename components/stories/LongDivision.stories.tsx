30090b27929a3fc91776bb3d86074248b1d0510b
Yaathavi <yaathavi@gmail.com>
2021-05-07 16:01:08 -0400

    Always aligns quotient with dividend
    Even if the divisor is a double digit number or triple digit number, the quotient will always be above the dividend now.

--- components/stories/LongDivision.stories.tsx
+++ components/stories/LongDivision.stories.tsx
@@ -17,5 +17,5 @@ const Template: Story<LongDivisionProp> = (args) => <LongDivision {...args} />;
 export const Primary = Template.bind({});
 
 Primary.args = {
-  question: "10 / 2"
+  question: "100 / 20"
 };
