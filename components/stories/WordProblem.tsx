386314513e630e5880ab74f8c66f21485d35d273
vithushan19 <vithushan19@gmail.com>
2021-05-07 14:02:52 -0400

    Format

--- components/stories/WordProblem.tsx
+++ components/stories/WordProblem.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { noun } from '../../pages/api/WordProblemModel';
-import { Button } from './Button';
-import { Input } from './Input';
+import React, { useState } from "react";
+import { noun } from "../../pages/api/WordProblemModel";
+import { Button } from "./Button";
+import { Input } from "./Input";
 
 export interface WordProblemProp {
   submitGuess: (e) => void;
@@ -24,7 +24,7 @@ export const WordProblem: React.FC<WordProblemProp> = ({
   noun2,
   ...props
 }) => {
-  const [guess, setGuess] = useState('');
+  const [guess, setGuess] = useState("");
   const handleKeypress = (e) => {
     //it triggers by pressing the enter key
     if (e.charCode === 13) {
@@ -32,14 +32,14 @@ export const WordProblem: React.FC<WordProblemProp> = ({
     }
   };
   const parse = () => {
-    const parts = question.split(' ');
+    const parts = question.split(" ");
     return {
       first: parts[0],
-      second: parts[2]
+      second: parts[2],
     };
   };
   const title = (noun, number) => {
-    if (number == '1') {
+    if (number == "1") {
       return noun.singleTitle;
     }
     return noun.pluralTitle;
@@ -49,14 +49,19 @@ export const WordProblem: React.FC<WordProblemProp> = ({
       <div className="text-2xl flex flex-wrap">
         <p className="align-left">
           {name} has a {itemContainer} of {noun1.type}. Inside, there are
-					<span className={noun1.colour}>{' ' + parse().first + ' '}</span>
+          <span className={noun1.colour}>{" " + parse().first + " "}</span>
           {title(noun1, parse().first)} and
-					<span className={noun2.colour}>{' ' + parse().second + ' '}</span>
-          {title(noun2, parse().second)}. How many {noun1.type} are in the {itemContainer}?
-				</p>
+          <span className={noun2.colour}>{" " + parse().second + " "}</span>
+          {title(noun2, parse().second)}. How many {noun1.type} are in the{" "}
+          {itemContainer}?
+        </p>
       </div>
       <div className="text-2xl flex flex-wrap">
-        <Input guess={guess} setGuess={setGuess} handleKeypress={handleKeypress} />
+        <Input
+          guess={guess}
+          setGuess={setGuess}
+          handleKeypress={handleKeypress}
+        />
       </div>
       <div className="flex flex-wrap mt-2">
         <img src={noun1.image} width="60px" height="85px" />
@@ -65,7 +70,12 @@ export const WordProblem: React.FC<WordProblemProp> = ({
         <img src={noun2.image} width="60px" height="85px" />
         <img src={noun1.image} width="60px" height="85px" />
       </div>
-      <Button onClick={submitGuess} label="Submit" backgroundColor="blue" textColor="white" />
+      <Button
+        onClick={submitGuess}
+        label="Submit"
+        backgroundColor="blue"
+        textColor="white"
+      />
     </div>
   );
 };
