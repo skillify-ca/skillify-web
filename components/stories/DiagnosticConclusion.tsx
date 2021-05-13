37052aca10a4ff925a1465817e49e5c8d68afc4e
LavaBoy202 <lavannithi2@gmail.com>
2021-05-12 20:24:32 -0400

    Add Diagnostic Conclusion file

---
+++ components/stories/DiagnosticConclusion.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type DiagnosticDataProps = {
+  questions: Array<string>;
+  guessAns: Array<string>;
+};
+
+const DiagnosticConclusion = ({ questions, guessAns }: DiagnosticDataProps) => {
+  return (
+    <div>
+      <p> Report Card - Ontario Curriculum</p>
+      <p> Average Grade Level - Grade 3 </p>
+      <p className="mb-12"> Conclusion </p>
+      <div className="flex justify-between w-1/4 border-b border-black p-2">
+        <span> Topic: </span>
+        <span className="pr-4"> Grade Level: </span>
+      </div>
+      <div className="flex justify-between flex-row w-1/4 p-2">
+        <div>Addition Subtraction Division Multiplication</div>
+        <div>Grade 3 Grade 3 Grade 3 Grade 3</div>
+      </div>
+    </div>
+  );
+};
+
+export default DiagnosticConclusion;
