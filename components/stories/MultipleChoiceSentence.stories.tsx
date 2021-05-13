085ea00c62d9c6ffe218b10bc82f5f040de2689d
Harrishan Loganathan <harrishanlogan@gmail.com>
2021-05-13 11:50:25 -0400

    Storybook is updated to use MCoption and MCmodel

--- components/stories/MultipleChoiceSentence.stories.tsx
+++ components/stories/MultipleChoiceSentence.stories.tsx
@@ -20,9 +20,9 @@ const Template: Story<MultipleChoiceSentenceProp> = (args) => (
 export const Primary = Template.bind({});
 Primary.args = {
   displayQuestion: "Which Equation Shows the Associative Property",
-  option1: { question: "3 + 2 = 2 + 3", type: null },
-  option2: { question: "(3 + 5) + 2 = 3 + (5 + 2)", type: null },
-  option3: { question: "2 + 0 = 2", type: null },
-  option4: { question: "2 + 1 = 1 + 2", type: null },
+  option1: { text: "3 + 2 = 2 + 3", id: "a" },
+  option2: { text: "(3 + 5) + 2 = 3 + (5 + 2)", type: "b" },
+  option3: { text: "2 + 0 = 2", type: "c" },
+  option4: { text: "2 + 1 = 1 + 2", type: "d" },
   answer: AdditionProperty.ASSOCIATIVE,
 };
