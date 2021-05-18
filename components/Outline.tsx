95087ad8339cd9de69c6d4e4055d9b63e3b4ebac
vithushan19 <vithushan19@gmail.com>
2021-05-18 14:43:22 -0400

    Update Outline.tsx

--- components/Outline.tsx
+++ components/Outline.tsx
@@ -6,6 +6,7 @@ import { signIn, useSession } from "next-auth/client";
 import { INIT_USER_SKILLS } from "../graphql/initUserSkills";
 import { userId } from "../graphql/utils/constants";
 import Card from "./stories/Card";
+import { Button } from "./stories/Button";
 export default function Outline() {
   const skillsEndRef = useRef(null);
   const [session, loading] = useSession();
@@ -54,9 +55,9 @@ export default function Outline() {
     return 0;
   };
 
-  return (
-    <div className="flex flex-col gap-8 justify-between w-full col-span-2 items-center mb-4 p-16 mx-auto">
-      <Card size='large'>
+  const loggedInComponent = (
+    <div className="max-w-screen-lg">
+      <Card size="large">
         <div className="flex flex-col gap-8 items-center">
           <p className="text-xl font-bold font-sans">Math Knowledge Tree</p>
           <p className="text-sm">Practice different math-related skills</p>
@@ -65,43 +66,111 @@ export default function Outline() {
           </p>
         </div>
       </Card>
-      <div className="max-w-screen-lg">
-        <div className="flex flex-wrap justify-center gap-8">
-          {userSkillsData.loading && <p>Loading ...</p>}
-          {unlockedSkills.map((skill, index) => (
-            <div
+      <div className="flex flex-wrap justify-center gap-8">
+        {userSkillsData.loading && <p>Loading ...</p>}
+        {unlockedSkills.map((skill, index) => (
+          <div
+            key={skill.skill.title}
+            className={
+              index == unlockedSkills.length - 1 ? "animate-bounce" : ""
+            }
+          >
+            <SkillCard
               key={skill.skill.title}
-              className={
-                index == unlockedSkills.length - 1 ? "animate-bounce" : ""
-              }
-            >
+              title={skill.skill.title}
+              image={skill.skill.image}
+              link={`${skill.skill.title.toLowerCase()}`}
+              rating={skill.stars}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="col-span-2 my-8">
+        <p className="text-xl text-center font-bold">{"Locked"}</p>
+      </div>
+      <div className="flex flex-wrap justify-center gap-4">
+        {skills
+          .filter((it) => it.locked == true)
+          .map((skill, index) => (
+            <div key={skill.skill.title}>
               <SkillCard
                 key={skill.skill.title}
                 title={skill.skill.title}
-                image={skill.skill.image}
-                link={`${skill.skill.title.toLowerCase()}`}
-                rating={skill.stars}
+                disabled={true}
               />
             </div>
           ))}
+      </div>
+    </div>
+  );
+
+  const loggedOutComponent = (
+    <div className="flex flex-col gap-8">
+      <div className="flex lg:flex-row flex-col gap-8">
+        <div className="flex flex-col gap-8">
+          <p className="text-6xl">Make math fun to practice</p>
+          <p className="text-xl">
+            Learning with Math Champ is engaging and fun. Earn points for
+            correct answers, play with friends and level up. Our
+            curriculum-based lessons are effective and efficient.
+          </p>
+          <div className="flex gap-8">
+            <Button backgroundColor="blue" label="Log In" textColor="white" />
+            <Button
+              backgroundColor="yellow"
+              label="Sign Up"
+              textColor="white"
+            />
+          </div>
         </div>
-        <div className="col-span-2 my-8">
-          <p className="text-xl text-center font-bold">{session ? "Locked" : "Please log in with a parents email"}</p>
+        <img
+          className="w-full lg:w-1/2"
+          alt="student-image"
+          src="https://images.unsplash.com/photo-1596495578065-6e0763fa1178?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDEzNDF8MHwxfHNlYXJjaHwxMDZ8fG1hdGh8ZW58MHx8fA&ixlib=rb-1.2.1&q=85&w=528&dpr=2"
+        />
+      </div>
+      <div className="bg-blue-400 flex-col text-center p-8">
+        <p className="text-6xl text-white">K-12 Math Activities and Quizzes</p>
+        <p className="text-xl text-white">
+          Help your students feel more confident with numbers with content
+          aligned to the Canadian curriculum
+        </p>
+      </div>
+      <div className="bg-blue-400 flex md:flex-row flex-col text-center gap-8 p-8">
+        <div
+          className={`flex flex-col justify-center items-center p-8 bg-white shadow-md rounded-xl max-w-screen-lg"`}
+        >
+          <p className="text-xl">Lesson Mode</p>
+          <p>
+            Explore and watch different videos to get introduced to
+            a topic.
+          </p>
         </div>
-        <div className="flex flex-wrap justify-center gap-4">
-          {skills
-            .filter((it) => it.locked == true)
-            .map((skill, index) => (
-              <div key={skill.skill.title}>
-                <SkillCard
-                  key={skill.skill.title}
-                  title={skill.skill.title}
-                  disabled={true}
-                />
-              </div>
-            ))}
+        <div
+          className={`flex flex-col justify-center items-center p-8 bg-white shadow-md rounded-xl max-w-screen-lg"`}
+        >
+          <p className="text-xl">Practice Mode</p>
+          <p>
+            Practice questions and reflect on their learning using
+            flashcards.
+          </p>
+        </div>
+        <div
+          className={`flex flex-col justify-center items-center p-8 bg-white shadow-md rounded-xl max-w-screen-lg"`}
+        >
+          <p className="text-xl">Quiz Mode</p>
+          <p>
+            Complete formative and summative assessments to prove
+            their understanding.
+          </p>
         </div>
       </div>
+    </div>
+  );
+
+  return (
+    <div className="flex flex-col gap-8 justify-between w-full col-span-2 items-center mb-4 p-16 mx-auto">
+      {session ? loggedInComponent : loggedOutComponent}
     </div>
   );
 }
