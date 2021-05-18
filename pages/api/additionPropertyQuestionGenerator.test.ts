4187d2d5b059e251b831764963f2d1965869df88
Harrishan Loganathan <harrishanlogan@gmail.com>
2021-05-18 12:50:25 -0400

    Changed to correct test

--- pages/api/additionPropertyQuestionGenerator.test.ts
+++ pages/api/additionPropertyQuestionGenerator.test.ts
@@ -8,7 +8,8 @@ test("Fill in the Blanks test", async () => {
     const question = getFillBlankQuestion(10, 11, 12, '+', Skill.ADDITION_PROPERTIES);
   
     // Assert
-    expect(question.fillInTheBlank.options[0].text).toBe("10 + 10 + 12 = 11 + 10 + 12");
+    expect(question.fillInTheBlank.options[0].text).toBe("10 + 11 + 12 = 11 + 10 + 12");
+    expect(question)
   });
 
   //`${a} + ${b} + ${c} = ${b} + ${a} + ${c}`
--- pages/api/question.ts
+++ pages/api/question.ts
@@ -35,6 +35,5 @@ export type fillBlankModel = {
 
 
 export type FillOption = {
-  id :  string;
   text: string;
 }
