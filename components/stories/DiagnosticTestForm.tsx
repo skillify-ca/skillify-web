3d85b10e4cff820a310efc540e9162eeb9ecb178
Vithushan <vithushan19@users.noreply.github.com>
2021-05-17 20:54:41 -0400

    Change diagnostic input to take grade level (#83)
    * Create grader for diagnostic conclusions
    
    * Update DiagnosticEvidence.tsx
    
    * Change diagnostic input to grade level

--- components/stories/DiagnosticTestForm.tsx
+++ components/stories/DiagnosticTestForm.tsx
@@ -1,59 +1,30 @@
 import React, { useRef, useState } from "react";
 import { Topic } from "../../pages/api/skill";
 import { Button } from "./Button";
+import { Input } from "./Input";
 import Toggle from "./Toggle";
 
 type DiagnosticTestFormProps = {
-  onClick: (topics: Topic[]) => void;
+  onClick: (grade: string) => void;
 };
 
 const DiagnosticTestForm = ({ onClick }: DiagnosticTestFormProps) => {
-  const [topics, setTopics] = useState([]);
-  const [grade, setGrade] = useState(1);
-
-  const toggle = (topic) => {
-    if (topics.includes(topic)) {
-      setTopics(topics.filter((it) => it !== topic));
-    } else {
-      let newTopics = [];
-      newTopics.push(topic);
-      newTopics.push(...topics);
-      setTopics(newTopics);
-    }
-  };
+  const [grade, setGrade] = useState("3");
 
   return (
     <div className="flex flex-col items-center bg-white rounded-lg p-4">
       <p className="text-xl font-bold mb-8">Diagnostic Test</p>
 
-      <div className="grid grid-cols-2 w-full gap-4 mb-8">
-        <p className="text-sm font-bold text-gray-700">Topics</p>
-
-        <div className="flex flex-col">
-          <div className="flex gap-4">
-            <Toggle onClick={() => toggle(Topic.ADDITION)} />
-            <p>Addition</p>
-          </div>
-          <div className="flex gap-4">
-            <Toggle onClick={() => toggle(Topic.SUBTRACTION)} />
-            <p>Subtraction</p>
-          </div>
-          <div className="flex gap-4">
-            <Toggle onClick={() => toggle(Topic.MULTIPLICATION)} />
-            <p>Multiplication</p>
-          </div>
-          <div className="flex gap-4">
-            <Toggle onClick={() => toggle(Topic.DIVISION)} />
-            <p>Division</p>
-          </div>
-        </div>
+      <div className="flex flex-row items-center w-full gap-4 mb-8">
+        <p className="text-xl font-bold text-gray-700">Grade</p>
+        <Input guess={grade} setGuess={setGrade} />
       </div>
 
       <Button
         backgroundColor="blue"
         label="Start"
         textColor="white"
-        onClick={(e) => onClick(topics)}
+        onClick={(e) => onClick(grade)}
       />
     </div>
   );
