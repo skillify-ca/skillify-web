d7217ea49f975e606a28543632f0f833a494030e
LavaBoy202 <lavannithi2@gmail.com>
2021-05-19 12:11:21 -0400

    Map out Array images

---
+++ pages/api/ArrayQMap.tsx
@@ -0,0 +1,30 @@
+export const tweleveMap = {
+  1: {},
+  2: {},
+  3: {},
+  4: {
+    one: "1",
+    "2X2": "/ArrayQ/2X2.png",
+    four: "4",
+  },
+  5: {
+    one: "1",
+    five: "5",
+  },
+  6: {
+    "2X3": "/ArrayQ/2X3.png",
+  },
+  8: {
+    "2X4": "/ArrayQ/2X4.png",
+  },
+  9: {
+    "3X3": "/ArrayQ/3X3.png",
+  },
+  10: {
+    "2X5": "/ArrayQ/2X5.png",
+  },
+  12: {
+    "2X6": "/ArrayQ/2X6.png",
+    "3X4": "/ArrayQ/3X4.png",
+  },
+};
