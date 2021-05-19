410f0a9fa27bc3110c03ce99925a2c3eb57ca712
Harrishan Loganathan <harrishanlogan@gmail.com>
2021-05-18 20:16:54 -0400

    changed to as htmlelement

--- components/stories/FillBlank.tsx
+++ components/stories/FillBlank.tsx
@@ -28,27 +28,27 @@ export const FillBlank: React.FC<FillBlankProp> = ({
   function onButton1Click(e) {
     setButton2Visible(true);
     setButton1Visible(false);
-    document.getElementById("input1").disabled = true;
-    document.getElementById("input2").disabled = false;
+    (document.getElementById("input1") as HTMLElement).disabled = true;
+    (document.getElementById("input2") as HTMLElement).disabled = false;
   }
   function onButton2Click(e) {
     setButton3Visible(true);
     setButton2Visible(false);
-    document.getElementById("input2").disabled = true;
-    document.getElementById("input3").disabled = false;
+    (document.getElementById("input2") as HTMLElement).disabled = true;
+    (document.getElementById("input3") as HTMLElement).disabled = false;
   }
   function onButton3Click(e) {
     setButton4Visible(true);
     setButton3Visible(false);
-    document.getElementById("input3").disabled = true;
-    document.getElementById("input4").disabled = false;
+    (document.getElementById("input3") as HTMLElement).disabled = true;
+    (document.getElementById("input4") as HTMLElement).disabled = false;
   }
   function onButton4Click(e) {
     submitGuess({ guess: "", isCorrect: true });
     setButton1Visible(true);
     setButton4Visible(false);
-    document.getElementById("input4").disabled = true;
-    document.getElementById("input1").disabled = false;
+    (document.getElementById("input4") as HTMLElement).disabled = true;
+    (document.getElementById("input1") as HTMLElement).disabled = false;
     document.getElementById("input1").value = "";
     document.getElementById("input2").value = "";
     document.getElementById("input3").value = "";
@@ -158,15 +158,12 @@ export const FillBlank: React.FC<FillBlankProp> = ({
           type="number"
         ></input>
         {button3Visible && (
-
-
           <Button
             onClick={onButton3Click}
             label="Lock-in"
             textColor="white"
             backgroundColor="red"
           ></Button>
-
         )}
       </p>
 
@@ -186,7 +183,6 @@ export const FillBlank: React.FC<FillBlankProp> = ({
             textColor="white"
             backgroundColor="red"
           ></Button>
-
         )}
       </p>
     </div>
