7f3bb957e9b075b2a648d266aa7d3701ffbb2f15
Vithushan <vithushan19@users.noreply.github.com>
2021-05-18 22:59:53 -0400

    Save colors to tailwind config (#102)
    * Save colors
    
    * Update diagnosticQuestionGenerator.ts

--- components/stories/DiagnosticResults.tsx
+++ components/stories/DiagnosticResults.tsx
@@ -13,7 +13,7 @@ const DiagnosticResults = ({
 }: DiagnosticResultsProps) => {
   const percentage = Math.round((correctGuesses / index) * 100);
   return (
-    <div className="flex flex-col items-center bg-white w-2/4 bg-auto p-16">
+    <div className="flex flex-col items-center bg-white w-full sm:w-3/4 p-16">
       <div className="text-2xl font-bold mb-16">Results</div>
       <div className=" bg-gray-300 h-32 w-32 flex items-center justify-center rounded-full mb-4 text-2xl">
         {" "}
