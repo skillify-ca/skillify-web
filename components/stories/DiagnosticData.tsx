4c47216e9398af6dd37ce26025a342571161ab50
Yaathavi <yaathavi@gmail.com>
2021-05-19 00:17:49 -0400

    Data displays questions based on skill chosen

--- components/stories/DiagnosticData.tsx
+++ components/stories/DiagnosticData.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import React from "react";
 import {
+  getAnswerForSkill,
+  getQuestionForSkill,
   getResultForSkill,
   getSkillsForTopic,
 } from "../../pages/api/diagnostic/diagnosticGrader";
@@ -14,14 +16,28 @@ type DiagnosticDataProps = {
 
 const DiagnosticData = ({ topic, results }: DiagnosticDataProps) => {
   const skills = getSkillsForTopic(topic);
+
   return (
     <>
       <p className="mb-12">
-        Diagnostic Report : {topic && topic.charAt(0).toUpperCase() + topic.slice(1)}
+        Diagnostic Report :{" "}
+        {topic && topic.charAt(0).toUpperCase() + topic.slice(1)}
       </p>
       <div className="flex justify-between w-1/4 border-b border-black p-2">
         <span> Question: </span>
-        <span className="pr-6"> Guess: </span>
+        <span className="pl-4"> Guess: </span>
+      </div>
+      <div className="flex flex-row justify-between w-1/4 ml-4">
+        <div>
+          {skills.map((skill) =>
+            getQuestionForSkill(skill, results).map((item) => <div>{item}</div>)
+          )}
+        </div>
+        <div>
+          {skills.map((skill) =>
+            getAnswerForSkill(skill, results).map((item) => <div>{item}</div>)
+          )}
+        </div>
       </div>
       <Link href="/diagnostic">
         <button className="items-end bg-blue-500 rounded p-3 text-white text-sm">
