2f2f3537b8acca3c5bd71ef3f4aa2905d257e0f6
Vithushan <vithushan19@users.noreply.github.com>
2021-05-20 12:14:24 -0400

    Create a tools page for email signups (#112)
    * Style diagnostic page
    
    * Create a tools page
    
    * responsive design

---
+++ pages/tools.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import DiagnosticNavbar from "../components/DiagnosticNavbar";
+import { Button } from "../components/stories/Button";
+
+export default function Tools(props) {
+  return (
+    <div className="flex flex-col justify-center overflow-auto bg-scroll heropattern-piefactory-blue-100 bg-gray-100">
+      <DiagnosticNavbar />
+      <div className="p-4 flex flex-col items-center justify-center gap-8">
+        <div className="flex flex-col items-center bg-white shadow-lg gap-8 rounded-lg p-4">
+          <p className="text-xl font-bold">Math Champ Tools</p>
+          <div className="flex flex-col gap-4 sm:max-w-2xl">
+            <p>
+              Math Champ is developing a suite of online math activities and
+              games to help your child become better at math. Enter your email
+              to be notified when they become available.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col sm:flex-row gap-8">
+          <div className="bg-white shadow-lg rounded-lg w-full p-4">
+            <div className="flex flex-col gap-4">
+              <p className="font-bold">Practice Tracker</p>
+              <p className="">
+                Help your child become self-suffienct with the Math Champ
+                Practice Tracker. Students will learn and practice new skills
+                aligned to the Ontario curriculum. Parents will receive weekly
+                reports to track their child's progress.
+              </p>
+              <div className="bg-white flex sm:flex-row gap-4 items-center rounded-lg">
+                <input
+                  id="guess"
+                  type="text"
+                  autoComplete="off"
+                  className={`text-left p-2 border rounded-md shadow-md focus:outline-none focus:ring-indigo-500 text-md lg:text-md`}
+                  placeholder="Email"
+                />
+                <Button
+                  backgroundColor="blue"
+                  textColor="white"
+                  label="Notify Me"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="bg-white shadow-lg rounded-lg w-full p-4">
+            <div className="flex flex-col gap-4">
+              <p className="font-bold">Math Battle</p>
+              <p className="">
+                Math Battle is a competitive multiplayer game where students
+                earn points and battle against their friends. This game helps
+                children and adults become fast and fluent with math facts.
+              </p>
+              <div className="bg-white flex sm:flex-row gap-4 items-center rounded-lg">
+                <input
+                  id="guess"
+                  type="text"
+                  autoComplete="off"
+                  className={`text-left p-2 border rounded-md shadow-md focus:outline-none focus:ring-indigo-500 text-md lg:text-md`}
+                  placeholder="Email"
+                />
+                <Button
+                  backgroundColor="blue"
+                  textColor="white"
+                  label="Notify Me"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
