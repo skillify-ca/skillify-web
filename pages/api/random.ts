0355aa4bd7b9afe0fbbd303a8b2829283a565c46
LavaBoy202 <lavannithi2@gmail.com>
2021-05-20 17:04:05 -0400

    Added a random colour selector and implemented it in Array Multiplication Questions

--- pages/api/random.ts
+++ pages/api/random.ts
@@ -7,3 +7,22 @@ export function getRandomItemFromArray(arr: any[]) {
 export function getRndInteger(min: number, max: number) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+export function getRndColour() {
+  let colourArray = ["red", "blue", "green", "yellow", "purple"]; 
+  let colour = getRandomItemFromArray(colourArray); 
+  return colour;
+}
+
+
+/*
+  return red
+
+  return ["bg-red-500", "bg-red-200"]
+  return {
+    "background": "bg-red-500",
+    "hover": ...
+  }
+  "bg-${color}-500 hover:bg-${color}-200"
+*/
+//getranitem as helper
