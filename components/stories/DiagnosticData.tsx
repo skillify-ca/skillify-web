8a32546158a17fc53cd26fcb527495796f3e4b18
Yaathavi <46689185+Yaathavi@users.noreply.github.com>
2021-05-20 12:31:48 -0400

    Create multiplication by equal groups question type (#113)
    * Displays skill topic
    
    * Displays skill topic
    
    Uses the enum instead of a string
    
    * Creates Evidence stage
    
    * Edit the buttons to advance to each stage
    
    * Sorts diagnostic questions by skill description instead of difficulty level
    
    * Fixes Data Screen and Long Division Question
    
    * Made changes to last PR
    
    * Update questionGenerator.ts
    
    * Assigns skill description to questions
    
    * Update Question type
    
    * Fix build issues
    
    * Displays skill description on evidence page
    
    * Shows each skill description only once
    
    * Naming changes
    
    * Conclusion page links to evidence/topic
    
    * Data page loads based on topic chosen in evidence page
    
    * add @testing-library/jest-dom library
    
    * Fix run issues and capitalize first letter of the topic
    
    * Data displays questions based on skill chosen
    
    * Data page displays questions&ans based on skill description
    
    * Create function that returns readable title
    
    * style evidence page
    
    * Background changes color based on skill level
    
    * Style Data page
    
    * Create multiplication by equal groups question type
    
    * Edits to recent PR
    
    Co-authored-by: Vithushan <vithushan19@users.noreply.github.com>

--- components/stories/DiagnosticData.tsx
+++ components/stories/DiagnosticData.tsx
@@ -15,25 +15,25 @@ type DiagnosticDataProps = {
 
 const DiagnosticData = ({ skill, results }: DiagnosticDataProps) => {
   return (
-    <div>
-      <p className="mb-12">{SkillDescription(skill)}</p>
-      <div className="flex justify-between w-1/4 border-b border-black p-2">
-        <span> Question: </span>
-        <span className="pl-4"> Guess: </span>
-      </div>
-      <div className="flex flex-row justify-between w-1/4 ml-4">
-        <div>
+    <div className="p-8 flex flex-col gap-4 heropattern-piefactory-blue-300 bg-blue-200 h-screen">
+      <p className="mb-2 text-center font-black text-xl">
+        {SkillDescription(skill)}
+      </p>
+      <div className="bg-white p-4 rounded-lg grid grid-cols-2">
+        <p className="p-4 font-bold border-b border-black"> Question: </p>
+        <p className="p-4 font-bold border-b border-black"> Guess: </p>
+
+        <div className="grid-cols-1">
           {getQuestionForSkill(skill, results).map((item) => (
-            <div>{item}</div>
+            <p className="p-4 border-b border-black">{item}</p>
           ))}
         </div>
         <div>
           {getAnswerForSkill(skill, results).map((item) => (
-            <div>{item}</div>
+            <p className="p-4 border-b border-black">{item}</p>
           ))}
         </div>
       </div>
-
       <Link href="/diagnostic">
         <button className="items-end bg-blue-500 rounded p-3 text-white text-sm">
           Take Diagnostic Again
