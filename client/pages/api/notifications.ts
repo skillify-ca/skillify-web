d797b1a67dcce2d2242d54d5aaac994cf712b47d
vithushan19 <vithushan19@gmail.com>
2021-05-24 15:45:01 -0400

    Update notifications.ts

--- client/pages/api/notifications.ts
+++ client/pages/api/notifications.ts
@@ -1,15 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const SLACK_WEBHOOK_URL =
     "https://hooks.slack.com/services/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";
 
   const email = req.body.email;
   const product = req.query["product"];
 
-  await axios.post(SLACK_WEBHOOK_URL, {
-    text: `Hello, ${email} signed up for ${product}!`,
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+    body: JSON.stringify({
+      text: `Hello, ${email} signed up for ${product}!`,
+    }),
+  };
+
+  fetch(SLACK_WEBHOOK_URL, options).then((response) => {
+    console.log(response.status);
   });
-  res.status(200).json({ name: "John Doe" });
+
+  res.status(200).json({ email });
 }
