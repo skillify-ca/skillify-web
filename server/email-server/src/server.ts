e818d5df911cdf4b2a3163d6fa946fcc51cd5af3
vithushan19 <vithushan19@gmail.com>
2021-05-24 10:05:47 -0400

    Create new koa server app

---
+++ server/email-server/src/server.ts
@@ -0,0 +1,12 @@
+var koa = require("koa");
+var app = new koa();
+
+app.use(function* () {
+  this.body = "Hello world!";
+});
+
+app.listen(3000, function () {
+  console.log("Server running on https://localhost:3000");
+});
+
+export {};
