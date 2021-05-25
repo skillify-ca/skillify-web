aa327c3aa6ba7c546786c55ce985ad70e2ba9cba
vithushan19 <vithushan19@gmail.com>
2021-05-24 22:27:31 -0400

    Create endpoint to send email

---
+++ client/pages/api/email.ts
@@ -0,0 +1,27 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+import * as sgMail from "@sendgrid/mail";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: req.body.email, // Change to your recipient
+    from: process.env.SENDGRID_SENDER, // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  res.status(200).json({ status: "OK" });
+}
