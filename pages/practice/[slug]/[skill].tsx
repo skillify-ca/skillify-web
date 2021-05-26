ad4b6838b041ad9b54585de5e10e0922d6cbb96c
Harrishan Loganathan <76184691+harrishanlogan@users.noreply.github.com>
2021-05-26 13:20:52 -0400

    Fixed Practice Styling (#149)
    * Added score to the practice page
    
    * Fixed build errors
    
    * Next Question Button appears after answering a question correctly
    
    * Fixed a score bug and next question doesnt appear for last question
    
    * Score now updates with each question
    
    so instead of starting at 0/5 it starts at 0/1 and increases onwards
    
    * Next Question Button appears if you get it wrong or right
    
    * Display of correct and wrong answer shows for each question
    
    * Fixed bug where both options could show if you clicked submit twice before next question
    
    * Fixed guess being empty on double click bug and styling before popup
    
    * Flipping Mechanic Works to an extent
    
    npm install --save react-card-flip
    
    * Card flip
    
    npm install --save react-card-flip
    
    * Change to vertical might be better
    
    * Question number and score do not flip anymore
    
    * fix build errors
    
    * Score and Quiz on right side of the screen
    
    * Better Styling
    
    * True or False Answers display correctly
    
    * Fill blank displays answers
    
    * Multiple Choice Word answers display correctly
    
    * Fixed pull error
    
    * Fixed practice styling
    
    * Cleaned up with conditional rendering

--- pages/practice/[slug]/[skill].tsx
+++ pages/practice/[slug]/[skill].tsx
@@ -85,17 +85,18 @@ const PracticeQuiz = ({ slug, skill }) => {
   return (
     <div>
       <Navbar />
-      <div className=" flex flex-row-reverse justify-items-end">
+      <div className="flex flex-row w-full p-4 bg-yellow-400 justify-center gap-64">
         <p className="font-bold text-gray-400">
           Question: {index + 1} / {questionData.length}
-          <br></br>
+        </p>
+        <p className="font-bold text-gray-400">
           Score: {correctGuess} / {index + 1}
         </p>
       </div>
 
       <ReactCardFlip
         isFlipped={isFlipped}
-        flipDirection="vertical"
+        flipDirection="horizontal"
         infinite={true}
       >
         <div className="justify-items-center align-middle w-50">
@@ -108,40 +109,42 @@ const PracticeQuiz = ({ slug, skill }) => {
             score={correctGuess}
             practice={true}
           />
-          <Hint skill={Skill.ADDITION_PROPERTIES}></Hint>
         </div>
-        <Card size="large">
-          {correctAnswer ? (
-            <p className="font-bold text-gray-400 underline">
-              Correct,{" "}
-              <span className="font-bold text-green-400">{guessAttempt}</span>{" "}
-              was the answer!
-            </p>
-          ) : wrongAnswer ? (
-            <div className="italic text-gray-400 font-bold space-y-16">
-              <p>The correct answer was</p>
-              <span className="font-bold text-green-400">
-                {questionData[index].answer.toString()}
-              </span>
-              <br></br>
-              <p>Your answer was </p>
-              <span className="font-bold text-red-500"> {guessAttempt} </span>
-            </div>
-          ) : (
-            ""
-          )}
 
-          {nextQuestionButton ? (
-            <Button
-              label="Next Question"
-              backgroundColor="yellow"
-              onClick={applyNextQuestion}
-            ></Button>
-          ) : (
-            ""
-          )}
-        </Card>
+        <div className="flex flex-col justify-center items-center gap-8">
+          <Card size="large">
+            {correctAnswer ? (
+              <p className="font-bold text-gray-400 underline">
+                Correct,{" "}
+                <span className="font-bold text-green-400">{guessAttempt}</span>{" "}
+                was the answer!
+              </p>
+            ) : wrongAnswer ? (
+              <div className="italic text-gray-400 font-bold space-y-16">
+                <p>The correct answer was</p>
+                <span className="font-bold text-green-400">
+                  {questionData[index].answer.toString()}
+                </span>
+                <br></br>
+                <p>Your answer was </p>
+                <span className="font-bold text-red-500"> {guessAttempt} </span>
+              </div>
+            ) : (
+              ""
+            )}
+
+            {nextQuestionButton && (
+              <Button
+                label="Next Question"
+                backgroundColor="yellow"
+                onClick={applyNextQuestion}
+              ></Button>
+            )}
+          </Card>
+        </div>
+
       </ReactCardFlip>
+      <Hint skill={Skill.ADDITION_PROPERTIES}></Hint>
     </div>
   );
 };
