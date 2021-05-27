8b2f176f2969a46c924a8b02a524c90983812ddc
vithushan <vithushan19@gmail.com>
2021-05-27 00:47:41 -0400

    Try

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -2,6 +2,7 @@
 // https://github.com/sendgrid/sendgrid-nodejs
 import * as sgMail from "@sendgrid/mail";
 import { NextApiRequest, NextApiResponse } from "next";
+import PDFDocument from "pdfkit";
 import fs from "fs";
 
 export default async function handler(
@@ -18,7 +19,9 @@ export default async function handler(
     html: "<strong>and easy to do anywhere, even with Node.js</strong>", // for email clients that can render CSS and HTML
     attachments: worksheets.map((it) => {
       const pathToAttachment = `${process.env.ROOT_DIR}${it.pdf}`;
-      const data_base64 = base64_encode(pathToAttachment);
+      const pdf = generatePDF(req);
+      const data_base64 = base64_encode("output.pdf");
+
       return {
         filename: `${it.title}`,
         content: data_base64,
@@ -42,4 +45,53 @@ export default async function handler(
 function base64_encode(file) {
   let bitmap = fs.readFileSync(file);
   return new Buffer(bitmap).toString("base64");
+}
+
+function generatePDF(req) {
+  // Create a document
+  const doc = new PDFDocument();
+
+  // Pipe its output somewhere, like to a file or HTTP response
+  // See below for browser usage
+  doc.pipe(fs.createWriteStream("output.pdf"));
+
+  // Embed a font, set the font size, and render some text
+  doc
+    // .font("fonts/PalatinoBold.ttf")
+    .fontSize(25)
+    .text("Some text with an embedded font!", 100, 100);
+
+  doc.fontSize(25).text(JSON.stringify(req.body.email), 100, 100);
+
+  // Add an image, constrain it to a given size, and center it vertically and horizontally
+  doc.image("public/images/apple.jpeg", {
+    fit: [250, 300],
+    align: "center",
+    valign: "center",
+  });
+
+  // Add another page
+  doc.addPage().fontSize(25).text("Here is some vector graphics...", 100, 100);
+
+  // Draw a triangle
+  doc.save().moveTo(100, 150).lineTo(100, 250).lineTo(200, 250).fill("#FF3300");
+
+  // Apply some transforms and render an SVG path with the 'even-odd' fill rule
+  doc
+    .scale(0.6)
+    .translate(470, -380)
+    .path("M 250,75 L 323,301 131,161 369,161 177,301 z")
+    .fill("red", "even-odd")
+    .restore();
+
+  // Add some text with annotations
+  doc
+    .addPage()
+    .fillColor("blue")
+    .text("Here is a link!", 100, 100)
+    .underline(100, 100, 160, 27, { color: "#0000FF" })
+    .link(100, 100, 160, 27, "http://google.com/");
+
+  // Finalize PDF file
+  doc.end();
 }
