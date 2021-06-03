205ae8a5dfe9bd11c2a88b32ec635efcd9f85ca9
LavaBoy202 <lavannithi2@gmail.com>
2021-06-03 14:54:10 -0400

    Styled multiplicationTopicOverview

--- pages/multiplicationTopicOverview.tsx
+++ pages/multiplicationTopicOverview.tsx
@@ -11,13 +11,13 @@ export default function divisionTopicOverview(props) {
       <div className="p-4 flex flex-col items-center justify-center">
         <div className="flex flex-col gap-8 w-full">
           <div className="bg-blue-500 heropattern-architect-blue-400 rounded-xl shadow-lg flex-col text-center p-8">
-            <p className="text-5xl text-white">Division Topic Overview</p>
+            <p className="text-5xl text-white">Multiplication Topic Overview</p>
             <p className="text-xl text-white">
-              Here are some insights of Divison. It will go through how Divison
-              will look like in the real world, a place where your child can
-              practice these essential skills, and finally an enviornment where
-              your child can be evaluated on their understanding of the
-              material.
+              Here are some insights of Multiplication. It will go through how
+              Multiplication will look like in the real world, a place where
+              your child can practice these essential skills, and finally an
+              enviornment where you can evaluate yourself on your understanding
+              of the material.
             </p>
           </div>
           <div className="bg-purple-300 shadow-inner flex-col p-2 grid-cols-none">
@@ -29,11 +29,11 @@ export default function divisionTopicOverview(props) {
           <div className="grid grid-cols-3 gap-8">
             <div className="bg-white shadow-lg rounded-lg w-full p-4 object-contain">
               <div>
-                <p className="font-bold mb-4"> Basic Division</p>
+                <p className="font-bold mb-4"> Basic Multiplication</p>
                 <iframe
                   width="560"
                   height="315"
-                  src="https://www.youtube.com/embed/biwYDuCHwXo?start=7"
+                  src="https://www.youtube.com/embed/eW2dRLyoyds?start=115"
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -42,28 +42,28 @@ export default function divisionTopicOverview(props) {
 
                 <img
                   className="h-30 object-cover mt-12"
-                  src="https://www.enchantedlearning.com/math/divide/visual/fishintanks.png"
+                  src="https://study.com/cimages/videopreview/videopreview-full/ul9t4di046.jpg"
                 ></img>
                 <p className="text-sm mt-8 border-2 border-purple-400">
-                  Basic Divison is where it all starts! You will be given a
-                  select number of items and it will be your job to seperate
-                  them into equal groups. This procedure is what we call
-                  <b> Division</b> and is one of the 4 primary operations.
+                  Basic Multiplication is where it all starts! You will be have
+                  a certain number of groups and a select number of items per
+                  group. And it will be your job to figure how many items you
+                  have in total! This is what we call <b> Multiplication</b>.
+                  Let's learn this together in the video above.
                 </p>
-                <img
-                  className="h-30 object-cover mt-12"
-                  src="https://www.smartick.com/blog/wp-content/uploads/division_pastelitos.png"
-                ></img>
               </div>
             </div>
 
             <div className="bg-white shadow-lg rounded-lg w-full p-4 object-contain">
               <div>
-                <p className="font-bold mb-4"> Division with Remainders </p>
+                <p className="font-bold mb-4">
+                  {" "}
+                  Finding the Area of a Rectangle{" "}
+                </p>
                 <iframe
                   width="560"
                   height="315"
-                  src="https://www.youtube.com/embed/KGMf314LUc0"
+                  src="https://www.youtube.com/embed/8cz_IB65pZM?start=64"
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -71,25 +71,25 @@ export default function divisionTopicOverview(props) {
                 ></iframe>
                 <img
                   className="h-30 object-cover w-full"
-                  src="https://satprepget800.com/wp-content/uploads/2013/03/remainder.png"
+                  src="https://cdn.tutors.com/assets/images/courses/math/geometry-help/how-to-find-the-area-of-a-rectangle-2.jpg"
                 ></img>
                 <p className="text-sm mt-8 border-2 border-purple-400">
-                  What happens if we have 7 cookies and 2 friends? Will the
-                  cookies divide equally for all 2 friends? They won't be able
-                  to to divide equally. We will have 1 cookie leftover. This
-                  cookie leftover is what we call a <b> Remainder</b>. Check out
-                  the video above to learn more about remainders and how they
-                  look!
+                  Ever wonder how big a soccer field is? Well, I have and there
+                  is actually a way to find it! All we have to do is multiply
+                  the length of the field by the width of the field. This is how
+                  we find the <b> Area</b> of a Rectangle. We will run you
+                  through this idea in the video above. Make sure to check it
+                  out!
                 </p>
               </div>
             </div>
             <div className="bg-white shadow-lg rounded-lg w-full p-4 object-contain">
               <div>
-                <p className="font-bold mb-4"> Fractions </p>
+                <p className="font-bold mb-4"> Exponents </p>
                 <iframe
                   width="560"
                   height="315"
-                  src="https://www.youtube.com/embed/362JVVvgYPE?start=97"
+                  src="https://www.youtube.com/embed/-zUmvpkhvW8?start=60"
                   title="YouTube video player"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -97,16 +97,14 @@ export default function divisionTopicOverview(props) {
                 ></iframe>
                 <img
                   className="h-30 object-cover"
-                  src="https://teachablemath.com/wp-content/uploads/2016/05/Screenshot-2016-05-03-14.27.17-1.png"
+                  src="https://study.com/cimages/multimages/16/3_squared.png"
                 ></img>
                 <p className="text-sm mt-4 border-2 border-purple-400">
-                  What if we only had 1 cookie and we wanted to share it with
-                  each other. How would we do it? We would split the cookie is 2
-                  halves and you will get 1 half and I will get 1 half. This is
-                  what we call
-                  <b> Fractions</b>. If you have siblings you probably already
-                  sharing you cookies with them. So knowing your fractions will
-                  help you a lot!
+                  What is 2 x 2? If you guessed 4 that's correct! But there's
+                  another way to write 2 X 2 and that is 2^2 which also equals
+                  4. Pretty cool! This is what we call an <b> Exponent</b>.
+                  Exponents are similar to multiplication but are a bit
+                  different. Find out how they're different in the video above!
                 </p>
               </div>
             </div>
@@ -127,27 +125,27 @@ export default function divisionTopicOverview(props) {
           </div>
 
           <div className="grid grid-cols-3 gap-8">
-            <div className="bg-white shadow-lg rounded-lg p-4 w-full">
+            <div className="bg-white shadow-lg rounded-lg w-full p-4">
               <div>
                 <p className="font-bold mb-4">
                   {" "}
-                  Equal Sharing to 12 (Grade 1){" "}
+                  Equal Grouping to 12 (Grade 1){" "}
                 </p>
                 <img
                   className="h-30 object-cover"
-                  src="https://pbs.twimg.com/media/EW6u028WkAIAqz9.jpg"
+                  src="https://i.pinimg.com/originals/33/62/00/3362009a75d30c1da2f8afe6a9d96547.jpg"
                 ></img>
               </div>
             </div>
-            <div className="bg-white shadow-lg rounded-lg w-full p-4 object-contain">
+            <div className="bg-white shadow-lg rounded-lg w-full p-4">
               <div>
                 <p className="font-bold mb-4">
                   {" "}
-                  Divide equally to 12 (Grade 2)
+                  Multiplying factors up to 12 (Grade 2)
                 </p>
                 <img
                   className="h-30 object-cover mt-40"
-                  src="https://oercommons.s3.amazonaws.com/media/editor/60284/fullsizeoutput_2165.jpeg"
+                  src="https://media.proprofs.com/images/QM/user_images/2503852/New%20Project%20(42)(186).jpg"
                 ></img>
               </div>
             </div>
@@ -155,11 +153,11 @@ export default function divisionTopicOverview(props) {
               <div className="flex flex-col">
                 <p className="font-bold mb-4">
                   {" "}
-                  Divide equally to 100 (Grade 3)
+                  Multiplying up to 5 X 5 (Grade 3)
                 </p>
                 <img
                   className="h-full w-full"
-                  src="https://cdn.inchcalculator.com/wp-content/uploads/2020/07/long-division-parts.png"
+                  src="https://www.learningstreet.co.uk/wp-content/uploads/2017/05/Screen-Shot-2017-05-24-at-16.04.01.png"
                 ></img>
               </div>
             </div>
