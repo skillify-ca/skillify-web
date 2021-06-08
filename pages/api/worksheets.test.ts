e00a340e6c7f2efc07c27dd4932a4792d2a4dcbe
LavaBoy202 <79107199+LavaBoy202@users.noreply.github.com>
2021-06-08 13:29:44 -0400

    Recommend 3 grade 4 Level worksheets for student whom score perfect on the Diagnostic (#191)
    * Added file for divisionTopicOverview
    
    * added Topic Overview files for Addition, Subtraction, and Multiplication
    
    * Styled multiplicationTopicOverview
    
    * Create SubtractionTopicOverview page
    
    * Crated AdditionTopicOverview pages
    
    * Reverted Original resources page
    
    * Update quiz message on TopicOverview page
    
    * Re-formatted Quiz section for Topic Overview
    
    * Add content to practice section of divisionTopicOverview
    
    * Resizing of images in practice section of subtractionTopic Overview
    
    * Edit wording of quiz section for topicOverview
    
    * Fix spelling and wording of phrases of Topic Overview pages
    
    * Fix naming of classes and remove unused libraries
    
    * Update DivisionTopicOverview pages
    
    * Update divisionTopicOverview.tsx
    
    * Take out box around numbers for subtraction  numbers
    
    * Remove border for multiplication word problems
    
    * Add name as input in the Diagnostic
    
    * Adjusted test cases
    
    * Add name to email request body
    
    * Change Format of Diagnostic starting page
    
    * remove name from request body
    
    * Delete email.ts file
    
    * Add grade selector for quiz component in the Topic Overview Page
    
    * The select grade text
    
    * Renaming and return number instead of variables
    
    * Recommend 3 Grade 4 level worksheets for students with perfect scores
    
    * Added a test case and adjusted a previous one
    
    * Corrected Test cases
    
    * Edit title from Addition to Addition Topic Overview

--- pages/api/worksheets.test.ts
+++ pages/api/worksheets.test.ts
@@ -28,7 +28,7 @@ test("test worksheets", async () => {
   const worksheets = getWorkSheets(state);
 
   // Assert
-  expect(worksheets.length).toBe(0);
+  expect(worksheets.length).toBe(3);
 });
 
 test("test worksheets2", async () => {
@@ -62,5 +62,55 @@ test("test worksheets2", async () => {
 
   // Assert
   expect(worksheets.length).toBe(1);
-  expect(worksheets[0].title).toBe("Equal Groups to 10 Worksheet");
+  expect(worksheets[0].title).toBe("Multiplication To 5 Worksheet");
+});
+test("test worksheets3", async () => {
+  const dummyDivisionQuestion = {
+    text: "4/2=",
+    answer: "2",
+    answerType: AnswerType.NUMBER,
+    skill: Skill.DIVIDE_12_EQUALLY,
+    questionType: QuestionType.LONG_DIVISION_PROBLEM,
+  };
+  const dummyMultiplicationQuestion = {
+    text: "4*2=",
+    answer: "8",
+    answerType: AnswerType.NUMBER,
+    skill: Skill.MULTIPLICATION_5,
+    questionType: QuestionType.HORIZONTAL_EQUATION,
+  };
+  const dummyAdditionQuestion = {
+    text: "100+100=",
+    answer: "200",
+    answerType: AnswerType.NUMBER,
+    skill: Skill.ADDITION_TRIPLE,
+    questionType: QuestionType.HORIZONTAL_EQUATION,
+  };
+  const dummySubtractionQuestion = {
+    text: "4-2=",
+    answer: "2",
+    answerType: AnswerType.NUMBER,
+    skill: Skill.SUBTRACTION_SINGLE,
+    questionType: QuestionType.HORIZONTAL_EQUATION,
+  };
+
+  // Arrange
+  const state: DiagnosticState = {
+    questions: [dummyDivisionQuestion, dummyMultiplicationQuestion, dummyAdditionQuestion,dummySubtractionQuestion ],
+    guessAns: ["Incorrect", "Incorrect", "Incorrect", "Incorrect"],
+    guesses: ["1", "3","5", "7"],
+    email: "test@gmail.com",
+    grade: "Grade 2",
+    name: "Vijaykumar"
+  };
+
+  // Act
+  const worksheets = getWorkSheets(state);
+
+  // Assert
+  expect(worksheets.length).toBe(4);
+  expect(worksheets[0].title).toBe("Division To 12 Worksheet");
+  expect(worksheets[1].title).toBe("Multiplication To 5 Worksheet");
+  expect(worksheets[2].title).toBe("Addition 3-digits Worksheet");
+  expect(worksheets[3].title).toBe("Subtraction 1-digit Worksheet");
 });
