c3abff672781861358da4ed72cf39af5348889a8
vithushan19 <vithushan19@gmail.com>
2021-06-09 16:49:28 -0400

    Revert "Hl mc (#193)"
    This reverts commit 4b47832795594c864100312aa148a1370a91908a.

--- pages/api/questionTypes.ts
+++ pages/api/questionTypes.ts
@@ -5,9 +5,8 @@ export enum QuestionType {
   COMPARISON_WORD_PROBLEM,
   LONG_DIVISION_PROBLEM,
   TRUE_OR_FALSE_PROBLEM,
-  MULTIPLE_CHOICE_WORD, // Addition Property with words as mc options
-  MULTIPLE_CHOICE_SENTENCE, // Addition Property with numbers as mc options
-  MULTIPLE_CHOICE, // Multiple choice answer to a binary question
+  MULTIPLE_CHOICE_WORD,
+  MULTIPLE_CHOICE_SENTENCE,
   FILL_IN_THE_BLANK_PROBLEM,
   ARRAY_QUESTION,
   MULTIPLICATION_EQUAL_GROUPS,
