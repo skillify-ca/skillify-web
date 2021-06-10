bde15c3bc47d8bc3f253148cf03cf7a0a3695fae
Vithushan <vithushan19@users.noreply.github.com>
2021-06-10 15:43:17 -0400

    Save redux state to localstorage (#213)

--- redux/store.ts
+++ redux/store.ts
@@ -1,15 +1,33 @@
-import { configureStore, Action } from '@reduxjs/toolkit'
-import { useDispatch } from 'react-redux'
-import { ThunkAction } from 'redux-thunk'
+import { configureStore, Action } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { ThunkAction } from "redux-thunk";
+import { applyMiddleware, createStore } from "redux";
 
-import rootReducer, { RootState } from './rootReducer'
+import rootReducer, { RootState } from "./rootReducer";
 
-const store = configureStore({
-    reducer: rootReducer,
-})
+// Import the necessary methods for saving and loading
+import { save, load } from "redux-localstorage-simple";
 
-export type AppDispatch = typeof store.dispatch
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export type AppThunk = ThunkAction<void, RootState, unknown, Action>
+/*
+    Saving to LocalStorage is achieved using Redux 
+    middleware. The 'save' method is called by Redux 
+    each time an action is handled by your reducer.
+*/
+const createStoreWithMiddleware = applyMiddleware(
+  save() // Saving done here
+)(createStore);
 
-export default store
+/*
+    Loading from LocalStorage happens during
+    creation of the Redux store.
+*/
+const store = createStoreWithMiddleware(
+  rootReducer,
+  load() // Loading done here
+);
+
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export type AppThunk = ThunkAction<void, RootState, unknown, Action>;
+
+export default store;
