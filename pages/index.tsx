b6f5ff0f4cddb7e6a387912a0412369a67a2f691
Vithushan <vithushan19@users.noreply.github.com>
2021-06-14 15:45:38 -0400

    Point to old landing page for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import React, {  } from "react";
 import DiagnosticNavbar from "../components/DiagnosticNavbar";
 import LandingPage from "../components/stories/LandingPage";
-import LandingPagev2 from "../components/stories/LandingPagev2";
-
 
 
 const HomePage = () => {
@@ -10,7 +8,7 @@ const HomePage = () => {
   return (
     <div className="flex flex-col overflow-auto bg-scroll heropattern-piefactory-blue-100 bg-gray-100">
       <DiagnosticNavbar />
-      <LandingPagev2 />
+      <LandingPage />
       </div>
   );
 };
