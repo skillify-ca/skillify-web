9e228ca2d04b9c8fc53169ccd259cbfc20ab5ec4
vithushan19 <vithushan19@gmail.com>
2021-06-14 06:43:10 -0400

    Use LandingPage as logged out component on practice

--- components/Outline.tsx
+++ components/Outline.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import { useMutation, useQuery } from "@apollo/client";
-import SkillCard, { PracticeTopic } from "./PracticeTopic";
+import { PracticeTopic } from "./PracticeTopic";
 import { FETCH_USER_SKILLS } from "../graphql/fetchUserSkills";
 import { signIn, useSession } from "next-auth/client";
 import { INIT_USER_SKILLS } from "../graphql/initUserSkills";
@@ -9,11 +9,9 @@ import Card from "./stories/Card";
 import { Button } from "./stories/Button";
 import ProgressRing from "./stories/ProgressRing";
 import { lockedTopics, unlockedTopics } from "../pages/api/topics";
+import LandingPage from "./stories/LandingPage";
 export default function Outline() {
-  const skillsEndRef = useRef(null);
   const [session, loading] = useSession();
-  const [skills, setSkills] = React.useState([]);
-  const [unlockedSkills, setUnlockedSkills] = React.useState([]);
 
   const userSkillsData = useQuery(FETCH_USER_SKILLS, {
     variables: {
@@ -34,13 +32,8 @@ export default function Outline() {
   useEffect(() => {
     // Don't run if the session hasn't loaded yet
     if (userSkillsData.data && userId(session) != "-1") {
-      setSkills(userSkillsData.data.user_skills);
-      if (userSkillsData.data.user_skills.length > 0) {
-        const filteredSkills = userSkillsData.data.user_skills.filter(
-          (it) => it.locked == false
-        );
-        setUnlockedSkills(filteredSkills);
-      } else {
+      if (false) {
+        // TODO new user doesn't have any unlocked badges. We should initialize them in GraphQL
         initUserSkills({
           variables: {
             userId: userId(session),
@@ -51,10 +44,7 @@ export default function Outline() {
   }, [userSkillsData, session]);
 
   const getOverallProgress = () => {
-    if (skills && skills.length > 0) {
-      return Math.floor((100 * (unlockedSkills.length - 1)) / skills.length);
-    }
-    return 0;
+    return 0; // TODO calculate progress based off unlocked badges / total badges
   };
 
   const loggedInComponent = (
@@ -97,67 +87,7 @@ export default function Outline() {
     </div>
   );
 
-  const loggedOutComponent = (
-    <div className="flex flex-col gap-8">
-      <div className="flex flex-col sm:flex-row bg-white shadow-lg rounded-xl p-4 gap-8">
-        <div className="flex flex-col w-full sm:w-1/2 gap-8">
-          <p className="text-5xl">Make math fun to practice</p>
-          <p className="text-xl">
-            Learning with Math Champ is engaging and fun. Earn points for
-            correct answers, play with friends and level up. Our
-            curriculum-based lessons are effective and efficient.
-          </p>
-          <div className="flex gap-8">
-            <Button backgroundColor="blue" label="Log In" textColor="white" />
-            <Button
-              backgroundColor="yellow"
-              label="Sign Up"
-              textColor="white"
-            />
-          </div>
-        </div>
-        <img
-          className="w-full sm:w-1/2 object-cover"
-          alt="student-image"
-          src="https://images.unsplash.com/photo-1596495578065-6e0763fa1178?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDEzNDF8MHwxfHNlYXJjaHwxMDZ8fG1hdGh8ZW58MHx8fA&ixlib=rb-1.2.1&q=85&w=528&dpr=2"
-        />
-      </div>
-      <div className="bg-blue-500 heropattern-architect-blue-400 rounded-xl shadow-lg flex-col text-center p-8">
-        <p className="text-5xl text-white">K-12 Math Activities and Quizzes</p>
-        <p className="text-xl text-white">
-          Help your students feel more confident with numbers with content
-          aligned to the Canadian curriculum
-        </p>
-      </div>
-      <div className="heropattern-dominos-blue-400 bg-blue-500 rounded-xl shadow-lg flex md:flex-row flex-col text-center gap-8 p-8">
-        <div
-          className={`flex flex-col justify-center items-center p-8 bg-white shadow-md rounded-xl max-w-screen-lg"`}
-        >
-          <p className="text-xl">Lesson Mode</p>
-          <p>
-            Explore and watch different videos to get introduced to a topic.
-          </p>
-        </div>
-        <div
-          className={`flex flex-col justify-center items-center p-8 bg-white shadow-md rounded-xl max-w-screen-lg"`}
-        >
-          <p className="text-xl">Practice Mode</p>
-          <p>
-            Practice questions and reflect on their learning using flashcards.
-          </p>
-        </div>
-        <div
-          className={`flex flex-col justify-center items-center p-8 bg-white shadow-md rounded-xl max-w-screen-lg"`}
-        >
-          <p className="text-xl">Quiz Mode</p>
-          <p>
-            Complete formative and summative assessments to prove their
-            understanding.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+  const loggedOutComponent = <LandingPage />;
 
   return (
     <div className="flex flex-col gap-8 justify-between w-full col-span-2 items-center mb-4 p-4 mx-auto">
