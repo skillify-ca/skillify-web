6ca5e969f260948c37f1eb5c45df5fd4fa2816df
LavaBoy202 <lavannithi2@gmail.com>
2021-06-17 11:46:21 -0400

    End on quiz home button redirects to knowledge tree instead of landing page

--- pages/quiz/[slug].tsx
+++ pages/quiz/[slug].tsx
@@ -195,7 +195,7 @@ const Quiz = ({ slug }) => {
           >
             Replay
           </button>
-          <Link href="/">
+          <Link href="/practice">
             <button className="group relative w-3/4 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
               Home
             </button>
