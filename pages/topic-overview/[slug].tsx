2a1a046b9d2cad4701ab637371cfa8a1ef1f6c17
Yaathavi <yaathavi@gmail.com>
2021-06-22 19:03:31 -0400

    Style Skill Overview Page

--- pages/topic-overview/[slug].tsx
+++ pages/topic-overview/[slug].tsx
@@ -13,7 +13,7 @@ import { getSkillsForTopicGrade, Grade, SkillDescription } from "../api/skill";
 
 const TopicOverviewPage = ({ slug }) => {
   const [session, user] = useSession();
-  const [grade, setGrade] = useState(Grade.GRADE_3);
+  const [grade, setGrade] = useState(Grade.GRADE_1);
   const onGradeChange = (e: any) => {
     setGrade(e.target.value);
   };
@@ -61,24 +61,49 @@ const TopicOverviewPage = ({ slug }) => {
 
   const levelComponent = (
     <div>
-      <div className="flex gap-8">
-        <p className="font-bold"> Select Grade:</p>
-        <select
-          value={grade}
-          onChange={onGradeChange}
-          className="border border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none"
-        >
-          <option>Grade 1</option>
-          <option>Grade 2</option>
-          <option>Grade 3</option>
-        </select>
-      </div>
+      <span className="font-bold m-4"> Select Level:</span>
+      <select
+        value={grade}
+        onChange={onGradeChange}
+        className="border border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none"
+      >
+        <option>Grade 1</option>
+        <option>Grade 2</option>
+        <option>Grade 3</option>
+      </select>
     </div>
   );
 
+  const skillComponent = (
+    <div className="flex justify-center">
+      <div className="flex flex-col sm:flex-row bg-white shadow-lg rounded-xl p-4 gap-8 m-4 w-1/2 h-72">
+        <div className="flex flex-col w-full sm:w-1/2 gap-8">
+          {getSkillsForTopicGrade(slug, grade).map((skill) => (
+            <Link href={`/skill-overview/${skill}`}>
+              <p className="border-blue-400 border-4">
+                {" "}
+                {SkillDescription(skill)}{" "}
+              </p>
+            </Link>
+          ))}
+        </div>
+        <div className="">
+          {skillBadge.map((badge) => {
+            return badge.locked ? (
+              <div className="">
+                <img src="/images/lockedPic.png" className="w-32" />
+              </div>
+            ) : (
+              <img src={badge.badge.image} className="w-32" />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
   const quizComponent = (
     <div>
-      <div className="flex flex-col sm:flex-row bg-white shadow-lg rounded-xl p-4 gap-8">
+      <div className="flex flex-col sm:flex-row bg-white shadow-lg rounded-xl p-4 gap-8 m-4">
         <div className="flex flex-col w-full sm:w-1/2 gap-8">
           <p className="text-5xl"> Quiz Time!</p>
           <p className="text-xl">
@@ -112,32 +137,57 @@ const TopicOverviewPage = ({ slug }) => {
   return (
     <div className="flex flex-col justify-center overflow-auto bg-scroll heropattern-piefactory-blue-100 bg-gray-100">
       <Navbar />
-      <div className="p-4 flex flex-col items-center justify-center">
-        {levelComponent}
-        {getSkillsForTopicGrade(slug, grade).map((skill) => (
-          <Link href={`/skill-overview/${skill}`}>
-            <p className="border-blue-400 border-4">
-              {" "}
-              {SkillDescription(skill)}{" "}
-            </p>
-          </Link>
-        ))}
-        {quizComponent}
+      <div className="bg-blue-500 heropattern-architect-blue-400 rounded-xl shadow-lg flex-col text-center p-8 m-4">
+        <p className="text-5xl text-white mb-4">Addition Topic Overview</p>
+        <p className="text-xl text-white">
+          Addition is taking two or more numbers and adding them together! Watch
+          the videos on the explore tab to learn more and do the practice
+          questions to apply your knowledge. Once you feel confident in your
+          addition skills, take the quiz to evaluate your understanding!
+        </p>
       </div>
-      <div className="grid gap-x-8 gap-y-4 grid-cols-3 w-1/2 m-auto p-4">
-        {skillBadge.map((badge) => {
-          return badge.locked ? (
-            <div className="">
-              <img src="/images/lockedPic.png" className="w-32" />
-            </div>
-          ) : (
-            <img src={badge.badge.image} className="w-32" />
-          );
-        })}
-      </div>
+      {levelComponent}
+      {skillComponent}
+      {quizComponent}
     </div>
   );
 };
+
+//   /* <div className="p-4 flex flex-col items-center justify-center">
+//         {levelComponent}
+//         <div className="">
+//           {skillBadge.map((badge) => {
+//             return badge.locked ? (
+//               <div className="">
+//                 <img src="/images/lockedPic.png" className="w-32" />
+//               </div>
+//             ) : (
+//               <img src={badge.badge.image} className="w-32" />
+//             );
+//           })}
+//         </div>
+//         {getSkillsForTopicGrade(slug, grade).map((skill) => (
+//           <Link href={`/skill-overview/${skill}`}>
+//             <p className="border-blue-400 border-4">
+//               {" "}
+//               {SkillDescription(skill)}{" "}
+//             </p>
+//           </Link>
+//         ))}
+//         {quizComponent}
+//       </div> */
+
+/* <div className="">
+{skillBadge.map((badge) => {
+  return badge.locked ? (
+    <div className="">
+      <img src="/images/lockedPic.png" className="w-32" />
+    </div>
+  ) : (
+    <img src={badge.badge.image} className="w-32" />
+  );
+})}
+</div> */
 
 export async function getStaticProps({ params }) {
   return {
