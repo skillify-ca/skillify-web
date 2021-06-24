f2e67eb5e7940e942a201da6f7fa119d947fd780
LavaBoy202 <lavannithi2@gmail.com>
2021-06-24 14:15:19 -0400

    Initialize Emoji values for each user_skill

--- graphql/initUserSkills.ts
+++ graphql/initUserSkills.ts
@@ -1,14 +1,25 @@
 import { gql } from "@apollo/client";
 
 export const INIT_USER_SKILLS = gql`
-mutation initUserSkills($userId: String = "", $skillId:) {
-  insert_user_skills(objects: 
-    [
-      {userId: $userId, skillId: $skillId, emoji: 101},
+mutation initUserSkills($userId: String = "") {
+  insert_user_skills(
+    objects: [
+      { user_id: $userId, skill_id: 1, emoji: null }
+      { user_id: $userId, skill_id: 2, emoji: null }
+      { user_id: $userId, skill_id: 3, emoji: null }
+      { user_id: $userId,  skill_id: 4, emoji: null }
+      { user_id: $userId, skill_id: 34, emoji: null }
+      { user_id: $userId, skill_id: 35, emoji: null }
+      { user_id: $userId,  skill_id: 36, emoji: null }
+      { user_id: $userId,  skill_id: 37, emoji: null }
+      { user_id: $userId,  skill_id: 38, emoji: null }
+      { user_id: $userId, skill_id: 39, emoji: null }
+      { user_id: $userId,  skill_id: 40, emoji: null }
+      { user_id: $userId, skill_id: 41, emoji: null }       
     ]
   ) {
     returning {
-      id
+      skill_id
     }
   }
 }
