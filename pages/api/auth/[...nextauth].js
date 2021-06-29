8f4c2fa05760515cf9237921850ecb9a748e78ba
Yaathavi <46689185+Yaathavi@users.noreply.github.com>
2021-06-29 15:32:34 -0400

    initialize user info with login (#266)

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -35,15 +35,57 @@ const userSync = async (token) => {
   const nickname = token.name;
   const url = "https://talented-duckling-40.hasura.app/v1/graphql";
   const upsertUserQuery = `
-    mutation ($userId: String!, $name:String){
-  insert_users(objects: [{ id: $userId, name:$name }], on_conflict: { constraint: users_pkey, update_columns: [] }) {
-    affected_rows
+  
+  mutation initializeUser ($userId: String!, $name:String){
+    insert_users(objects: [{ id: $userId, name:$name }], on_conflict: { constraint: users_pkey, update_columns: [] }) {
+      affected_rows
+  }
+   insert_user_badges(
+      objects: [
+        { userId: $userId, locked: true, badgeId: 1 }
+        { userId: $userId, locked: true, badgeId: 2 }
+        { userId: $userId, locked: true, badgeId: 3 }
+        { userId: $userId, locked: true, badgeId: 4 }
+        { userId: $userId, locked: true, badgeId: 6 }
+        { userId: $userId, locked: true, badgeId: 7 }
+        { userId: $userId, locked: true, badgeId: 8 }
+        { userId: $userId, locked: true, badgeId: 9 }
+        { userId: $userId, locked: true, badgeId: 10 }
+        { userId: $userId, locked: true, badgeId: 11 }
+        { userId: $userId, locked: true, badgeId: 12 }
+        { userId: $userId, locked: true, badgeId: 13 }
+      ]
+    ) {
+      returning {
+        badgeId
+      }
+    }
+  insert_user_skills(
+    objects: [
+      { user_id: $userId, skill_id: 1, emoji: null }
+      { user_id: $userId, skill_id: 2, emoji: null }
+      { user_id: $userId, skill_id: 3, emoji: null }
+      { user_id: $userId,  skill_id: 4, emoji: null }
+      { user_id: $userId, skill_id: 34, emoji: null }
+      { user_id: $userId, skill_id: 35, emoji: null }
+      { user_id: $userId,  skill_id: 36, emoji: null }
+      { user_id: $userId,  skill_id: 37, emoji: null }
+      { user_id: $userId,  skill_id: 38, emoji: null }
+      { user_id: $userId, skill_id: 39, emoji: null }
+      { user_id: $userId,  skill_id: 40, emoji: null }
+      { user_id: $userId, skill_id: 41, emoji: null }  
+      { user_id: $userId, skill_id: 42, emoji: null }     
+    ]
+  ) {
+    returning {
+      skill_id
+    }
   }
 }
 `;
   // TODO if user was already in the user table, then update the last seen column
   // If the user was newly added to the user table, then initialize their skills, and badges
-  // Then we can remove the initialization checks in the practice tracker
+  // Then we can remove the initialization checks in the profile and knowledge tree
   const graphqlReq = {
     query: upsertUserQuery,
     variables: { userId: userId, name: nickname },
