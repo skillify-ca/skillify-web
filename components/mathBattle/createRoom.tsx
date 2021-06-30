9a81c9a62a74617bf628a0d193922ac9445801d6
LavaBoy202 <lavannithi2@gmail.com>
2021-06-30 12:34:41 -0400

    Style the PVP join screen

--- components/mathBattle/createRoom.tsx
+++ components/mathBattle/createRoom.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "../stories/Button";
 import * as Colyseus from "colyseus.js";
-import Card from "../stories/Card";
 
 const CreateRoom = () => {
   const [room, setRoom] = useState<Colyseus.Room>();
@@ -33,28 +32,29 @@ const CreateRoom = () => {
 
   return (
     <div className="mr-10 ml-10">
-      <div className="flex flex-row bg-gray-100 rounded-lg gap-4">
-        <div className="h-75px">
-          <img src="/images/PVPIconBackground.png" />
+      <div className="bg-white rounded-lg gap-4 flex flex-col md:flex-row">
+        <div className="items-center content-around">
+          <img src="/images/PVPIconBackground.png" width="300" height="300" />
         </div>
-        <div className="flex flew-col">
-          <h1 className="text-6xl text-blue-600 font-bold">Player Vs Player</h1>
-          <p className="text-2xl">
+        <div className="flex flex-col p-6">
+          <h1 className="text-5xl text-blue-600 font-bold">Player Vs Player</h1>
+          <p className="text-2xl mt-4 mb-3">
             To go head to head with another Math Champ in this heated Player Vs
             Player Mode. You will be racing with eachother to see who can get
-            the most correct answers and who can do the fastest. So let's see
-            who will come up top?
+            the most correct answers and who can complete it the fastest. So
+            let's see who will come up top!
           </p>
-
-          <Button
-            onClick={onCreateClick}
-            label="Join Game"
-            textColor="white"
-            backgroundColor="blue"
-          ></Button>
-          {players.map((it) => (
-            <p>{it}</p>
-          ))}
+          <div className="w-1/2 sm:items-center md:self-start">
+            <Button
+              onClick={onCreateClick}
+              label="Join Game"
+              textColor="white"
+              backgroundColor="blue"
+            ></Button>
+            {players.map((it) => (
+              <p>{it}</p>
+            ))}
+          </div>
         </div>
       </div>
     </div>
