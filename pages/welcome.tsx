19e4091a86f3da53db35790d571dce6f57c355a7
Yaathavi <46689185+Yaathavi@users.noreply.github.com>
2021-07-01 15:01:07 -0400

    Style Welcome page (#276)
    * style welcome/sign in page
    
    * Add google logo
    
    Co-authored-by: vithushan19 <vithushan19@gmail.com>

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -2,12 +2,9 @@ import React, { useEffect, useRef, useState } from "react";
 import DiagnosticNavbar from "../components/DiagnosticNavbar";
 import SignInPage from "../components/SignInPage";
 
-
 const Welcome = () => {
-
-
   return (
-    <div className="flex flex-col overflow-auto bg-scroll heropattern-piefactory-blue-100 bg-gray-100 h-screen">
+    <div className="flex flex-col overflow-auto bg-scroll  heropattern-architect-blue-50 bg-blue-100 h-screen">
       <div className="p-4 flex flex-col items-center justify-center">
         <SignInPage />
       </div>
