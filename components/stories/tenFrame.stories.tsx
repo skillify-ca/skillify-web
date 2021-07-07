e11b1e78df64cedbae39eceb0509bedbf96fe6bd
Harrishan Loganathan <harrishanlogan@gmail.com>
2021-07-07 12:54:50 -0400

    Made tenframe more dynamic

--- components/stories/tenFrame.stories.tsx
+++ components/stories/tenFrame.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Story, Meta } from "@storybook/react";
-import TenFrame from "./tenFrame";
+import TenFrame, { TenFrameProp } from "./tenFrame";
 
 export default {
   title: "Ten Frame",
@@ -8,7 +8,9 @@ export default {
   argTypes: {},
 } as Meta;
 
-const Template: Story = (args) => <TenFrame {...args} />;
+const Template: Story<TenFrameProp> = (args) => <TenFrame {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  num: 6,
+};
