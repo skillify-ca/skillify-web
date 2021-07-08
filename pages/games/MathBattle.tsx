8e225d5fd28bee5283e0861b140f6862f7131377
vithushan <vithushan19@gmail.com>
2021-07-08 18:37:52 -0400

    wss

--- pages/games/MathBattle.tsx
+++ pages/games/MathBattle.tsx
@@ -36,7 +36,7 @@ const MathBattle = () => {
       skill: Skill.ADDITION_SINGLE,
     },
   ]);
-  var client = new Colyseus.Client("ws://math-game-server.herokuapp.com/");
+  var client = new Colyseus.Client("wss://math-game-server.herokuapp.com");
   const onJoinClick = () => {
     client
       .joinById(code)
