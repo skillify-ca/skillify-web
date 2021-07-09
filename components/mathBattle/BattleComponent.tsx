5c33060eefb6d7e197b045918cd5080f529a546b
Vithushan <vithushan19@users.noreply.github.com>
2021-07-09 15:35:36 -0400

    Send both players to the game stage on button click (#298)
    * send both players to the game stage on button click
    
    * make host generate questions and send to colyseus
    
    * add game over screen
    
    * use prod game server
    
    * wss
    
    * Use localhost
    
    Co-authored-by: LavaBoy202 <lavannithi2@gmail.com>

---
+++ components/mathBattle/BattleComponent.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Button } from "../stories/Button";
+import * as Colyseus from "colyseus.js";
+import Link from "next/link";
+import { Player } from "../../pages/games/MathBattle";
+import QuestionSet from "../stories/QuestionSet";
+import { AnswerType, Question } from "../../pages/api/question";
+import { QuestionType } from "../../pages/api/questionTypes";
+import { generateQuestions } from "../../pages/api/quiz/quizQuestionGenerator";
+import { Skill } from "../../pages/api/skill";
+import { GuessData } from "../../pages/api/guessData";
+
+export interface BattleComponentProps {
+  questions: Question[];
+  room: Colyseus.Room;
+}
+
+const BattleComponent = ({ questions, room }: BattleComponentProps) => {
+  const [index, setIndex] = useState(0);
+  const inputElement = useRef(null);
+  const [winnerId, setWinnerId] = useState("");
+
+  const submitGuess = (currentGuess: GuessData) => {
+    console.log("currentGuess", currentGuess);
+    if (index + 1 < questions.length) {
+      setIndex(index + 1);
+      // notify colyseus that this player submitted a guess
+    } else {
+      // TODO currently whoever calls this is declared the winner
+      room.send("requestGameOver", room.sessionId);
+    }
+  };
+  room.onMessage("showGameOver", (message) => {
+    setWinnerId(message);
+  });
+  return (
+    <div>
+      {winnerId === "" ? (
+        <QuestionSet
+          title={"Battle"}
+          questionData={questions}
+          index={index}
+          inputElement={inputElement}
+          submitGuess={submitGuess}
+          score={1}
+          practice={false}
+        />
+      ) : winnerId === room.sessionId ? (
+        "Winner"
+      ) : (
+        "Loser"
+      )}
+    </div>
+  );
+};
+export default BattleComponent;
