364b50f40713c740e0ca62489881b3826bb64145
LavaBoy202 <lavannithi2@gmail.com>
2021-07-22 15:33:53 -0400

    Fix Bug

--- pages/api/questionGenerator.ts
+++ pages/api/questionGenerator.ts
@@ -275,14 +275,7 @@ export function getRandomAdditionQuestion(
   skill: Skill
 ) {
   let rndQuestionType = getRndInteger(0, 2);
-  if (
-    (false && rndQuestionType == 0) ||
-    skill == Skill.ADDITION_4_DIGIT ||
-    skill == Skill.ADDITION_TENTHS
-  ) {
-    const add = (a: number, b: number) => a + b;
-    return getRandomBinaryQuestion(min, max, "+", add, skill);
-  } else {
+  if (rndQuestionType > 0 && skill == Skill.ADDITION_SINGLE) {
     let a = getRndInteger(min, max);
     let b = getRndInteger(min, max);
     let text = `${a} + ${b} =`;
@@ -297,6 +290,8 @@ export function getRandomAdditionQuestion(
       displayNum: getRndInteger(0, 3),
     };
   }
+  const add = (a: number, b: number) => a + b;
+  return getRandomBinaryQuestion(min, max, "+", add, skill);
 }
 function getRandomSubtractionQuestion(min: number, max: number, skill: Skill) {
   const subtract = (a: number, b: number) => a - b;
