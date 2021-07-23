54d308fd8b12ab735f552895d19f588b516cbafb
Mayuran Ganesathas <mayu@Mayurans-MacBook-Pro.local>
2021-07-23 11:27:40 -0400

    updatedCard

--- components/ui/Mayu.tsx
+++ components/ui/Mayu.tsx
@@ -2,17 +2,18 @@ import { ReactNode } from "react";
 
 export interface CardProps {
   children: ReactNode;
-  size: "small" | "large";
+  bgColor: "blue" | "red";
+
 }
 
-const Card = ({ children, size }: CardProps) => {
+const Mayu = ({ children, bgColor}: CardProps) => {
   return (
     <div
-      className={`flex flex-col justify-center space-y-16 items-center p-8 bg-white shadow-md rounded-xl max-w-screen-lg
-       ${size === "large" ? " w-96 h-96" : "w-48 h-24"}`}
-    >
+      className={`flex flex-col justify-center space-y-16 items-center p-4 bg-blue shadow-md rounded-xl max-w-screen-lg
+     ${bgColor === "blue" ? "bg-blue" : "bg-red"}`}
+    >gi
       {children}
     </div>
   );
 };
-export default Card;
+export default Mayu;
