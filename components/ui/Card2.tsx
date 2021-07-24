381dc30646ae9bbd2166d5d1c1e3f536a1ee33fd
Daniel Thevarasalingam <65379304+Danielthev@users.noreply.github.com>
2021-07-24 13:00:30 -0400

    First Commit

---
+++ components/ui/Card2.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+export interface CardProps {
+  children: ReactNode;
+}
+
+const Card = ({children}: CardProps) => {
+  return (
+    <div
+      className={`flex flex-col justify-center space-y-16 items-center p-8 bg-white shadow-md rounded-xl max-w-screen-lg`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
