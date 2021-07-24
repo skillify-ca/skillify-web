f54e8abdd8440a08319e7626b94f62af54ab9804
brianghub <87824467+brianghub@users.noreply.github.com>
2021-07-24 12:55:08 -0400

    this is a testing (pls ignore)
    testing commit function (learning time)

--- components/ui/brian.tsx
+++ components/ui/brian.tsx
@@ -9,7 +9,7 @@ const Brian = ({ children, size }: BrianProps) => {
   return (
     <div
       className={`flex flex-col justify-center space-y-16 items-center p-8 bg-grey shadow-md rounded-xl max-w-screen-lg
-       ${size === "large" ? " w-40 h-40" : "w-48 h-24"}`}
+       ${size === "large" ? " w-100 h-50" : "w-48 h-24"}`}
     >
       {children}
     </div>
