421a56a7a771367d510f7d89cd39da6478613ac7
Mayuran Ganesathas <mayu@Mayurans-MacBook-Pro.local>
2021-07-24 22:47:51 -0400

    Personal_Card_Addition

--- components/ui/Mayu.tsx
+++ components/ui/Mayu.tsx
@@ -1,17 +1,19 @@
 import { ReactNode } from "react";
 
-export interface CardProps {
+export interface MayuProps {
   children: ReactNode;
-  bgColor: "blue" | "red";
-
+  bgColor: "blueBackground" | "redBackground";
+//probably better to use boolean logic than options
 }
 
-const Mayu = ({ children, bgColor}: CardProps) => {
+const Mayu = ({ children, bgColor}: MayuProps) => {
   return (
     <div
-      className={`flex flex-col justify-center space-y-16 items-center p-4 bg-blue shadow-md rounded-xl max-w-screen-lg
-     ${bgColor === "blue" ? "bg-blue" : "bg-red"}`}
-    >gi
+      className={`flex flex-col justify-center space-y-16 items-center shadow-md rounded-2xl	bgrounded-xl max-w-screen-lg w-96 h-48
+      ${bgColor === "blueBackground" ? " bg-blue-200" : "bg-red-200"}
+    `}
+    > 
+    <h1>Mayu's Card</h1>
       {children}
     </div>
   );
