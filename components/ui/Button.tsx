4eb0463f9c8fa6fa24e9f1ef2bf5e6435e857159
Vithushan <vithushan19@users.noreply.github.com>
2021-07-26 12:45:24 -0400

    Update Button.tsx

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -74,7 +74,7 @@ export const Button: React.FC<ButtonProps> = ({
       disabled={disabled}
       type="button"
       onClick={disabled ? null : onClick}
-      className={`bg-gradient-to-b px-4 py-2 font-bold border-b-4 rounded-xl                               
+      className={`bg-gradient-to-b px-4 py-2 font-bold border-b-4 rounded-xl
       ${disabled ? "bg-gray-400" : backgroundStyles}
       ${disabled ? "" : "active:border-b-2"}
       ${disabled ? "cursor-default" : "cursor-pointer"}
