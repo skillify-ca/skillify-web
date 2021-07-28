41fd90a9e1d24b940b76553af11c71c400cfc0b3
brianghub <87824467+brianghub@users.noreply.github.com>
2021-07-27 22:41:16 -0400

    storybook render

--- components/finance/ExpenseTable.tsx
+++ components/finance/ExpenseTable.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 const ExpenseTable = ({}) => {
   return (
-    <div className={`bg-white border-4 border-black`}>
-      <div className="grid grid-cols-3 gap-x-20 gap-y-5 align-middle">
+    <div className={`bg-white border-2 border-black`}>
+      <div className="grid grid-cols-3 gap-x-20 gap-y-5 border-1 border-black">
         <div className="">1</div>
         <div className="">2</div>
         <div className="">3 </div>
