9413df3c2da46b54a3985c4f7a7b5da75a281386
Mayuran Ganesathas <mayu@Mayurans-MacBook-Pro.local>
2021-07-28 07:51:43 -0400

    added financial profile page with FinancialProfileChart component

---
+++ pages/financial-profile.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import FinanceProfileChart from "../components/finance/FinanceProfileChart"
+
+
+const FinanceProfile= () => {
+  return (
+    <div className="flex flex-col overflow-auto bg-scroll heropattern-piefactory-blue-100 bg-gray-100">
+      <FinanceProfileChart 
+          indvidualOccupation = "Fireman"
+          individualSalary = "3000"
+          maritalStatus = "Single"
+          numberOfChildren = "3"
+          spouseOccupation = "Mailman"
+          spouseSalary = "3500"
+          />
+    </div>
+  );
+};
+
+export default FinanceProfile;
