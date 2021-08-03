6588669f7947a4723fb87cc5d09994262a9ae372
brianghub <87824467+brianghub@users.noreply.github.com>
2021-08-02 23:19:23 -0400

    Table Sytling

--- components/finance/CarExpenseTable.tsx
+++ components/finance/CarExpenseTable.tsx
@@ -35,14 +35,14 @@ const CarExpenseTable = ({
   return (
     <div>
       {" "}
-      <h1 className={"mb-2"}>Section 4: Car Expenses</h1>
-      <table className="table-fixed w-auto border-collapse">
+      <h1 className={"mb-2 font-bold"}>Section 4: Car Expenses</h1>
+      <table className="table-fixed border-collapse w-1/3">
         <thead>
           <tr>
             <th className="w-1/4"></th>
-            <th className="w-1/4"></th>
-            <th className="w-1/4"></th>
-            <th className="w-1/4"></th>
+            <th className="w-1/5"></th>
+            <th className="w-1/6"></th>
+            <th className="w-1/12"></th>
           </tr>
         </thead>
         <tbody>
@@ -51,26 +51,26 @@ const CarExpenseTable = ({
               {" "}
               <p className={"mx-2"}>Which car(s) did you buy?</p>
             </td>
-            <td className={"border-b-2 border-black"}>
+            <td className={"border-b-2 border-black flex flex-wrap"}>
               <input placeholder="Name of Car"></input>
             </td>
             <td>
               {" "}
               <p className={"mx-2"}>New or Used?</p>
             </td>
-            <td className={"border-b-2 border-black"}>
+            <td className={"border-b-2 border-black flex flex-wrap"}>
               <input placeholder="Select"></input>{" "}
             </td>
           </tr>
           <tr>
             <td></td>
-            <td className={"border-b-2 border-black"}>
+            <td className={"border-b-2 border-black flex flex-wrap"}>
               <input placeholder="Name of Car"></input>
             </td>
             <td>
               <p className={"mx-2"}>New or Used?</p>
             </td>
-            <td className={"border-b-2 border-black"}>
+            <td className={"border-b-2 border-black flex flex-wrap"}>
               <input placeholder="Select"></input>{" "}
             </td>
           </tr>
@@ -82,8 +82,14 @@ const CarExpenseTable = ({
       <table className="table-fixed w-auto border-collapse">
         <thead>
           <tr>
-            <th className="w-1/2"></th>
-            <th className="w-1/2"></th>
+            <th className="w-1/2 font-bold text-center bg-gray-200 border border-black">
+              {" "}
+              Expense
+            </th>
+            <th className="w-1/2 font-bold text-cetner bg-gray-200 border border-black">
+              {" "}
+              Cost Per Month
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -93,7 +99,7 @@ const CarExpenseTable = ({
               <p className={"mx-2"}> Car Payment #1</p>{" "}
             </td>
             <td className="border border-black flex flex-nowrap">
-              I.
+              <p className={"mx-2"}> I. </p>
               <div className={"ml-2"}>
                 <input
                   value={carPayment1}
@@ -109,7 +115,7 @@ const CarExpenseTable = ({
               <p className={"mx-2"}>Car Payment #2</p>
             </td>
             <td className="border border-black flex flex-nowrap">
-              J.{" "}
+              <p className={"mx-2"}> J. </p>{" "}
               <div className={"ml-2"}>
                 <input
                   value={carPayment2}
@@ -125,7 +131,7 @@ const CarExpenseTable = ({
               <p className={"mx-2"}>Insurance</p>
             </td>
             <td className="border border-black flex flex-wrap">
-              K.
+              <p className={"mx-2"}>K.</p>
               <div className={"ml-2"}>
                 <input
                   value={insuranceCar}
@@ -141,7 +147,7 @@ const CarExpenseTable = ({
               <p className={"mx-2"}>Gasoline</p>
             </td>
             <td className="border border-black flex flex-wrap">
-              L.
+              <p className={"mx-2"}>L.</p>
               <div className={"ml-2"}>
                 <input
                   value={gasoline}
@@ -153,10 +159,10 @@ const CarExpenseTable = ({
           </tr>
           <tr>
             <td className="border border-black bg-gray-300">
-              <p className={"mx-2"}>Total Monthly Income</p>
+              <p className={"mx-2 font-bold"}>Total Monthly Income</p>
             </td>
             <td className="border border-black flex flex-nowrap">
-              C.
+              <p className={"mx-2"}>M.</p>
               <div className={"ml-2"}>
                 <input
                   value={totalCarCosts}
