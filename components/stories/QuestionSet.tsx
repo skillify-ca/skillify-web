fcd9bbe43ae904db366b7779710f4402397ddde3
vithushan19 <vithushan19@gmail.com>
2021-08-03 22:45:09 -0400

    Update QuestionSet.tsx

--- components/stories/QuestionSet.tsx
+++ components/stories/QuestionSet.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { GuessData } from "../../pages/api/guessData";
 import { Question } from "../../pages/api/question";
 import { QuestionType } from "../../pages/api/questionTypes";
-import { getRndColour, getRndInteger } from "../../pages/api/random";
+import { getRandomItemFromArray, getRndColour, getRndInteger } from "../../pages/api/random";
 import { Skill } from "../../pages/api/skill";
 import { FillBlank } from "../questionTypes/FillBlank";
 import { HorizontalEquation } from "../questionTypes/HorizontalEquation";
@@ -243,6 +243,7 @@ const QuestionSet = ({
           <MultiplicationEqualGroups
             question={questionData[index]}
             submitGuess={submitGuess}
+            color={getRandomItemFromArray([0,1,2,3])}
           />
         );
       }
