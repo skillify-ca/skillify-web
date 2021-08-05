399db954810e306dc2ff817b82194282bac6c201
Mayuran Ganesathas <mayu@Mayurans-MacBook-Pro.local>
2021-08-05 16:03:49 -0400

    uppdated financial profile to hide missing rows

--- components/finance/FinanceProfileChartLine.tsx
+++ components/finance/FinanceProfileChartLine.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 //child component
 
 export interface FinanceProfileChartLineProps {
-  text: string;
+  text: string | number;
 }
 
 const FinanceProfileChartLine = ({ text }: FinanceProfileChartLineProps) => {
@@ -12,7 +12,7 @@ const FinanceProfileChartLine = ({ text }: FinanceProfileChartLineProps) => {
       className={`flex flex-col justify-center bg-green-300 rounded-l text-white max-w-screen w-full
     `}
     >
-      <div className={"flex justify-center"}>{text}</div>
+      <div className={"flex justify-center text-center"}>{text}</div>
     </div>
   );
 };
