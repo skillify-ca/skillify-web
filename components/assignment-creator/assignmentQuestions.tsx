a071238f577880f5a4b970efb2c4d506606a0368
Yaathavi <yaathavi@gmail.com>
2021-08-09 02:38:18 -0400

    allow user to select question type in second stage

--- components/assignment-creator/assignmentQuestions.tsx
+++ components/assignment-creator/assignmentQuestions.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { generateQuestionForSkill } from "../../pages/api/questionGenerator";
 import {
+  getQuestionTypesForSkill,
   getSkillsForTopic,
   getSkillsForTopicGrade,
   Grade,
@@ -10,23 +11,79 @@ import {
 } from "../../pages/api/skill";
 import { Button } from "../ui/Button";
 import Checkbox from "../ui/Checkbox";
+import { QuestionTypeForSkill } from "./assignmentCreationForm";
 import { generateAssignmentQuestions } from "./assignmentQuestionGenerator";
 
 type assignmentQuestionsProps = {
   selectedQuestions: Skill[];
+  selectedQuestionTypes: QuestionTypeForSkill[];
+  setSelectedQuestionTypes: (selectedQuestions: QuestionTypeForSkill[]) => void;
 };
 
 const AssignmentQuestions = ({
   selectedQuestions,
+  selectedQuestionTypes,
+  setSelectedQuestionTypes,
 }: assignmentQuestionsProps) => {
+  useEffect(() => {
+    // initialize dropdowns
+
+    const initialArray: QuestionTypeForSkill[] = [];
+
+    const initialQuestionTypes: QuestionTypeForSkill[] = selectedQuestions.map(
+      (skill) => {
+        return {
+          questionType: getQuestionTypesForSkill(skill)[0],
+          skill: skill,
+        };
+      }
+    );
+
+    initialArray.push(...initialQuestionTypes);
+    setSelectedQuestionTypes(initialArray);
+  }, []);
+
+  // just updates questiontype for the skill you changed in the array, not the other ones
+  const onQuestionTypeChange = (newQuestionType: string, skill: Skill) => {
+    const updatedArray = selectedQuestionTypes.map((item) => {
+      if (item.skill === skill) {
+        return { questionType: newQuestionType, skill: skill };
+      } else {
+        return item;
+      }
+    });
+    setSelectedQuestionTypes(updatedArray);
+  };
+  console.log("selected", selectedQuestionTypes);
+
+  const getQuestionTypeForSkill = (skill) => {
+    if (selectedQuestionTypes.length == 0) {
+      return "";
+    } else {
+      return selectedQuestionTypes.filter((it) => it.skill === skill)[0]
+        .questionType;
+    }
+  };
+
   return (
     <div className="flex flex-col gap-8 w-full">
-      <p>
-        {" "}
-        {selectedQuestions.map((skill) => (
-          <div> {generateAssignmentQuestions(skill)} </div>
-        ))}{" "}
-      </p>
+      {selectedQuestions.map((skill) => (
+        <div>
+          {" "}
+          {skill}
+          <select
+            value={getQuestionTypeForSkill(skill)}
+            onChange={(e) => onQuestionTypeChange(e.target.value, skill)}
+            multiple={false}
+            className="border border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none"
+          >
+            {getQuestionTypesForSkill(skill).map((questionType) => (
+              <option>{questionType}</option>
+            ))}
+          </select>
+        </div>
+      ))}
+
       <Button
         backgroundColor="blue"
         label="Generate Worksheet"
