6c4ebb31651f258662adb2ba3c831f6d21bacb8f
Mayuran Ganesathas <mayu@Mayurans-MacBook-Pro.local>
2021-08-10 22:17:38 -0400

    cupdated styling

--- components/finance/ServiceHeader.tsx
+++ components/finance/ServiceHeader.tsx
@@ -10,15 +10,20 @@ export const ServiceHeader = ({
   subHeader,
 }: ServiceHeaderProps) => {
   return (
-    <div className="grid grid-cols-2  border-4 w-5/6 rounded-xl shadow-xl	">
+    <div className="grid grid-cols-2  border-4 border-black w-5/6 rounded-xl 	">
       <div className="text-center text-5xl py-4 row-span-1 pl-2">
         {mainHeader}
+        <div>
+          <div className="grid row-start-2 text-center  text-xl pb-4 pl-4 py-6">
+            {subHeader}
+          </div>
+        </div>
       </div>
       <div>
-        <img src={imgHeader} className="grid row-end-1 place-items-end pl-32" />
-      </div>
-      <div className="grid row-start-2 text-center  text-xl pb-4 pl-4">
-        {subHeader}
+        <img
+          src={imgHeader}
+          className="grid row-end-1 place-items-end pl-40 py-6"
+        />
       </div>
     </div>
   );
