549c957d6d1eaeb49064554499199a5d0573f1ca
LavaBoy202 <lavannithi2@gmail.com>
2021-08-12 14:42:59 -0400

    update test cases

--- pages/api/questionGenerator.test.ts
+++ pages/api/questionGenerator.test.ts
@@ -19,24 +19,26 @@ test("generate Array Multiplication question", async () => {
 
   //Remainder Divison Tests
 });
-test("generate Array Multiplication question", async () => {
+test("generate Division with remainder question", async () => {
   // Arrange
-  let a = 4;
-  let b = 3;
+  let a = 11;
+  let b = 1;
 
   // Act
   const question = getRandomDivisionQuestionTestPath(
     a,
     b,
-    Skill.MULTIPLICATION_5
+    Skill.DIVISION_TWO_DIGIT_BY_ONE_DIGIT
   );
 
   // Assert
-  expect(question.text).toBe("hello" || "Bye");
+  expect(question.text).toBe("11 / 1 =");
+  expect(question.answer).toBe("11,0");
+  expect(question.questionType).toBe(QuestionType.HORIZONTAL_EQUATION);
 
   //Remainder Divison Tests
 });
-test("Generate Division with remainder question", async () => {
+test("Generate Division with remainder question #2", async () => {
   // Arrange
   let a = 40;
   let b = 3;
@@ -53,7 +55,24 @@ test("Generate Division with remainder question", async () => {
   expect(question.answer).toBe("13,1");
   expect(question.questionType).toBe(QuestionType.HORIZONTAL_EQUATION);
 });
-test("Generate Division with remainder question", async () => {
+test("Generate Division with remainder question #3", async () => {
+  // Arrange
+  let a = 99;
+  let b = 5;
+
+  // Act
+  const question = getRandomDivisionQuestionTestPath(
+    a,
+    b,
+    Skill.DIVISION_TWO_DIGIT_BY_ONE_DIGIT
+  );
+
+  // Assert
+  expect(question.text).toBe("99 / 5 =");
+  expect(question.answer).toBe("19,4");
+  expect(question.questionType).toBe(QuestionType.HORIZONTAL_EQUATION);
+});
+test("Generate Division with remainder question #4", async () => {
   // Arrange
   let a = 40;
   let b = 4;
