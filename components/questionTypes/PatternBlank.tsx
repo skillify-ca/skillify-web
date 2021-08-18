7d54ea6d2d6276a45e0a3ee504a155086e6e8ca3
Yaathavi <yaathavi@gmail.com>
2021-08-18 00:29:43 -0400

    fix spacing in PatternBlank question type

--- components/questionTypes/PatternBlank.tsx
+++ components/questionTypes/PatternBlank.tsx
@@ -29,23 +29,23 @@ export const PatternBlank: React.FC<PatternBlankProp> = ({
     <div className="flex flex-col items-center space-y-8">
       <h1 className="text-4m font-semibold text-center"> {displayQuestion} </h1>
       <div className="flex flex-row space-x-4 items-center">
-        {startNumber},
+        {startNumber},&nbsp;&nbsp;
         <input
-          className="border py-0.5 px-0.5 text-grey-darkest p-8 w-20"
+          className="border py-0.5 px-0.5 text-grey-darkest  w-20"
           type="number"
           value={guess1}
           onChange={(e) => setGuess1(e.target.value)}
         ></input>
         ,
         <input
-          className="border py-0.5 px-0.5 text-grey-darkest p-8 w-20"
+          className="border py-0.5 text-grey-darkest w-20"
           type="number"
           value={guess2}
           onChange={(e) => setGuess2(e.target.value)}
         ></input>
         ,
         <input
-          className="border py-0.5 px-0.5 text-grey-darkest p-8 w-20"
+          className="border py-0.5 px-0.5 text-grey-darkest w-20"
           type="number"
           value={guess3}
           onChange={(e) => setGuess3(e.target.value)}
