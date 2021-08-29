c63c2f12e88ef48dd10dfbc66710b9f8df216732
brianghub <87824467+brianghub@users.noreply.github.com>
2021-08-29 08:46:51 -0400

    FetchBadge ID based on User ID Query

---
+++ graphql/fetchBadgeOnUserID.ts
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+
+export const FETCH_BADGE_ON_USERID = gql`
+query FETCH_USER_BADGES ($userId: String ="") {
+    user_badges(where: {userId: {_eq: $userId}}) {
+      userId
+      badgeId
+    }
+  }
+`;
+
