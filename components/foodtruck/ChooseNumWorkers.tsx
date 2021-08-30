41d40fef3dbaf47b6d0135dfd48296c68b9e1edc
raveenreddy0 <66029765+raveenreddy0@users.noreply.github.com>
2021-08-30 17:29:46 -0400

    add foodtruck page and BuildAFoodTruck component (#413)
    * food truck page and component
    
    * rename bakery page
    
    * delete old bakery page
    
    * add new foodtruck components
    
    * add new foodtruck components
    
    * remove commented code

---
+++ components/foodtruck/ChooseNumWorkers.tsx
@@ -0,0 +1,106 @@
+import React, { ReactNode } from "react";
+
+export interface ChooseNumWorkersProps {
+  numWorkers: string;
+  setNumWorkers: (truck: string) => void;
+}
+
+const ChooseNumWorkers = ({
+  numWorkers,
+  setNumWorkers,
+}: ChooseNumWorkersProps) => {
+  const workers = ["1", "2", "3", "4"];
+  return (
+    <div className="flex flex-col border-2 border-black border-dashed p-4">
+      <h1 className="text-4xl text-black bold p-4">
+        How many workers do you want to employ?
+      </h1>
+      <label className="flex flex-row space-x-8 items-center p-4">
+        <input
+          className="form-radio h-8 w-8"
+          type="radio"
+          value="1"
+          checked={numWorkers === "1"}
+          onChange={(e) => setNumWorkers(e.target.value)}
+        />
+        <div className="flex flex-row">
+          <img
+            className="object-contain h-48 w-60 px-8"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3226091/chef-clipart.svg"
+          />
+        </div>
+      </label>
+      <label className="flex flex-row space-x-8 items-center p-4">
+        <input
+          className="form-radio h-8 w-8"
+          type="radio"
+          value="2"
+          checked={numWorkers === "2"}
+          onChange={(e) => setNumWorkers(e.target.value)}
+        />
+        <div className="flex flex-row">
+          <img
+            className="object-contain h-48 w-60 px-8"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3226091/chef-clipart.svg"
+          />
+          <img
+            className="object-contain h-48 w-60 px-8"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3226091/chef-clipart.svg"
+          />
+        </div>
+      </label>
+      <label className="flex flex-row space-x-8 items-center p-4">
+        <input
+          className="form-radio h-8 w-8"
+          type="radio"
+          value="3"
+          checked={numWorkers === "3"}
+          onChange={(e) => setNumWorkers(e.target.value)}
+        />
+        <div className="flex flex-row object-contain h-48 w-60">
+          <img
+            className="object-contain h-48 w-60 px-8"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3226091/chef-clipart.svg"
+          />
+          <img
+            className="object-contain h-48 w-60 px-8"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3226091/chef-clipart.svg"
+          />
+          <img
+            className="object-contain h-48 w-60 px-8"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3226091/chef-clipart.svg"
+          />
+        </div>
+      </label>
+      <label className="flex flex-row space-x-8 items-center p-4">
+        <input
+          className="form-radio h-8 w-8"
+          type="radio"
+          value="4"
+          checked={numWorkers === "4"}
+          onChange={(e) => setNumWorkers(e.target.value)}
+        />
+        <div className="flex flex-row">
+          <img
+            className="object-contain h-48 w-60 px-8"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3226091/chef-clipart.svg"
+          />
+          <img
+            className="object-contain h-48 w-60 px-8"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3226091/chef-clipart.svg"
+          />
+          <img
+            className="object-contain h-48 w-60 px-8"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3226091/chef-clipart.svg"
+          />
+          <img
+            className="object-contain h-48 w-60 px-8"
+            src="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/3226091/chef-clipart.svg"
+          />
+        </div>
+      </label>
+    </div>
+  );
+};
+
+export default ChooseNumWorkers;
---
+++ pages/foodtruck.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import BuildAFoodTruck from "../components/foodtruck/BuildAFoodTruck";
+import ChooseFoodType from "../components/foodtruck/ChooseFoodType";
+import ChooseNumWorkers from "../components/foodtruck/ChooseNumWorkers";
+import ChooseTruckType from "../components/foodtruck/ChooseTruckType";
+import { hotDog, smallTruck, Truck } from "./api/foodtruck/food";
+
+export default function FoodTruck(props) {
+  const [userName, setUserName] = useState("");
+  const [truckName, setTruckName] = useState("");
+  const [truckSlogan, setTruckSlogan] = useState("");
+  const [dollarAmount, setDollarAmount] = useState("");
+  const [truck, setTruck] = useState(smallTruck);
+  const [food, setFood] = useState(hotDog);
+  const [numWorkers, setNumWorkers] = useState("1");
+
+  const onSelectedTruckChanged = (truck: Truck) => {
+    setTruck(truck);
+    setFood(hotDog);
+  };
+  return (
+    <div>
+      <div className="flex flex-cols-3 border-2 border-bottom border-dashed border-black p-12">
+        <img
+          className="object-left object-contain h-28"
+          src={
+            "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/77310/food-truck-clipart-md.png"
+          }
+        />
+        <h1 className="p-8 text-black bold text-6xl">
+          Let's Build Your Own Food Truck!!!
+        </h1>
+        <img
+          className="object-right object-contain h-28 invert"
+          src={
+            "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/77310/food-truck-clipart-md.png"
+          }
+        />
+      </div>
+
+      <div className="grid grid-cols-2 p-4">
+        <div className="flex items-center">
+          <p className="col-span-3 text-black text-2xl">
+            You have been saving money for several years in hopes of starting
+            your very own food truck. Let's walk through all the things you'll
+            need to start your new food truck business and see whether you have
+            saved enough money!
+          </p>
+        </div>
+      </div>
+      <div className="grid grid-cols-2">
+        <BuildAFoodTruck
+          userName={userName}
+          setUserName={setUserName}
+          truckName={truckName}
+          setTruckName={setTruckName}
+          truckSlogan={truckSlogan}
+          setTruckSlogan={setTruckSlogan}
+          dollarAmount={dollarAmount}
+          setDollarAmount={setDollarAmount}
+        />
+
+        <ChooseTruckType truck={truck} setTruck={onSelectedTruckChanged} />
+        <ChooseFoodType
+          selectedFood={food}
+          setSelectedFood={setFood}
+          selectedTruck={truck}
+        />
+        <ChooseNumWorkers
+          numWorkers={numWorkers}
+          setNumWorkers={setNumWorkers}
+        />
+      </div>
+    </div>
+  );
+}
