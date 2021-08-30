65f50db3ca84b362bb166e0522828b233f77d45f
brianghub <87824467+brianghub@users.noreply.github.com>
2021-08-29 23:38:13 -0400

    testing pulling badges on ID
    created a new page to test on. will merge with Danny's stage once I lock this down. [not working thouugh]

--- graphql/fetchBadgeOnUserID.ts
+++ graphql/fetchBadgeOnUserID.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const FETCH_BADGE_ON_USERID = gql`
-query FETCH_USER_BADGES ($userId: String ="") {
-    user_badges(where: {userId: {_eq: $userId}}) {
+query FETCH_USER_BADGES ($userId: String="") {
+    user_badges(where: {userId: {_eq: "116309327098433793664"}}) {
       userId
       badgeId
     }
