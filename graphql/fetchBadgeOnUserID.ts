bff4f8a25feff7b5c4cd8d1c76b3a52834e53fbb
brianghub <87824467+brianghub@users.noreply.github.com>
2021-08-31 01:25:51 -0400

    query update

--- graphql/fetchBadgeOnUserID.ts
+++ graphql/fetchBadgeOnUserID.ts
@@ -1,10 +1,11 @@
 import { gql } from "@apollo/client";
 
 export const FETCH_BADGE_ON_USERID = gql`
-query FETCH_USER_BADGES ($userId: String="") {
-    user_badges(where: {userId: {_eq: "116309327098433793664"}}) {
+query FETCH_USER_BADGES($userId: String = "") {
+    user_badges(where: {userId: {_eq: $userId}, locked: {_eq: true}}) {
       userId
       badgeId
+      locked
     }
   }
 `;
--- pages/user-badge-check.tsx
+++ pages/user-badge-check.tsx
@@ -1,4 +1,5 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { data } from "browserslist";
 import React from "react";
 import { FETCH_BADGE_ON_USERID } from "../graphql/fetchBadgeOnUserID";
 import { userId } from "../graphql/utils/constants";
@@ -8,14 +9,13 @@ export default async function userbadge() {
     uri: "https://talented-duckling-40.hasura.app/v1/graphql",
     cache: new InMemoryCache(),
   });
-  const userbadges = [];
 
-  const { data: array } = await client.query({
+  const { data } = await client.query({
     query: FETCH_BADGE_ON_USERID,
     variables: {
-      userbadges: userId,
+      userId: "116309327098433793664",
     },
   });
 
-  return <div> hello world</div>;
+  console.log("data", data);
 }
