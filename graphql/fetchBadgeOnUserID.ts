507e6d28d59ba8b48627b26180401f10edc25ca2
brianghub <87824467+brianghub@users.noreply.github.com>
2021-08-31 15:50:42 -0400

    get the badges

--- graphql/fetchBadgeOnUserID.ts
+++ graphql/fetchBadgeOnUserID.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const FETCH_BADGE_ON_USERID = gql`
 query FETCH_USER_BADGES($userId: String = "") {
-    user_badges(where: {userId: {_eq: $userId}, locked: {_eq: true}}) {
+    user_badges(where: {userId: {_eq: $userId}, locked: {_eq: false}}) {
       userId
       badgeId
       locked
