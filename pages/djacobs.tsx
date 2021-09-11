ad56c9431e5a76418537f00e99cda02646b38bad
= <ajevan.mahadaya@uoit.net>
2021-09-10 23:42:04 -0400

    Add more questions to Quiz
    More questions added to Quiz

--- pages/djacobs.tsx
+++ pages/djacobs.tsx
@@ -7,6 +7,17 @@ import { MultipleChoice } from "../components/questionTypes/MultipleChoice";
 import { GuessData } from "./api/guessData";
 import { Button } from "../components/ui/Button";
 import { stages } from "konva/lib/Stage";
+import TeX from "@matejmazur/react-katex";
+import {
+  Question,
+  MCOption,
+  MCModel,
+  AnswerType,
+  FillOption,
+  fillBlankModel,
+} from "../pages/api/question";
+import { QuestionType } from "./api/questionTypes";
+import { react } from "@babel/types";
 
 enum Stage {
   START,
@@ -21,15 +32,45 @@ export default function djacobs(props) {
     "Which of the following letters has a right- angle in it?",
     "Use your protractor for this question! Which of the following are the interior angles of this pyramid?"
   */
-  const questions = [
+
+  const q = [
     "The sum of the interior (inside) angles of a triangle add up to what?",
+    "What is the measurement of <a? (Tip: Just write the number.) ",
     "Which 3 angles add up to 180 degrees?",
+    "A triangle has 2 angles that are 50 degrees. Is it possible for the last angle to be obtuse?",
+    "Which of the following letters has a right- angle in it?",
   ];
+
+  /*
 
   const guesses = [
     ["90", "180", "360", "None of the above"],
     ["<1 + <2 + <3", "<2 + <3 + <4", "<3 + <4 + <5", "<4 + <5 + <6"],
   ];
+
+  const optionsQ1 = [
+    { id: "option1", text: "90" },
+    { id: "option1", text: "180" },
+    { id: "option1", text: "360" },
+  ];
+
+  const Q1MC: MCModel = {
+    options: optionsQ1,
+    title: q[0],
+  };
+
+  const Q1: Question = {
+    text:
+      "The sum of the interior (inside) angles of a triangle add up to what?",
+    answer: "",
+    answerType: AnswerType.NUMBER,
+    questionType: QuestionType.MULTIPLE_CHOICE_WORD,
+    multipleChoice: Q1MC
+    skill:
+  };
+
+  const questionsWithOptions = [Q1];
+  */
 
   const onSubmit = (guess: GuessData) => {
     console.log(guess);
@@ -51,12 +92,120 @@ export default function djacobs(props) {
   };
 
   const nextQuestion = () => {
-    setCurrentQuestionIndex(
-      Math.min(questions.length - 1, currentQuestionIndex + 1)
-    );
+    setCurrentQuestionIndex(Math.min(q.length - 1, currentQuestionIndex + 1));
     console.log(currentQuestionIndex + 1);
   };
 
+  const Q1 = (
+    <React.Fragment>
+      <MultipleChoice
+        displayQuestion={q[0]}
+        option1={{
+          id: "option1",
+          text: "90",
+        }}
+        option2={{
+          id: "option2",
+          text: "180",
+        }}
+        option3={{
+          id: "option3",
+          text: "360",
+        }}
+        answer="120"
+        submitGuess={nextQuestion}
+      />
+    </React.Fragment>
+  );
+
+  const Q2 = (
+    <React.Fragment>
+      <p className="text-2xl text-center">{q[1]}</p>
+      <div className="text-center">
+        <label>Final Answer</label>
+        <input className="p-4 text-lg" placeholder="Any degree" />
+      </div>
+      <div className="flex flex-col items-center">
+        <Button
+          label="Submit"
+          backgroundColor="blue"
+          textColor="white"
+          onClick={nextQuestion}
+        />
+      </div>
+    </React.Fragment>
+  );
+
+  const Q3 = (
+    <React.Fragment>
+      <MultipleChoice
+        displayQuestion={q[2]}
+        option1={{
+          id: "option1",
+          text: "<1 + <2 + <3",
+        }}
+        option2={{
+          id: "option2",
+          text: "<2 + <3 + <4",
+        }}
+        option3={{
+          id: "option3",
+          text: "<3 + <4 + <5",
+        }}
+        answer="120"
+        submitGuess={nextQuestion}
+      />
+    </React.Fragment>
+  );
+
+  const Q4 = (
+    <React.Fragment>
+      <MultipleChoice
+        displayQuestion={q[3]}
+        option1={{
+          id: "option1",
+          text: "Yes",
+        }}
+        option2={{
+          id: "option2",
+          text: "No",
+        }}
+        option3={{
+          id: "option3",
+          text: "N/A",
+        }}
+        answer="Yes"
+        submitGuess={nextQuestion}
+      />
+    </React.Fragment>
+  );
+
+  const Q5 = (
+    <React.Fragment>
+      <MultipleChoice
+        displayQuestion={q[4]}
+        option1={{
+          id: "option1",
+          text: "J",
+        }}
+        option2={{
+          id: "option2",
+          text: "K",
+        }}
+        option3={{
+          id: "option3",
+          text: "L",
+        }}
+        answer="L"
+        submitGuess={nextQuestion}
+      />
+    </React.Fragment>
+  );
+
+  const questions = [Q1, Q2, Q3, Q4, Q5];
+
+  /*
+   */
   return (
     <div className="flex flex-col overflow-auto bg-scroll bg-blue-50">
       <Navbar />
@@ -64,7 +213,11 @@ export default function djacobs(props) {
         {stage == Stage.START && (
           <div className="flex flex-col gap-8">
             <p className="text-2xl text-center bg-blue-400">Giza Form</p>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center col gap-8">
+              <div className="text-center">
+                <label>Group Name</label>
+                <input className="p-4 text-lg" />
+              </div>
               <Button
                 label="Start"
                 backgroundColor="blue"
@@ -79,24 +232,8 @@ export default function djacobs(props) {
             <div id="FormHeader">
               <p className="text-2xl text-center bg-blue-400">Giza Form</p>
             </div>
-            <div id="FormBody">
-              <MultipleChoice
-                displayQuestion={questions[currentQuestionIndex]}
-                option1={{
-                  id: "option1",
-                  text: guesses[currentQuestionIndex][0],
-                }}
-                option2={{
-                  id: "option2",
-                  text: guesses[currentQuestionIndex][1],
-                }}
-                option3={{
-                  id: "option3",
-                  text: guesses[currentQuestionIndex][2],
-                }}
-                answer="120"
-                submitGuess={nextQuestion}
-              />
+            <div id="FormBody" className="flex flex-col gap-8">
+              {questions[currentQuestionIndex]}
             </div>
             <div id="FormEnd" className="flex flex-col items-center">
               <Button
