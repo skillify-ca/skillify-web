f051648aabc95d4fab8a1746787f6081afa6c6c3
ajevan_m <Ajevan@users.noreply.github.com>
2021-09-11 22:59:54 -0400

    Add more questions to Quiz and Main Quiz Page changes (#455)
    * Add more questions to Quiz
    
    More questions added to Quiz
    
    * Update File as per comments in PR
    
    Made as much updates as I could based on comments in PR
    
    * Change Q4 of Quiz and Skill not mandatory
    
    Changed the Q4 of the Quiz to True or False and Skill is now not mandatory for new questions that do not require a skill for now

--- pages/djacobs.tsx
+++ pages/djacobs.tsx
@@ -7,6 +7,19 @@ import { MultipleChoice } from "../components/questionTypes/MultipleChoice";
 import { GuessData } from "./api/guessData";
 import { Button } from "../components/ui/Button";
 import { stages } from "konva/lib/Stage";
+import TeX from "@matejmazur/react-katex";
+import {
+  Question,
+  MCOption,
+  MCModel,
+  AnswerType,
+  FillOption,
+  fillBlankModel,
+} from "../pages/api/question";
+import { QuestionType } from "./api/questionTypes";
+import { react } from "@babel/types";
+import { TrueorFalse } from "../components/questionTypes/TrueorFalse";
+import { Skill } from "./api/skill";
 
 enum Stage {
   START,
@@ -21,14 +34,13 @@ export default function djacobs(props) {
     "Which of the following letters has a right- angle in it?",
     "Use your protractor for this question! Which of the following are the interior angles of this pyramid?"
   */
-  const questions = [
-    "The sum of the interior (inside) angles of a triangle add up to what?",
-    "Which 3 angles add up to 180 degrees?",
-  ];
 
-  const guesses = [
-    ["90", "180", "360", "None of the above"],
-    ["<1 + <2 + <3", "<2 + <3 + <4", "<3 + <4 + <5", "<4 + <5 + <6"],
+  const questionData = [
+    "The sum of the interior (inside) angles of a triangle add up to what?",
+    "What is the measurement of <a? (Tip: Just write the number.) ",
+    "Which 3 angles add up to 180 degrees?",
+    "A triangle has 2 angles that are 50 degrees. Is it possible for the last angle to be obtuse?",
+    "Which of the following letters has a right- angle in it?",
   ];
 
   const onSubmit = (guess: GuessData) => {
@@ -52,10 +64,102 @@ export default function djacobs(props) {
 
   const nextQuestion = () => {
     setCurrentQuestionIndex(
-      Math.min(questions.length - 1, currentQuestionIndex + 1)
+      Math.min(questionData.length - 1, currentQuestionIndex + 1)
     );
     console.log(currentQuestionIndex + 1);
   };
+
+  const Q1 = (
+    <React.Fragment>
+      <MultipleChoice
+        displayQuestion={questionData[0]}
+        option1={{
+          id: "option1",
+          text: "90",
+        }}
+        option2={{
+          id: "option2",
+          text: "180",
+        }}
+        option3={{
+          id: "option3",
+          text: "360",
+        }}
+        answer="120"
+        submitGuess={nextQuestion}
+      />
+    </React.Fragment>
+  );
+
+  const Q2 = (
+    <React.Fragment>
+      <p className="text-2xl text-center">{questionData[1]}</p>
+      <div className="text-center">
+        <label>Final Answer</label>
+        <input className="p-4 text-lg" placeholder="Any degree" />
+      </div>
+      <div className="flex flex-col items-center">
+        <Button
+          label="Submit"
+          backgroundColor="blue"
+          textColor="white"
+          onClick={nextQuestion}
+        />
+      </div>
+    </React.Fragment>
+  );
+
+  const Q3Data: Question = {
+    text: questionData[2],
+    answer: "True",
+    answerType: AnswerType.BOOLEAN,
+    questionType: QuestionType.TRUE_OR_FALSE_PROBLEM,
+    skill: Skill.NUMBERS_50,
+  };
+
+  const Q3 = (
+    <React.Fragment>
+      <TrueorFalse question={Q3Data} answer="Yes" submitGuess={nextQuestion} />
+    </React.Fragment>
+  );
+
+  const Q4Data: Question = {
+    text: questionData[3],
+    answer: "True",
+    answerType: AnswerType.BOOLEAN,
+    questionType: QuestionType.TRUE_OR_FALSE_PROBLEM,
+    skill: Skill.NUMBERS_50,
+  };
+
+  const Q4 = (
+    <React.Fragment>
+      <TrueorFalse question={Q4Data} answer="Yes" submitGuess={nextQuestion} />
+    </React.Fragment>
+  );
+
+  const Q5 = (
+    <React.Fragment>
+      <MultipleChoice
+        displayQuestion={questionData[4]}
+        option1={{
+          id: "option1",
+          text: "J",
+        }}
+        option2={{
+          id: "option2",
+          text: "K",
+        }}
+        option3={{
+          id: "option3",
+          text: "L",
+        }}
+        answer="L"
+        submitGuess={nextQuestion}
+      />
+    </React.Fragment>
+  );
+
+  const questionComponent = [Q1, Q2, Q3, Q4, Q5];
 
   return (
     <div className="flex flex-col overflow-auto bg-scroll bg-blue-50">
@@ -64,7 +168,11 @@ export default function djacobs(props) {
         {stage == Stage.START && (
           <div className="flex flex-col gap-8">
             <p className="text-2xl text-center bg-blue-400">Giza Form</p>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center col gap-8">
+              <div className="text-center">
+                <label>Group Name</label>
+                <input className="p-4 text-lg" />
+              </div>
               <Button
                 label="Start"
                 backgroundColor="blue"
@@ -79,24 +187,8 @@ export default function djacobs(props) {
             <div id="FormHeader">
               <p className="text-2xl text-center bg-blue-400">Giza Form</p>
             </div>
-            <div id="FormBody">
-              <MultipleChoice
-                displayQuestion={questions[currentQuestionIndex]}
-                option1={{
-                  id: "option1",
-                  text: guesses[currentQuestionIndex][0],
-                }}
-                option2={{
-                  id: "option2",
-                  text: guesses[currentQuestionIndex][1],
-                }}
-                option3={{
-                  id: "option3",
-                  text: guesses[currentQuestionIndex][2],
-                }}
-                answer="120"
-                submitGuess={nextQuestion}
-              />
+            <div id="FormBody" className="flex flex-col gap-8">
+              {questionComponent[currentQuestionIndex]}
             </div>
             <div id="FormEnd" className="flex flex-col items-center">
               <Button
