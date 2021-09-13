d5fc14e51982f8229e66abc4eed5f2e0c41fe280
Vithushan <vithushan19@users.noreply.github.com>
2021-09-13 14:20:55 -0400

    Save student images to db (#460)

--- graphql/userAssignments/createUserAssignment.ts
+++ graphql/userAssignments/createUserAssignment.ts
@@ -1,17 +1,19 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_USER_ASSIGNMENT = gql`
-  mutation insertUserAssignment($user_id: String, $assignment_id: String, $user_solution: jsonb) {
+  mutation insertUserAssignment($user_id: String, $assignment_id: String, $user_solution: jsonb, $user_images: jsonb) {
     insert_user_assignments(
       objects: {
         assignment_id: $assignment_id
         user_id: $user_id
         user_solution: $user_solution
+        user_images: $user_images
       }
     ) {
       returning {
         id
         user_solution
+        user_images
       }
     }
   }
