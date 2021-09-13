f8db9bb8c5484679ded299e3029d2a7c87ebd389
vithushan19 <vithushan19@gmail.com>
2021-09-13 09:31:25 -0400

    Show all student submissions on teacher dashboard

--- graphql/userAssignments/createUserAssignment.ts
+++ graphql/userAssignments/createUserAssignment.ts
@@ -1,12 +1,12 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_USER_ASSIGNMENT = gql`
-  mutation insertUserAssignment($user_id: String, $assignment_id: String) {
+  mutation insertUserAssignment($user_id: String, $assignment_id: String, $user_solution: jsonb) {
     insert_user_assignments(
       objects: {
         assignment_id: $assignment_id
         user_id: $user_id
-        user_solution: {}
+        user_solution: $user_solution
       }
     ) {
       returning {
