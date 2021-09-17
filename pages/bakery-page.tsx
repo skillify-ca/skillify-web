595cd69c00a2aa953a2180a41bac5615ba1438eb
brianghub <87824467+brianghub@users.noreply.github.com>
2021-09-16 23:25:20 -0400

    Bakery Intro Component

---
+++ pages/bakery-page.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import BakeryInstructions from "../components/bakery/BakeryInstructions";
+
+const BakeryPage = () => {
+  return <BakeryInstructions />;
+};
+
+export default BakeryPage;
