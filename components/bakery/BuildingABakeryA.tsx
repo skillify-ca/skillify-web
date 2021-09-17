975085806b8f21388b71138223ea1930f0db9fd2
brianghub <87824467+brianghub@users.noreply.github.com>
2021-09-17 12:18:19 -0400

    Bakery

---
+++ components/bakery/BuildingABakeryA.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+const BuildingABakeryA = () => {
+  return (
+    <div>
+      <div className={"flex justify-center font-bold text-red-200 text-6xl"}>
+        Building A Bakery
+      </div>
+    </div>
+  );
+};
+
+export default BuildingABakeryA;
--- pages/bakery-page.tsx
+++ pages/bakery-page.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import BakeryInstructions from "../components/bakery/BakeryInstructions";
+import BuildingABakeryA from "../components/bakery/BuildingABakeryA";
 
 const BakeryPage = () => {
-  return <BakeryInstructions />;
+  return (
+    <div>
+      <BakeryInstructions />
+      <BuildingABakeryA />;
+    </div>
+  );
 };
 
 export default BakeryPage;
