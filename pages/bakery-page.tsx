017efc26a94ecf6aae2387b624a88228d3e19b5f
brianghub <87824467+brianghub@users.noreply.github.com>
2021-09-18 12:59:48 -0400

    Bakers rack (#473)
    * First Row table complete
    
    * Ready for PR Review

--- pages/bakery-page.tsx
+++ pages/bakery-page.tsx
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from "react";
 import BakeryInstructions from "../components/bakery/BakeryInstructions";
 import BuildingABakeryA from "../components/bakery/BuildingABakeryA";
 import BakersRack from "../components/bakery/BakersRack";
+
 import BakingGma from "../components/bakery/BakingGma";
+
 
 const BakeryPage = () => {
   return (
     <div className={"bg-white"}>
       <BakeryInstructions />
       <BuildingABakeryA />
+
+
       <BakersRack />
       <BakingGma />;
+
     </div>
   );
 };
