90d5258403f0e50eb5026536e51f11b88baf2c4e
= <ajevan.mahadaya@uoit.net>
2021-09-22 18:14:02 -0400

    Update Question 14 file to include guess object
    Update Question 14 file to include guess object

--- components/giza/Q14.tsx
+++ components/giza/Q14.tsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { GuessData } from "../../pages/api/guessData";
 import { Button } from "../ui/Button";
 
 //76
 const Q14 = (displayQuestion, nextQuestion) => {
+  const [guessString, setGuessString] = useState<string>("");
+
+  const answer = "60";
+
+  const onGuessChanged = (currentGuess: string) => {
+    const newGuess = currentGuess;
+    setGuessString(newGuess);
+  };
+
+  const onSubmit = () => {
+    const guess: GuessData = {
+      guess: guessString,
+      isCorrect: guessString == answer,
+    };
+    console.log(guess);
+    //Pass this guessData object into nextQuestion
+    nextQuestion();
+  };
+
   return (
     <React.Fragment>
       <p className="text-2xl text-center">{displayQuestion}</p>
       <div className="text-center">
         <label>Final Answer</label>
-        <input className="p-4 text-lg" placeholder="Any degree" />
+        <input
+          className="p-4 text-lg"
+          placeholder="Any degree"
+          value={guessString}
+          onChange={(e) => onGuessChanged(e.target.value)}
+        />
       </div>
       <div className="flex flex-col items-center">
         <Button
           label="Submit"
           backgroundColor="blue"
           textColor="white"
-          onClick={nextQuestion}
+          onClick={onSubmit}
         />
       </div>
     </React.Fragment>
