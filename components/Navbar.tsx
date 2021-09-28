bf5b78b0d94049e2add6bd9344fda032377ad210
Daniel Thevarasalingam <65379304+Danielthev@users.noreply.github.com>
2021-09-28 00:48:26 -0400

    Make junior diagnostic question set more dynamic (#478)
    * Make junior diagnostic question set more dynamic
    
    * add helper functions + build conditions for junior diagnostic questions
    
    * smol change
    
    * another small change
    
    * minor discrepancies
    
    * small change
    
    * small change
    
    * fix condition when student guesses incorrectly or moves to next topic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -120,12 +120,12 @@ export default function Navbar() {
                   {status === "loading"
                     ? ""
                     : !session && (
-                        <>
-                          <Link href="/welcome">
-                            <p className="text-white cursor-pointer">Sign in</p>
-                          </Link>
-                        </>
-                      )}
+                      <>
+                        <Link href="/welcome">
+                          <p className="text-white cursor-pointer">Sign in</p>
+                        </Link>
+                      </>
+                    )}
                   {session && (
                     <button
                       className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
