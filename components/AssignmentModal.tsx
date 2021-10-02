1cf04d97e4160af45dd9d288d19a659ce6068523
Mayuran Ganesathas <mayu@Mayurans-MacBook-Pro.local>
2021-10-02 14:52:14 -0400

    mg created modal

--- components/AssignmentModal.tsx
+++ components/AssignmentModal.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Button } from "./ui/Button";
 
-export interface AssignmentModalProps {}
+export interface AssignmentModalProps {
+  errorMessage: string;
+}
 
-export const AssignmentModal = ({}: AssignmentModalProps) => {
+export const AssignmentModal = ({ errorMessage }: AssignmentModalProps) => {
   return (
     <div className="flex-nowrap  h-1/3 w-1/3 shadow-lg rounded-xl bg-gray-50">
       <div className="flex justify-center">
         <img src="./images/uhoh.png" className=" w-1/3 h-1/3 " />
       </div>
-      <div className="text-center bg-gray-50 py-4">
-        It looks like you forgot to assign a question!
-      </div>
+      <div className="text-center bg-gray-50 py-4">{errorMessage}</div>
       <div className="text-center py-2">
         <Button
           label="Go Back"
