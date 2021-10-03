5a0cdd7e0c0efedbc1074a061472f096f2be50b8
Mayu <37229329+mayuranganesathas@users.noreply.github.com>
2021-10-03 12:11:27 -0400

    mg - bug fix: error modal when 0 questions enter (#546)
    * mg created modal
    
    * mg created modal
    
    * renamed components
    
    * mg-completed bug
    
    * mg-fixed minor back bug
    
    * removed unnecessary if logic
    
    * mg-fixed minor details
    
    * mg-fixed minor details
    
    * mg-better i guess?
    
    Co-authored-by: Mayuran Ganesathas <mayu@Mayurans-MacBook-Pro.local>

--- components/ErrorModal.tsx
+++ components/ErrorModal.tsx
@@ -1,25 +1,39 @@
 import React from "react";
+import { Modal, ModalTransition } from "react-simple-hook-modal";
 import { Button } from "./ui/Button";
 
 export interface ErrorModalProps {
   errorMessage: string;
+  close: () => void;
+  isErrorModalShowing: boolean;
 }
 
-export const ErrorModal = ({ errorMessage }: ErrorModalProps) => {
+export const ErrorModal = ({
+  errorMessage,
+  close,
+  isErrorModalShowing,
+}: ErrorModalProps) => {
   return (
-    <div className="flex-nowrap  h-1/3 w-1/3 shadow-lg rounded-xl bg-gray-50">
-      <div className="flex justify-center">
-        <img src="./images/uhoh.png" className=" w-1/3 h-1/3 " />
+    <Modal
+      id="error-modal"
+      isOpen={isErrorModalShowing}
+      transition={ModalTransition.SCALE}
+      modalClassName="h-1/2"
+    >
+      <div className="flex-nowrap shadow-xl rounded-xl  bg-gray-50 h-full w-full">
+        <div className="flex justify-center">
+          <img src="./images/uhoh.png" className=" w-1/3 h-1/3 " />
+        </div>
+        <div className="text-center py-4">{errorMessage}</div>
+        <div className="text-center py-2">
+          <Button
+            label="Go Back"
+            onClick={close}
+            backgroundColor="blue"
+            textColor="white"
+          ></Button>
+        </div>
       </div>
-      <div className="text-center bg-gray-50 py-4">{errorMessage}</div>
-      <div className="text-center py-2">
-        <Button
-          label="Go Back"
-          onClick={close}
-          backgroundColor="blue"
-          textColor="white"
-        ></Button>
-      </div>
-    </div>
+    </Modal>
   );
 };
