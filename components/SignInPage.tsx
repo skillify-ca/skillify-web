7c4341a1ce3edfa9a9a11dbcfb99a5510ff6879b
Vithushan <vithushan19@users.noreply.github.com>
2021-10-12 15:02:14 -0400

    Fix auth (#555)

--- components/SignInPage.tsx
+++ components/SignInPage.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import { getRedirectResult } from "@firebase/auth";
+import React, { useEffect } from "react";
 import { useAuth } from "../lib/authContext";
+import { auth } from "../lib/firebase";
+import { useRouter } from "next/router";
 
 export default function SignInPage() {
-  const { signIn, user } = useAuth();  
-  
+  const { signIn, user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    async function checkAuth() {
+      const result = await getRedirectResult(auth);
+      if (result) {
+        router.push("/practice");
+      }
+    }
+    checkAuth();
+  }, []);
+
   return (
     <div>
       <div className="flex flex-col justify-center items-center">
