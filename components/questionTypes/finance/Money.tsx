2843c442d24cfd1d0f93199527b940e3ed4bbc60
NishanthS05 <sadachcharamn@gmail.com>
2021-10-11 21:28:56 -0400

    Create money component

---
+++ components/questionTypes/finance/Money.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import { GuessData } from "../../../pages/api/guessData";
+import { Button } from "../../ui/Button";
+
+export interface MoneyProp {}
+
+export const Money: React.FC<MoneyProp> = ({}) => {
+  return (
+    <div className="flex flex-col items-center space-y-8">
+      <h1>How much money is there?</h1>
+      <img src="/images/money/nickle.jpeg" className="w-16" />
+      <img src="/images/money/loonie.jpeg" className="w-32" />
+    </div>
+  );
+};
