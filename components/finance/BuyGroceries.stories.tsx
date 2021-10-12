ae8abc693bd7ae6def7eec3004be19cfed4da2aa
vithushan19 <vithushan19@gmail.com>
2021-10-12 16:08:21 -0400

    Update BuyGroceries.stories.tsx

--- components/finance/BuyGroceries.stories.tsx
+++ components/finance/BuyGroceries.stories.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
 
-import { BuyGroceries, BuyGroceriesProps } from "./BuyGroceries";
+import { BuyGroceries } from "./BuyGroceries";
 export default {
   title: "Finance/BuyGroceries",
   component: BuyGroceries,
 } as Meta;
 
-const Template: Story<BuyGroceriesProps> = (args) => <BuyGroceries {...args} />;
+const Template: Story = (args) => <BuyGroceries {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
