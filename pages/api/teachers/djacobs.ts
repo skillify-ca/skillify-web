0c6de6bc83e688cc4b103f1f35db8493aeba3e04
vithushan19 <vithushan19@gmail.com>
2021-10-14 01:28:51 -0400

    djacobs MVP

--- pages/api/teachers/djacobs.ts
+++ pages/api/teachers/djacobs.ts
@@ -5,9 +5,10 @@ export const questions = [
   "\\frac{M - 7}{2} = 8, M = ?",
 ];
 
+export const answers = [8, 12, 29, 23];
+
 export const solutions = [
   ["3k + 6 = 30", "2k = 24", "k = 12"],
-  ["5w - 10 = 50", "5w = 60", "w = 12"],
   ["5w - 10 = 50", "5w = 60", "w = 12"],
   ["\\frac{R + 15}{4} = 11", "R + 15 = 44", "R  = 29"],
   ["\\frac{M - 7}{2} = 8", "M - 7 = 16", "M = 23"],
