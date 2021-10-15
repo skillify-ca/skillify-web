061c1d34a0423dd39b744e2292a56c0ae5797d16
NishanthS05 <sadachcharamn@gmail.com>
2021-10-15 13:53:57 -0400

    Update Money.tsx

--- components/questionTypes/finance/Money.tsx
+++ components/questionTypes/finance/Money.tsx
@@ -6,15 +6,21 @@ export interface MoneyProp {}
 
 export const Money: React.FC<MoneyProp> = ({}) => {
   return (
-    <div className="flex flex-col items-center space-y-8">
+    <div className="flex flex-col space-y-8">
       <h1>How much money is there?</h1>
-      <img src="/images/money/nickle.jpeg" className="w-16" />
-      <img src="/images/money/nickle.jpeg" className="w-16" />
-      <img src="/images/money/loonie.jpeg" className="w-24" />
-      <img src="/images/money/loonie.jpeg" className="w-24" />
-      <img src="/images/money/toonie.jpeg" className="w-28" />
-      <img src="/images/money/dime.jpeg" className="w-16" />
-      <img src="/images/money/dime.jpeg" className="w-16" />
+      <div className="flex flex-wrap w-108">
+        <img src="/images/money/nickle.jpeg" className="w-16 h-16" />
+        <img src="/images/money/nickle.jpeg" className="w-16 h-16" />
+        <img src="/images/money/loonie.jpeg" className="w-24" />
+        <img src="/images/money/loonie.jpeg" className="w-24" />
+        <img src="/images/money/toonie.jpeg" className="w-28" />
+        <img src="/images/money/dime.jpeg" className="w-16 h-16" />
+        <img src="/images/money/dime.jpeg" className="w-16 h-16" />
+      </div>
+      <div className="flex flex-start">
+        <p>$</p>
+        <input type="text" className="border-2 border-red-600" />
+      </div>
     </div>
   );
 };
