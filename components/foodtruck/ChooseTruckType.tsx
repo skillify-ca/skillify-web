d8855985f28b21a7516515b17fd312b49f880938
vithushan19 <vithushan19@gmail.com>
2021-10-18 18:20:09 -0400

    style food truck components

--- components/foodtruck/ChooseTruckType.tsx
+++ components/foodtruck/ChooseTruckType.tsx
@@ -8,19 +8,19 @@ export interface ChooseTruckTypeProps {
 
 const ChooseTruckType = ({ truck, setTruck }: ChooseTruckTypeProps) => {
   return (
-    <div className="flex flex-col p-4">
-      <h1 className="text-6xl text-black text-center bold p-4 mb-16">
+    <div className="flex flex-col">
+      <h1 className="text-3xl text-black text-center bold p-4 mb-8">
         What kind of stand do you want to buy?
       </h1>
       <div className="grid grid-cols-5 border-b-2 border-black border-dashed pb-8">
-        <p className="col-start-2 text-4xl">Stand Type</p>
-        <p className="col-start-3 text-4xl">Purchase Price</p>
-        <p className="col-start-4 text-4xl">Daily Cost</p>
-        <p className="col-start-5 text-4xl">Allowed Food</p>
+        <p className="col-start-2 text-center text-xl">Stand Type</p>
+        <p className="col-start-3 text-center text-xl">Purchase Price</p>
+        <p className="col-start-4 text-center text-xl">Daily Cost</p>
+        <p className="col-start-5 text-center text-xl">Allowed Food</p>
       </div>
       {allTrucks.map((t) => {
         return (
-          <label className="grid grid-cols-5 items-center py-8 border-b-2 border-black border-dashed p-8">
+          <label className="grid grid-cols-5 text-center items-center border-b-2 border-black border-dashed p-4">
             <input
               className="form-radio h-6 w-6"
               type="radio"
@@ -29,14 +29,12 @@ const ChooseTruckType = ({ truck, setTruck }: ChooseTruckTypeProps) => {
               onChange={(e) => setTruck(getTruck(e.target.value) as Truck)}
             />
             <figure>
-              <img className="object-contain h-48 w-60" src={t.imageUrl} />
-              <figcaption className="text-3xl text-justify">
-                {t.model}
-              </figcaption>
+              <img className="object-contain w-60" src={t.imageUrl} />
+              <figcaption className="">{t.model}</figcaption>
             </figure>
-            <span className="px-4 text-3xl">${t.fixedCost}</span>
-            <span className="px-4 text-3xl">${t.variableCost}</span>
-            <ul className="px-4 text-3xl">
+            <span className="text-center ">${t.fixedCost}</span>
+            <span className="text-center">${t.variableCost}</span>
+            <ul className="text-center">
               {t.allowedItems.map((it) => (
                 <li>{it.name}</li>
               ))}
