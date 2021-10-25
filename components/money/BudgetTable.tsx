b71168b4653e6064f75bfd0d544b7b39a5902a0e
Jon-Michael Gelua <geluajm@gmail.com>
2021-10-25 13:44:55 -0400

    Create budget table component and story (#563)
    * Create budget table and story
    
    * Add Buttons and styling
    
    * Add flex box to the buttons
    
    * Create an array and imported to budget table
    
    * Replace the table with map object and made properties lowercase
    
    * Implemented the alternating row colors
    
    * Added props to the budgetTable
    
    * Added padding to the tables
    
    * Set budget variable
    
    * Add logic to check answer
    
    * PR feedback

---
+++ components/money/BudgetTable.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode, useState } from "react";
+import { Button } from "../ui/Button";
+import { ItemDataTable } from "./BudgetTableData";
+
+export interface ItemProps {
+  budget: number;
+}
+
+const BudgetTable = ({ budget }) => {
+
+  const [ isCorrect, setIsCorrect ] = useState(false);
+
+  const calculateBudget = () => {
+    if( budget >= 1.56 ) {
+      setIsCorrect(true)
+    } else {
+      setIsCorrect(false)
+    }
+  }
+
+  return (
+    <div>
+      <h3 className="font-bold">Question:</h3>
+      <p>Cassie has ${budget}. Does she have enough to buy a roll of electrical tape and a light bulb?</p>
+      <p className="italic mb-4">Do not round.</p>
+
+      <div className="bg-green-300 mb-8">
+        {ItemDataTable.map((itemData, index) => (
+          <div className="grid grid-cols-2 odd:bg-green-200">
+            <div key={index} className="pl-4">{itemData.item}</div>
+            <div className="text-right pr-4">${itemData.cost}</div>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex gap-8 flex-col">
+        <Button backgroundColor="blue" textColor="white" label="Yes" onClick={ calculateBudget }/>
+        <Button backgroundColor="blue" textColor="white" label="No" onClick={ calculateBudget }/>
+      </div>
+
+    </div>
+  );
+};
+export default BudgetTable;
