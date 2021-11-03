ea7e0ba0b1f32b377b560cdb188eab560f0d64b7
ajevan_m <Ajevan@users.noreply.github.com>
2021-11-03 12:58:08 -0400

    Add question 5 to SA assignment and UI changes (#588)
    Add question 5 to SA assignment and UI changes to both question 2 and 4 so that it is device friendly.

--- pages/teachers/djacobs/surfaceArea.tsx
+++ pages/teachers/djacobs/surfaceArea.tsx
@@ -5,6 +5,7 @@ import SA_Q1 from "../../../components/surfaceArea/SA_Q1";
 import SA_Q2 from "../../../components/surfaceArea/SA_Q2";
 import SA_Q3 from "../../../components/surfaceArea/SA_Q3";
 import SA_Q4 from "../../../components/surfaceArea/SA_Q4";
+import SA_Q5 from "../../../components/surfaceArea/SA_Q5";
 import { Button } from "../../../components/ui/Button";
 import { GuessData } from "../../api/guessData";
 import { measureTime } from "../../api/time";
@@ -21,6 +22,7 @@ export default function djacobs(props) {
     "Write a multiplication question for the area of each face for the rectangular prisms in question 1, image also provided from question 1. Then find the surface area of the prism",
     "A net for the triangular prism is shown on the grid. On the net, label each face of the prism. Each square on the grid represents 1cm^2.",
     "Write a multiplication question for the area of each face for the triangular prisms in question 3, image also provided from question 3. Then find the surface area of the prism",
+    "Explain how the net of a prism helps you find the surface area of a prism",
   ];
 
   const [stage, setStage] = useState(Stage.START);
@@ -106,6 +108,7 @@ export default function djacobs(props) {
     SA_Q2(questionData[1], nextQuestion, isWrong),
     SA_Q3(questionData[2], nextQuestion, isWrong),
     SA_Q4(questionData[3], nextQuestion, isWrong),
+    SA_Q5(questionData[4], nextQuestion, isWrong),
   ];
 
   return (
