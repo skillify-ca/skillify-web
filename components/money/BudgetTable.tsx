94f2e9f9863a833204c6022d42a2376c173c7975
vithushan19 <vithushan19@gmail.com>
2021-11-03 15:49:47 -0400

    Update BudgetTable.tsx

--- components/money/BudgetTable.tsx
+++ components/money/BudgetTable.tsx
@@ -19,12 +19,12 @@ const BudgetTable: React.FC<BudgetItemProps> = ({ question, submitGuess, answer 
   }
 
   return (
-    <div>
+    <div className="flex flex-col max-h-96 py-4 overflow-y-hidden">
       <h3 className="font-bold">Question:</h3>
       <p>Cassie has ${question.text}. Does she have enough to buy a roll of electrical tape and a light bulb?</p>
       <p className="italic mb-4">Do not round.</p>
 
-      <div className="bg-green-300 mb-8">
+      <div className="bg-green-300 mb-4">
         {question.budgetCostModel.map((item, index) => (
           <div key={index} className="grid grid-cols-2 odd:bg-green-200">
             <div className="pl-4">{item.title}</div>
@@ -33,7 +33,7 @@ const BudgetTable: React.FC<BudgetItemProps> = ({ question, submitGuess, answer 
         ))}
       </div>
 
-      <div className="flex gap-8 flex-col">
+      <div className="grid grid-cols-2 gap-4">
         <Button backgroundColor="blue" textColor="white" label="Yes" onClick={()=> onSubmit("Yes")}/>
         <Button backgroundColor="blue" textColor="white" label="No" onClick={()=> onSubmit("No")}/>
       </div>
