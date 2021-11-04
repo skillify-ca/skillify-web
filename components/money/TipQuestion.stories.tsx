7553ac263d272dab29c06e354515e3024aa572c9
jmski <geluajm@gmail.com>
2021-11-04 13:24:49 -0400

    Create the tip question storybook component

---
+++ components/money/TipQuestion.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import TipQuestion, { TipQuestionProps } from "./TipQuestion";
+
+export default {
+  title: "Tip Question",
+  component: TipQuestion,
+  argTypes: {},
+} as Meta;
+
+const Template: Story = (args) => <TipQuestion {...args} />;
+
+
+export const Primary = Template.bind({});
+Primary.args = {
+  tip: 20,
+};
+
---
+++ components/money/TipQuestion.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { GuessData } from "../../pages/api/guessData";
+import { Button } from "../ui/Button";
+import { Question } from "../../pages/api/question";
+
+export interface TipQuestionProps {
+  answer: string;
+  question: Question;
+  submitGuess: (guess:GuessData) => void;
+}
+
+const TipQuestion: React.FC<TipQuestionProps> = ({ question, submitGuess, answer }) => {
+  const onSubmit = (guess: string) => {
+    submitGuess({
+      guess: guess,
+      isCorrect: guess.toString() == answer.toString()
+    });
+  }
+  return(
+    <div className="flex flex-col max-h-96 py-4 overflow-y-hidden gap-4">
+      <p>Estimate the amount of tip by rounding the bill to the nearest dollar before calculating.</p>
+      {""}
+      <p className="pl-10">{question.text}% tip on a bill of $99.23</p>
+      {""}
+      <div className="max-w">
+        <p className="py-4">The amount of the tip is approximately
+          $<input className="border-2 border-gray-300 w-20"></input>.
+        </p>
+      </div>
+      <Button backgroundColor="blue" textColor="white" label="Submit" onClick={()=> onSubmit(answer)}/>
+    </div>
+  )
+}
+
+export default TipQuestion;
