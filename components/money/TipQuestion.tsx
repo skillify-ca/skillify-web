08148fc93d16c03f892473d2428e34015e0c193a
jmski <geluajm@gmail.com>
2021-11-04 22:26:42 -0400

    Add guess validation

--- components/money/TipQuestion.tsx
+++ components/money/TipQuestion.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GuessData } from "../../pages/api/guessData";
 import { Button } from "../ui/Button";
 import { Question } from "../../pages/api/question";
@@ -9,13 +9,23 @@ export interface TipQuestionProps {
   submitGuess: (guess:GuessData) => void;
 }
 
-const TipQuestion: React.FC<TipQuestionProps> = ({ question, submitGuess, answer }) => {
+const TipQuestion: React.FC<TipQuestionProps> = ({ 
+  question, 
+  submitGuess, 
+  answer,
+}) => {
   const onSubmit = (guess: string) => {
     submitGuess({
       guess: guess,
       isCorrect: guess.toString() == answer.toString()
     });
   }
+  /** Reference user input and store it in a state, then clear input values by next question */
+  const [ guess, setGuess ] = useState("");
+  useEffect(() => {
+    (document.getElementById("input") as HTMLInputElement).value = "";
+  }, []);
+
   return(
     <div className="flex flex-col max-h-96 py-4 overflow-y-hidden gap-4">
       <p>Estimate the amount of tip by rounding the bill to the nearest dollar before calculating.</p>
@@ -24,10 +34,16 @@ const TipQuestion: React.FC<TipQuestionProps> = ({ question, submitGuess, answer
       {""}
       <div className="max-w">
         <p className="py-4">The amount of the tip is approximately
-          $<input className="border-2 border-gray-300 w-20"></input>.
+          $<input 
+              id="input"
+              type="string"
+              value={guess}
+              className="border-2 border-gray-300 w-20"
+              onChange={(e) => setGuess(e.target.value)}
+            ></input>.
         </p>
       </div>
-      <Button backgroundColor="blue" textColor="white" label="Submit" onClick={()=> onSubmit("")}/>
+      <Button backgroundColor="blue" textColor="white" label="Submit" onClick={()=> onSubmit(guess)}/>
     </div>
   )
 }
