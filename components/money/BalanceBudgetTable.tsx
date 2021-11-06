a2c1f1a23ac72ba27f860a3f418f53faf6f22c1a
Jon-Michael Gelua <geluajm@gmail.com>
2021-11-06 14:57:01 -0400

    Add more CSS styling to react component

--- components/money/BalanceBudgetTable.tsx
+++ components/money/BalanceBudgetTable.tsx
@@ -19,36 +19,38 @@ const BalanceBudget: React.FC<BalanceBudgetProps> = ({ question, submitGuess, an
   }
   return(
     <div className="flex flex-col max-h-96 py-4 overflow-y-hidden">
-      <p>
+      <p className="mb-4">
         This table shows Tiana's monthly budget.
         How much money does she need to earn tutoring to balance her budget?
         Complete the table.
       </p>
       <div className="grid bg-blue-600 text-white border-b border-grey-500">
         <div className="flex justify-self-center">
-          <span>Tiana's March Budget</span>
+          <span className="py-2">Tiana's March Budget</span>
         </div>
       </div>
-        <div className="grid grid-cols-2 bg-blue-300 text-white border-r border-grey-500">
-          <div className="flex justify-self-center">
-            <span>Income</span>
+        <div className="grid grid-cols-2 bg-blue-300 text-white">
+          <div className="border-r border-grey-500">
+              <div className="flex justify-center">
+                <span className="py-3">Income</span>
+              </div>
           </div>
-          <div className="flex justify-self-center">
-            <p>Expenses</p>
+          <div className="flex justify-center">
+            <span className="py-3">Expenses</span>
           </div>
         </div>
-        <div className="grid grid-cols-2">
-          <div className="flex flex-col border-r border-l border-grey-500">
-            <span className="border-b border-grey-500">Job at a flower shop: $140</span>
-            <span className="border-b border-grey-500">Tutoring: $<input></input></span>
-            <span className="border-b border-grey-500">Gift from brother: $15</span>
-            <span className="border-b border-grey-500">Total:<input></input></span>
+        <div className="grid grid-cols-2 mb-4 border border-grey-500">
+          <div className="flex flex-col border-r border-grey-500">
+            <span className="p-3 border-b border-grey-500">Job at a flower shop: $140</span>
+            <span className="p-3 border-b border-grey-500">Tutoring: $<input className="h-min border border-grey-500 text-black font-bold w-12 text-right"></input></span>
+            <span className="p-3 border-b border-grey-500">Gift from brother: $15</span>
+            <span className="p-3">Total: <input className="border border-grey-500 text-black font-bold w-12 text-right"></input></span>
           </div>
-          <div className="flex flex-col border-r">
-            <span className="border-b border-grey-500">Calculator: $75</span>
-            <span className="border-b border-grey-500">Haircut: $55</span>
-            <span className="border-b border-grey-500">Books: $65</span>
-            <span className="border-b border-grey-500">Total:<input></input></span>
+          <div className="flex flex-col">
+            <span className="p-3 border-b border-grey-500">Calculator: $75</span>
+            <span className="p-3 border-b border-grey-500">Haircut: $55</span>
+            <span className="p-3 border-b border-grey-500">Books: $65</span>
+            <span className="p-3">Total: <input className="border border-grey-500 text-black font-bold w-12 text-right"></input></span>
           </div>
         </div>
         
