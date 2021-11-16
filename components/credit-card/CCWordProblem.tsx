52ccbded9db66c0de6384200d97dd6d6627e9339
Jon-Michael Gelua <geluajm@gmail.com>
2021-11-16 17:55:40 -0500

    Credit card page with intro (#611)
    * Create credit-card.tsx
    
    * Create stages and add first component
    
    * Add previous and next buttons
    
    * Add styling to credit info component
    
    without pictures
    
    * Add conditional rendering for buttons
    
    * Update credit-card.tsx

---
+++ components/credit-card/CCWordProblem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CCWordProblem = () => {
+    return (
+        <div>
+            <h1>CCWordProblem</h1>
+        </div>
+    )
+}
+
+export default CCWordProblem
