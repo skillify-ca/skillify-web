31a0df62abf9815d78c88e51abab5bdca911af79
Jon-Michael Gelua <geluajm@gmail.com>
2021-11-17 10:49:05 -0500

    Create boilerplate components and render to /credit-card page (#612)
    * Add all the necessary stage components using boilerplate code
    
    * Add base components to page
    
    * Bug fixes
    
    * removed arrow text
    
    * Resolve changes

---
+++ components/credit-card/CCDCMultiData.ts
@@ -0,0 +1,88 @@
+import { MultiQuestion } from "../../pages/api/credit-card/MultiQuestion";
+
+export const multiQuestionData: MultiQuestion[] = [
+  {
+    question: "Which of the following describes a credit card but not a debit card?",
+    options: [
+      {
+        id: "a",
+        answer: "a small plastic card with a magnetic strip on the back",
+      },
+      {
+        id: "b",
+        answer: "purchases with the card are withdrawn directly from a bank account"
+      },
+      {
+        id: "c",
+        answer: "has a PIN associated with the card"
+      },
+      {
+        id: "d",
+        answer: "purchases with the card can be paid over time"
+      },
+    ]
+  },
+  {
+    question: "What is an advantage of using a debit card instead of a credit card to pay for a purchase?",
+    options: [
+      {
+        id: "a",
+        answer: "using a debit card allows someone to pay for a purchase over time",
+      },
+      {
+        id: "b",
+        answer: "using a debit card builds a person's credit history if it is used responsibly"
+      },
+      {
+        id: "c",
+        answer: "using a debit card pulls money directly from a person's checking account so it's more difficult to overspend"
+      },
+      {
+        id: "d",
+        answer: "using a debit card is never an advantage over a credit card"
+      },
+    ]
+  },
+  {
+    question: "Ryan wants to make a tuition payment for school but does not have enough money. Which is NOT an option for Ryan?",
+    options: [
+      {
+        id: "a",
+        answer: "Use his credit card to pay for tuition now",
+      },
+      {
+        id: "b",
+        answer: "Use a debit card to pay for tuition now"
+      },
+      {
+        id: "c",
+        answer: "Save money and use his debit card to pay for tuition at a later date"
+      },
+      {
+        id: "d",
+        answer: "save money and write a check to pay for tuition at a later date"
+      },
+    ]
+  },
+  {
+    question: "Which of the following best describes purchases made with a credit card?",
+    options: [
+      {
+        id: "a",
+        answer: "a PIN is used at the time of transaction to verify a person's identity",
+      },
+      {
+        id: "b",
+        answer: "purchases made with the card are withdrawn from a person's checking account"
+      },
+      {
+        id: "c",
+        answer: "purchases made with the card can be paid over time"
+      },
+      {
+        id: "d",
+        answer: "if a person does not have enough money, an overdraft fee may be charged"
+      },
+    ]
+  },
+]
