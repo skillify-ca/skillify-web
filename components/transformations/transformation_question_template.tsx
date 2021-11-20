467ddcf80719a3723eded09edeb3dc640120ebdc
ajevan_m <Ajevan@users.noreply.github.com>
2021-11-20 18:22:40 -0500

    Add Free Drawing component to transformation assignment (#616)
    * Add Free Drawing component to transformation assignment
    
    Add Free Drawing component to transformation assignment (there is an issue with only one question and going back to that one question, hoping to discuss in a sync session)
    
    * Update transformation tsx file
    
    Update transformation tsx file

--- components/transformations/transformation_question_template.tsx
+++ components/transformations/transformation_question_template.tsx
@@ -1,7 +1,13 @@
 import { isBoolean } from "lodash";
+import dynamic from "next/dynamic";
 import React, { useState } from "react";
 import { GuessData } from "../../pages/api/guessData";
 import { Button } from "../ui/Button";
+import { LineData } from "../ui/FreeDrawing";
+
+const FreeDrawing = dynamic(() => import("../ui/FreeDrawing"), {
+  ssr: false,
+});
 
 type Q1Props = {
   displayQuestion: string;
@@ -9,6 +15,10 @@ type Q1Props = {
   nextQuestion: (guess: GuessData) => void;
   guessString: string;
   setGuessString: (guessString: string) => void;
+  linesForQuestions: LineData[][];
+  setLinesForQuestions: (linesForQuestions: LineData[][]) => void;
+  historyStepForQuestions: number[];
+  setHistoryStepForQuestions: (historyStepForQuestions: number[]) => void;
 };
 
 //Future component, name: TypeAnswerQuestion
@@ -18,10 +28,36 @@ const Q1 = ({
   nextQuestion,
   guessString,
   setGuessString,
+  linesForQuestions,
+  setLinesForQuestions,
+  historyStepForQuestions,
+  setHistoryStepForQuestions,
 }: Q1Props) => {
   const onGuessChangedA = (currentGuess: string) => {
     const newGuess = currentGuess;
     setGuessString(newGuess);
+  };
+
+  const setLinesForCurrentQuestion = (lines: LineData[]) => {
+    const newLines = linesForQuestions.map((l, index) => {
+      if (index === 0) {
+        return lines;
+      } else {
+        return l;
+      }
+    });
+    setLinesForQuestions(newLines);
+  };
+
+  const setHistoryForCurrentQuestion = (historyStep: number) => {
+    const newHistory = historyStepForQuestions.map((h, index) => {
+      if (index === 0) {
+        return historyStep;
+      } else {
+        return h;
+      }
+    });
+    setHistoryStepForQuestions(newHistory);
   };
 
   const onSubmit = () => {
@@ -37,16 +73,26 @@ const Q1 = ({
   return (
     <React.Fragment>
       <div className="flex flex-col gap-8 items-center">
+        <style jsx>{`
+          p {
+            white-space: pre-line;
+          }
+        `}</style>
         <p className="text-2xl text-center">{displayQuestion}</p>
         <div id="quizImage">
           <img className="animate-fadeIn" src={imagePath}></img>
         </div>
-        <input
-          type="file"
-          accept="image/*"
-          value={guessString}
-          onChange={(e) => onGuessChangedA(e.target.value)}
-        />
+        {linesForQuestions[0] && (
+          <FreeDrawing
+            saveImage={() => {}}
+            lines={linesForQuestions[0]}
+            setLines={setLinesForCurrentQuestion}
+            historyStep={historyStepForQuestions[0]}
+            setHistoryStep={setHistoryForCurrentQuestion}
+            disabled={false}
+          />
+        )}
+        <input type="file" accept="image/*" />
         <div className="flex flex-col items-center">
           <Button
             label="Submit"
