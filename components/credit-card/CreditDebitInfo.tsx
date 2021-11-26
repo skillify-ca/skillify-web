a215a03a526372c251b8b2c2f92242fbe347a2b7
Jon-Michael Gelua <geluajm@gmail.com>
2021-11-26 01:28:11 -0500

    Update CreditDebitInfo.tsx

--- components/credit-card/CreditDebitInfo.tsx
+++ components/credit-card/CreditDebitInfo.tsx
@@ -1,12 +1,4 @@
 import React from 'react'
-<<<<<<< Updated upstream
-
-export interface CreditDebitInfoProps {
-
-}
-
-const CreditDebitInfo = () => {
-=======
 import { 
     borrowArray,
     payArray,
@@ -25,7 +17,6 @@ import { DropDownMenu } from './DropDownMenu';
 const CreditDebitInfo = () => {
 
 
->>>>>>> Stashed changes
     return (
         <div className="flex flex-col p-12">
             <div className="mb-12 flex justify-center">
@@ -44,13 +35,6 @@ const CreditDebitInfo = () => {
                 <div className="grid grid-cols-2 gap-11">
                     <div className="w-full bg-purple-200 p-4">
                         <h2 className="text-xl mb-1">Credit Cards</h2>
-<<<<<<< Updated upstream
-                        <p>Allow consumers to borrow money from a financial institution and pay it back later</p>
-                    </div>
-                    <div className="w-full bg-green-200 p-4">
-                        <h2 className="text-xl mb-1">Debit Cards</h2>
-                        <p>Allow consumers to spend money they have deposited in their bank account</p>
-=======
                         <div>
                             Allow consumers to <DropDownMenu value="borrow" words={borrowArray}/> money from a financial institution and <DropDownMenu value="pay" words={payArray}/> it back later
                         </div>
@@ -60,7 +44,6 @@ const CreditDebitInfo = () => {
                         <div>
                             Allow consumers to <DropDownMenu value="spend" words={spendArray}/> money they have <DropDownMenu value="deposit" words={depositArray}/> in their bank account
                         </div>
->>>>>>> Stashed changes
                     </div>
                 </div>
             </div>
@@ -69,17 +52,10 @@ const CreditDebitInfo = () => {
                 <h2 className="text-2xl">How are they the same?</h2>
                 <div className=" bg-gray-200 p-4">
                     <ul className="flex flex-col gap-4">
-<<<<<<< Updated upstream
-                        <li>Allow a person to make purchases</li>
-                        <li>If card is lost or stolen, report import it immediately</li>
-                        <li>A hard plastic card with information stored on a magnetic strip.</li>
-                        <li>Owner's name and an expiration date on the card</li>
-=======
                         <li>Allow a person to make <DropDownMenu value="purchase" words={purchaseArray}/></li>
                         <li>If card is <DropDownMenu value="lost" words={lostArray}/> or <DropDownMenu value="stolen" words={stolenArray}/>, report import it immediately</li>
                         <li>Hard, <DropDownMenu value="plastic" words={plasticArray}/> card with information stored on a <DropDownMenu value="magnetic" words={magneticArray}/> strip.</li>
                         <li>Owner's <DropDownMenu value="name" words={nameArray}/> and an <DropDownMenu value="expiration" words={expirationArray}/> date on the card</li>
->>>>>>> Stashed changes
                     </ul>
                 </div>
             </div>
