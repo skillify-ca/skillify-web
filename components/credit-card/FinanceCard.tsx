294f1966d3ffef926869c52d2b98fe462098d1e9
Jon-Michael Gelua <geluajm@gmail.com>
2021-11-26 02:00:03 -0500

    Create FinanceCard.tsx

---
+++ components/credit-card/FinanceCard.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+export interface FinanceCardProps {
+    question: string,
+    answer: string,
+}
+
+const FinanceCard = ({question, answer}: FinanceCardProps) => {
+    const [color, setColor ] = useState("bg-gray-300")
+    const [borderColor, setBorderColor ] = useState("border-gray-500")
+    const [cardTitle, setCardTitle ] = useState("Click to Change")
+    
+    const onColorChangeRequested = () => {
+        if (color === "bg-gray-300" || color === "bg-yellow-300") {
+            setColor("bg-green-300")
+            setBorderColor("border-green-500")
+            setCardTitle("debit")
+
+        } else if (color === "bg-green-300") {
+            setColor("bg-purple-300");
+            setBorderColor("border-purple-500")
+            setCardTitle("credit")
+
+        } else if (color === "bg-purple-300") {
+            setColor("bg-yellow-300")
+            setBorderColor("border-yellow-500")
+            setCardTitle("both")
+
+        }
+    }
+
+    return (
+        <div 
+            onClick={onColorChangeRequested}
+            className={`max-w-96 h-48 bg-grey-200 border-4 rounded-2xl ${borderColor}`}>
+          <p className="px-4">{cardTitle.toUpperCase()}</p>
+          <div className={`max-full ${color} h-8 mb-4`}></div>
+          <p className="px-4">{question}</p>
+          <span className="flex justify-center text-red-500">{answer}</span>
+        </div>
+    )
+}
+
+export default FinanceCard
