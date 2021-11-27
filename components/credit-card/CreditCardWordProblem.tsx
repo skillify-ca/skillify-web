58beb8d871d40f147a53a92e95c63a77f5bd7d49
Jon-Michael Gelua <geluajm@gmail.com>
2021-11-26 23:57:01 -0500

    Add arrays

--- components/credit-card/CreditCardWordProblem.tsx
+++ components/credit-card/CreditCardWordProblem.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import { DropDownMenu } from './DropDownMenu'
 import { 
+    applyArray,
+    approvedArray,
+    billArray,
+    buildArray,
     chargeArray,
     creditCardArray,
+    creditLimitArray,
+    doesntArray,
+    interestArray,
+    lateFeeArray,
+    minimumArray,
+    payArray,
+    responsibleArray,
+    unpaidArray,
 
  } from './CreditCardWordProblemData'
 
@@ -26,8 +38,7 @@ const CreditCardWordProblem = () => {
                         </div>
                         <div className="flex justify-center p-8 bg-purple-300 rounded-2xl mx-12 my-4">
                             <span>
-                                Credit card company sends you a <DropDownMenu value="" words={chargeArray}/>
-                                and you <DropDownMenu value="" words={chargeArray}/> the credit
+                                Credit card company sends you a <DropDownMenu value="" words={billArray}/> and you <DropDownMenu value="" words={payArray}/> the credit
                                 card company
                             </span>
                         </div>
@@ -38,19 +49,19 @@ const CreditCardWordProblem = () => {
                 <div className="flex flex-col-reverse md:flex-row-reverse">
                     <img className="max-w-xs" src="../../images/credit-card/credit-receipt.svg"></img>
                     <ul className="p-12 flex flex-col text-2xl gap-4 rounded-2xl md:mx-12 m-12 bg-gradient-to-r from-gray-200 to-gray-0">
-                        <li>You receive a <DropDownMenu value="" words={chargeArray}/> each month</li>
-                        <li>Full bill <DropDownMenu value="" words={chargeArray}/> need to be paid</li>
-                        <li><DropDownMenu value="" words={chargeArray}/> is charged on <DropDownMenu value="" words={chargeArray}/> amount</li>
-                        <li>a <DropDownMenu value="" words={chargeArray}/> payment is required each month</li>
-                        <li><DropDownMenu value="" words={chargeArray}/> is billed not paid on time</li>
+                        <li>You receive a <DropDownMenu value="" words={billArray}/> each month</li>
+                        <li>Full bill <DropDownMenu value="" words={doesntArray}/> need to be paid</li>
+                        <li><DropDownMenu value="" words={interestArray}/> is charged on <DropDownMenu value="" words={unpaidArray}/> amount</li>
+                        <li>a <DropDownMenu value="" words={minimumArray}/> payment is required each month</li>
+                        <li><DropDownMenu value="" words={lateFeeArray}/> is billed not paid on time</li>
                     </ul>
                 </div>
                 <div className="flex flex-col-reverse md:flex-row bg-gray-100 rounded-2xl md:mx-12 m-12">
                     <img className="max-w-sm p-4" src="../../images/credit-card/credit-checklist.svg"></img>
                     <ul className="flex flex-col gap-8 p-12 text-2xl">
-                        <li>Spending is limited by a <DropDownMenu value="" words={chargeArray}/></li>
-                        <li>You <DropDownMenu value="" words={chargeArray}/> for a credit card and need to be <DropDownMenu value="" words={chargeArray}/></li>
-                        <li><DropDownMenu value="" words={chargeArray}/> your credit with <DropDownMenu value="" words={chargeArray}/> credit card use</li>
+                        <li>Spending is limited by a <DropDownMenu value="" words={creditLimitArray}/></li>
+                        <li>You <DropDownMenu value="" words={applyArray}/> for a credit card and need to be <DropDownMenu value="" words={approvedArray}/></li>
+                        <li><DropDownMenu value="" words={buildArray}/> your credit with <DropDownMenu value="" words={responsibleArray}/> credit card use</li>
                     </ul>
                 </div>
             </div>
