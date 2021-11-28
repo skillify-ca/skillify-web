54edc742e132aa09b544fc8cc3530607b60534bc
jmski <geluajm@gmail.com>
2021-11-28 18:48:31 -0500

    Removed unnecessary key props

--- components/credit-card/CreditDebitInfo.tsx
+++ components/credit-card/CreditDebitInfo.tsx
@@ -41,8 +41,8 @@ const CreditDebitInfo = () => {
                             <div className="flex flex-col justify-center bg-purple-200 p-12 mx-12 rounded-2xl">
                                 <h2 className="text-4xl mb-4">Credit Cards</h2>
                                 <span className="text-2xl">
-                                    Allow consumers to <DropDownMenu key={borrowArray[0]} words={borrowArray} />
-                                    money from a financial institution and <DropDownMenu key={payArray[0]} words={payArray} />
+                                    Allow consumers to <DropDownMenu words={borrowArray} />
+                                    money from a financial institution and <DropDownMenu words={payArray} />
                                     it back later
                                 </span>
                             </div>
@@ -54,7 +54,7 @@ const CreditDebitInfo = () => {
                             <div className="flex flex-col justify-center bg-green-200 p-12 mx-12 rounded-2xl">
                                 <h2 className="text-4xl mb-4">Debit Cards</h2>
                                 <span className="text-2xl">
-                                    Allow consumers to <DropDownMenu key={spendArray[0]} words={spendArray} /> money they have <DropDownMenu key={depositArray[0]} words={depositArray} /> in their bank account
+                                    Allow consumers to <DropDownMenu words={spendArray} /> money they have <DropDownMenu words={depositArray} /> in their bank account
                                 </span>
                             </div>
                             <img className="max-w-md" src="/images/credit-card/card-with-hand.svg" />
@@ -68,10 +68,10 @@ const CreditDebitInfo = () => {
                 <div className=" flex flex-col justify-center bg-gray-200 p-12 mx-12 rounded-2xl">
                     <h2 className="text-4xl mb-12">So how are they similar?</h2>
                     <ul className="flex flex-col gap-4 text-2xl">
-                        <li>Allow a person to make <DropDownMenu key={purchaseArray[0]} words={purchaseArray} /></li>
-                        <li>If card is <DropDownMenu key={lostArray[0]} words={lostArray} /> or <DropDownMenu key={stolenArray[0]} words={stolenArray} />, report import it immediately</li>
-                        <li>Hard, <DropDownMenu key={plasticArray[0]} words={plasticArray} /> card with information stored on a <DropDownMenu key={magneticArray[0]} words={magneticArray} /> strip.</li>
-                        <li>Owner's <DropDownMenu key={nameArray[0]} words={nameArray} /> and an <DropDownMenu key={expirationArray[0]} words={expirationArray} /> date on the card</li>
+                        <li>Allow a person to make <DropDownMenu words={purchaseArray} /></li>
+                        <li>If card is <DropDownMenu words={lostArray} /> or <DropDownMenu words={stolenArray} />, report import it immediately</li>
+                        <li>Hard, <DropDownMenu words={plasticArray} /> card with information stored on a <DropDownMenu words={magneticArray} /> strip.</li>
+                        <li>Owner's <DropDownMenu words={nameArray} /> and an <DropDownMenu words={expirationArray} /> date on the card</li>
                     </ul>
                 </div>
             </div>
--- components/credit-card/DebitCardWordProblem.tsx
+++ components/credit-card/DebitCardWordProblem.tsx
@@ -24,7 +24,7 @@ const DebitCardWordProblem = () => {
                     <h1 className="text-green-500 font-bold text-5xl md:text-7xl uppercase">Debit Cards</h1>
                 </div>
                 <div className="flex justify-center mb-12">
-                    <span className="text-4xl">You <DropDownMenu key={makeArray[0]} words={makeArray} /> a purchase to your debit card</span>
+                    <span className="text-4xl">You <DropDownMenu words={makeArray} /> a purchase to your debit card</span>
                 </div>
                 <div className="flex flex-col justify-center items-center">
                     <div className="px-12">
@@ -32,10 +32,10 @@ const DebitCardWordProblem = () => {
                     </div>
                     <div className="text-2xl">
                         <div className="flex justify-center p-8 bg-green-200 rounded-2xl mx-12">
-                            <span>The <DropDownMenu key={bankArray[0]} words={bankArray} /> send your <DropDownMenu key={moneyArray[0]} words={moneyArray} /> to the store</span>
+                            <span>The <DropDownMenu words={bankArray} /> send your <DropDownMenu words={moneyArray} /> to the store</span>
                         </div>
                         <div className="flex justify-center p-8 bg-green-300 rounded-2xl mx-12 my-4">
-                            <span>Money is <DropDownMenu key={deductedArray[0]} words={deductedArray} /> from your <DropDownMenu key={checkingArray[0]} words={checkingArray} /> account</span>
+                            <span>Money is <DropDownMenu words={deductedArray} /> from your <DropDownMenu words={checkingArray} /> account</span>
                         </div>
                     </div>
                 </div>
@@ -43,11 +43,11 @@ const DebitCardWordProblem = () => {
             <div className="flex flex-col-reverse gap-12 items-center">
                 <img className="max-w-sm p-4" src="/images/credit-card/debit-wallet.svg" />
                 <ul className="flex flex-col gap-8 p-12 text-2xl">
-                    <li>Issued by a <DropDownMenu key={bankArray2[1]} words={bankArray2} /> and linked to customer's <DropDownMenu key={bankArray3[2]} words={bankArray3} /> account</li>
-                    <li>Spending <DropDownMenu key={limitedArray[0]} words={limitedArray} /> by how much is in the bank account</li>
-                    <li>May be <DropDownMenu key={overdraftArray[0]} words={overdraftArray} /> fees for spending more than what is in the account</li>
-                    <li>Overdraft <DropDownMenu key={protectionArray[0]} words={protectionArray} /> may be available</li>
-                    <li>A <DropDownMenu key={pinArray[0]} words={pinArray} /> is required to make a purchase or get money out of <DropDownMenu key={atmArray[0]} words={atmArray} /></li>
+                    <li>Issued by a <DropDownMenu words={bankArray2} /> and linked to customer's <DropDownMenu words={bankArray3} /> account</li>
+                    <li>Spending <DropDownMenu words={limitedArray} /> by how much is in the bank account</li>
+                    <li>May be <DropDownMenu words={overdraftArray} /> fees for spending more than what is in the account</li>
+                    <li>Overdraft <DropDownMenu words={protectionArray} /> may be available</li>
+                    <li>A <DropDownMenu words={pinArray} /> is required to make a purchase or get money out of <DropDownMenu words={atmArray} /></li>
                 </ul>
                 <img className="max-w-sm p-4" src="/images/credit-card/debit-vault.svg" />
             </div>
