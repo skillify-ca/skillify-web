a7a3fa5f726b9ed784db5991ec66d48f89609452
jmski <geluajm@gmail.com>
2021-11-28 23:21:17 -0500

    Add progress tracker to page

---
+++ components/credit-card/CreditCardProgressTracker.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+export interface CreditCardProgressTrackerProps {
+  score?: number;
+  total?: number;
+  name?: string;
+}
+
+const CreditCardProgressTracker: React.FC<CreditCardProgressTrackerProps> = ({
+  score,
+  total,
+  name,
+}) => {
+  return (
+    <div className="w-full h-min sticky top-0 border border-purple-800 bg-gradient-to-r from-gray-200 to-purple-200">
+      <div className="flex flex-row justify-evenly items-center gap-12 text-md md:text-xl uppercase p-2 md:p-4">
+        <h3>
+          name: <span className="text-yellow-500 font-extrabold">{name}</span>
+        </h3>
+        <h3>
+          score: <span className="text-purple-500 font-extrabold">{score}</span>
+        </h3>
+        <h3>
+          total: <span className="text-green-500 font-extrabold">{total}</span>
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default CreditCardProgressTracker;
--- pages/credit-card.tsx
+++ pages/credit-card.tsx
@@ -1,95 +1,87 @@
-import React, { useState } from 'react';
-import CardColorProblem from '../components/credit-card/CardColorProblem';
-import MultipleChoiceWordProblem from '../components/credit-card/MultipleChoiceWordProblem';
-import CreditCardWordProblem from '../components/credit-card/CreditCardWordProblem';
-import CreditDebitInfo from '../components/credit-card/CreditDebitInfo';
-import DebitCardWordProblem from '../components/credit-card/DebitCardWordProblem';
-import { Button } from '../components/ui/Button';
+import React, { useState } from "react";
+import CardColorProblem from "../components/credit-card/CardColorProblem";
+import MultipleChoiceWordProblem from "../components/credit-card/MultipleChoiceWordProblem";
+import CreditCardWordProblem from "../components/credit-card/CreditCardWordProblem";
+import CreditDebitInfo from "../components/credit-card/CreditDebitInfo";
+import DebitCardWordProblem from "../components/credit-card/DebitCardWordProblem";
+import { Button } from "../components/ui/Button";
+import CreditCardProgressTracker from "../components/credit-card/CreditCardProgressTracker";
 
 export default function CreditCard(props) {
-    enum STAGE {
-        CreditDebitInfo, //intro with info
-        CreditCardWordProblem, //credit card word problem
-        DebitCardWordProblem, //debit card word problem
-        MultipleChoiceWordProblem, //multiple choice
-        CardColorProblem, //credit debit both color problem
+  enum STAGE {
+    CreditDebitInfo, //intro with info
+    CreditCardWordProblem, //credit card word problem
+    DebitCardWordProblem, //debit card word problem
+    MultipleChoiceWordProblem, //multiple choice
+    CardColorProblem, //credit debit both color problem
+  }
+
+  //manages state
+  const [stage, setStage] = useState(STAGE.CreditDebitInfo);
+
+  // CreditDebitInfo states
+  const [q1, setq1] = useState("");
+  const [a1, seta1] = useState("");
+
+  const previousStage = () => {
+    if (stage > STAGE.CreditDebitInfo) {
+      setStage(stage - 1);
     }
+  };
 
-    //manages state 
-    const [stage, setStage] = useState(STAGE.CreditDebitInfo);
-
-    // CreditDebitInfo states
-    const [q1, setq1] = useState("");
-    const [a1, seta1] = useState("");
-
-
-
-    const previousStage = () => {
-        if (stage > STAGE.CreditDebitInfo) {
-            setStage(stage - 1);
-        }
-    };
-
-    const nextStage = () => {
-        if (stage < STAGE.CardColorProblem) {
-            setStage(stage + 1);
-        }
-    };
-
-
-    const getComponent = (stage: STAGE) => {
-        if (stage == STAGE.CreditDebitInfo) {
-            return <CreditDebitInfo />
-        } else if (stage == STAGE.CreditCardWordProblem) {
-            return <CreditCardWordProblem />
-        } else if (stage == STAGE.DebitCardWordProblem) {
-            return <DebitCardWordProblem />
-        } else if (stage == STAGE.MultipleChoiceWordProblem) {
-            return <MultipleChoiceWordProblem />
-        } else if (stage == STAGE.CardColorProblem) {
-            return <CardColorProblem />
-        }
+  const nextStage = () => {
+    if (stage < STAGE.CardColorProblem) {
+      setStage(stage + 1);
     }
+  };
 
-    return (
-        <div className={"bg-white"}>
-            <div>{getComponent(stage)}</div>
-            <div className="flex flex-row space-x-8 justify-center">
-                <Button
-                    label="submit"
-                    backgroundColor="yellow"
-                    textColor="white" />
-            </div>
-            {(stage == STAGE.CreditDebitInfo)
-                ?
-                <div className="flex flex-col min-w-full p-12">
-                    <Button
-                        backgroundColor="purple"
-                        textColor="white"
-                        label="Next"
-                        onClick={nextStage}
-                    />
-                </div>
-                :
-                <div className="flex flex-row space-x-8 justify-center p-12">
-                    <Button
-                        backgroundColor="purple"
-                        textColor="white"
-                        label="Go Back"
-                        onClick={previousStage}
-                    />
+  const getComponent = (stage: STAGE) => {
+    if (stage == STAGE.CreditDebitInfo) {
+      return <CreditDebitInfo />;
+    } else if (stage == STAGE.CreditCardWordProblem) {
+      return <CreditCardWordProblem />;
+    } else if (stage == STAGE.DebitCardWordProblem) {
+      return <DebitCardWordProblem />;
+    } else if (stage == STAGE.MultipleChoiceWordProblem) {
+      return <MultipleChoiceWordProblem />;
+    } else if (stage == STAGE.CardColorProblem) {
+      return <CardColorProblem />;
+    }
+  };
 
-                    <Button
-                        backgroundColor="green"
-                        textColor="white"
-                        label="Next"
-                        onClick={nextStage}
-                    />
-                </div>
-            }
-
+  return (
+    <div className={"bg-white"}>
+      <CreditCardProgressTracker />
+      <div>{getComponent(stage)}</div>
+      <div className="flex flex-row space-x-8 justify-center">
+        <Button label="submit" backgroundColor="yellow" textColor="white" />
+      </div>
+      {stage == STAGE.CreditDebitInfo ? (
+        <div className="flex flex-col min-w-full p-12">
+          <Button
+            backgroundColor="purple"
+            textColor="white"
+            label="Next"
+            onClick={nextStage}
+          />
         </div>
-    )
-};
+      ) : (
+        <div className="flex flex-row space-x-8 justify-center p-12">
+          <Button
+            backgroundColor="purple"
+            textColor="white"
+            label="Go Back"
+            onClick={previousStage}
+          />
 
-
+          <Button
+            backgroundColor="green"
+            textColor="white"
+            label="Next"
+            onClick={nextStage}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
