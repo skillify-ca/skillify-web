3af9adc03021c8f2cf68b8a71c7b6b348201897f
= <ajevan.mahadaya@uoit.net>
2021-11-30 11:32:21 -0500

    Initial Redux for War Game
    Initial Redux for War Game including unit tests to check if redux functions are working as expected

--- redux/rootReducer.ts
+++ redux/rootReducer.ts
@@ -1,28 +1,35 @@
 import { combineReducers, Reducer } from "@reduxjs/toolkit";
 import { bakersRackBSlice, BakersRackBState } from "./bakerBSlice";
 import { diagnosticSlice, DiagnosticState } from "./diagnosticSlice";
+import { warGameSlice, WarGameState } from "./warGameSlice";
 import {
   studentProfileSlice,
   StudentProfileState,
 } from "./studentProfileSlice";
-import { assignmentSessionSlice, assignmentSessionState } from "./assignmentSession"
-
+import {
+  assignmentSessionSlice,
+  assignmentSessionState,
+} from "./assignmentSession";
 
 type State = {
   diagnostic: DiagnosticState;
   studentProfile: StudentProfileState;
   bakersRackB: BakersRackBState;
   assignmentSession: assignmentSessionState;
+  warGameState: WarGameState;
 };
 const diagnosticReducer: Reducer = diagnosticSlice.reducer;
 const studentProfileReducer = studentProfileSlice.reducer;
 const bakersRackBReducer = bakersRackBSlice.reducer;
 const assignmentSessionReducer = assignmentSessionSlice.reducer;
+const warGameReducer = warGameSlice.reducer;
+
 const rootReducer = combineReducers({
   diagnostic: diagnosticReducer,
   studentProfile: studentProfileReducer,
   bakersBRack: bakersRackBReducer,
-  assignmentSession: assignmentSessionReducer
+  assignmentSession: assignmentSessionReducer,
+  warGame: warGameReducer,
 });
 
 export type RootState = State;
