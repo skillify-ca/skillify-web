6d5e9bef04aff4bee501e15b0522f0060c1ac701
= <ajevan.mahadaya@uoit.net>
2021-11-30 13:21:24 -0500

    Remove comments as per PR recommendations
    Remove comments as per PR recommendations

--- redux/warGameSlice.test.ts
+++ redux/warGameSlice.test.ts
@@ -83,36 +83,3 @@ test("test both players ready and start round action", () => {
     },
   });
 });
-
-// test("should handle a todo being added to an empty list", () => {
-//   const previousState = [];
-//   expect(reducer(previousState, todoAdded("Run the tests"))).toEqual([
-//     {
-//       text: "Run the tests",
-//       completed: false,
-//       id: 0,
-//     },
-//   ]);
-// });
-
-// test("should handle a todo being added to an existing list", () => {
-//   const previousState = [
-//     {
-//       text: "Run the tests",
-//       completed: true,
-//       id: 0,
-//     },
-//   ];
-//   expect(reducer(previousState, todoAdded("Use Redux"))).toEqual([
-//     {
-//       text: "Run the tests",
-//       completed: true,
-//       id: 0,
-//     },
-//     {
-//       text: "Use Redux",
-//       completed: false,
-//       id: 1,
-//     },
-//   ]);
-// });
