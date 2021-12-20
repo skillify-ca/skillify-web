b938ae81e0522e4c95b5f5d6c25d51e02e31d779
vithushan19 <vithushan19@gmail.com>
2021-12-20 13:04:59 -0500

    Reverse String Lesson

--- components/coding/EvaluateExpression.tsx
+++ components/coding/EvaluateExpression.tsx
@@ -155,32 +155,4 @@ const Interactive = ({ state }) => {
     </div>
   );
 };
-const Resources = () => {
-  return (
-    <div className="flex flex-col justify-center gap-4 p-8 bg-white shadow-lg rounded-xl">
-      <p className="font-bold">Resources</p>
-      <a
-        href="https://leetcode.com/problems/basic-calculator/"
-        className="text-blue-500 underline"
-      >
-        Leetcode #224
-      </a>
-      <a
-        href="https://www.geeksforgeeks.org/expression-evaluation/"
-        className="text-blue-500 underline"
-      >
-        GeeksForGeeks Article
-      </a>
-      <iframe
-        width="560"
-        height="315"
-        className="w-full"
-        src="https://www.youtube.com/embed/081AqOuasw0"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-    </div>
-  );
-};
 export default EvaluateExpression;
---
+++ pages/coding/reverseString.tsx
@@ -0,0 +1,196 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import EvaluateExpression from "../../components/coding/EvaluateExpression";
+import Resources from "../../components/coding/Resources";
+import { Button } from "../../components/ui/Button";
+import {
+  evaluateExpressionSelector,
+  onNext,
+  onPrevious,
+  reset,
+  setInput,
+} from "../../redux/evaluateExpressionSlice";
+
+import { useAppDispatch } from "../../redux/store";
+
+export default function ReverseString(props) {
+  const onResetRequested = () => {
+    setIsSwapping(false);
+    setStr(original.split(""));
+    setHead(0);
+    setTail(s.length - 1);
+  };
+  const onAdvanceRequested = () => {
+    setHead(head + 1);
+    setTail(tail - 1);
+  };
+  const onClearRequested = () => {
+    setOriginal("");
+    setStr([]);
+    setHead(0);
+    setTail(s.length - 1);
+  };
+
+  const onInputChangeRequested = (val) => {
+    setOriginal(val);
+    setStr(val.split(""));
+    setHead(0);
+    setTail(val.length - 1);
+  };
+
+  const onSwapRequested = () => {
+    setIsSwapping(true);
+    const next = [...s];
+
+    var currentChar = next[head];
+    next[head] = next[tail]; //o l l e o
+    next[tail] = currentChar; // o l l e h
+    setStr(next);
+  };
+
+  const isComplete = () => {
+    return head >= tail;
+  };
+
+  const [s, setStr] = useState([]);
+  const [original, setOriginal] = useState("");
+  const [isSwapping, setIsSwapping] = useState(false);
+  const [head, setHead] = useState(0);
+  const [tail, setTail] = useState(s.length - 1);
+  const getCharColour = (index) => {
+    if (index === head && index === tail)
+      return "bg-gradient-to-b from-green-300 to-yellow-300";
+    if (index === head) return "bg-green-300";
+    if (index === tail) return "bg-yellow-300";
+    return "bg-blue-300";
+  };
+
+  return (
+    <>
+      <div className="sticky top-0 w-full shadow-md">
+        <h1 className="w-full p-4 text-2xl font-bold text-center bg-blue-300">
+          344. Reverse String
+        </h1>
+        <p className="w-full p-4 text-center bg-blue-300">
+          Write a function that reverses a string. The input string is given as
+          an array of characters s. You must do this by modifying the input
+          array in-place with O(1) extra memory.
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-4 heropattern-bamboo-gray-300">
+        <div className="grid w-full grid-cols-1 gap-4 p-4 sm:grid-cols-2">
+          <div className="flex flex-col justify-center gap-4 p-8 bg-white shadow-lg rounded-xl">
+            <p className="font-bold">Resources</p>
+            <a
+              href="https://leetcode.com/problems/reverse-string/"
+              className="text-blue-500 underline"
+            >
+              Leetcode #344
+            </a>
+            <a
+              href="https://stackoverflow.com/questions/228038/best-way-to-reverse-a-string"
+              className="text-blue-500 underline"
+            >
+              StackOverflow Thread
+            </a>
+            <iframe
+              width="110"
+              height="200"
+              className="w-full"
+              src="https://www.youtube.com/embed/NA--2JiDaeQ"
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div className="flex flex-col gap-4 p-4 bg-white shadow-lg rounded-xl">
+            <p className="font-bold">Algorithm</p>
+            Use a head and a tail pointer to traverse the string. Swap the head
+            and tail characters each time. Stop when the head passes the tail.
+          </div>
+          <div className="flex flex-col gap-2 p-4 bg-white shadow-lg rounded-xl">
+            <h2 className="font-bold">String Input</h2>
+            <input
+              className="p-4 bg-blue-50"
+              value={original}
+              onChange={(e) => onInputChangeRequested(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-4 p-4 bg-white shadow-lg rounded-xl">
+            <p className="font-bold">Controls</p>
+            <div className="grid grid-cols-2 gap-4">
+              <Button
+                disabled={isComplete()}
+                label="Swap"
+                backgroundColor="blue"
+                textColor="white"
+                onClick={onSwapRequested}
+              />
+              <Button
+                disabled={isComplete()}
+                label="Advance"
+                backgroundColor="blue"
+                textColor="white"
+                onClick={onAdvanceRequested}
+              />
+              <Button
+                label="Reset"
+                backgroundColor="blue"
+                textColor="white"
+                onClick={onResetRequested}
+              />
+              <Button
+                label="Clear"
+                backgroundColor="blue"
+                textColor="white"
+                onClick={onClearRequested}
+              />
+            </div>
+          </div>
+          <div className="col-span-2">
+            <div className="flex flex-col gap-4 p-4 bg-white shadow-lg rounded-xl">
+              <div className="flex flex-col">
+                <h2 className="font-bold">Interactive</h2>
+                {isComplete() && (
+                  <p className="text-green-600">
+                    Congrats! You reversed the string!
+                  </p>
+                )}
+              </div>
+              <div className="flex items-center justify-center gap-2">
+                <p className="text-5xl">{"["}</p>
+                {s &&
+                  s.map((it, index) => (
+                    <div className="flex flex-col">
+                      <p
+                        onAnimationEnd={() => setIsSwapping(false)}
+                        className={`${
+                          isSwapping && (index === head || index === tail)
+                            ? "animate-fadeIn_half"
+                            : ""
+                        } p-4 whitespace-pre ${getCharColour(index)}`}
+                      >
+                        {'"' + it + '"'}
+                      </p>
+                    </div>
+                  ))}
+
+                <p className="text-5xl">{"]"}</p>
+              </div>
+
+              <div className="flex gap-4">
+                {s.length > 0 && (
+                  <h1 className="p-4 bg-green-300 w-max">Head</h1>
+                )}
+                {s.length > 0 && (
+                  <h1 className="p-4 bg-yellow-300 w-max">Tail</h1>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
