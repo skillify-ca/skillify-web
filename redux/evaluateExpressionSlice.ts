db58a0a1ed9880a5459b2a3e428aeffd81b0ae0c
Vithushan <vithushan19@gmail.com>
2021-12-25 12:14:32 -0500

    Update evaluateExpressionSlice.ts

--- redux/evaluateExpressionSlice.ts
+++ redux/evaluateExpressionSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction, Slice } from "@reduxjs/toolkit";
-import { SimpleCalculatorProps } from "../components/coding/SimpleCalculator";
+import { SimpleCalculatorProps } from "../components/coding/evaluateExpression/SimpleCalculator";
 import { RootState } from "./rootReducer";
 
 export enum Stage {
