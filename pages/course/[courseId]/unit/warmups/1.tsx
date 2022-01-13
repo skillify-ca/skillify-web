e77f508dc3566350d5d52fc6f272d632e77cf117
= <lavannithi2@gmail.com>
2022-01-13 14:21:28 -0500

    installing node.js instructions

--- pages/course/[courseId]/unit/warmups/1.tsx
+++ pages/course/[courseId]/unit/warmups/1.tsx
@@ -70,6 +70,31 @@ const Warmups1 = () => {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
+          <div>
+            <p className="text-green-700 font-bold text-lg">
+              {" "}
+              How to download Node.js{" "}
+            </p>
+            <ol>
+              <li>
+                1. Google "Node.js" and click this link
+                https://nodejs.org/en/download/
+              </li>
+              <img src="/images/download.png" height="50px" />
+              <li>
+                2. You should see this homepage. Download for your operating
+                system.
+              </li>
+              <li>3. Click on the Downloaded Package</li>
+              <li>
+                4. It should open up soemthing similar to the image below. Agree
+                to the terms and conditions and press continue
+              </li>
+              <img src="/images/terms-and-conditions.png" />
+              <li>5. Once you reach this end screen you're good to go!</li>
+              <img src="/images/end-screen.png" />
+            </ol>
+          </div>
         </div>
       </div>
     </>
