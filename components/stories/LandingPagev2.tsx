47d15c5267c063dd5e069382d865d2511b0cdfd9
= <lavannithi2@gmail.com>
2022-02-15 16:26:52 -0500

    Landing Page

--- components/stories/LandingPagev2.tsx
+++ components/stories/LandingPagev2.tsx
@@ -9,83 +9,16 @@ import {
 import ContactForms from "../ContactForms";
 
 const LandingPagev2 = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: false,
-  };
-
-  const features = [
-    {
-      name: "Practice Tracker",
-      description:
-        "Practice skills from our infinite set of questions. Prove mastery by unlocking badges.",
-      icon: GlobeAltIcon,
-    },
-    {
-      name: "Multiplayer Games",
-      description:
-        "Connect with friends online with our competitive and coopeartive games.",
-      icon: ScaleIcon,
-    },
-    {
-      name: "Interactive Lessons",
-      description:
-        "Use our videos, puzzles and lessons to introduce new math concepts.",
-      icon: AnnotationIcon,
-    },
-    {
-      name: "Weekly Progress Reports",
-      description:
-        "Stay in the know with weekly reports sent straight to your inbox.",
-      icon: LightningBoltIcon,
-    },
-  ];
-
   return (
     <div>
-      <div className="py-12 bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="lg:text-center">
-            <h2 className="text-base text-blue-600 font-semibold tracking-wide uppercase">
-              Activities and Quizzes
-            </h2>
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              Motivate yourself to become a self-learner
-            </p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-              Become confident working with numbers and code
-            </p>
-          </div>
-
-          <div className="mt-10">
-            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-              {features.map((feature) => (
-                <div key={feature.name} className="relative">
-                  <dt>
-                    <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-                      <feature.icon className="h-6 w-6" aria-hidden="true" />
-                    </div>
-                    <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                      {feature.name}
-                    </p>
-                  </dt>
-                  <dd className="mt-2 ml-16 text-base text-gray-500">
-                    {feature.description}
-                  </dd>
-                </div>
-              ))}
-            </dl>
-          </div>
-          <div className="mt-10">
-            <ContactForms />
-          </div>
-        </div>
+      <div className="flex flex-col w-full bg-gray-300">
+        <div>Navigation</div>
+        <div>Hero</div>
+        <div>Features</div>
+        <div>Email Capture</div>
+        <div>Testimonials</div>
+        <div>Email Capture 2</div>
+        <div> Footer</div>
       </div>
     </div>
   );
