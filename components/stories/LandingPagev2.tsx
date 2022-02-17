1f72ed7c96e42e1a12ec12e309e91cada03882a8
= <lavannithi2@gmail.com>
2022-02-17 15:56:52 -0500

    fix vercel errors

--- components/stories/LandingPagev2.tsx
+++ components/stories/LandingPagev2.tsx
@@ -294,7 +294,7 @@ const LandingPagev2 = () => {
             </svg>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -308,7 +308,7 @@ const LandingPagev2 = () => {
             </svg>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
