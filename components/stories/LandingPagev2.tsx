cb2d6a2915fdde9b7e15bee35838a511e87a4bf7
= <lavannithi2@gmail.com>
2022-02-17 16:00:50 -0500

    Fix vercel errors

--- components/stories/LandingPagev2.tsx
+++ components/stories/LandingPagev2.tsx
@@ -280,7 +280,7 @@ const LandingPagev2 = () => {
           <div className="gap-x-2 flex flex-row">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
