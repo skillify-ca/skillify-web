102d70d5d759a66477de49792a151757b2d36457
Vithushan <vithushan19@gmail.com>
2022-03-02 17:51:28 -0500

    Create unit header

--- components/coding/studentPortal/UnitView.tsx
+++ components/coding/studentPortal/UnitView.tsx
@@ -9,8 +9,10 @@ export type UnitViewProps = {
 
 export const UnitView: React.FC<UnitViewProps> = ({ data }: UnitViewProps) => {
   return (
-    <div className="bg-gray-300 grid grid-cols-1 gap-4">
-      <p>{data.title}</p>
+    <div className="grid grid-cols-1 gap-4">
+      <p className="bg-blue-900 w-48 p-4 text-white rounded-full text-center">
+        {data.title}
+      </p>
       <div>
         {data.nodes.map((it, index) => (
           <UnitNodeView
