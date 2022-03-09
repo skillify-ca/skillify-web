031a70346044f4b103bd9cb746175f784259b54e
Lavan Nithi <79107199+LavaBoy202@users.noreply.github.com>
2022-03-09 11:12:44 -0500

    Sidebar Component (#662)
    * Sidebar Component
    
    * Minor edits
    
    * Remove 5 col grid
    
    * Remove Stages pattern

--- components/coding/studentPortal/Sidebar.tsx
+++ components/coding/studentPortal/Sidebar.tsx
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 export type SidebarProps = {};
 
 export const Sidebar: React.FC<SidebarProps> = ({}: SidebarProps) => {
-  return <div className="w-full">RIGHT SIDEBAR</div>;
+  return (
+    //Full width then restrict in page
+    <div className="">
+      <div className="grid grid-rows-2 place-content-between ">
+        <div className="grid grid-rows-4 gap-y-8 ">
+          <div className="p-2 flex">
+            <div>
+              {" "}
+              <img className="w-12 h-12" src="/images/logo.png" />
+            </div>
+            <div>
+              <p className="font-bold ml-4">Hey There!</p>
+              <p className="font-medium ml-4">Student</p>
+            </div>
+          </div>
+          <div className={`"border-charmander text-charmander"`}>
+            <div className={`p-4 border-l-4 border-charmander text-charmander`}>
+              <div className="flex flex-wrap">
+                <img
+                  className="w-8 h-8 mr-4"
+                  src="/images/dashBoardActive.svg"
+                />
+                Dashboard
+              </div>
+            </div>
+          </div>
+          <a
+            className={`p-4 border-l-4`}
+            href="https://www.skillify.ca/profile"
+          >
+            <div className="flex flex-wrap">
+              <img
+                className="w-8 h-8 mr-4"
+                src="/images/classroomInactive.svg"
+              />
+              Classroom
+            </div>
+          </a>
+
+          <a
+            className={`p-4 border-l-4
+            `}
+            href="https://www.skillify.ca/profile"
+          >
+            <div className="flex flex-wrap">
+              <img className="w-8 h-8 mr-4" src="/images/profileInactive.svg" />
+              Profile
+            </div>
+          </a>
+        </div>
+        <div className="flex flex-col justify-end mb-18">
+          <div className="flex flex-wrap ">
+            <img className="w-8 h-8 mr-4" src="/images/logoutButton.svg" />
+            Logout
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
