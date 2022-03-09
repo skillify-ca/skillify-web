79497d34b2d1ff1ba2a2ed546edd67d5d5578ca5
Lavan Nithi <79107199+LavaBoy202@users.noreply.github.com>
2022-03-09 15:32:42 -0500

    Progress Bar Component (#664)
    * Sidebar Component
    
    * Minor edits
    
    * Remove 5 col grid
    
    * Remove Stages pattern
    
    * Landing Page Edits to Colour and Mobile Responsive Design
    
    * Progress Bar Component

---
+++ components/coding/studentPortal/ProgressBar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export type ProgressBarProps = {
+  completed: number;
+};
+
+export const ProgressBar: React.FC<ProgressBarProps> = ({
+  completed,
+}: ProgressBarProps) => {
+  const fillerStyles = {
+    height: "100%",
+    width: `${completed}%`,
+    borderRadius: "inherit",
+  };
+  return (
+    <div>
+      <div className="h-8 w-full bg-gray-300 m-32 border rounded-full">
+        <div style={fillerStyles} className="text-right bg-rattata">
+          <span className="p-5 text-white font-bold text-center">{`${completed}%`}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
