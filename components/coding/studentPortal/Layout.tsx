eabb505560b2b0b9dc56fc0212634b61f5b47d70
Vithushan <vithushan19@gmail.com>
2022-03-18 05:03:10 -0400

    add layout and navbar changes

--- components/coding/studentPortal/Layout.tsx
+++ components/coding/studentPortal/Layout.tsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../ui/Navbar";
 import Sidebar from "./Sidebar";
 
 export const Layout: React.FC = ({ children }) => {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className="grid grid-cols-12">
-      <div className="col-span-12 flex lg:hidden">
-        <Navbar />
+    <div className="">
+      <div className="col-span-12 flex z-10 fixed w-full">
+        <Navbar active={active} setActive={setActive} />
       </div>
-      <div className="hidden lg:flex col-span-2">
+      <div className="hidden lg:flex w-48 fixed mt-16 bg-white h-full">
         <Sidebar />
       </div>
-      <div className="grid grid-cols-1 col-span-12 lg:col-span-10 gap-4 bg-gray-100 shadow-lg p-8">
+
+      <div
+        className={`fixed lg:hidden mt-16 w-48 ${
+          active ? "" : "-ml-48"
+        } h-full bg-white lg:w-0 transition-all transform duration-500 ease-in-out`}
+      >
+        <Sidebar />
+      </div>
+      <div
+        className={`transition-all transform duration-500 ease-in-out grid grid-cols-1 gap-4 bg-gray-100 shadow-lg pt-20 lg:pl-8 px-4 lg:ml-48 ${
+          active ? "ml-48" : ""
+        } `}
+      >
         <main>{children}</main>
       </div>
     </div>
