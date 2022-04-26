59cbd20356def067c7b5bb6f9c44a62a4d3806bc
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-04-26 18:01:44 -0400

    Update Input.tsx

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -20,11 +20,10 @@ export const Input: React.FC<InputProps> = ({
     <input
       autoFocus={autoFocus}
       id="guess"
-      type="number"
       autoComplete="off"
       value={guess}
       onChange={(e) => setGuess(e.target.value)}
-      className="w-full sm:w-64 appearance-none py-4 text-center border rounded-md shadow-md focus:outline-none focus:ring-indigo-500 bg-blue-100 focus:z-10 sm:text-sm"
+      className="w-full py-4 text-center bg-white border rounded-md shadow-lg appearance-none sm:w-64 focus:outline-none focus:ring-indigo-500 focus:z-10 sm:text-sm"
       placeholder="Enter Answer"
       onKeyPress={handleKeypress}
     />
