f44919bdddb7820d0dede8226a08bf0677f9959f
Jason Rajasegaram <jrajasegaram@idc.com>
2022-05-15 20:35:58 -0400

    Branch setup

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Profile(props) {
   return (
     <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll bg-white">
-      Profile - Coming Soon
+      Profile - Jason
     </div>
   );
 }
