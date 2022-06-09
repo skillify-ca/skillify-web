ae1e4f6f36c687a27ed3384a2423947ce3207038
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-06-09 15:59:57 -0400

    Update CSS Lesson

--- pages/studentPortal/intro/CSS/1.tsx
+++ pages/studentPortal/intro/CSS/1.tsx
@@ -39,6 +39,27 @@ const CSS1 = ({ lessonComponents, totalSteps }) => {
         {lessonComponents.map((it) => (
           <LessonComponent data={it} />
         ))}
+        <div className="pb-56 mb-8 h-96">
+          <iframe
+            src="https://www.loom.com/embed/0e30b6386ed847e490b849a2d24d6b60"
+            frameBorder="0"
+            webkit-allowfullscreen
+            moz-allowfullscreen
+            allowFullScreen
+            className="w-full h-96"
+          ></iframe>
+        </div>
+        <div className="pb-56 mb-8 h-96">
+          <iframe
+            src="https://www.loom.com/embed/13bf6c0abba840eba25daf94e332244f"
+            frameBorder="0"
+            webkit-allowfullscreen
+            moz-allowfullscreen
+            allowFullScreen
+            className="w-full h-96"
+          ></iframe>
+        </div>
+
         <div className="flex mt-8 sm:justify-end">
           <a href={"/studentPortal/intro/CSS/2"}>
             <Button
@@ -123,11 +144,34 @@ export async function getServerSideProps({ params }) {
       description: "Do all levels.",
     },
     {
-      title: "CSS Diner",
+      title: "CSS Tricks: Grid",
       image:
-        "https://i.pinimg.com/originals/8a/b1/ad/8ab1ad8128508785f956eb6f58779b47.jpg",
-      link: "https://flukeout.github.io/",
-      description: "",
+        "https://i0.wp.com/css-tricks.com/wp-content/uploads/2021/12/default-social-css-tricks.png",
+      link: "https://css-tricks.com/snippets/css/complete-guide-grid/",
+      description:
+        "Skim through this reference guide on CSS Grid. But keep it handy when building websites.",
+    },
+    {
+      title: "CSS Tricks: Flexbox",
+      image:
+        "https://i0.wp.com/css-tricks.com/wp-content/uploads/2021/12/default-social-css-tricks.png",
+      link: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
+      description:
+        "Skim through this reference guide on CSS Flexbox. But keep it handy when building websites.",
+    },
+    {
+      title: "CSS Grid Layout Crash Course",
+      image:
+        "https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s176-c-k-c0x00ffffff-no-rj",
+      link: "https://www.youtube.com/watch?v=jV8B24rSN5o",
+      description: "Optional - A 30 minute video explaining CSS Grid",
+    },
+    {
+      title: "CSS Tricks: Flexbox",
+      image:
+        "https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s176-c-k-c0x00ffffff-no-rj",
+      link: "https://www.youtube.com/watch?v=JJSoEo8JSnc",
+      description: "Optional - A 30 minute video explaining CSS Flexbox.",
     },
   ];
   const lessonComponents: LessonComponentData[] = [
@@ -137,7 +181,8 @@ export async function getServerSideProps({ params }) {
     },
     {
       component: "description",
-      text: "CSS stands for cascading style sheets. It's another language that you need to learn that has different rules and keywords compared to HTML. In this lesson you will learn about different styles that you can apply to your HTML elements.",
+      text:
+        "CSS stands for cascading style sheets. It's another language that you need to learn that has different rules and keywords compared to HTML. In this lesson you will learn about different styles that you can apply to your HTML elements.",
     },
     {
       component: "resource-list",
