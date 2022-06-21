c6953a7305cf7a2b62650313a298c0c9876a6feb
Raveen Reddy <raveenreddy89@gmail.com>
2022-06-21 12:29:00 -0400

    make challenges collapsable

--- pages/studentPortal/intro/Javascript/sports/instructions.tsx
+++ pages/studentPortal/intro/Javascript/sports/instructions.tsx
@@ -8,6 +8,13 @@ const JS11 = ({ lessonComponents }) => {
 
   const [playerData, setPlayerData] = useState(null);
 
+  const [warmUpChallengeVisible, setWarmUpChallengeVisible] = useState(true);
+  const [challengeOneVisible, setChallengeOneVisible] = useState(false);
+  const [challengeTwoVisible, setChallengeTwoVisible] = useState(false);
+  const [challengeThreeVisible, setChallengeThreeVisible] = useState(false);
+  const [challengeFourVisible, setChallengeFourVisible] = useState(false);
+  const [challengeFiveVisible, setChallengeFiveVisible] = useState(false);
+
   useEffect(() => {
     fetch(playerDataURL)
       .then((res) => {
@@ -29,65 +36,149 @@ const JS11 = ({ lessonComponents }) => {
             The following challenges will test your ability to write javascript
             functions that manipulate JSON data to produce insights on NBA data
           </p>
+          <div className="flex flex-row space-x-4">
+            <h1 className="font-bold">Warmup Challenge</h1>
+            <Button
+              label={warmUpChallengeVisible ? "-" : "+"}
+              size={"small"}
+              backgroundColor={"blue"}
+              textColor={"yellow"}
+              onClick={() =>
+                setWarmUpChallengeVisible((prevValue) => !prevValue)
+              }
+            />
+          </div>
 
-          <h1 className="font-bold">Warmup Challenge</h1>
-          <p>
-            The following dataset contains a list of all NBA players
-            <a className="text-red-500" href={playerDataURL}>
-              {" "}
-              (link)
-            </a>
-          </p>
+          {warmUpChallengeVisible && (
+            <div className="space-y-4">
+              <p>
+                The following dataset contains a list of all NBA players
+                <a className="text-red-500" href={playerDataURL}>
+                  {" "}
+                  (link)
+                </a>
+              </p>
 
-          <p>
-            Using the .map() and .filter() methods, display a list of all
-            players on the Toronto Raptors.
-          </p>
-          <p className="space-y-2">
-            For example, here is a list of a few players from the Golden State
-            Warriors:
-            {playerData
-              .filter((player) => player.Team === "GS")
-              .slice(0, 5)
-              .map((it) => {
-                return (
-                  <p className="ml-5">{it.FirstName + " " + it.LastName}</p>
-                );
-              })}
-          </p>
+              <p>
+                Using the .map() and .filter() methods, display a list of all
+                players on the Toronto Raptors.
+              </p>
+              <p className="space-y-2">
+                For example, here is a list of a few players from the Golden
+                State Warriors:
+                {playerData &&
+                  playerData
+                    .filter((player) => player.Team === "GS")
+                    .slice(0, 5)
+                    .map((it) => {
+                      return (
+                        <p className="ml-5">
+                          {it.FirstName + " " + it.LastName}
+                        </p>
+                      );
+                    })}
+              </p>
+            </div>
+          )}
 
-          <h1 className="font-bold">Challenge 1 </h1>
-          <p>
-            Write a function that returns the player name who was projected to
-            make the most number of free throws.
-          </p>
-          <h1 className="font-bold">Challenge 2 </h1>
-          <p>
-            Write a function that can take a player from this array, analyze
-            their fantasy stats and return a rating from 1 to 100 for that
-            player. You have creativity here on how you want to come up with
-            that rating but base it off their stats.
-          </p>
-          <h1 className="font-bold">Challenge 3 </h1>
-          <p>
-            Write a function that sorts this array based on your rating function
-            and print out the top 10 players. // (hint. use map() to add a new
-            rating property to each item in the array, then sort by that
-            property)
-          </p>
-          <h1 className="font-bold">Challenge 4 </h1>
-          <p>
-            Adapt your rating function to take in a boolean parameter called
-            isFreeThrowPunt your rating function should return a different
-            rating depending on this parameter being true or false (hint. an if
-            statement will be helpful here). Sort the players
-          </p>
-          <h1 className="font-bold">Challenge 5 </h1>
-          <p>
-            Adapt your rating function to support other types of punt categories
-            by changing your boolean parameter to a string parameter (hint. more
-            if statements would be needed to support each type of punt category)
-          </p>
+          <div className="flex flex-row space-x-4">
+            <h1 className="font-bold">Challenge One</h1>
+            <Button
+              label={challengeOneVisible ? "-" : "+"}
+              size={"small"}
+              backgroundColor={"blue"}
+              textColor={"yellow"}
+              onClick={() => setChallengeOneVisible((prevValue) => !prevValue)}
+            />
+          </div>
+
+          {challengeOneVisible && (
+            <p>
+              Write a function that returns the player name who was projected to
+              make the most number of free throws.
+            </p>
+          )}
+
+          <div className="flex flex-row space-x-4">
+            <h1 className="font-bold">Challenge Two</h1>
+            <Button
+              label={challengeTwoVisible ? "-" : "+"}
+              size={"small"}
+              backgroundColor={"blue"}
+              textColor={"yellow"}
+              onClick={() => setChallengeTwoVisible((prevValue) => !prevValue)}
+            />
+          </div>
+
+          {challengeTwoVisible && (
+            <p>
+              Write a function that can take a player from this array, analyze
+              their fantasy stats and return a rating from 1 to 100 for that
+              player. You have creativity here on how you want to come up with
+              that rating but base it off their stats.
+            </p>
+          )}
+
+          <div className="flex flex-row space-x-4">
+            <h1 className="font-bold">Challenge Three</h1>
+            <Button
+              label={challengeThreeVisible ? "-" : "+"}
+              size={"small"}
+              backgroundColor={"blue"}
+              textColor={"yellow"}
+              onClick={() =>
+                setChallengeThreeVisible((prevValue) => !prevValue)
+              }
+            />
+          </div>
+
+          {challengeThreeVisible && (
+            <p>
+              Write a function that sorts this array based on your rating
+              function and print out the top 10 players. // (hint. use map() to
+              add a new rating property to each item in the array, then sort by
+              that property)
+            </p>
+          )}
+
+          <div className="flex flex-row space-x-4">
+            <h1 className="font-bold">Challenge Four</h1>
+            <Button
+              label={challengeFourVisible ? "-" : "+"}
+              size={"small"}
+              backgroundColor={"blue"}
+              textColor={"yellow"}
+              onClick={() => setChallengeFourVisible((prevValue) => !prevValue)}
+            />
+          </div>
+
+          {challengeFourVisible && (
+            <p>
+              Adapt your rating function to take in a boolean parameter called
+              isFreeThrowPunt your rating function should return a different
+              rating depending on this parameter being true or false (hint. an
+              if statement will be helpful here). Sort the players
+            </p>
+          )}
+
+          <div className="flex flex-row space-x-4">
+            <h1 className="font-bold">Challenge Five</h1>
+            <Button
+              label={challengeFiveVisible ? "-" : "+"}
+              size={"small"}
+              backgroundColor={"blue"}
+              textColor={"yellow"}
+              onClick={() => setChallengeFiveVisible((prevValue) => !prevValue)}
+            />
+          </div>
+          {challengeFiveVisible && (
+            <p>
+              Adapt your rating function to support other types of punt
+              categories by changing your boolean parameter to a string
+              parameter (hint. more if statements would be needed to support
+              each type of punt category)
+            </p>
+          )}
         </div>
       </div>
     </>
