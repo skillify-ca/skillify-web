7bdb93205579b5d62a8e7f98cc07960e23d17b35
iamnotreddy <98791180+iamnotreddy@users.noreply.github.com>
2022-06-24 15:27:50 -0400

    create user goals table in DB, render on profile (#707)
    * add FETCH_USER_GOALS gql query
    
    * add types
    
    * add user goals useQuery hook to profile
    
    * replace static goals with DB Response
    
    * add conditional rendering
    
    * remove redundant UserGoalsResponse type
    
    * add loading for profile data
    
    * add goals section component
    
    * remove goals code from parent profile component
    
    * add new user profile component
    
    * add header to profile section child component
    
    * remove old code from parent profile component
    
    * remove other old code
    
    * remove unnecessary map

---
+++ graphql/fetchUserGoals.ts
@@ -0,0 +1,28 @@
+import { gql } from "@apollo/client";
+
+export const FETCH_USER_GOALS = gql`
+  query fetchUserGoals($userId: String = "") {
+    user_goals(where: { userId: { _eq: $userId } }) {
+      createdAt
+      goalName
+      id
+      isActive
+      updatedAt
+      userId
+    }
+  }
+`;
+
+export type FetchUserGoalsDataResponse = {
+  user_goals: Array<UserGoalsData>;
+};
+
+export type UserGoalsData = {
+  __typename: string;
+  createdAt: Date;
+  goalName: string;
+  id: string;
+  isActive: boolean;
+  updatedAt: Date;
+  userId: string;
+};
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,83 +1,23 @@
-import { useQuery } from "@apollo/client";
 import React, { useState } from "react";
-import {
-  FetchUserProfileDataResponse,
-  FETCH_USER_PROFILE_DATA,
-  UserProfileData,
-} from "../graphql/fetchUserProfile";
+
 import { useAuth } from "../lib/authContext";
-import { format } from "date-fns";
+
+import GoalsSection from "../components/coding/GoalsSection";
+import UserProfileSection from "../components/coding/UserProfileSection";
 
 export default function Profile(props) {
   const { user } = useAuth();
 
-  const { loading, error, data } = useQuery<FetchUserProfileDataResponse>(
-    FETCH_USER_PROFILE_DATA,
-    {
-      variables: {
-        userId: user.uid,
-      },
-      onCompleted: (data) => {
-        setUserProfileData({
-          typeName: data.users[0].__typename,
-          createdAt: data.users[0].created_at,
-          email: data.users[0].email,
-          lastSeen: data.users[0].last_seen,
-          name: data.users[0].name,
-          profileImage: data.users[0].profile_image,
-        });
-      },
-    }
-  );
-
-  const [userProfileData, setUserProfileData] =
-    useState<UserProfileData>(Object);
-
   return (
     <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll">
-      <div className="flex items-center justify-between">
-        {userProfileData && (
-          <h1 className="text-3xl font-bold">{userProfileData.name}</h1>
-        )}
-        <div className="w-32 text-center sm:justify-end px-2 py-1 text-gray-400 border-2 border-gray-400 rounded-md cursor-pointer hover:bg-gray-50 hover:border-charmander hover:text-charmander dark:hover:bg-gray-800">
-          Edit
-        </div>
-      </div>
-      {userProfileData && (
-        <div className="grid grid-cols-1 mt-12 sm:grid-cols-8">
-          <img
-            className="w-32 rounded-full"
-            src={userProfileData.profileImage}
-          />
-          <div className="col-span-2 mt-2">
-            <p className="text-lg font-bold"></p>
-            <p className="text-base text-gray-400">{userProfileData.name}</p>
-            <p className="text-base text-gray-400">{userProfileData.email}</p>
-            <div className="mt-4">
-              <img
-                className="inline"
-                src="/images/profile/clock-solid-1.svg"
-              ></img>
-              <span className="ml-2 text-base">
-                {userProfileData.createdAt
-                  ? "Joined " +
-                    format(new Date(userProfileData.createdAt), "MMMM yyyy")
-                  : userProfileData.createdAt}
-              </span>
-            </div>
-          </div>
-        </div>
-      )}
+      <UserProfileSection user={user} />
 
       <h2 className="mt-14 mb-9 font-bold text-lg">Goals</h2>
+
       <div className="grid grid-cols-1 sm:grid-cols-3 mb-16">
-        <div className="text-white bg-murkrow text-center rounded-full mx-5 py-2 mb-5">
-          Get A Tech Job
-        </div>
-        <div className="text-white bg-murkrow text-center rounded-full mx-5 py-2 mb-5">
-          Learn A New Skill
-        </div>
+        <GoalsSection user={user} />
       </div>
+
       <h2 className="font-bold text-lg mb-9">Achievements</h2>
       <div className="grid grid-cols-1 sm:grid-cols-6">
         <div className="text-gray-400 bg-gray-200 rounded-full text-center py-4">
