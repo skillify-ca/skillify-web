4b18afba9192b4cadfe57684838da50fc8af226f
Raveen Reddy <raveenreddy89@gmail.com>
2022-06-24 12:41:54 -0400

    remove old code from parent profile component

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -36,42 +36,7 @@ export default function Profile(props) {
 
   return (
     <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll">
-      <div className="flex items-center justify-between">
-        {userProfileData && (
-          <h1 className="text-3xl font-bold">{userProfileData.name}</h1>
-        )}
-        <div className="w-32 text-center sm:justify-end px-2 py-1 text-gray-400 border-2 border-gray-400 rounded-md cursor-pointer hover:bg-gray-50 hover:border-charmander hover:text-charmander dark:hover:bg-gray-800">
-          Edit
-        </div>
-      </div>
       <UserProfileSection user={user} />
-      {userProfileLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="grid grid-cols-1 mt-12 sm:grid-cols-8">
-          <img
-            className="w-32 rounded-full"
-            src={userProfileData.profileImage}
-          />
-          <div className="col-span-2 mt-2">
-            <p className="text-lg font-bold"></p>
-            <p className="text-base text-gray-400">{userProfileData.name}</p>
-            <p className="text-base text-gray-400">{userProfileData.email}</p>
-            <div className="mt-4">
-              <img
-                className="inline"
-                src="/images/profile/clock-solid-1.svg"
-              ></img>
-              <span className="ml-2 text-base">
-                {userProfileData.createdAt
-                  ? "Joined " +
-                    format(new Date(userProfileData.createdAt), "MMMM yyyy")
-                  : userProfileData.createdAt}
-              </span>
-            </div>
-          </div>
-        </div>
-      )}
 
       <h2 className="mt-14 mb-9 font-bold text-lg">Goals</h2>
 
