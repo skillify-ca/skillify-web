1fec4125760f61aa622bc7e2f0ce64c46646c0ff
Raveen Reddy <raveenreddy89@gmail.com>
2022-06-28 14:20:50 -0400

    move section definitions to goalsSections var and map through to render sections

--- pages/goals.tsx
+++ pages/goals.tsx
@@ -13,6 +13,7 @@ import {
 } from "../graphql/fetchUserGoals";
 import { useQuery } from "@apollo/client";
 import GoalsSectionComponent from "../components/coding/GoalsSectionComponent";
+import { GoalsSectionType } from "./api/goals/goalsHelpers";
 
 export default function Goals(props) {
   const { user } = useAuth();
@@ -33,44 +34,54 @@ export default function Goals(props) {
 
   const tailwindIconSize = "h-5 w-5";
 
+  // create goal sections here, map through below to render
+
+  const goalsSections: GoalsSectionType[] = [
+    {
+      header: {
+        sectionName: "Current Goals",
+        manageIcons: [
+          <ArchiveIcon className={tailwindIconSize} />,
+          <CheckCircleIcon className={tailwindIconSize} />,
+        ],
+      },
+      rows: userGoals.filter((goal) => goal.isActive),
+    },
+    {
+      header: {
+        sectionName: "Completed Goals",
+        manageIcons: [
+          <ArchiveIcon className={tailwindIconSize} />,
+          <TrashIcon className={tailwindIconSize} />,
+        ],
+      },
+      rows: userGoals.filter((goal) => goal.isComplete),
+    },
+    {
+      header: {
+        sectionName: "Archived Goals",
+        manageIcons: [<TrashIcon className={tailwindIconSize} />],
+      },
+      rows: userGoals.filter((goal) => !goal.isComplete && !goal.isActive),
+    },
+  ];
+
   return (
     <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll">
       {userGoalsLoading ? (
         <div>Loading...</div>
       ) : (
         <div>
-          <GoalsSectionComponent
-            // only fetch active goals
-            userGoals={userGoals.filter((goal) => goal.isActive)}
-            header={{
-              sectionName: "Current Goals",
-              manageIcons: [
-                <ArchiveIcon className={tailwindIconSize} />,
-                <CheckCircleIcon className={tailwindIconSize} />,
-              ],
-            }}
-          />
-          <GoalsSectionComponent
-            // only fetch complete goals
-            userGoals={userGoals.filter((goal) => goal.isComplete)}
-            header={{
-              sectionName: "Completed Goals",
-              manageIcons: [
-                <ArchiveIcon className={tailwindIconSize} />,
-                <TrashIcon className={tailwindIconSize} />,
-              ],
-            }}
-          />
-          <GoalsSectionComponent
-            // only fetch archived goals (not complete or not active)
-            userGoals={userGoals.filter(
-              (goal) => !goal.isComplete && !goal.isActive
-            )}
-            header={{
-              sectionName: "Archived Goals",
-              manageIcons: [<TrashIcon className={tailwindIconSize} />],
-            }}
-          />
+          {goalsSections.map((section) => {
+            return (
+              <div>
+                <GoalsSectionComponent
+                  userGoals={section.rows}
+                  header={section.header}
+                />
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
