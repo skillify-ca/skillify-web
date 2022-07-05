be1e1e1f74cc1435f274ffb9b92c2f05fbdc10c3
jasonrajasegaram <jason.rajasegaram19@gmail.com>
2022-07-04 20:31:32 -0400

    Dynamic badges and ordering nodes (#714)
    * Dynamic badges and ordering nodes
    
    * Move data fetching into badge section
    
    * Update user node query
    
    Co-authored-by: Jason Rajasegaram <jrajasegaram@idc.com>
    Co-authored-by: Vithushan Namasivayasivam <vithushan19@gmail.com>

---
+++ components/profile/BadgesSection.tsx
@@ -0,0 +1,64 @@
+import { useQuery } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import {
+  FETCH_CODING_BADGES,
+  IntroCourseUnit,
+} from "../../graphql/coding/userBadges/fetchUserBadges";
+
+export type BadgesSectionProps = {
+  user: any;
+};
+
+export default function BadgesSection({ user }: BadgesSectionProps) {
+  const { data } = useQuery(FETCH_CODING_BADGES, {
+    variables: {
+      userId: user.uid,
+    },
+  });
+
+  const [units, setUnits] = useState<IntroCourseUnit[]>([]);
+  useEffect(() => {
+    if (typeof data !== "undefined") {
+      setUnits(data.intro_course_unit);
+    }
+  }, [data]);
+
+  return (
+    <>
+      {units.map((unit) => {
+        if (unit.coding_badges.length > 0) {
+          return (
+            <div>
+              <div className="grid grid-cols-1 sm:grid-cols-6">
+                <div className="py-4 text-center text-gray-400 bg-gray-200 rounded-full">
+                  {unit.title}
+                </div>
+              </div>
+              <div className="grid grid-cols-1 mb-16 sm:grid-cols-3 mt-7">
+                {unit.coding_badges.map((badge) => (
+                  <div className="flex flex-col items-center justify-center">
+                    <div className="flex items-center justify-center p-8 bg-gray-200 rounded-full w-36 mb-7">
+                      {badge.user_coding_badges.length > 0 ? (
+                        <img
+                          className="w-28"
+                          src="/images/profile/achievement-badge-active.svg"
+                        ></img>
+                      ) : (
+                        <img
+                          className="w-28"
+                          src="/images/profile/achievement-badge.svg"
+                        ></img>
+                      )}
+                    </div>
+                    <p className="text-base">{unit.title}</p>
+                    <p className="mb-8 text-base sm:mb-0">{badge.title}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        }
+      })}
+    </>
+  );
+}
