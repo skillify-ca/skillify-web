e3d609c9f47da5b0296f14b74bc563ee4963d051
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-07-04 20:10:14 -0400

    Move data fetching into badge section

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,41 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import { useAuth } from "../lib/authContext";
-import { useMutation, useQuery } from "@apollo/client";
 
 import ProfileGoalsSection from "../components/coding/ProfileGoalsSection";
 import UserProfileSection from "../components/coding/UserProfileSection";
 import BadgesSection from "../components/profile/BadgesSection";
-import {FETCH_CODING_BADGES, Data, IntroCourseUnit} from "../graphql/coding/userBadges/fetchUserBadges";
 
 export default function Profile(props) {
   const { user } = useAuth();
-  const { data } = useQuery(FETCH_CODING_BADGES, {
-    variables: {
-      userId: user.uid,
-    },
-  });
-
-  const [units, setUnits] = useState<IntroCourseUnit[]>([]);
-  useEffect(() => {
-    if(typeof(data) !== 'undefined')
-    { 
-      setUnits(data.intro_course_unit);
-    }
-  }, [data]);
 
   return (
     <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll">
       <UserProfileSection user={user} />
 
-      <h2 className="mt-14 mb-9 font-bold text-lg">Goals</h2>
+      <h2 className="text-lg font-bold mt-14 mb-9">Goals</h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-3 mb-16">
+      <div className="grid grid-cols-1 mb-16 sm:grid-cols-3">
         <ProfileGoalsSection user={user} />
       </div>
 
-      <h2 className="font-bold text-lg mb-9">Achievements</h2>
-      <BadgesSection units={units} />
+      <h2 className="text-lg font-bold mb-9">Achievements</h2>
+      <BadgesSection user={user} />
     </div>
   );
 }
