4d85a0c2bdc99605a11be48f354ca7f679af4e82
Raveen Reddy <raveenreddy89@gmail.com>
2022-07-06 15:11:40 -0700

    small style and design changes

--- components/coding/GoalsSectionComponent.tsx
+++ components/coding/GoalsSectionComponent.tsx
@@ -17,23 +17,25 @@ export default function GoalsSection({
     <div>
       <h2 className="mt-4 mb-4 font-bold text-lg border-b-2">{sectionName}</h2>
 
-      <div className="grid grid-cols-12 text-center">
-        <p className="col-span-1 font-bold">#</p>
-        <p className="col-span-5 font-bold">Goal Name</p>
-        <p className="col-span-2 font-bold">Date Added</p>
-        <p className="col-span-2 font-bold">Target Completion</p>
-        <p className="col-span-2"></p>
-      </div>
+      {sectionName == "Current Goals" && (
+        <div className="grid grid-cols-12 text-center">
+          <p className="col-start-2 col-span-5 font-bold">Goal Name</p>
+          <p className="col-span-2 font-bold">Date Added</p>
+          <p className="col-span-2 font-bold">Target Completion</p>
+          <p className="col-span-2"></p>
+        </div>
+      )}
+
       {userGoals.map((goal, index) => {
         return (
           <div className="grid grid-cols-12 text-center">
             <p className="col-span-1">{index + 1}</p>
             <p className="col-span-5">{goal.goalName}</p>
             <p className="col-span-2">
-              {format(new Date(goal.createdAt), "MMMM yyyy")}
+              {format(new Date(goal.createdAt), "MM/dd/yyyy")}
             </p>
             <p className="col-span-2">
-              {format(new Date(goal.targetDate), "MMMM yyyy")}
+              {format(new Date(goal.targetDate), "MM/dd/yyyy")}
             </p>
             <div>
               <Link href={"/goals/" + goal.id}>
