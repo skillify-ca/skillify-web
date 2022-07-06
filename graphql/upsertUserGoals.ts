8a07232130075f1a6abae8268a2e4dd775fb5718
iamnotreddy <98791180+iamnotreddy@users.noreply.github.com>
2022-07-06 15:28:58 -0400

    create edit goals form (#715)
    * set up edit form template
    
    * add upsert user goals gql
    
    * add check icon, remove comments
    
    * add conditional icon colors
    
    * add editedGoalValues state var, call setter on input change / click
    
    * adjust layout
    
    * save goal to DB on click
    
    * add __typename workaround
    
    * send back to goals after save
    
    * disable save button until changes are made

---
+++ graphql/upsertUserGoals.ts
@@ -0,0 +1,36 @@
+import { gql } from "@apollo/client";
+
+// updates goal with new values in $objects if existing id is provided; inserts new goal if no ID provided
+// userId in objects must exist in users table; if id is provided, it must exist in goals table
+
+export const UPSERT_USER_GOALS = gql`
+  mutation upsertUserGoals($objects: [user_goals_insert_input!]!) {
+    insert_user_goals(
+      objects: $objects
+      on_conflict: {
+        constraint: user_goals_pkey
+        update_columns: [
+          goalName
+          isArchived
+          isComplete
+          isActive
+          updatedAt
+          targetDate
+        ]
+      }
+    ) {
+      affected_rows
+      returning {
+        id
+        goalName
+        isActive
+        isArchived
+        isComplete
+        targetDate
+        updatedAt
+        userId
+        createdAt
+      }
+    }
+  }
+`;
--- pages/goals/[slug].tsx
+++ pages/goals/[slug].tsx
@@ -1,13 +1,21 @@
 import { useQuery } from "@apollo/client";
+import {
+  ArchiveIcon,
+  CheckCircleIcon,
+  TrashIcon,
+} from "@heroicons/react/solid";
 import { format } from "date-fns";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import { Button } from "../../components/ui/Button";
 import {
   FetchUserGoalsDataResponse,
   FETCH_USER_GOAL_DETAIL,
   UserGoalsData,
 } from "../../graphql/fetchUserGoals";
 import { useAuth } from "../../lib/authContext";
+import { useMutation } from "@apollo/client";
+import { UPSERT_USER_GOALS } from "../../graphql/upsertUserGoals";
 
 const EditGoalsPage = () => {
   const { user } = useAuth();
@@ -15,12 +23,21 @@ const EditGoalsPage = () => {
   const router = useRouter();
   const { slug } = router.query;
 
+  const [editedGoalValues, setEditedGoalValues] = useState<UserGoalsData>();
+
+  const [saveEditedGoals] = useMutation(UPSERT_USER_GOALS, {
+    onCompleted: () => router.push("/goals"),
+  });
+
   const goalDetailResults = useQuery<FetchUserGoalsDataResponse>(
     FETCH_USER_GOAL_DETAIL,
     {
       variables: {
         userId: user.uid,
         id: slug,
+      },
+      onCompleted: (data) => {
+        setEditedGoalValues(data.user_goals[0]);
       },
     }
   );
@@ -33,24 +50,100 @@ const EditGoalsPage = () => {
   return (
     <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll">
       <h1 className="text-3xl font-bold mb-4">Edit Goal</h1>
-      {goalDetail && (
-        <div className="grid grid-cols-12">
-          <p className="col-span-2">Goal Name:</p>
-          <p className="col-span-2">{goalDetail.goalName}</p>
-          <p className="col-start-1 col-span-2">Target Completion:</p>
-          <p className="col-span-2">
-            {goalDetail.targetDate ?? "No Target Date Set"}
-          </p>
-          <p className="col-start-1 col-span-2">Created Date:</p>
-          <p className="col-span-2">
-            {format(new Date(goalDetail.targetDate), "MMMM dd yyyy")}
-          </p>
-          <p className="col-start-1 col-span-2">Is Goal Active?</p>
-          <p className="col-span-2">{goalDetail.isActive ? "Yes" : "No"}</p>
-          <p className="col-start-1 col-span-2">Is Goal Complete?</p>
-          <p className="col-span-2">{goalDetail.isComplete ? "Yes" : "No"}</p>
-          <p className="col-start-1 col-span-2">Is Goal Archived?</p>
-          <p className="col-span-2">{goalDetail.isArchived ? "Yes" : "No"}</p>
+      {goalDetail && editedGoalValues && (
+        <div>
+          <div className="flex flex-col space-y-2">
+            <p className="font-bold">Goal Name</p>
+            <input
+              type="text"
+              className="text-left p-2 border rounded-md shadow-md w-1/2"
+              placeholder={goalDetail.goalName}
+              value={editedGoalValues.goalName}
+              onChange={(e) => {
+                setEditedGoalValues((prevState) => ({
+                  ...prevState,
+                  goalName: e.target.value,
+                }));
+              }}
+            />
+            <p className="font-bold">Created On</p>
+            <input
+              type="text"
+              className="text-left p-2 border rounded-md shadow-md w-1/4"
+              value={format(new Date(goalDetail.createdAt), "MMMM dd yyyy")}
+              disabled
+            />
+            <p className="font-bold">Last Updated</p>
+            <input
+              type="text"
+              className="text-left p-2 border rounded-md shadow-md w-1/4"
+              value={format(new Date(goalDetail.updatedAt), "MMMM dd yyyy")}
+              disabled
+            />
+            <p className="font-bold">Target Completion Date</p>
+            <input
+              type="text"
+              className="text-left p-2 border rounded-md shadow-md w-1/4"
+              value={
+                format(new Date(goalDetail.targetDate), "MMMM dd yyyy") ??
+                "No Date Set"
+              }
+              disabled
+            />
+          </div>
+          <div className="grid grid-cols-12 items-center mt-8">
+            <p className="text-center font-bold">Archive Goal</p>
+            <ArchiveIcon
+              className={
+                editedGoalValues.isArchived
+                  ? "h-10 w-10 text-yellow-600"
+                  : "h-10 w-10"
+              }
+              onClick={() =>
+                setEditedGoalValues((prevState) => ({
+                  ...prevState,
+                  isArchived: !prevState.isArchived,
+                }))
+              }
+            />
+
+            <p className="text-center font-bold">Complete Goal</p>
+            <CheckCircleIcon
+              className={
+                editedGoalValues.isComplete
+                  ? "h-10 w-10 text-green-600"
+                  : "h-10 w-10"
+              }
+              onClick={() =>
+                setEditedGoalValues((prevState) => ({
+                  ...prevState,
+                  isComplete: !prevState.isComplete,
+                }))
+              }
+            />
+            <Button
+              label="Save"
+              disabled={goalDetail === editedGoalValues}
+              onClick={() => {
+                // this is a workaround to remove __typename from the gql response which causes mutation to fail
+                const removeTypeNameForHasura = {
+                  goalName: editedGoalValues.goalName,
+                  userId: editedGoalValues.userId,
+                  id: editedGoalValues.id,
+                  isArchived: editedGoalValues.isArchived,
+                  isComplete: editedGoalValues.isComplete,
+                  targetDate: editedGoalValues.targetDate,
+                  isActive: editedGoalValues.isActive,
+                };
+
+                saveEditedGoals({
+                  variables: {
+                    objects: removeTypeNameForHasura,
+                  },
+                });
+              }}
+            />
+          </div>
         </div>
       )}
     </div>
