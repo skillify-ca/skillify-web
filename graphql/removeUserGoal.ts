4d98834d46c20f55cc5d4d7b06481b9ae188faf5
Raveen Reddy <raveenreddy89@gmail.com>
2022-07-07 14:36:27 -0700

    rename mutation

--- graphql/removeUserGoal.ts
+++ graphql/removeUserGoal.ts
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client";
 
-export const UPSERT_USER_GOALS = gql`
+export const REMOVE_USER_GOAL = gql`
   mutation removeUserGoal($id: uuid) {
     delete_user_goals(where: { id: { _eq: $id } }) {
       affected_rows
