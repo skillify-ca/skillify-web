04012e15adf0329b425b17b509c79ea95673f036
Raveen Reddy <raveenreddy89@gmail.com>
2022-07-19 18:23:56 -0700

    add db response transformer; add child SkillSection to parent

---
+++ pages/skillRatings.tsx
@@ -0,0 +1,52 @@
+import { useQuery } from "@apollo/client";
+import React, { useState } from "react";
+import SkillSection, {
+  RenderUserSkill,
+} from "../components/coding/SkillSection";
+import { Button } from "../components/ui/Button";
+import {
+  FetchUserSkillsRatings,
+  FETCH_USER_SKILLS_RATINGS,
+  UserSkillsRatings,
+} from "../graphql/fetchUserSkillsRatings";
+
+import { useAuth } from "../lib/authContext";
+
+export default function SkillRatings(props) {
+  const { user } = useAuth();
+
+  const { data: userSkillRatings } = useQuery<FetchUserSkillsRatings>(
+    FETCH_USER_SKILLS_RATINGS,
+    {
+      variables: {
+        userId: user.uid,
+      },
+    }
+  );
+
+  const transformSkillRating = (skillRatings: UserSkillsRatings[]) => {
+    const mappedSkillRatings: RenderUserSkill[] = skillRatings.map((row) => {
+      return {
+        sectionName: row.intro_course_skill["intro_course_unit"]["title"],
+        skillName: row.intro_course_skill["name"],
+        skillRating: parseInt(row.studentRating),
+      };
+    });
+
+    return mappedSkillRatings;
+  };
+
+  return (
+    <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll">
+      <div>
+        <Button label="Save" />
+      </div>
+      <p className="text-xl font-bold">Skills</p>
+      <SkillSection
+        skillSection={transformSkillRating(
+          userSkillRatings.intro_course_skills_user
+        )}
+      />
+    </div>
+  );
+}
