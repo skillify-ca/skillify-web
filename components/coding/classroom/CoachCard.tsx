cf2fca5be0082349b805ff325c3bec7dac27be57
vithushan19 <vithushan19@gmail.com>
2022-07-22 14:58:21 -0400

    Update CoachCard.tsx

--- components/coding/classroom/CoachCard.tsx
+++ components/coding/classroom/CoachCard.tsx
@@ -6,18 +6,18 @@ type CoachCardProps = {
 };
 export default function CoachCard({ coach }: CoachCardProps) {
   return (
-    <div className="grid grid-cols-1 p-4 bg-white shadow-lg lg:grid-cols-7 dark:bg-gray-900">
-      <img src={coach.user.profile_image} className="w-24 rounded-full" />
-      <div className="flex flex-col items-end justify-center col-span-1 ml-4 sm:items-start">
-        <p className="text-lg font-bold">{coach.user.name}</p>
+    <div className="grid grid-cols-1 gap-4 p-4 bg-white shadow-lg dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center">
+        <img src={coach.user.profile_image} className="w-24 rounded-full" />
+        <p className="text-lg font-bold text-center">{coach.user.name}</p>
       </div>
       <div className="flex items-center col-span-3">
-        <p className="">
+        <p className="w-full text-center">
           <span className="mr-2 font-bold">I can help with:</span>
           {coach.competencies}
         </p>
       </div>
-      <div className="flex items-center justify-end">
+      <div className="flex items-center justify-center ">
         <a href={coach.link} target="_blank">
           <Button label="Book Time" />
         </a>
