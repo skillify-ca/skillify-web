878f68fa3086d1ef1f4159a922b905fcd90ef323
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-08-23 18:11:29 +0300

    starting sales tax problem

---
+++ components/finance/SalesTaxQuestion.tsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { GuessData } from "../../pages/api/guessData";
+import { name } from "../../pages/api/names";
+import { Button } from "../ui/Button";
+
+export interface SalesTaxQuestionProps {
+  personName: string;
+  number: number;
+  taxRate: number;
+  submitGuess: (guess: GuessData) => void;
+  oneFruit: string;
+  multipleFruit: string;
+  image2: string;
+  answer: string;
+  text: string;
+}
+
+const SalesTaxQuestion: React.FC<SalesTaxQuestionProps> = ({
+  personName,
+  number,
+  taxRate,
+  submitGuess,
+  image2,
+  oneFruit,
+  multipleFruit,
+  answer,
+  text,
+}) => {
+  onSubmit = (guess: string) => {
+    submitGuess({
+      guess: guess.toString(),
+      isCorrect: guess === answer,
+    }),
+      setGuess("");
+  };
+  const [guess, setGuess] = useState("");
+  useEffect(() => {
+    (document.getElementById("input") as HTMLInputElement).value = "";
+  }, []);
+  return (
+    <div>
+      <div className="">
+        <p>
+          Find the sales tax on a piece of fruit by rounding to two decimal
+          places. <span className="italic">after</span>
+          you make your final calculations.
+        </p>
+        {""}
+        <p className="pl-10">
+          {personName} intends to purchase {number}{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
--- pages/api/labs/finance/questionGenerator.ts
+++ pages/api/labs/finance/questionGenerator.ts
@@ -12,7 +12,7 @@ export const generateQuestionForFinanceSkill = (skill: Skill): Question => {
     case Skill.FINANCE_BUDGET:
       return getRandomFinanceQuestion();
     case Skill.FINANCE_UNIT_PRICES:
-      const noun1 = getRandomItemFromMap(fruitsMap);
+      let noun1 = getRandomItemFromMap(fruitsMap);
       let randomTotal = randomize(11, 100);
       let randomNumberOfObjects = randomize(1, 10);
       let name = nameSelector();
@@ -32,5 +32,24 @@ export const generateQuestionForFinanceSkill = (skill: Skill): Question => {
           image: image,
         },
       };
+      case Skill.FINANCE_SALES_TAX_PROBLEM:
+        let noun2 = getRandomItemFromMap(fruitsMap);
+        let randomNumber = randomize(1,20);
+        let taxRate = Math.floor(Math.random() * 19) + 1;
+        let personName = nameSelector();
+        let oneFruit = noun2.singleTitle;
+        let multipleFruit = noun2.pluralTitle;
+        let image2 = noun2.image;
+        return {
+          questionType: QuestionType.FINANCE_SALES_TAX_PROBLEM,
+          answer: (randomNumber*taxRate).toString(),
+          text: "",
+          salesTaxModel: {
+            personName: personName,
+            oneFruit: oneFruit,
+            multipleFruit: multipleFruit,
+            image: image,
+          },
+        };
   }
 };
