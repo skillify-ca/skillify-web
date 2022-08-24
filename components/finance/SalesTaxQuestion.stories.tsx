3fbaaa50a34bee66a7ef39eb9151552f79903353
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-08-24 19:58:27 +0300

    changed arguments

--- components/finance/SalesTaxQuestion.stories.tsx
+++ components/finance/SalesTaxQuestion.stories.tsx
@@ -3,14 +3,18 @@ import { Story, Meta } from "@storybook/react";
 import SalesTaxQuestion, { SalesTaxQuestionProps } from "./TipQuestion";
 import { Question } from "../../../pages/api/question";
 import { QuestionType } from "../../../pages/api/questionTypes";
+import { generateKeyPair } from "crypto";
+import { monthsToYears } from "date-fns";
 
 export default {
   title: "finance/Sales Tax",
-  component: SalesTax,
+  component: SalesTaxQuestion,
   argTypes: {},
 } as Meta;
 
-const Template: Story<SalesTaxQuestionProps> = (args) => <SalesTaxQuestion {...args} />;
+const Template: Story<SalesTaxQuestionProps> = (args) => (
+  <SalesTaxQuestion {...args} />
+);
 
 export const Primary = Template.bind({});
 const question: Question = {
@@ -19,8 +23,11 @@ const question: Question = {
   questionType: QuestionType.FINANCE_TIP_PROBLEM,
 };
 Primary.args = {
-  price: 1,
-  numberOfToys: 10
-  taxRate: .15 
-  question,
+  price: 1.0,
+  numberOfToys: 10,
+  taxRate: 15,
+  question: text,
+  personName: "Kari",
+  multipleAnimals: "goat toys",
+  image: "/images/goat.jpg",
 };
