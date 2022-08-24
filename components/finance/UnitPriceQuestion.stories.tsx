f6cbfa6986c6a7f10fdfc3a92eb8774e281e8a65
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-08-24 17:17:23 +0300

    started storybook for unit price question
    Now it does not show up again. not sure why.

--- components/finance/UnitPriceQuestion.stories.tsx
+++ components/finance/UnitPriceQuestion.stories.tsx
@@ -1,37 +1,32 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react";
 
-import { UnitPriceModel } from "../../pages/api/question";
-import { UnitPriceQuestion };
-import { UnitPriceQuestionProps } from "./UnitPriceQuestion";
-import { Question } from "../../../pages/api/question";
-import { QuestionType } from "../../../pages/api/questionTypes";
-
-import { LongDivision } from "../questionTypes/LongDivision";
+import UnitPriceQuestion, { UnitPriceQuestionProps } from "./UnitPriceQuestion";
+import { QuestionType } from "../../pages/api/questionTypes";
+import { Question } from "../../pages/api/question";
 
 export default {
-  title: "math/Unit Price",
-  component: <UnitPriceQuestion>,
+  title: "finance/Unit Price",
+  component: UnitPriceQuestion,
   argTypes: {},
 } as Meta;
 
-const Template: Story<UnitPriceQuestion> = (args) => <UnitPriceQuestion {...args} />;
+const Template: Story<UnitPriceQuestionProps> = (args) => (
+  <UnitPriceQuestion {...args} />
+);
 
 export const Primary = Template.bind({});
 const question: Question = {
   text: "Question",
-  answer: "1000/10",
+  answer: "100",
   questionType: QuestionType.FINANCE_UNIT_PRICE_PROBLEM,
 };
 Primary.args = {
-  unitPriceModel: {
-    total: 1000,
-    numberOfObjects: 10,
-    name: "George",
-    singularFruit: "banana",
-    pluralFruit: "bananas",
-    image: picture of banana
-  
-  tip: 20,
+  total: 1000,
+  numberOfObjects: 10,
+  image,
+  name,
+  singularFruit,
+  pluralFruit,
   question,
-};;
+};
