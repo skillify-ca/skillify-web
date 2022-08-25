27009912c8abe98b69bcc949a6a0be8774162c10
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-08-25 21:41:10 +0300

    created second player component
    added second player component to change squares to blue for the second player

--- components/math/MultiplicationBlock.tsx
+++ components/math/MultiplicationBlock.tsx
@@ -4,22 +4,26 @@ export interface MultiplicationBlockProps {
   text: string;
   selected: boolean;
   onClick: boolean;
+  player: boolean;
 }
 
 export const MultiplicationBlock: React.FC<MultiplicationBlockProps> = ({
   text,
   onClick,
+  player,
   ...props
 }) => {
   const [selected, setSelected] = useState(false);
+
   const handleSelected = () => {
     setSelected(!selected);
   };
+
   return (
     <div>
       <ul
         className={`w-20 h-20 flex justify-center items-center border-2 text-green-50 float-left
-        ${!selected ? "bg-green-400" : "bg-red-400"}`}
+        ${!selected ? "bg-green-400" : "bg-red-400"} `}
         onClick={handleSelected}
       >
         <p>{text}</p>
