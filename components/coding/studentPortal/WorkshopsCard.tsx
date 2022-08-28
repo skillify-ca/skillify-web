664098eb8236de5f2af036e272134651e10f3415
Sukeesan <sukeesank@gmail.com>
2022-08-28 12:42:17 -0400

    defined type for props, cleaned up code by creating an array of WorkshopsCardProps and using map to render

--- components/coding/studentPortal/WorkshopsCard.tsx
+++ components/coding/studentPortal/WorkshopsCard.tsx
@@ -1,17 +1,23 @@
 import { Button } from "../../ui/Button";
 
-// type WorkshopsCardProps = {
-//   title: string;
-// };
+type WorkshopsCardProps = {
+  title: string;
+  video: string;
+  starterFile?: string;
+};
 
-export default function WorkshopsCard(props) {
+export default function WorkshopsCard({
+  title,
+  video,
+  starterFile,
+}: WorkshopsCardProps) {
   return (
     <div className="bg-white p-6 rounded-md shadow-md">
-      <h2 className="font-bold">{props.title}</h2>
-      <video src={props.video} controls={true} />
-      <a href={props.starterFile}>
+      <h2 className="font-bold">{title}</h2>
+      <video src={video} controls={true} />
+      <a href={starterFile}>
         <div className="mt-4">
-          {props.starterFile ? <Button label={"Starter Files"} /> : null}
+          {starterFile ? <Button label={"Starter Files"} /> : null}
         </div>
       </a>
     </div>
--- pages/workshops.tsx
+++ pages/workshops.tsx
@@ -13,37 +13,47 @@ import { Button } from "../components/ui/Button";
 import { useRouter } from "next/router";
 import WorkshopsCard from "../components/coding/studentPortal/WorkshopsCard";
 
-export default function Workshops(props) {
-  const { user } = useAuth();
-  const router = useRouter();
-
+const WorkshopsPage = () => {
+  const workshopsData = [
+    {
+      title: "Course HTML/CSS Page",
+      video:
+        "https://d3jppm0n5ndqu2.cloudfront.net/header-section-workshop.mp4",
+      starterFile:
+        "https://d3jppm0n5ndqu2.cloudfront.net/course-workshop-files.zip",
+    },
+    {
+      title: "War Game: JS Basics - August 8, 2022",
+      video: "https://d3jppm0n5ndqu2.cloudfront.net/war-workshop.mp4",
+    },
+    {
+      title: "Pokedex - July 20, 2022",
+      video: "https://d3jppm0n5ndqu2.cloudfront.net/pokedex-workshop-final.mp4",
+    },
+    {
+      title: "Pokedex - July 21, 2022",
+      video: "https://d3jppm0n5ndqu2.cloudfront.net/pokedex-july-21.mov",
+    },
+  ];
   return (
     <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll">
       <div>
         <div className="mb-8 text-3xl">Workshops</div>
 
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
-          <WorkshopsCard
-            title={"Course HTML/CSS Page"}
-            video="https://d3jppm0n5ndqu2.cloudfront.net/header-section-workshop.mp4"
-            starterFile="https://d3jppm0n5ndqu2.cloudfront.net/course-workshop-files.zip"
-          />
-          <WorkshopsCard
-            title={"War Game: JS Basics - August 8, 2022"}
-            video="https://d3jppm0n5ndqu2.cloudfront.net/war-workshop.mp4"
-          />
-          <WorkshopsCard
-            title={"Pokedex - July 20, 2022"}
-            video="https://d3jppm0n5ndqu2.cloudfront.net/pokedex-workshop-final.mp4"
-          />
-          <WorkshopsCard
-            title={"Pokedex - July 21, 2022"}
-            video="https://d3jppm0n5ndqu2.cloudfront.net/pokedex-july-21.mov"
-          />
+          {workshopsData.map((it) => (
+            <div>
+              <WorkshopsCard
+                title={it.title}
+                video={it.video}
+                starterFile={it.starterFile}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
-Workshops.auth = true;
+export default WorkshopsPage;
