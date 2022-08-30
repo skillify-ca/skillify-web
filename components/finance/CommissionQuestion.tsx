bad42d26b92004e73ed229fbe143dafb7c470fb7
kavanparam <kavan.param@gmail.com>
2022-08-30 14:43:28 -0400

    Update CommissionQuestion.tsx
    - reused components used in similar questions
    - working UI buttons

--- components/finance/CommissionQuestion.tsx
+++ components/finance/CommissionQuestion.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { GuessData } from "../../pages/api/guessData";
 // import { name } from "../../pages/api/names";
+import { Input } from "../ui/Input";
 import { Button } from "../ui/Button";
 
 export interface CommissionQuestionProps{
@@ -25,18 +26,25 @@ const CommissionQuestion: React.FC<CommissionQuestionProps> = ({
   text,
 }) => {
 
-  // const onSubmit = (guess: string) => {
-  //   submitGuess({
-  //     guess: guess.toString(),
-  //     isCorrect: guess === answer,
-  //   }),
-  //     setGuess("");
-  // };
+  // Fix & understand
+  const onSubmit = () => {
+    submitGuess({
+      guess: guess.toString(),
+      isCorrect: guess === answer,
+    }),
+      setGuess("");
+  };
 
-  // const [guess, setGuess] = useState("");
-  // useEffect(() => {
-  //   (document.getElementById("input") as HTMLInputElement).value = "";
-  // }, []);
+  // Fix & understand
+  const [guess, setGuess] = useState("");
+  
+
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.charCode === 13) {
+      onSubmit();
+    }
+  };
 
   personName = "Jerry";
   commission = 12;
@@ -45,11 +53,36 @@ const CommissionQuestion: React.FC<CommissionQuestionProps> = ({
 
 
   return(
-    <div>
+    <div className="flex flex-col items-center gap-4">
       <p>{personName} earns {commission}% interest for every contract they sell. 
       If they sell a house for ${price}, how much in commission did they earn?</p>
 
+{/*       <input
+          id="input"
+          // type="number"
+          value={guess}
+          className=
+          // "w-20 font-bold text-right border-2 border-gray-300"
+          // "flex flex-wrap justify-center w-full text-2xl"
+          "w-full py-4 text-center bg-white border rounded-md shadow-lg appearance-none sm:w-64 focus:outline-none focus:ring-indigo-500 focus:z-10 sm:text-sm"
+          onChange={(e) => setGuess(e.target.value)}
+      ></input> */}
+
+      <div className="flex justify-center w-full">
+        <Input
+          value={guess}
+          setValue={setGuess}
+          handleKeypress={handleKeypress}
+        />
+      </div>
       
+      <Button
+          onClick={onSubmit}
+          label="Submit"
+          backgroundColor="blue"
+          textColor="white"
+        />
+
     </div>
   );
 };
