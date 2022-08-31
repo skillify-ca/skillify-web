d95b520f63cbf7ee722039ce3785de99c9496ed6
kavanparam <kavan.param@gmail.com>
2022-08-31 15:27:09 -0400

    Added link to math1 page
    - and welcome message inside main component

---
+++ pages/studentPortal/labs/multiplication-connect/Index.tsx
@@ -0,0 +1,13 @@
+// http://localhost:3000/studentPortal/labs/multiplication-connect/Index
+
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <h1 className='text-3xl text-center'>Welcome to Multiplication Connect Four</h1>
+    </div>
+  )
+}
+
+export default index;
