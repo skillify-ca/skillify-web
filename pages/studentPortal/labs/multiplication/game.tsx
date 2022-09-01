a80038f85fde672debde41325727dd1faff8b920
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-09-01 23:51:52 +0300

    create a random number generator
    working to create a function that will map to the text key of the object gameState that will generate a pair of strings that will be "x*y" and its product.  Those will be the values showing on the blocks.

--- pages/studentPortal/labs/multiplication/game.tsx
+++ pages/studentPortal/labs/multiplication/game.tsx
@@ -1,13 +1,12 @@
+import { shuffle } from "lodash";
 import React, { useState } from "react";
 import CalculateWinner from "../../../../components/math/longestStreak/CalculateWinner";
 import MultiplicationBlock, {
   BlockState,
 } from "../../../../components/math/longestStreak/MultiplicationBlock";
-import MultiplicationBlockSetRules, {
-  SetRules,
-} from "../../../../components/math/longestStreak/SetRules";
-import StatementRow from "../../../../components/math/stories/StatementRow";
+import SetRules from "../../../../components/math/longestStreak/SetRules";
 import { Button } from "../../../../components/ui/Button";
+import { getRndInteger } from "../../../api/random";
 
 export enum STAGE {
   SET_RULES,
@@ -69,203 +68,233 @@ export function showEndGameImage(array: GameBlockState[]) {
 export const [stage, setStage] = useState(STAGE.SET_RULES);
 export const [isPlayerOneActive, setPlayerOneActive] = useState(false);
 
-export function handleReset() {
-  setGameState(initialGameState);
-}
+
 
 export function handlePlayer() {
   setPlayerOneActive(!isPlayerOneActive);
 }
 
-export const initialGameState: GameBlockState[] = [
-  {
-    text: "72",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x9",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x1",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "16",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "2x8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "72",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x9",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x1",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "16",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "2x8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "72",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x9",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x1",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "16",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "2x8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "72",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x9",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x1",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "72",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x9",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x1",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "16",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "2x8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "72",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x9",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x1",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "72",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x9",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x1",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "16",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "2x8",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "72",
-    state: BlockState.NOT_SELECTED,
-  },
-  {
-    text: "8x9",
-    state: BlockState.NOT_SELECTED,
-  },
-];
 
-export const [gameState, setGameState] =
-  useState<GameBlockState[]>(initialGameState);
-
-export function handleSelect(index) {
-  console.log("BLOCK WAS CLICKED: index ", index);
-  console.log(gameState[index].text);
-
-  let gameState2 = [...gameState];
-  if (isPlayerOneActive === true) {
-    gameState2[index].state = BlockState.PLAYER_ONE_SELECTED;
-  } else if (isPlayerOneActive === false) {
-    gameState2[index].state = BlockState.PLAYER_TWO_SELECTED;
-  }
-
-  setGameState(gameState2);
-}
-
-export function handlePlayGame() {
-  setStage(STAGE.PLAY_GAME);
-}
-
-export function handleCalculateWinner() {
-  setStage(STAGE.CALCULATE_WINNER);
-}
 
 export default function BlockComponentGallery() {
+
+  const initialGameState: GameBlockState[] = [
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+    {
+      text: "",
+      state: BlockState.NOT_SELECTED,
+    },
+  ];
+  
+  function randomNumberProductList(
+    min,
+    max,
+  ) {
+    let x = getRndInteger(1, 9);
+    let y = getRndInteger(1, 9);
+    let z = x * y
+    let product = `x " x " y`
+    let randomList=[]
+    for (let i=0; i<=20; i++) {
+      randomList.push(z.toString())
+    }
+    for (let i=0; i<=20; i++) {
+      randomList.push(product)
+    }
+    //shuffle list
+    let shuffledList=shuffle(randomList)
+  
+    //map array to the gameState.text
+    let answer = [shuffledList].reduce((gameState.text, v) => ({ gameState.text, [v]: v}), {})
+    return answer  
+    
+  }
+  
+  
+  export function handleReset() {
+    setGameState(initialGameState);
+  }
+  
+  export const [gameState, setGameState] =
+    useState<GameBlockState[]>(initialGameState);
+  
+  export function handleSelect(index) {
+    console.log("BLOCK WAS CLICKED: index ", index);
+    console.log(gameState[index].text);
+  
+    let gameState2 = [...gameState];
+    if (isPlayerOneActive === true) {
+      gameState2[index].state = BlockState.PLAYER_ONE_SELECTED;
+    } else if (isPlayerOneActive === false) {
+      gameState2[index].state = BlockState.PLAYER_TWO_SELECTED;
+    }
+  
+    setGameState(gameState2);
+  }
+  
+  export function handlePlayGame() {
+    setStage(STAGE.PLAY_GAME);
+  }
+  
+  export function handleCalculateWinner() {
+    setStage(STAGE.CALCULATE_WINNER);
+  }
+
   return (
     <div>
       {stage === STAGE.SET_RULES ? (
@@ -326,3 +355,7 @@ export default function BlockComponentGallery() {
     </div>
   );
 }
+function initialGameState(initialGameState: any) {
+  throw new Error("Function not implemented.");
+}
+
