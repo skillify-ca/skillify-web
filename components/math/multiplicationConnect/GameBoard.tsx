60f3e1007f382d3e592a5c40572008a12f2cefe2
kavanparam <kavan.param@gmail.com>
2022-09-01 01:58:08 -0400

    Added GameBoard grid

--- components/math/multiplicationConnect/GameBoard.tsx
+++ components/math/multiplicationConnect/GameBoard.tsx
@@ -2,8 +2,48 @@ import React from 'react'
 
 const GameBoard = () => {
   return (
-    <div className='grid'>GameBoard</div>
-  )
+    <div className="px-10">
+      <h2 className='text-xl pb-4'>GameBoard</h2>
+      <div className="grid grid-cols-5">
+        <div className='flex justify-center border-2 h-28'>1</div>
+        <div className='flex justify-center border-2 h-28'>2</div>
+        <div className='flex justify-center border-2 h-28'>3</div>
+        <div className='flex justify-center border-2 h-28'>4</div>
+        <div className='flex justify-center border-2 h-28'>5</div>
+        <div className='flex justify-center border-2 h-28'>2</div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'>3</div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'>4</div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'>5</div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'>6</div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'>7</div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+        <div className='flex justify-center border-2 h-28'></div>
+
+      </div>
+    </div>
+  );
 }
 
 export default GameBoard
