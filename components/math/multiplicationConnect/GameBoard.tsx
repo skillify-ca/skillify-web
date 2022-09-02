27a18871bcbbc30e96b322c5e938b0346a7009d9
kavanparam <kavan.param@gmail.com>
2022-09-02 13:51:42 -0400

    minor UI

--- components/math/multiplicationConnect/GameBoard.tsx
+++ components/math/multiplicationConnect/GameBoard.tsx
@@ -18,9 +18,9 @@ const GameBoard = () => {
     return (
         <div className="px-28">
             <h2 className='text-xl pb-4'>GameBoard</h2>            
-            <div className="grid grid-cols-5 border-t-2 border-l-2 border-t-blue-600 border-l-blue-600 bg-blue-900 text-white">
-                {grid.map(i => <div className='flex justify-center items-center border-r-2 border-b-2 h-28 
-                        border-b-blue-600 border-r-blue-600 bg-blue-900' key={i.id}>{i.gridNumber}</div>)}
+            <div className="grid grid-cols-5 border-t-2 border-l-2 border-t-[#149ECA] border-l-[#149ECA] text-white">
+                {grid.map(i => <div className='flex justify-center items-center border-r-2 border-b-2 h-28 border-b-[#149ECA] border-r-[#149ECA] bg-blue-900' 
+                    key={i.id}>{i.gridNumber}</div>)}
             </div>
         </div>
     );
