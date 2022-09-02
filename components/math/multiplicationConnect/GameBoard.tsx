7862a7dd219cebffe56abb26f16fadd60c9c9fe7
kavanparam <kavan.param@gmail.com>
2022-09-02 01:43:43 -0400

    Using useState hook to initialize grid

--- components/math/multiplicationConnect/GameBoard.tsx
+++ components/math/multiplicationConnect/GameBoard.tsx
@@ -13,13 +13,13 @@ const createGrid = () => {
 
 
 const GameBoard = () => {
-    const [grid, setGrid] = useState([]);
+    const [grid, setGrid] = useState(createGrid);
 
     return (
         <div className="px-28">
             <h2 className='text-xl pb-4'>GameBoard</h2>            
             <div className="grid grid-cols-5 border-t-2 border-l-2 border-t-blue-600 border-l-blue-600 bg-blue-900 text-white">
-                {createGrid().map(i => <div className='flex justify-center items-center border-r-2 border-b-2 h-28 
+                {grid.map(i => <div className='flex justify-center items-center border-r-2 border-b-2 h-28 
                         border-b-blue-600 border-r-blue-600 bg-blue-900' key={i.id}>{i.gridNumber}</div>)}
             </div>
         </div>
