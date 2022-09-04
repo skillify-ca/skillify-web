610cca7a6f180df6895375fc3ada7b8c5546f98e
kavanparam <kavan.param@gmail.com>
2022-09-04 16:56:49 -0400

    Using an array with possible values for random grid numbers
    For better performance versus old method

--- components/math/multiplicationConnect/GameBoard.tsx
+++ components/math/multiplicationConnect/GameBoard.tsx
@@ -1,27 +1,30 @@
 import React, { useState } from 'react'
-import { getRndInteger } from '../../../pages/api/random';
+import { getRandomItemFromArray, getRndInteger } from '../../../pages/api/random';
 
 const createGrid = () => {
     let arr = [];
-    do{
-        let gridNumber = getRndInteger(4, 25);
-        (gridNumber%2===0) ? (
-            arr.push({
-                id: arr.length,
-                gridNumber: gridNumber,
-            })
-        ) : "" ;
-    }while(arr.length<35)
+    let grid = [];
 
-    return arr;
+    for(let i=4; i<25; i++){
+        (i%2===0) ? arr.push(i) : "";
+    }
+
+    for(let i=0; i<35; i++){
+        let gridNumber = getRandomItemFromArray(arr);
+        grid.push({
+            id: i,
+            gridNumber: gridNumber,
+        });
+    }
+
+    return grid;
 }; 
-
 
 const GameBoard = () => {
     const [grid, setGrid] = useState(createGrid);
 
     return (
-        <div className="px-28">
+        <div className="px-28 pb-8">
             <h2 className='text-xl pb-4'>GameBoard</h2>            
             <div className="grid grid-cols-5 border-t-2 border-l-2 border-t-[#149ECA] border-l-[#149ECA] text-white text-2xl">
                 {grid.map(i => <div className='flex justify-center items-center border-r-2 border-b-2 h-28 border-b-[#149ECA] border-r-[#149ECA] bg-blue-900' 
