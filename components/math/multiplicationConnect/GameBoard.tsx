b62c7abc5381b7b7a68d0491d4ede48a948eb4f8
kavanparam <42287924+kavanparam@users.noreply.github.com>
2022-09-04 17:17:00 -0400

    Merging completed work from branches (#758)
    - GameBoard now populated with random numbers according to game mechanics
    - Dice now rolling two random numbers
    - UI font size changes for readability

--- components/math/multiplicationConnect/GameBoard.tsx
+++ components/math/multiplicationConnect/GameBoard.tsx
@@ -1,28 +1,35 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { getRandomItemFromArray, getRndInteger } from '../../../pages/api/random';
 
 const createGrid = () => {
     let arr = [];
-    for(let i=1; i<36; i++){
-        arr.push({
-            key: i,
-            gridNumber: i,  //**To be randomized according to GameBoard setup
-        })
+    let grid = [];
+
+    for(let i=4; i<25; i++){
+        (i%2===0) ? arr.push(i) : "";
     }
-    const gridItems = arr.map(i => 
-        <div className='flex justify-center items-center border-r-2 border-b-2 h-28 border-b-blue-600 border-r-blue-600 bg-blue-900'>{i.gridNumber}</div>
-    );
-    
-    console.log(gridItems);
-    return <div className="grid grid-cols-5 border-t-2 border-l-2 border-t-blue-600 border-l-blue-600 bg-blue-900 text-white">{gridItems}</div>;
+
+    for(let i=0; i<35; i++){
+        let gridNumber = getRandomItemFromArray(arr);
+        grid.push({
+            id: i,
+            gridNumber: gridNumber,
+        });
+    }
+
+    return grid;
 }; 
 
-
 const GameBoard = () => {
+    const [grid, setGrid] = useState(createGrid);
+
     return (
-        <div className="px-28">
-            <h2 className='text-xl pb-4'>GameBoard</h2>
-            {createGrid()}
+        <div className="px-28 pb-8">
+            <h2 className='text-xl pb-4'>GameBoard</h2>            
+            <div className="grid grid-cols-5 border-t-2 border-l-2 border-t-[#149ECA] border-l-[#149ECA] text-white text-2xl">
+                {grid.map(i => <div className='flex justify-center items-center border-r-2 border-b-2 h-28 border-b-[#149ECA] border-r-[#149ECA] bg-blue-900' 
+                    key={i.id}>{i.gridNumber}</div>)}
+            </div>
         </div>
     );
 }
