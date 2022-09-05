914fcc0cb59c099416978673dd029b05d3c00db7
kavanparam <kavan.param@gmail.com>
2022-09-04 21:29:57 -0400

    GameBoard processes block clicks
    - currently just logged to console
    - added hover UI and pointer to indicate that block is selectable
    - added some more UI throughout App

--- components/math/multiplicationConnect/GameBoardBlock.tsx
+++ components/math/multiplicationConnect/GameBoardBlock.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const GameBoardBlock = ({block}) => {
+const GameBoardBlock = ({block, onClick}) => {
   return (
-    <>
-        <p>{block.gridNumber}</p>
-    </>
-  )
+    <div
+      className="flex justify-center items-center h-full w-full cursor-pointer rounded-full hover:bg-[#F20000]/50 transition-all"
+      onClick={onClick}
+    >
+      <p>{block.gridNumber}</p>
+    </div>
+  );
 }
 
 export default GameBoardBlock
--- components/math/multiplicationConnect/PlayerSection.tsx
+++ components/math/multiplicationConnect/PlayerSection.tsx
@@ -4,13 +4,19 @@ import { Input } from '../../ui/Input'
 const PlayerSection = () => {
   return (
     <div>
-        <div className="flex justify-center items-center gap-24">
-            <input className='bg-inherit placeholder:text-inherit max-w-[200px] h-12' placeholder='Player 1'></input>
-            <input className='bg-inherit placeholder:text-inherit max-w-[200px] h-12' placeholder='Player 2 (*WIP)'></input>
-            <p className='text-md align-middle'>👈🏼 Click to rename</p>
-        </div>
+      <div className="flex justify-center items-center gap-24">
+        <input
+          className="bg-inherit placeholder:text-inherit max-w-[200px] h-12 text-center cursor-pointer rounded-xl enabled:bg-[#F20000]/40"
+          placeholder="Player 1"
+        ></input>
+        <input
+          className="bg-inherit placeholder:text-inherit max-w-[200px] h-12 text-center cursor-pointer rounded-xl enabled:bg-[#FFDB00]/40"
+          placeholder="Player 2 (*WIP)"
+        ></input>
+        <p className="text-md align-middle">👈🏼 Click to rename</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default PlayerSection
