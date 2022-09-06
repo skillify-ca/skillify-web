0bd6cb76aab537eca507325826ec76fd8a071580
vithushan19 <vithushan19@gmail.com>
2022-09-06 16:36:56 -0400

    Create strand

--- components/math/practiceTracker/unitOverview/PracticePreview.tsx
+++ components/math/practiceTracker/unitOverview/PracticePreview.tsx
@@ -23,58 +23,68 @@ const PracticePreview = ({ loading, userSkills, skills, courseId }) => {
   const units = courseId === "finance" ? financeUnits : mathUnits;
 
   return (
-    <div className="flex justify-center">
-      <div className="grid items-stretch max-w-4xl grid-cols-1 bg-white shadow-lg rounded-t-xl">
-        <div className="p-4 space-y-8 sm:p-8">
-          <div className="flex flex-col space-y-4">
-            <p className="text-4xl font-bold capitalize text-murkrow">
-              {" "}
-              PRACTICE TIME
-            </p>
-            <p className="text-murkrow">
-              Select a skill to practice questions. You can practice as many
-              times as you wish. At the end, you'll be asked to rate your skill
-              confidence.
-            </p>
+    <>
+      {units.map((unit) => {
+        return (
+          <div>
+            {skills.filter((skill) => skill.unit.title === unit.title).length >
+            0 ? (
+              <>
+                <div className="flex items-center w-full p-8 font-bold text-white shadow-lg bg-slate-500">
+                  <img
+                    className="w-24 mr-8 hover:animate-shake"
+                    src={unit.image}
+                  />
+                  <p>{unit.title}</p>
+                </div>
+                <SkillsTable
+                  skills={skills}
+                  unit={unit}
+                  loading={loading}
+                  userSkills={userSkills}
+                  courseId={courseId}
+                />
+              </>
+            ) : null}
           </div>
-        </div>
-
-        {units.map((unit) => {
-          return (
-            <div>
-              {skills.filter((skill) => skill.unit.title === unit.title)
-                .length > 0 ? (
-                <>
-                  <div className="flex items-center w-full p-8 font-bold text-white shadow-lg bg-slate-500">
-                    <img
-                      className="w-24 mr-8 hover:animate-shake"
-                      src={unit.image}
-                    />
-                    <p>{unit.title}</p>
-                  </div>
-                  <div className="grid grid-cols-1 p-8 sm:grid-cols-4">
-                    {skills
-                      .filter((skill) => skill.unit.title === unit.title)
-                      .sort((a, b) => a.unit.level - b.unit.level)
-                      .map((skill) => (
-                        <div className="p-2">
-                          <SkillCard
-                            loading={loading}
-                            userSkillData={userSkills}
-                            skill={skill}
-                            courseId={courseId}
-                          />
-                        </div>
-                      ))}
-                  </div>
-                </>
-              ) : null}
-            </div>
-          );
-        })}
-      </div>
-    </div>
+        );
+      })}
+    </>
   );
 };
+
+type SkillsTableProps = {
+  skills: any;
+  unit: any;
+  loading: boolean;
+  userSkills: any;
+  courseId: string;
+};
+
+function SkillsTable({
+  skills,
+  unit,
+  loading,
+  userSkills,
+  courseId,
+}: SkillsTableProps) {
+  return (
+    <div className="grid grid-cols-1 p-8 sm:grid-cols-4">
+      {skills
+        .filter((skill) => skill.unit.title === unit.title)
+        .sort((a, b) => a.unit.level - b.unit.level)
+        .map((skill) => (
+          <div className="p-2">
+            <SkillCard
+              loading={loading}
+              userSkillData={userSkills}
+              skill={skill}
+              courseId={courseId}
+            />
+          </div>
+        ))}
+    </div>
+  );
+}
 
 export default PracticePreview;
--- pages/studentPortal/labs/[courseId]/index.tsx
+++ pages/studentPortal/labs/[courseId]/index.tsx
@@ -4,8 +4,17 @@ import { FETCH_UNIT_OVERVIEW } from "../../../../graphql/fetchUnitOverview";
 import { useAuth } from "../../../../lib/authContext";
 import { FETCH_SKILLS_FOR_COURSE } from "../../../../graphql/fetchSkillsForCourse";
 import Link from "next/link";
+import { useState } from "react";
 
 const UnitOverviewPage = ({ courseId, skillData }) => {
+  enum Stage {
+    SELECT_STRAND,
+    SELECT_SKILL,
+  }
+  const strands = ["Operations", "Algebra"];
+  const [stage, setStage] = useState<Stage>(Stage.SELECT_STRAND);
+  const [selectedStrand, setSelectedStrand] = useState("");
+
   const { user } = useAuth();
 
   type UserSkillData = {
@@ -33,20 +42,74 @@ const UnitOverviewPage = ({ courseId, skillData }) => {
 
   const lessons = {
     finance: [{ title: "Credit Card Lesson", link: "finance/credit-card" }],
-    math1: [{ title: "Multiplication Game", link: "multiplication/game" }, { title: "Multiplication Connect Four", link: "multiplication-connect/Index" }],
+    math1: [
+      { title: "Multiplication Game", link: "multiplication/game" },
+      {
+        title: "Multiplication Connect Four",
+        link: "multiplication-connect/Index",
+      },
+    ],
   };
 
   return (
     <div className="flex flex-col justify-center overflow-auto bg-scroll bg-blue-100 ">
-      <div className="flex flex-col p-4 space-y-8">
-        {skillData && (
-          <PracticePreview
-            loading={loading}
-            userSkills={data}
-            skills={skillData.skills}
-            courseId={courseId}
-          />
-        )}
+      <div className="flex flex-row-reverse w-full h-16 space-x-4 bg-white">
+        <div
+          className="m-4 cursor-pointer text-charmander"
+          onClick={() => {
+            setStage(Stage.SELECT_STRAND);
+            setSelectedStrand("");
+          }}
+        >
+          Practice
+        </div>
+        <div className="m-4 text-murkrow">Games</div>
+      </div>
+      <div className="flex flex-col justify-center w-full p-4 space-y-8">
+        <div className="flex justify-center">
+          <div className="grid items-stretch max-w-4xl grid-cols-1 bg-white shadow-lg rounded-t-xl">
+            <div className="flex flex-col justify-center max-w-4xl grid-cols-1 bg-white shadow-lg rounded-t-xl">
+              <div className="p-4 space-y-8 sm:p-8">
+                <div className="flex flex-col space-y-4">
+                  <p className="text-4xl font-bold capitalize text-murkrow">
+                    {" "}
+                    PRACTICE TIME
+                  </p>
+                  <p className="text-murkrow">
+                    Select a skill to practice questions. You can practice as
+                    many times as you wish. At the end, you'll be asked to rate
+                    your skill confidence.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col justify-center gap-4 p-8">
+              {stage === Stage.SELECT_SKILL &&
+              selectedStrand == "Operations" &&
+              skillData ? (
+                <PracticePreview
+                  loading={loading}
+                  userSkills={data}
+                  skills={skillData.skills}
+                  courseId={courseId}
+                />
+              ) : (
+                <div
+                  className="flex justify-center gap-4 cursor-pointer"
+                  onClick={() => {
+                    setStage(Stage.SELECT_SKILL);
+                  }}
+                >
+                  {strands.map((strand) => (
+                    <div onClick={() => setSelectedStrand(strand)}>
+                      <Strand name={strand} />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
       {lessons[courseId] !== undefined ? (
         <div className="p-4 m-4 bg-white text-murkrow">
@@ -84,6 +147,19 @@ export async function getServerSideProps({ params }) {
   }
 
   return { props: { skillData: data, courseId: params.courseId } };
+}
+
+type StrandProps = {
+  name: string;
+};
+
+function Strand({ name }: StrandProps) {
+  return (
+    <div className="flex flex-col items-center justify-center w-64 p-8 rounded-lg shadow-lg bg-slate-100 border-murkrow text-murkrow">
+      {name}
+      <div className="w-16 h-16 mt-4 border-4 rounded-full bg-murkrow heropattern-jigsaw-slate-300 border-charmander" />
+    </div>
+  );
 }
 
 export default UnitOverviewPage;
