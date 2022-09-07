e27bce24a884355a7d3aaaa089c574257c28a90b
vithushan19 <vithushan19@gmail.com>
2022-09-07 10:57:37 -0400

    Add start of AI selection

--- redux/longestStreakSlice.ts
+++ redux/longestStreakSlice.ts
@@ -3,10 +3,8 @@ import { RootState } from "./rootReducer";
 import { BlockState } from "../components/math/longestStreak/MultiplicationBlock";
 import { GameBlockState } from "../pages/studentPortal/labs/multiplication/game";
 import { STAGE } from "../pages/studentPortal/labs/multiplication/game";
-import { Stage } from "konva/lib/Stage";
 import { getRandomItemFromArray, getRndInteger } from "../pages/api/random";
 import { shuffle } from "lodash";
-import { Action } from "lottie-react";
 
 export interface LongestStreakState {
   stage: STAGE;
@@ -17,32 +15,31 @@ export interface LongestStreakState {
 }
 
 function initializeGameState(): GameBlockState[] {
-  let dummyArray = [];
+  let dummyArray: GameBlockState[] = [];
   for (let i = 0; i <= 20; i++) {
     let x = getRndInteger(1, 9);
     let y = getRndInteger(1, 9);
-    let z = x * y;
-    let product = x + " x " + y;
+    let product: number = x * y;
+    let productString: string = x + " x " + y;
 
-    let initiateBlockState = {};
-
-    initiateBlockState = {
-      text: z.toString(),
+    let initiateBlockState: GameBlockState = {
+      text: product.toString(),
+      value: product,
       state: BlockState.NOT_SELECTED,
     };
     dummyArray.push(initiateBlockState);
 
     initiateBlockState = {
-      text: product.toString(),
+      text: productString,
+      value: product,
       state: BlockState.NOT_SELECTED,
     };
     dummyArray.push(initiateBlockState);
   }
 
   //shuffle list
-  dummyArray = shuffle(dummyArray)
-  return dummyArray
-
+  dummyArray = shuffle(dummyArray);
+  return dummyArray;
 }
 const initialState: LongestStreakState = {
   stage: STAGE.SET_RULES,
@@ -50,79 +47,94 @@ const initialState: LongestStreakState = {
   isPlayerOneActive: false,
   handlePlayerSelect: 0,
   isPlayerSelecting: false,
-
 };
-//selected = G
-export function getRandomItem(arr, selected) {
-  const randomIndex = Math.floor(Math.random() * arr.length);
-  const item = arr[randomIndex];
-  if (item != selected && ) {
-    return item;
-}
-  }
-  
-
 
 export const longestStreakSlice: Slice = createSlice({
   name: "longestStreak",
   initialState,
   reducers: {
     setStage: (state: LongestStreakState, action: PayloadAction<STAGE>) => {
-        const stageOfGame = action.payload as STAGE;
+      const stageOfGame = action.payload as STAGE;
 
-        state.stage=stageOfGame
+      state.stage = stageOfGame;
     },
 
-    setBlocks: (state: LongestStreakState, action: PayloadAction<GameBlockState[]>) => {
+    setBlocks: (
+      state: LongestStreakState,
+      action: PayloadAction<GameBlockState[]>
+    ) => {
       const selectedBlock = action.payload as GameBlockState[];
-      state.blocks = selectedBlock
-  },
+      state.blocks = selectedBlock;
+    },
     initializeGame: (state: LongestStreakState, action: PayloadAction) => {
-      state.blocks = initializeGameState()
+      state.blocks = initializeGameState();
     },
 
     setPlayerOneActive: (state, action: PayloadAction<boolean>) => {
-        if (action.type === "longestStreak/setPlayerOneActive") {
-          state.isPlayerOneActive = action.payload;
-        }
-      },
-    isPlayerSelecting: (state, action: PayloadAction<boolean>) => {
-      if (action.type === "longestStreak/isPlayerSelecting") {
-        state.isPlayerSelecting = action.payload;
-        for (let clickState=0; clickState<=2; clickState++) {
-          if (clickState===1) {
-            state.isPlayerSelecting === false;
-          } else if (clickState===2) {
-            state.isPlayerSelecting===false
-            clickState--
-          }
-        }
+      if (action.type === "longestStreak/setPlayerOneActive") {
+        state.isPlayerOneActive = action.payload;
       }
-      },
-      //here's where to add the AI randomly selecting next block.
-    
-      
+    },
+    isPlayerSelecting: (state, action: PayloadAction<boolean>) => {
+      // if (action.type === "longestStreak/isPlayerSelecting") {
+      //   state.isPlayerSelecting = action.payload;
+      //   for (let clickState=0; clickState<=2; clickState++) {
+      //     if (clickState===1) {
+      //       state.isPlayerSelecting === false;
+      //     } else if (clickState===2) {
+      //       state.isPlayerSelecting===false
+      //       clickState--
+      //     }
+      //   }
+      // }
+    },
+    //here's where to add the AI randomly selecting next block.
+
     handlePlayerSelect: (state, action: PayloadAction<number>) => {
       if (action.type === "longestStreak/handlePlayerSelect") {
         const index = action.payload;
         if (state.isPlayerSelecting === false) {
-          state.blocks[index].state = BlockState.PLAYER_ONE_SELECTED
+          // Player selected first block
+          state.blocks[index].state = BlockState.PLAYER_ONE_SELECTED;
+          state.isPlayerSelecting = true;
         } else if (state.isPlayerSelecting === true) {
-          state.blocks[index].state = BlockState.PLAYER_ONE_SELECTED }
-          if (state.blocks[index].state === BlockState.NOT_SELECTED && state.blocks[index].text)===state.blocks[index].product {
-            let computerSelected=getRandomItemFromArray(state.blocks)
-            computerSelected=state.blocks[index].state = BlockState.PLAYER_TWO_SELECTED;
-            let secondComputerSelected=getRandomItemFromArray(state.blocks)
-            secondComputerSelected=state.blocks[index].state = BlockState.PLAYER_TWO_SELECTED;
-          
-        } 
-    }
-}}})
+          // Player selected second block
+          state.blocks[index].state = BlockState.PLAYER_ONE_SELECTED;
+          state.isPlayerSelecting = false;
+          handleAISelection(state);
+        }
+      }
+    },
+  },
+});
 
+function handleAISelection(state: LongestStreakState) {
+  const unselectedBlocks = state.blocks.filter(
+    (block) => block.state === BlockState.NOT_SELECTED
+  );
 
-export const { setStage, setBlocks, isPlayerSelecting, setTwoPalyer, setPlayerOneActive, handlePlayerSelect, initializeGame, setlongestStreakQuestions } =
-  longestStreakSlice.actions;
+  let computerSelected: GameBlockState =
+    getRandomItemFromArray(unselectedBlocks);
+  const indexOfComputerSelected = state.blocks.indexOf(computerSelected);
 
-export const longestStreakSelector = (state: RootState) => state.longestStreakState;
+  state.blocks[indexOfComputerSelected].state = BlockState.PLAYER_TWO_SELECTED;
+  const valueToSearchFor = computerSelected.value;
+
+  // TODO find a a non-selected block that has the value to search for and then select that
+}
+
+export const {
+  setStage,
+  setBlocks,
+  isPlayerSelecting,
+  setTwoPalyer,
+  setPlayerOneActive,
+  handlePlayerSelect,
+  initializeGame,
+  setlongestStreakQuestions,
+} = longestStreakSlice.actions;
+
+export const longestStreakSelector = (state: RootState) =>
+  state.longestStreakState;
 
 export default longestStreakSlice;
