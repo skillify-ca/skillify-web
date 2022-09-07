7869d670b3039169c1628f19ae6622c8c873bc50
gajans12345 <90165455+gajans12345@users.noreply.github.com>
2022-09-07 13:31:46 -0400

    Create new challenge

---
+++ public/coding/JavaScript/JsFunctions/Basketball/basketball-Practice.js
@@ -0,0 +1,34 @@
+/*
+  
+  * In this challenge you will be writing a function for to find the winner of a basketball game !
+    * In basketball, there are 3 pointers and 2 pointers and in this game, their are 2 teams, the Raptors and the Warriors
+    * A 3 pointer is worth 3 points and a 2 pointer is worth 2.
+    * You will be given the number of 3 pointers and 2 pointers for the Raptors and Warriors and your job is to determine the winner
+    * Your final answer must be in this format : "The final score is 56 to 64, therefore the Warriors are the winner !"
+    * When returning the final score, state the Raptors first, then Warriors.
+    * You may assume their will be a winner, hence a tie is impossible.
+
+  
+
+  * Hints / Tips:
+    
+    * The use of variables may be useful when finding out the total number of points for each time 
+      * "8.32 % 1" returns the decimal remainder .32
+    
+    * Conditional statements (if , else if, else) are helpful when needing to see if a specific criteria is met
+
+
+  */
+ 
+function basketball(threepointRaptors, twopointRaptors, threepointWarriors, twopointWarriors) {
+    
+    // your code goes here
+}
+    
+  
+  
+  console.log(basketball(4,3,2,5)); //output: The final score is 18 to 15, therefore the Raptors are the winner !
+  console.log(basketball(10,0,4,4)); //output: The final score is 30 to 20, therefore the Raptors is the winner !
+  console.log(basketball(0,6,10,0)); //output: The final score is 18 to 30, therefore the Warriors is the winner !
+  
+  
