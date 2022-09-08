7f041d205b33aecf4566c650c5bbc5a61e4235e2
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-09-07 21:39:56 -0400

    Create card components for math games

--- pages/studentPortal/labs/[courseId]/games.tsx
+++ pages/studentPortal/labs/[courseId]/games.tsx
@@ -14,13 +14,16 @@ export default function CourseGamesPage({ courseId, gameList }) {
       </div>
       <div className="p-4 bg-slate-400 text-murkrow">
         <h2 className="text-5xl font-bold text-murkrow">Games</h2>
-        {gameList.map((game) => (
-          <Link href={game.link}>
-            <div className="w-64 p-4 my-4 bg-gray-100 shadow-lg cursor-pointer rounded-xl">
-              <p>{game.title}</p>
-            </div>
-          </Link>
-        ))}
+        <div className="flex gap-8">
+          {gameList.map((game) => (
+            <Link href={game.link}>
+              <div className="flex flex-col items-center w-64 my-4 transition-all bg-gray-100 shadow-lg cursor-pointer hover:bg-slate-200 hover:scale-110 rounded-xl">
+                <img src={game.image} className="w-full h-40 rounded-t-lg" />
+                <p className="p-4">{game.title}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -30,10 +33,17 @@ export async function getServerSideProps({ params }) {
   const games = {
     finance: [{ title: "Credit Card Lesson", link: "finance/credit-card" }],
     math1: [
-      { title: "Multiplication Game", link: "multiplication/game" },
+      {
+        title: "Longest Streak",
+        link: "/studentPortal/labs/multiplication/game",
+        image:
+          "https://images.unsplash.com/photo-1502214380024-fec72aa40e76?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80",
+      },
       {
         title: "Multiplication Connect Four",
-        link: "multiplication-connect/Index",
+        link: "/studentPortal/labs/multiplication-connect/Index",
+        image:
+          "https://images.unsplash.com/photo-1611996575749-79a3a250f948?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
       },
     ],
   };
