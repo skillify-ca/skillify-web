ee11de3b013e0982d8a6fc35b76dfe04d8a52510
kavanparam <kavan.param@gmail.com>
2022-09-14 23:24:02 -0400

    Merge mistake, don't know how this happened

--- components/math/multiplicationConnect/GameBoardBlock.tsx
+++ components/math/multiplicationConnect/GameBoardBlock.tsx
@@ -26,41 +26,6 @@ const GameBoardBlock: FC<GameBoardBlockProps> = ({
 }) => {
   // console.log("Block", blockData);
 
-  const blockClick = (block: GameBoardBlock, gridData: GameBoardBlock[]) => {
-    if (!block.isSelected) {
-      block.isSelected = true;
-      /*fixme: don't modify state directly, it should be read-only
-          look more into useState (watch tutorials) and refactor this. */
-      // setBlock((prevState) => ({ ...prevState, isSelected: true }));
-      /* setObject((prevState) => ({
-              ...prevState,
-              secondKey: 'value',
-            }));
-            */
-      calculateWinner(gridData);
-    } else {
-      // block.isSelected = false;
-      console.log("This block is already selected!");
-    }
-    console.log("block data:", block);
-    return { ...block };
-  };
-
-  useEffect(() => {
-    setBlock(blockData);
-  }, [blockData]);
-
-  // console.log("block data:", block);
-
-  /* todo: 
-      - toggle this state on block to determine the colour to display on board
-      - merge this into kp-twoPlayerSupport & get button working there before merge into kp-multiplicationConnect
-      
-      Options to determine winner:
-        3. add an isPlayerOne variable to GameBoardBlock & evaluate accordingly in calculateWinner() — to make sure 4 blocks in a row are pressed by the same player
-  */
-  const [isPlayerOne, setIsPlayerOne] = useState(true);
-
   return (
     <div
       onClick={() => {
