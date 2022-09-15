bd82d70d67f48a374c14faae2f6e1d9f02077ea8
kavanparam <kavan.param@gmail.com>
2022-09-15 15:18:52 -0400

    minor UI

--- components/math/multiplicationConnect/PlayerSection.tsx
+++ components/math/multiplicationConnect/PlayerSection.tsx
@@ -6,12 +6,12 @@ const PlayerSection: FC = () => {
     <div>
       <div className="flex items-center justify-evenly">
         <input
-          className="bg-inherit placeholder:text-inherit max-w-[200px] h-12 text-center cursor-pointer rounded-xl enabled:bg-[#F20000]/40"
+          className="bg-inherit placeholder:text-inherit max-w-[200px] h-12 text-center cursor-pointer rounded-xl enabled:bg-[#F20000]/10"
           placeholder="Player 1"
         ></input>
         <input
-          className="bg-inherit placeholder:text-inherit max-w-[200px] h-12 text-center cursor-pointer rounded-xl enabled:bg-[#FFDB00]/40"
-          placeholder="Player 2 (*WIP)"
+          className="bg-inherit placeholder:text-inherit max-w-[200px] h-12 text-center cursor-pointer rounded-xl enabled:bg-[#FFDB00]/10"
+          placeholder="Player 2"
         ></input>
         <p className="align-middle text-md">👈🏼 Click to rename</p>
       </div>
