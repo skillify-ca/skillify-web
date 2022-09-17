3391c808791805d8d39b7c0ea13b5320707a7a25
kavanparam <kavan.param@gmail.com>
2022-09-17 18:38:52 -0400

    Outlining test cases

--- pages/studentPortal/labs/multiplication-connect/Index.test.ts
+++ pages/studentPortal/labs/multiplication-connect/Index.test.ts
@@ -5,11 +5,19 @@ import { calculateWinner } from "../multiplication/game";
 // Assert
 
 it.todo('An empty to-do test')
-// check a player one win
-// check a player two win
-/* check that no win occurs:
-    - mixed blocks consecutive blocks are selected
-    - a draw
-    - when no blocks are selected
-    - when board is full with no 4 in a row
-*/
+
+
+/* test calculateWinner():
+    * check that SelectedBy type is working as expected:
+        - PlayerOne selection
+        - PlayerTwo selection
+        - Unselected blocks
+        - Any other value for the type produces an error
+    * check a PlayerOne win
+    * check a PlayerTwo win
+    * check that no win occurs:
+        - mixed blocks consecutive blocks are selected
+        - a draw
+        - when no blocks are selected
+        - when board is full with no 4 in a row
+ */
