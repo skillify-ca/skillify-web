6cd413ab940804d2056707148f329d565d20c4db
kavanparam <kavan.param@gmail.com>
2022-09-17 18:26:18 -0400

    Outlining the test cases

--- pages/studentPortal/labs/multiplication-connect/Index.test.ts
+++ pages/studentPortal/labs/multiplication-connect/Index.test.ts
@@ -1,3 +1,15 @@
 import { calculateWinner } from "../multiplication/game";
 
+// Arrange
+// Act
+// Assert
 
+it.todo('An empty to-do test')
+// check a player one win
+// check a player two win
+/* check that no win occurs:
+    - mixed blocks consecutive blocks are selected
+    - a draw
+    - when no blocks are selected
+    - when board is full with no 4 in a row
+*/
