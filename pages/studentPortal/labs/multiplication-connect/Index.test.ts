a99b408c84d4bdfca76ed7faa4d71c7724fc8355
kavanparam <kavan.param@gmail.com>
2022-09-19 10:50:21 -0400

    Rework the test inside a describe function

--- pages/studentPortal/labs/multiplication-connect/Index.test.ts
+++ pages/studentPortal/labs/multiplication-connect/Index.test.ts
@@ -4,20 +4,21 @@ import { calculateWinner } from "../multiplication/game";
 // Act
 // Assert
 
-it.todo('An empty to-do test')
 
-
-/* test calculateWinner():
-    * check that SelectedBy type is working as expected:
-        - PlayerOne selection
-        - PlayerTwo selection
-        - Unselected blocks
-        - Any other value for the type produces an error
-    * check a PlayerOne win
-    * check a PlayerTwo win
-    * check that no win occurs:
-        - mixed blocks consecutive blocks are selected
-        - a draw
-        - when no blocks are selected
-        - when board is full with no 4 in a row
+describe("calculateWinner()", () => {
+    /*  Outline for testing calculateWinner():
+        * check that SelectedBy type is working as expected:
+            - PlayerOne selection
+            - PlayerTwo selection
+            - Unselected blocks
+            - Any other value for the type produces an error
+        * check a PlayerOne win
+        * check a PlayerTwo win
+        * check that no win occurs:
+            - mixed blocks consecutive blocks are selected
+            - a draw
+            - when no blocks are selected
+            - when board is full with no 4 in a row
  */
+    test.todo("An empty to-do test");
+});
