97e0f758851fe0192a3b33a91517c5e998403984
kavanparam <kavan.param@gmail.com>
2022-09-19 15:16:20 -0400

    Does this commit pass checks?
    - testing to see what happens here

--- pages/studentPortal/labs/multiplication-connect/Index.test.ts
+++ pages/studentPortal/labs/multiplication-connect/Index.test.ts
@@ -4,9 +4,17 @@ import { calculateWinner } from "../multiplication/game";
 // Act
 // Assert
 
+/* To set the test up:
+    - setup whatever calculateWinner() needs right here in the test file
+    - might need to export the GameBoardBlock type for the grid variable to work as expected
+    - and will also need to initialize isPlayerOne somewhere
+    - create a mock grid to line up with the diff scenarios below
+    - pass these to calculateWinner and see what happens
 
-describe("calculateWinner()", () => {
-    /*  Outline for testing calculateWinner():
+    - AFTER REFACTOR: expect(calculateWinner).toBe/toEqual diff returns based on the provided grid
+*/
+
+ /*  Outline for testing calculateWinner():
         * check that SelectedBy type is working as expected:
             - PlayerOne selection
             - PlayerTwo selection
@@ -20,5 +28,9 @@ describe("calculateWinner()", () => {
             - when no blocks are selected
             - when board is full with no 4 in a row
  */
-    test.todo("An empty to-do test");
-});
+
+/* Refactor calculateWinner():
+    - to return a string s.t. the function is testable.
+*/
+
+test.todo("calculateWinner()");
