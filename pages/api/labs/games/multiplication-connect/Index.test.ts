0323b53ca5dc54109c6bcfa37b87fe8a9357e878
kavanparam <kavan.param@gmail.com>
2022-09-20 13:15:00 -0400

    Removed comments

--- pages/api/labs/games/multiplication-connect/Index.test.ts
+++ pages/api/labs/games/multiplication-connect/Index.test.ts
@@ -1,34 +1,5 @@
 import { calculateWinner, createGrid, SelectedBy } from "../../../../studentPortal/labs/multiplication-connect/Index";
 
-// Arrange
-// Act
-// Assert
-
-/* To set the test up:
-    - setup whatever calculateWinner() needs right here in the test file
-    - might need to export the GameBoardBlock type for the grid variable to work as expected
-    - and will also need to initialize isPlayerOne somewhere
-    - create a mock grid to line up with the diff scenarios below
-    - pass these to calculateWinner and see what happens
-
-    - AFTER REFACTOR: expect(calculateWinner).toBe/toEqual diff returns based on the provided grid
-*/
-
- /*  Outline for testing calculateWinner():
-        * check that SelectedBy type is working as expected:
-            - PlayerOne selection
-            - PlayerTwo selection
-            - Unselected blocks
-            - Any other value for the type produces an error
-        * check a PlayerOne win
-        * check a PlayerTwo win
-        * check that no win occurs:
-            - mixed blocks consecutive blocks are selected
-            - a draw
-            - when no blocks are selected
-            - when board is full with no 4 in a row
- */
-
 /* More testing:
     - block an invalid block selection: 
         * block that's already selected
