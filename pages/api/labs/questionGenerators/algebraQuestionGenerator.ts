ad4e0604ac16f036d1702a866c965252880e2b98
LuckyHariharan <lharihar@uwaterloo.ca>
2022-09-22 08:26:28 -0400

    Style Changes And Reformatting
    Style changes to code and reformatted AlgebraSolveQuestion object to remove unused properties.

--- pages/api/labs/questionGenerators/algebraQuestionGenerator.ts
+++ pages/api/labs/questionGenerators/algebraQuestionGenerator.ts
@@ -6,71 +6,67 @@ import {
   getRandomItemFromArray,
 } from "../../random";
 import { name } from "../../../api/names";
+
 export type AlgebraSolveQuestion = {
   questionType: QuestionType.ALGEBRA_SOLVE_VARIABLE;
+  algebraSolveModel: AlgebraSolveModel;
+  answer: string;
+};
+export type AlgebraSolveModel = {
   variableLetter: string;
   variableProblem: string;
-  algebraSolveModel: AlgebraSolveModel;
-  text: string;
   answer: string;
   personName: string;
 };
 
-type algebraObject = {
+type AlgebraObject = {
   algebraProblem: string;
   algebraSolution: string;
-}
+};
 
-export function algebrahelper():algebraObject{
-let problemtype = getRandomItemFromArray(["-", "+", "/", "x"]);
-const a = getRndInteger(1,50);
-const b = getRndInteger(1,50);
-const d = a + getRndInteger(1,50);
-const e = getRndInteger(2,10)
-const f = getRndInteger(1,10)
-const g = getRndInteger(2,10)*e
-  if(problemtype == "+") {
-    const problem: algebraObject = {
-      algebraProblem: " " + problemtype + " " + a.toString() + " = " + d.toString(),
-      algebraSolution:(d-a).toString(),
-    }
+export function algebraHelper(): AlgebraObject {
+  let problemtype = getRandomItemFromArray(["-", "+", "/", "x"]);
+  const a = getRndInteger(1, 50);
+  const b = getRndInteger(1, 50);
+  const d = a + getRndInteger(1, 50);
+  const e = getRndInteger(2, 10);
+  const f = getRndInteger(1, 10);
+  const g = getRndInteger(2, 10) * e;
+  if (problemtype == "+") {
+    const problem: AlgebraObject = {
+      algebraProblem:
+        " " + problemtype + " " + a.toString() + " = " + d.toString(),
+      algebraSolution: (d - a).toString(),
+    };
     return problem;
-  }else if(problemtype == "-"){
-    const problem: algebraObject = {
-      algebraProblem: " " + problemtype + " " + a.toString() + " = " + b.toString(),
-      algebraSolution: (b+a).toString(),
-    }
+  } else if (problemtype == "-") {
+    const problem: AlgebraObject = {
+      algebraProblem:
+        " " + problemtype + " " + a.toString() + " = " + b.toString(),
+      algebraSolution: (b + a).toString(),
+    };
     return problem;
-  }else if(problemtype == "/"){
-    const problem: algebraObject = {
-      algebraProblem: "  " + problemtype + " " + e.toString() + " = " + f.toString(),
-      algebraSolution: (e*f).toString(),
-    }
+  } else if (problemtype == "/") {
+    const problem: AlgebraObject = {
+      algebraProblem:
+        "  " + problemtype + " " + e.toString() + " = " + f.toString(),
+      algebraSolution: (e * f).toString(),
+    };
     return problem;
-  }else{
-    const problem: algebraObject = {
-      algebraProblem: "  " + problemtype + " " + e.toString() + " = " + g.toString(),
-      algebraSolution: (g/e).toString(),
-    }
+  } else {
+    const problem: AlgebraObject = {
+      algebraProblem:
+        "  " + problemtype + " " + e.toString() + " = " + g.toString(),
+      algebraSolution: (g / e).toString(),
+    };
     return problem;
   }
 }
 
-
-export type AlgebraSolveModel = {
-  variableLetter: string;
-  variableProblem: string;
-  text: string;
-  answer: string;
-  personName: string;
-};
-
 export function generateAlgebraQuestion(): AlgebraSolveQuestion {
-  let algebraproblem = algebrahelper()
+  let algebraproblem = algebraHelper();
   return {
     questionType: QuestionType.ALGEBRA_SOLVE_VARIABLE,
-    variableLetter: " ",
-    variableProblem: "x",
     algebraSolveModel: {
       variableLetter: getRandomItemFromArray([
         "a",
@@ -94,17 +90,13 @@ export function generateAlgebraQuestion(): AlgebraSolveQuestion {
         "u",
         "v",
         "w",
-        "x",
         "y",
         "z",
       ]),
       variableProblem: algebraproblem.algebraProblem,
-      text: "",
       answer: algebraproblem.algebraSolution,
       personName: getRandomItemFromArray(name),
     },
-    personName: "",
     answer: algebraproblem.algebraSolution,
-    text: "",
   };
 }
