fdf5260a41a23d144b74d8e65b5a249534d5d28d
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-09-25 13:52:23 -0400

    setup google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -74,31 +74,28 @@ function MyApp({ Component, pageProps: { ...pageProps } }) {
           `,
         }}
       />
-        <Script
-        id="linkedin-pixel"
+      {/* <!-- Google Tag Manager --> */}
+       <Script
+        id="fb-pixel"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-          _linkedin_partner_id = "${process.env.NEXT_PUBLIC_LINKED_PX_ID}";
-          window._linkedin_data_partner_ids = 
-          window._linkedin_data_partner_ids || [];                             
-         window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-          (function(l) { 
-              if (!l){window.lintrk = function(a,b) 
-          {window.lintrk.q.push([a,b])};
-          window.lintrk.q=[]} var s = 
-          document.getElementsByTagName("script")[0];
-          var b = document.createElement("script");
-          b.type = "text/javascript";
-          b.async = true;
-          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-          s.parentNode.insertBefore(b, s);})(window.lintrk);
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WD9273S');
           `,
         }}
       />
+      {/* <!-- End Google Tag Manager --> */}
+      {/* <!-- Google Tag Manager (noscript) --> */}
       <noscript>
-        <img height="1" width="1" style={{display: "none"}} alt="" src={`https://px.ads.linkedin.com/collect/?pid=${process.env.NEXT_PUBLIC_LINKED_PX_ID}&fmt=gif`} />
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WD9273S"
+          height="0" width="0" style={{display:"none", visibility:"hidden"}}>
+        </iframe>
       </noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
       <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
         <ReduxProvider store={store}>
           <AuthProvider>
