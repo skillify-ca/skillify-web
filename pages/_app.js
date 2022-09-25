b119a5404ff9024a43b584720f92defd5bcd9750
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-09-25 12:47:40 -0400

    add linkedin insights tag

--- pages/_app.js
+++ pages/_app.js
@@ -74,6 +74,31 @@ function MyApp({ Component, pageProps: { ...pageProps } }) {
           `,
         }}
       />
+        <Script
+        id="linkedin-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          _linkedin_partner_id = "${process.env.NEXT_PUBLIC_LINKED_PX_ID}";
+          window._linkedin_data_partner_ids = 
+          window._linkedin_data_partner_ids || [];                             
+         window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          (function(l) { 
+              if (!l){window.lintrk = function(a,b) 
+          {window.lintrk.q.push([a,b])};
+          window.lintrk.q=[]} var s = 
+          document.getElementsByTagName("script")[0];
+          var b = document.createElement("script");
+          b.type = "text/javascript";
+          b.async = true;
+          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+          s.parentNode.insertBefore(b, s);})(window.lintrk);
+          `,
+        }}
+      />
+      <noscript>
+        <img height="1" width="1" style="display:none;" alt="" src={`https://px.ads.linkedin.com/collect/?pid=${process.env.NEXT_PUBLIC_LINKED_PX_ID}&fmt=gif`} />
+      </noscript>
       <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
         <ReduxProvider store={store}>
           <AuthProvider>
