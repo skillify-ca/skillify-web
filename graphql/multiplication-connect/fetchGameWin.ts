1ff04404e8c219f107d1622b34e4af2200293b3c
kavanparam <kavan.param@gmail.com>
2022-10-01 19:19:03 -0400

    Changed file name and added query to fetch user data
    - fetching the various data outlined in the multicationConnectData gQL table

--- graphql/multiplication-connect/fetchGameWin.ts
+++
@@ -1,6 +0,0 @@
-import { gql } from "@apollo/client";
-
-gql;
-/* todo: setup a mutation(?) to track the current user and add it to the table 
-  - on save button click the user's id should be written to the Hasura database
-  - setup this file to look like other gQL requests in this folder */
