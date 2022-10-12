d61e14dd4717946970220cd7615e7c6a426ee185
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-10-12 18:17:42 +0300

    add goals sidebar icon

--- components/coding/studentPortal/Sidebar.tsx
+++ components/coding/studentPortal/Sidebar.tsx
@@ -138,6 +138,26 @@ export const Sidebar: React.FC<SidebarProps> = ({}: SidebarProps) => {
           }
         />
         <SidebarItem
+          name={"Goals"}
+          link={"/goals"}
+          page={"goals"}
+          icon={
+            <svg
+              className="h-5 w-5 mr-4 text-black"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />{" "}
+              <polyline points="22 4 12 14.01 9 11.01" />
+            </svg>
+          }
+        />
+        <SidebarItem
           name={"Workshops"}
           link={"/workshops"}
           page={"workshops"}
