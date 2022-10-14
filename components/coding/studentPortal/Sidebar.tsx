cb6bb3ec5a92018dfc0273142c864080ed0be4d4
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-10-14 19:44:06 +0300

    change icon
    Make it a hero icon, consistent with the others on the sidebar

--- components/coding/studentPortal/Sidebar.tsx
+++ components/coding/studentPortal/Sidebar.tsx
@@ -1,8 +1,10 @@
+import { useQuery } from "@apollo/client/react";
 import { link } from "fs";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { FETCH_USER_GOALS } from "../../../graphql/fetchUserGoals";
 import { useAuth } from "../../../lib/authContext";
 import {
   activePageSelector,
@@ -10,7 +12,7 @@ import {
   SidebarProps,
 } from "../../../redux/sidebarSlice";
 
-const SidebarItem = ({ name, link, page, icon }) => {
+const SidebarItem = ({ name, link, page, icon, number }) => {
   const { activePage } = useSelector(activePageSelector);
 
   return (
@@ -22,6 +24,7 @@ const SidebarItem = ({ name, link, page, icon }) => {
       >
         {icon}
         {name}
+        {number}
       </div>
     </Link>
   );
@@ -50,6 +53,19 @@ export const Sidebar: React.FC<SidebarProps> = ({}: SidebarProps) => {
       dispatch(setActivePage("dashboard"));
     }
   }, [router.pathname]);
+
+  const { data } = useQuery(FETCH_USER_GOALS, {
+    variables: {
+      userId: user.uid,
+    },
+  });
+
+  const [number, setNumber] = useState();
+  useEffect(() => {
+    if (typeof data !== "undefined") {
+      setNumber(data.remainingNumberOfDays);
+    }
+  }, [data]);
 
   return (
     //Full width then restrict in page
@@ -85,6 +101,7 @@ export const Sidebar: React.FC<SidebarProps> = ({}: SidebarProps) => {
               <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
             </svg>
           }
+          number={undefined}
         />
         <SidebarItem
           name={"Coaches"}
@@ -100,6 +117,7 @@ export const Sidebar: React.FC<SidebarProps> = ({}: SidebarProps) => {
               <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
             </svg>
           }
+          number={undefined}
         />
         <SidebarItem
           name={"Labs"}
@@ -119,6 +137,7 @@ export const Sidebar: React.FC<SidebarProps> = ({}: SidebarProps) => {
               />
             </svg>
           }
+          number={undefined}
         />
         <SidebarItem
           name={"Profile"}
@@ -138,6 +157,7 @@ export const Sidebar: React.FC<SidebarProps> = ({}: SidebarProps) => {
               />
             </svg>
           }
+          number={undefined}
         />
         <SidebarItem
           name={"Goals"}
@@ -145,19 +165,19 @@ export const Sidebar: React.FC<SidebarProps> = ({}: SidebarProps) => {
           page={"goals"}
           icon={
             <svg
-              className="h-5 w-5 mr-4 text-black"
+              xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              fill="currentColor"
+              className="w-6 h-6 mr-4"
             >
-              {" "}
-              <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />{" "}
-              <polyline points="22 4 12 14.01 9 11.01" />
+              <path
+                fill-rule="evenodd"
+                d="M5.166 2.621v.858c-1.035.148-2.059.33-3.071.543a.75.75 0 00-.584.859 6.753 6.753 0 006.138 5.6 6.73 6.73 0 002.743 1.346A6.707 6.707 0 019.279 15H8.54c-1.036 0-1.875.84-1.875 1.875V19.5h-.75a2.25 2.25 0 00-2.25 2.25c0 .414.336.75.75.75h15a.75.75 0 00.75-.75 2.25 2.25 0 00-2.25-2.25h-.75v-2.625c0-1.036-.84-1.875-1.875-1.875h-.739a6.706 6.706 0 01-1.112-3.173 6.73 6.73 0 002.743-1.347 6.753 6.753 0 006.139-5.6.75.75 0 00-.585-.858 47.077 47.077 0 00-3.07-.543V2.62a.75.75 0 00-.658-.744 49.22 49.22 0 00-6.093-.377c-2.063 0-4.096.128-6.093.377a.75.75 0 00-.657.744zm0 2.629c0 1.196.312 2.32.857 3.294A5.266 5.266 0 013.16 5.337a45.6 45.6 0 012.006-.343v.256zm13.5 0v-.256c.674.1 1.343.214 2.006.343a5.265 5.265 0 01-2.863 3.207 6.72 6.72 0 00.857-3.294z"
+                clip-rule="evenodd"
+              />
             </svg>
           }
+          number={undefined}
         />
         <SidebarItem
           name={"Workshops"}
@@ -173,6 +193,7 @@ export const Sidebar: React.FC<SidebarProps> = ({}: SidebarProps) => {
               <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
             </svg>
           }
+          number={undefined}
         />
 
         <div>
