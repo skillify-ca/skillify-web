c3c7d6016bc432c63bae41913f753a7e4d52a772
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-10-18 21:03:30 +0300

    corrected fetchUserGoalsCount query
    checked it again in hasura query builder and found an error that I fixed.

--- graphql/fetchUserGoalsCount.ts
+++ graphql/fetchUserGoalsCount.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const FETCH_USER_GOALS_COUNT = gql`
-  query fetchUserGoalsCount($userId: String = "") {
-    user_goals__aggregate(where: { userId: { _eq: $userId } }) {
+    query fetchUserGoalsCount($userId: String = "") {
+    user_goals_aggregate(where: {userId: {_eq: $userId}}) {
       aggregate {
         count
       }
