89ec17d0cb9f17233359987ec7871ff3866bbb6c
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-10-20 01:22:19 -0400

    expose react lesson

--- pages/studentPortal/web/React/components.tsx
+++ pages/studentPortal/web/React/components.tsx
@@ -1,17 +1,27 @@
+import { Router, useRouter } from "next/router";
 import React from "react";
 import Card, { CardData } from "../../../../components/coding/Card";
 import LessonComponent, {
   LessonComponentData,
 } from "../../../../components/coding/studentPortal/LessonComponent";
+import { Button } from "../../../../components/ui/Button";
 import Navbar from "../../../../components/ui/Navbar";
 
 const React2 = ({ lessonComponents }) => {
+  const router = useRouter();
+
+  const handleContinue = () => {
+    router.push("/studentPortal/web/React/props");
+  };
   return (
     <>
       <div className="grid grid-cols-1 gap-8 px-4 pt-4 m-8 sm:px-12">
         {lessonComponents.map((it) => (
           <LessonComponent data={it} />
         ))}
+      </div>
+      <div className="flex mt-8 sm:justify-end">
+        <Button onClick={handleContinue} label="Continue" />
       </div>
     </>
   );
@@ -38,7 +48,8 @@ export async function getServerSideProps({ params }) {
     },
     {
       component: "description",
-      text: "Components are the building blocks of React apps. You can use components to split up the UI into independent, reusable pieces. In this tutorial we will take HTML that renders a table of expenses, and turn it into an efficient series of React components. A component - at its simplest form - is just a Javascript function that returns HTML to be rendered on a page. ",
+      text:
+        "Components are the building blocks of React apps. You can use components to split up the UI into independent, reusable pieces. In this tutorial we will take HTML that renders a table of expenses, and turn it into an efficient series of React components. A component - at its simplest form - is just a Javascript function that returns HTML to be rendered on a page. ",
     },
 
     {
@@ -50,16 +61,19 @@ export async function getServerSideProps({ params }) {
     },
     {
       component: "description",
-      text: "We can use functional components to make our code cleaner and less repeatable. Take a look at this basic HTML page displaying a table of expenses:",
+      text:
+        "We can use functional components to make our code cleaner and less repeatable. Take a look at this basic HTML page displaying a table of expenses:",
     },
     {
       component: "code-sandbox",
       title: "HTML Budget",
-      link: "https://codesandbox.io/embed/skillify-messy-web-page-xk00mt?fontsize=14&hidenavigation=1&theme=dark",
+      link:
+        "https://codesandbox.io/embed/skillify-messy-web-page-xk00mt?fontsize=14&hidenavigation=1&theme=dark",
     },
     {
       component: "description",
-      text: "This code works but it's tedious. Imagine if we had to repeat the <p> tag 100 or 1000 times. What if we made a function component for each row?",
+      text:
+        "This code works but it's tedious. Imagine if we had to repeat the <p> tag 100 or 1000 times. What if we made a function component for each row?",
     },
     {
       component: "code-snippet",
@@ -96,11 +110,13 @@ export async function getServerSideProps({ params }) {
     {
       component: "code-sandbox",
       title: "Cleaner, Componentized HTML Budget",
-      link: "https://codesandbox.io/embed/skillify-componentized-react-page-z1hego?fontsize=14&hidenavigation=1&theme=dark",
+      link:
+        "https://codesandbox.io/embed/skillify-componentized-react-page-z1hego?fontsize=14&hidenavigation=1&theme=dark",
     },
     {
       component: "description",
-      text: "This is much cleaner but we still have to write a new component for each row. The next lesson will cover props, which will allow us to make a reusable row component",
+      text:
+        "This is much cleaner but we still have to write a new component for each row. The next lesson will cover props, which will allow us to make a reusable row component",
     },
   ];
   return { props: { lessonComponents } };
