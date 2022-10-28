0113ab164bc4e45a741b007ccdaa77f21fe80472
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-10-28 20:02:32 +0300

    corrected variable name to goalNotes
    also removed 60 char guard due to the fact it's a descriptive field.

--- pages/goals/[slug].tsx
+++ pages/goals/[slug].tsx
@@ -76,6 +76,12 @@ const EditGoalsPage = () => {
                 }));
               }}
             />
+
+            {editedGoalValues.goalName.length > 60 && (
+              <p className="text-xs text-red-600">
+                please keep your goal under 60 characters
+              </p>
+            )}
             <p className="font-bold">Write An Actionable Goal Outline</p>
 
             <textarea
@@ -85,15 +91,11 @@ const EditGoalsPage = () => {
               onChange={(e) => {
                 setEditedGoalValues((prevState) => ({
                   ...prevState,
-                  goalName: e.target.value,
+                  goalNotes: e.target.value,
                 }));
               }}
             />
-            {editedGoalValues.goalName.length > 60 && (
-              <p className="text-xs text-red-600">
-                please keep your goal under 60 characters
-              </p>
-            )}
+
             <p className="font-bold">Created On</p>
             <input
               type="text"
