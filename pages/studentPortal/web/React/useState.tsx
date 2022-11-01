55fdb957ebe6f6812e94ce62793cbe3ad9b45ee2
Raveen Reddy <raveenreddy89@gmail.com>
2022-11-01 09:38:38 -0400

    add to tutorial

--- pages/studentPortal/web/React/useState.tsx
+++ pages/studentPortal/web/React/useState.tsx
@@ -48,7 +48,56 @@ export async function getServerSideProps({ params }) {
     },
     {
       component: "description",
-      text: "State refers to how a component acts and behaves",
+      text: "State refers to how an application changes over time. You can also think of it as what is displayed to the user at any given point in time. Take a single Instagram post for example which contains multiple pieces of state that are constantly changing. Likes, posts, and comments are constantly being added by users, updated, and re-rendered by application. The `state` of the application is constantly changing ",
+    },
+    {
+      component: "code-snippet",
+      text: "In React, state is represented by an object. The state for a singular instagram post might look something like: ",
+      code: `const state = {
+        likes: 4,
+        numComments: 5,
+        reposts: 4,
+        recentComments: [
+            {
+                userName: 'raveen123',
+                post: 'awesome picture!'
+            },
+            {
+                userName: 'raveen123',
+                post: 'Congratulations!'
+            },
+    
+        ]
+    }
+    
+    export default function DisplayState() {
+        return <div>{JSON.stringify(state)}<p>*** we can't render objects directly on a React page, we have to convert them into a string using JSON.stringify</p></div>;
+      }`,
+    },
+
+    {
+      component: "description",
+      text: "State refers to how an application changes over time. You can also think of it as what is displayed to the user at any given point in time. Take a single Instagram post for example which contains multiple pieces of state that are constantly changing. Likes, posts, and comments are constantly being added by users, updated, and re-rendered by application. The `state` of the application is constantly changing ",
+    },
+
+    {
+      component: "description",
+      text: "React provides us with the useState hook to easily access and update state. The useState hook always follows this convention: ",
+    },
+
+    {
+      component: "description",
+      text: "const [stateVariable, setStateVariable] = useState(initialValue)",
+    },
+
+    {
+      component: "description",
+      text: "The first variable in the state array is the current state at any given point in time. The second variable is a setter function which we can use to change the state in our application. There always needs to be an initial value set as well",
+    },
+
+    {
+      component: "description",
+      text: "Let's look at a small example of an application that rolls dice. We call useState twice to define state for each die. Each time we press the roll dice button, the app calls the rollDie function and sets the value of each die",
     },
 
     {
