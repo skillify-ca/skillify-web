6a5586f9e3692f3ed0b837db14375d3dc53399d7
kwiedenhaupt <111075855+kwiedenhaupt@users.noreply.github.com>
2022-11-09 21:10:44 +0200

    Assignment Component (#809)
    * addGoalNotes
    
    1. added textbox to goals slug
    2. updated upsertUserGoals and fetchUserGoalsDetail queries and UserGoalsType
    3. Made goalNotes optional
    
    * corrected variable name to goalNotes
    
    also removed 60 char guard due to the fact it's a descriptive field.
    
    * added goalNotes to the object variable
    
    * add hero icon to edit goals
    
    1. use local state to make icon yellow when goals are editable.
    
    * changes made in previous branch
    
    * updated query to filter out isArchived and isComplete table values
    
    -- needed to include the null values on the isArchived column of the table
    
    * update icon for goal notes
    
    1.  used the pencil icon
    2.  made it a pointer and yellow upon hover
    
    * moved icon to right and added px-2 for spacing
    
    * only highlight current goals; adjust threshold and change styling
    
    * lightened up highlight colors
    
    * began assignment component
    
    1.  defined the AssignmentComponentData type
    2.  defined an HTML render for each component type
    3.  downloaded sandpack package to run sandbox
    4.  created test file as a dummy
    
    * added test page shell
    
    added the page to the directory as follows: /pages/studentPortal/web/React/assignments/test.tsx
    
    * removed the input box language to simplify the page to get it working.
    
    * WIP assignmentInputBox and getting AssignmentComponent to render
    
    * WIP -- rendering correctly.
    
    need to flesh out the data and the AssignmentInputBox component
    
    * fleshing out test page with data
    
    - added sample text and icons
    
    * updated input box styling
    
    * set a toggle for the hits
    
    created a useState hook for showDescription
    
    * added queries
    
    1. added queries to AssignmentInputBox
    2. adjusted state and other variables on AssignmentInputBox page
    
    * remove assignmentInputBox component
    
    * Revert "remove assignmentInputBox component"
    
    This reverts commit 53c209aa36093d8717647fbc19aca35278154aa8.
    
    * remove extra code
    
    * fixed hints
    
    * fix hint row components
    
    * remove loom code
    
    * update upsert mutation; change table name in hasura
    
    * changes for kari
    
    * rename test file to fix build issue
    
    * fixing mutation upsertUserCodingAssignments
    
    updated variables and updated fetchQuery names
    
    * fixed file name issue
    
    changed file name to template.tsx
    
    * updated name to template
    
    * updated queries to new table/fields to match graphQL
    
    * updated query and mutation names in AssignmentInputBox file
    
    * deleted unused queries and fixed spelling of assignment component
    
    * updated imports because of name change on file
    
    Co-authored-by: Raveen Reddy <raveenreddy89@gmail.com>

---
+++ components/coding/studentPortal/AssignmentComponent.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { Sandpack } from "@codesandbox/sandpack-react";
+import HintRow from "./HintRow";
+import AssignmentInputBox from "./AssignmentInputBox";
+
+export type Hint = {
+  description: string;
+  icon?: string;
+  link?: string;
+};
+
+export type AssignmentComponentData =
+  | {
+      component: "title";
+      text: string;
+    }
+  | {
+      component: "prompt";
+      text: string;
+    }
+  | {
+      component: "hint-list";
+      hintRow: Hint[];
+    }
+  | {
+      component: "output";
+      title: string;
+      screenshot: string;
+    }
+  | {
+      component: "submission";
+      codeSandboxTitle: string;
+      link: string;
+      placeholder: string;
+    }
+  | {
+      component: "custom";
+      children: React.ReactNode;
+    }
+  | {
+      component: "code-snippet";
+      text?: string;
+      code: string;
+    };
+
+export type AssignmentComponentProps = {
+  data: AssignmentComponentData;
+};
+export default function AssignmentComponent({
+  data,
+}: AssignmentComponentProps) {
+  if (data.component === "title") {
+    return <h1 className="text-5xl font-bold">{data.text}</h1>;
+  } else if (data.component === "prompt") {
+    return <p>{data.text}</p>;
+  } else if (data.component === "code-snippet") {
+    return (
+      <div className="mx-4 space-y-4">
+        {data.text && <p>{data.text}</p>}
+        <pre className="p-4 border-2 bg-slate-100 border-black-transparent rounded-xl">
+          <Sandpack template="react-ts" files={{ "App.tsx": data.code }} />
+        </pre>
+      </div>
+    );
+  } else if (data.component === "hint-list") {
+    return (
+      <>
+        <h1 className="font-bold">Hints</h1>
+        <div className="flex flex-col gap-8">
+          {data.hintRow.map((it, index) => (
+            <HintRow description={it.description} link={it.link} />
+          ))}
+        </div>
+      </>
+    );
+  } else if (data.component === "submission") {
+    return (
+      <>
+        <h1 className="font-bold">{data.codeSandboxTitle}</h1>
+        <AssignmentInputBox
+          placeholder={data.placeholder}
+          submission_link={""}
+        />
+        <div className="aspect-w-16 aspect-h-9">
+          <iframe className="rounded-xl" src={data.link} />
+        </div>
+      </>
+    );
+  } else if (data.component === "output") {
+    return (
+      <div className="pb-56 mb-8 h-96">
+        <p>{data.title}</p>
+        <img src={data.screenshot} className="object-cover w-64 h-32 mb-4" />
+      </div>
+    );
+  } else {
+    return <h1>{data.component}</h1>;
+  }
+}
---
+++ components/coding/studentPortal/AssignmentInputBox.tsx
@@ -0,0 +1,59 @@
+import { useMutation } from "@apollo/client";
+import router from "next/router";
+import React, { useState } from "react";
+import { UPSERT_USER_ASSIGNMENT_SUBMISSIONS } from "../../../graphql/upsertUserAssignmentSubmissions";
+import { useAuth } from "../../../lib/authContext";
+import { Button } from "../../ui/Button";
+
+export interface AssignmentInputBoxProps {
+  placeholder: string;
+  submission_link: string;
+}
+
+export const AssignmentInputBox: React.FC<AssignmentInputBoxProps> = ({
+  placeholder,
+  submission_link,
+}: AssignmentInputBoxProps) => {
+  const [submissionInput, setSubmissionInput] = useState("");
+  const [saveAssignmentInput] = useMutation(
+    UPSERT_USER_ASSIGNMENT_SUBMISSIONS,
+    {
+      onCompleted: () => router.push("/studentPortal"),
+    }
+  );
+
+  const { user } = useAuth();
+  const submissionVariables = {
+    user_id: user.uid,
+    submission_link: submissionInput,
+  };
+
+  return (
+    <div className="grid grid-cols-1 gap-4 p-6 bg-white shadow-lg dark:bg-gray-900 ">
+      <input
+        className={`text-left p-2 border rounded-md shadow-md w-full  text-murkrow 
+              }`}
+        id="input"
+        type="string"
+        value={submissionInput}
+        placeholder={placeholder}
+        onChange={(e) => {
+          setSubmissionInput(e.target.value);
+        }}
+      />
+      <div className="col-start-1 mt-8">
+        <Button
+          label="Save"
+          onClick={() => {
+            saveAssignmentInput({
+              variables: {
+                objects: submissionVariables,
+              },
+            });
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+export default AssignmentInputBox;
