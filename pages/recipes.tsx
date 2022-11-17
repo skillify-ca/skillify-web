030eaa3ab81527503d1d9a830c4e78819ef9d453
Raveen Reddy <raveenreddy89@gmail.com>
2022-11-17 16:26:48 -0500

    add another recipe

--- pages/recipes.tsx
+++ pages/recipes.tsx
@@ -24,18 +24,26 @@ export async function getServerSideProps({ params }) {
 
     {
       component: "description",
-      text: "This page contains small, contained CodeSandbox examples of React and / or Javascript code created by Skillify's coaches. Take a look for reference to help in your coding journey!",
+      text: "This page contains small, contained CodeSandbox examples of React and / or Javascript code created by Skillify's coaches. Use these recipes to help you with your assignments or apps",
     },
     {
       component: "resource-list",
       resources: [
         {
-          title: "Child to Child communication",
+          title: "Communicating between child components",
           description:
             "React example where two child components communicate between each other via the parent app component. Also shows you how to pass functions as props ",
           image:
             "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
           link: "https://codesandbox.io/s/child-to-child-communication-via-parent-kpb10d",
+        },
+        {
+          title: "Using onBlur with input forms",
+          description:
+            "Example of using the onBlur method to update a state variable attached to an input form. This is an alternative to a form's onChange property",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+          link: "https://codesandbox.io/s/onblur-example-x1hfc3",
         },
       ],
     },
