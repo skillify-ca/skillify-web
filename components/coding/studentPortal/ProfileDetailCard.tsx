c45c2977c9fcb2469e6676a9debc08e62e695566
LuckyHariharan <lharihar@uwaterloo.ca>
2022-12-01 17:00:39 -0500

    Coaching DashBoard Draft

---
+++ components/coding/studentPortal/ProfileDetailCard.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button } from "../../ui/Button";
+
+type ProfileDetailCard = {
+  avatar: string;
+  name: string;
+  joinDate: string;
+  badges: string;
+  currentFocus: string;
+  nextGoal: string;
+};
+
+function ProfileDetailCard({
+  avatar,
+  name,
+  joinDate,
+  badges,
+  currentFocus,
+  nextGoal,
+}: ProfileDetailCard) {
+  return (
+    <div className="bg-gray-900 text-sm w-60 p-6 space-y-8 rounded-md ">
+      <div className="flex flex-col grow items-center text-center gap-4 font-bold  ">
+        <img className="rounded-full border-2 w-20 " src={avatar} />
+        <p className=" text-black"> {name}</p>
+        <p className="">join date: {joinDate}</p>
+        <p className="">badges {badges}</p>
+      </div>
+      <div className="flex flex-col gap-4">
+        <p className="underline-offset-8">current focus: {currentFocus}</p>
+        <p>next goal: {nextGoal}</p>
+      </div>
+      {/* <a href={starterFile}>
+        <div className="mt-4">
+          {starterFile ? <Button label={"Starter Files"} /> : null}
+        </div>
+      </a> */}
+    </div>
+  );
+}
+
+export default ProfileDetailCard;
