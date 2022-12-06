47d6ec093297e49dffd7f6af36c1f04dd7eff4c5
LuckyHariharan <lharihar@uwaterloo.ca>
2022-12-06 11:50:43 -0500

    Loading Div

--- pages/coachingdashboard.tsx
+++ pages/coachingdashboard.tsx
@@ -10,7 +10,8 @@ const coachingdashboard = () => {
   const { loading, data } = useQuery<FetchUserProfileCardResponse>(
     FETCH_USER_PROFILE_CARD
   );
-  if (loading) return "Loading...";
+  if (loading)
+    return <div className="flex place-content-center">"Loading..."</div>;
   return (
     <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll">
       <div>
