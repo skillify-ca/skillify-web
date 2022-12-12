baa541d5c374ac8eccbcc926fe6b80bb690c9a5c
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-12-11 18:47:37 -0600

    added ExpandableContainer to AssignmentSectionComponent

--- components/coding/profileV2/AssignmentSectionComponent.tsx
+++ components/coding/profileV2/AssignmentSectionComponent.tsx
@@ -13,6 +13,7 @@ import {
 } from "../../../graphql/fetchUserAssignmentSubmissions";
 import { User } from "../../../graphql/fetchUserProfile";
 import { useAuth } from "../../../lib/authContext";
+import ExpandableContainer from "../ExpandableContainer";
 
 export type AssignmentSectionComponentProps = {};
 
@@ -35,50 +36,52 @@ export default function AssignmentsSection({}: AssignmentSectionComponentProps) 
       }
     );
   return (
-    <div>
-      {userAssignments.length > 0 && (
-        <div className="grid grid-cols-5 text-sm font-semibold text-center border-b-2 md:grid-cols-12 md:text-lg">
-          <p className="col-span-2 md:col-span-5">Assignment</p>
-          <p className="hidden md:block md:col-span-2">Completed</p>
-          <p className="font-semibold md:col-span-2">In Review</p>
-        </div>
-      )}
+    <ExpandableContainer open={false} title={""}>
+      <div>
+        {userAssignments.length > 0 && (
+          <div className="grid grid-cols-5 text-sm font-semibold text-center border-b-2 md:grid-cols-12 md:text-lg">
+            <p className="col-span-2 md:col-span-5">Assignment</p>
+            <p className="hidden md:block md:col-span-2">Completed</p>
+            <p className="font-semibold md:col-span-2">In Review</p>
+          </div>
+        )}
 
-      {userAssignmentsLoading ? (
-        <div>Loading...</div>
-      ) : userAssignments.length === 0 ? (
-        <div className="col-span-3 p-8 text-center shadow-md bg-slate-300 dark:bg-slate-900">
-          No Active Assignments
-        </div>
-      ) : (
-        userAssignments.map((assignment, index) => {
-          return (
-            <div
-              className={`grid grid-cols-5 my-2 text-sm text-center md:grid-cols-12 md:text-lg place-items-center ${assignment}`}
-            >
-              <p>{index + 1}.</p>
-              <p className="col-span-2 md:col-span-4">{assignment.id}</p>
-              <p className="hidden md:block md:col-span-2">
-                {assignment.submission_link ? (
-                  <CheckCircleIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
-                ) : (
-                  <XCircleIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
-                )}
-              </p>
-              <p className="hidden md:block col-span-1 md:col-span-2">
-                {assignment.review_link ? (
-                  <CheckCircleIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
-                ) : (
-                  <XCircleIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
-                )}
-              </p>
-              <Link href={"/web/React/assignments/template/"}>
-                <PencilAltIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
-              </Link>
-            </div>
-          );
-        })
-      )}
-    </div>
+        {userAssignmentsLoading ? (
+          <div>Loading...</div>
+        ) : userAssignments.length === 0 ? (
+          <div className="col-span-3 p-8 text-center shadow-md bg-slate-300 dark:bg-slate-900">
+            No Active Assignments
+          </div>
+        ) : (
+          userAssignments.map((assignment, index) => {
+            return (
+              <div
+                className={`grid grid-cols-5 my-2 text-sm text-center md:grid-cols-12 md:text-lg place-items-center ${assignment}`}
+              >
+                <p>{index + 1}.</p>
+                <p className="col-span-2 md:col-span-4">{assignment.id}</p>
+                <p className="hidden md:block md:col-span-2">
+                  {assignment.submission_link ? (
+                    <CheckCircleIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
+                  ) : (
+                    <XCircleIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
+                  )}
+                </p>
+                <p className="hidden md:block col-span-1 md:col-span-2">
+                  {assignment.review_link ? (
+                    <CheckCircleIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
+                  ) : (
+                    <XCircleIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
+                  )}
+                </p>
+                <Link href={"/web/React/assignments/template/"}>
+                  <PencilAltIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
+                </Link>
+              </div>
+            );
+          })
+        )}
+      </div>
+    </ExpandableContainer>
   );
 }
