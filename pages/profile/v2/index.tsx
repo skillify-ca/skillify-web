f8a54e7ca1ce24065220c46db62279caa9c1a1d6
kwiedenhaupt <111075855+kwiedenhaupt@users.noreply.github.com>
2022-12-13 14:45:14 -0600

    Profile v2 assignment component beginning (#826)
    * adding AssignmentSectionComponent
    
    WIP need to figure out how/where to implement it
    
    * removing commented out code
    
    * adding the assignment component to the profileV2 page
    
    added a query called FETCH_ALL_USER_ASSIGNMENTS to remove the assignmentId field so that I could get all assignments for my userId instead of just one -- probably will have to readjust it/do another to include all assignments for all students if the role of the user is a coach.
    
    * removing unused imports
    
    * added ExpandableContainer to AssignmentSectionComponent
    
    * added ExpandableContainer to userGoals
    
    * added the clock icon instead of the x and created one status column
    
    1. if there is a submission_link and no review_link, you get the clock.
    2. if there is a submission_link and review_link there is a check
    if there is no submission_link, it's null
    
    * added assignment _name to the query
    
    1. required the reworking of the DataResponse to add the type for the new query that included assignment_name
    2. called the assignment_name instead of id.
    3. fixed some mobile and regular view spacing.
    
    * updated link to take to the template assignment
    
    * updated type for FetchUserAssignmentSubmissionsDataResponse
    
    1. Added assignment_link to coding_assignment table
    2. linked to coding_assignment with the pencil
    
    * added x icon if there is no review_link or submission_link

--- pages/profile/v2/index.tsx
+++ pages/profile/v2/index.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/client";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import GoalsSectionComponent from "../../../components/coding/GoalsSectionComponent";
+import AssignmentSectionComponent from "../../../components/coding/profileV2/AssignmentSectionComponent";
 import ProfileHeaderComponent from "../../../components/coding/profileV2/ProfileHeaderComponent";
 import ProjectsSection from "../../../components/coding/ProjectsSection";
 import BadgesSection from "../../../components/profile/BadgesSection";
@@ -34,10 +35,10 @@ export default function Profile(props) {
     <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll">
       <ProfileHeaderComponent user={user} />
 
-      <h2 className="text-lg font-bold mt-14 mb-9">Projects</h2>
+      <h2 className="text-lg font-bold mt-14 mb-9">Assignments</h2>
 
-      <div className="grid grid-cols-1 mb-4 sm:grid-cols-3">
-        <ProjectsSection user={user} />
+      <div className="grid grid-cols-1 mb-4">
+        <AssignmentSectionComponent />
       </div>
 
       <h2 className="text-lg font-bold mt-14 mb-9">Goals</h2>
