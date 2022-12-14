9e399e51522c5c53dc40f6cf426556ee1ba0150b
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-12-14 17:23:05 -0600

    updated tests and slice after consulting with Raveen!
    took out the userGoals/userGoals and sectionName in the expect

--- redux/userGoalsSlice.test.ts
+++ redux/userGoalsSlice.test.ts
@@ -2,53 +2,55 @@
 import { UserGoalsData } from "../graphql/fetchUserGoals";
 import reducer, { GoalSection, setGoalsSections, setUserGoals, UserGoalsState } from "./userGoalsSlice";
 
-const userGoalsInitialState: UserGoalsData[] = [
-  ];
-  
-  const userGoalsTestState: UserGoalsData[] =
-[
-                {
-                createdAt: new Date(2022, 12, 14),
-                goalName: "Write Functions",
-                goalNotes: "recurring goal",
-                id: "12345",
-                updatedAt: new Date(2022, 12, 10),
-                userId: "BR9u18SJzvVNzcrkEIcJDPxv1ws1",
-                isComplete: true,
-                targetDate: new Date(2022, 12, 14),
-                isArchived: false,
-                },
-              ];
-  
-  test("should return the initial state", () => {
-    expect(reducer(userGoalsInitialState, { type: "no action" })).toEqual(userGoalsInitialState);
-  });
-  
-  test("set userGoals to userGoalsTestState from initialState", () => {
-    // Arrange
-  
-    // Act
-    const userGoalsNextState = reducer(userGoalsInitialState, setUserGoals(userGoalsTestState));
-  
-    // Assert
-    expect(userGoalsNextState["userGoals"][0]).toEqual({
-        userGoals: [
-                {
-                createdAt: new Date(2022, 12, 14),
-                goalName: "Write Functions",
-                goalNotes: "recurring goal",
-                id: "12345",
-                updatedAt: new Date(2022, 12, 10),
-                userId: "BR9u18SJzvVNzcrkEIcJDPxv1ws1",
-                isComplete: true,
-                targetDate: new Date(2022, 12, 14),
-                isArchived: false,
-                },
-              ],
-            });
-  });
+const userGoalsInitialState: UserGoalsData[] = [];
 
-const goalSectionsInitialState: GoalSection[] = [
+const userGoalsTestState: UserGoalsData[] = [
+  {
+    createdAt: new Date(2022, 12, 14),
+    goalName: "Write Functions",
+    goalNotes: "recurring goal",
+    id: "12345",
+    updatedAt: new Date(2022, 12, 10),
+    userId: "BR9u18SJzvVNzcrkEIcJDPxv1ws1",
+    isComplete: true,
+    targetDate: new Date(2022, 12, 14),
+    isArchived: false,
+  },
+];
+
+test("should return the initial state", () => {
+  expect(reducer(userGoalsInitialState, { type: "no action" })).toEqual(
+    userGoalsInitialState
+  );
+});
+
+test("set userGoals to userGoalsTestState from initialState", () => {
+  // Arrange
+
+  // Act
+  const userGoalsNextState = reducer(
+    userGoalsInitialState,
+    setUserGoals(userGoalsTestState)
+  );
+
+  // Assert
+  expect(userGoalsNextState["userGoals"][0]).toEqual({
+    createdAt: new Date(2022, 12, 14),
+    goalName: "Write Functions",
+    goalNotes: "recurring goal",
+    id: "12345",
+    updatedAt: new Date(2022, 12, 10),
+    userId: "BR9u18SJzvVNzcrkEIcJDPxv1ws1",
+    isComplete: true,
+    targetDate: new Date(2022, 12, 14),
+    isArchived: false,
+  });
+});
+
+
+
+
+const goalsSectionsInitialState: GoalSection[] = [
     { sectionName: "",
       userGoals: [],
     },
@@ -75,17 +77,20 @@ const goalSectionsInitialState: GoalSection[] = [
   ];
   
   test("should return the initial state", () => {
-    expect(reducer(goalSectionsInitialState, { type: "no action" })).toEqual(goalSectionsInitialState);
+    expect(reducer(goalsSectionsInitialState, { type: "no action" })).toEqual(goalsSectionsInitialState);
   });
   
-  test("set goalsSections to GoalsSectionsTestState from initialState", () => {
+  test("set goalsSections to goalsSectionsState from initialState", () => {
     // Arrange
   
     // Act
-    const goalsSectionsNextState = reducer(goalSectionsInitialState, setGoalsSections(goalsSectionsTestState));
+    const goalsSectionsNextState = reducer(
+      goalsSectionsInitialState,
+      setGoalsSections(goalsSectionsTestState)
+    );
   
     // Assert
-    expect(goalsSectionsNextState["sectionGoals"][0]).toEqual({
+    expect(goalsSectionsNextState["goalsSections"][0]).toEqual({
         sectionName: "Archived",
         userGoals: [
                 {
@@ -102,4 +107,3 @@ const goalSectionsInitialState: GoalSection[] = [
               ],
             });
   });
-  
