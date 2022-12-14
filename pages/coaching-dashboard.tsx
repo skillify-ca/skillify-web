64ebd6a06055e1dbb319075a53c1d72fdf8f148a
LuckyHariharan <lharihar@uwaterloo.ca>
2022-12-14 12:37:41 -0500

    Store users in local state

--- pages/coaching-dashboard.tsx
+++ pages/coaching-dashboard.tsx
@@ -1,15 +1,19 @@
 import { useQuery } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import ProfileDetailCard from "../components/coding/studentPortal/ProfileDetailCard";
 import {
   FetchUserProfileCardResponse,
   FETCH_USER_PROFILE_CARD,
+  Users,
 } from "../graphql/fetchUserProfileCard";
 import { format } from "date-fns";
 import Link from "next/link";
 const coachingDashboard = () => {
+  const [userList, setUserList] = useState<Users[]>([]);
+
   const { loading, data } = useQuery<FetchUserProfileCardResponse>(
-    FETCH_USER_PROFILE_CARD
+    FETCH_USER_PROFILE_CARD,
+    { onCompleted: (data) => setUserList(data.users) }
   );
 
   if (loading) {
@@ -20,26 +24,28 @@ const coachingDashboard = () => {
     <div className="flex flex-col p-4 m-4 ">
       <p className="mb-8 text-3xl">Coaching Dashboard</p>
       <h2 className="mb-4">Enrolled Students</h2>
+
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
-        {data.users.map((it) => (
-          <Link href={"profile/" + it.link}>
-            <div className="">
-              <ProfileDetailCard
-                avatar={
-                  it.profile_image == null
-                    ? "../../images/logo-2.png"
-                    : it.profile_image
-                }
-                name={it.name}
-                joinDate={it.created_at}
-                badges={it.badges_earned}
-                currentFocus={""}
-                nextGoal={""}
-                link={it.link}
-              />
-            </div>
-          </Link>
-        ))}
+        {userList.length > 0 &&
+          userList.map((it) => (
+            <Link href={"profile/" + it.link}>
+              <div className="">
+                <ProfileDetailCard
+                  avatar={
+                    it.profile_image == null
+                      ? "../../images/logo-2.png"
+                      : it.profile_image
+                  }
+                  name={it.name}
+                  joinDate={it.created_at}
+                  badges={it.badges_earned}
+                  currentFocus={""}
+                  nextGoal={""}
+                  link={it.link}
+                />
+              </div>
+            </Link>
+          ))}
       </div>
     </div>
   );
