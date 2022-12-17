ccee7be656690427e826792a9a772cbb0c73a83f
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2022-12-17 17:08:35 -0600

    adjusted where the ExpandableContainer is implemented in userGoals
    1.  This fixes the bug -- if the section is empty such as "archived" it will no longer show an empty container.

--- components/coding/GoalsSectionComponent.tsx
+++ components/coding/GoalsSectionComponent.tsx
@@ -30,9 +30,9 @@ export default function GoalsSection({
   userGoals,
 }: GoalsSectionProps) {
   return (
-    <ExpandableContainer open={true} title={""}>
-      <div className="dark:text-white">
-        {userGoals.length > 0 && (
+    <div className="dark:text-white">
+      {userGoals.length > 0 && (
+        <ExpandableContainer open={true} title={""}>
           <div className="grid grid-cols-5 text-sm font-semibold text-center border-b-2 md:grid-cols-12 md:text-lg">
             <p className="font-semibold">{sectionName}</p>
             <p className="col-span-2 md:col-span-4">Goal</p>
@@ -40,39 +40,36 @@ export default function GoalsSection({
             <p className="font-semibold md:col-span-2">Target Date</p>
             <p className="hidden md:block md:col-span-2">Days Remaining</p>
           </div>
-        )}
+        </ExpandableContainer>
+      )}
 
-        {userGoals.map((goal, index) => {
-          return (
-            <div
-              className={`grid grid-cols-5 my-2 text-sm text-center md:grid-cols-12 md:text-lg place-items-center ${returnGoalStyle(
-                goal
-              )}`}
-            >
-              <p>{index + 1}.</p>
-              <p className="col-span-2 md:col-span-4">{goal.goalName}</p>
-              <p className="hidden md:block md:col-span-2">
-                {format(new Date(goal.createdAt), "MM/dd/yyyy")}
-              </p>
-              <p className="hidden md:block col-span-1 md:col-span-2">
-                {format(new Date(goal.targetDate), "MM/dd/yyyy")}
-              </p>
-              <p className="md:hidden col-span-1">
-                {format(new Date(goal.targetDate), "MM/dd")}
-              </p>
-              <p className="hidden md:block md:col-span-2">
-                {differenceInCalendarDays(
-                  new Date(goal.targetDate),
-                  new Date()
-                )}
-              </p>
-              <Link href={"/goals/" + goal.id}>
-                <PencilAltIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-    </ExpandableContainer>
+      {userGoals.map((goal, index) => {
+        return (
+          <div
+            className={`grid grid-cols-5 my-2 text-sm text-center md:grid-cols-12 md:text-lg place-items-center ${returnGoalStyle(
+              goal
+            )}`}
+          >
+            <p>{index + 1}.</p>
+            <p className="col-span-2 md:col-span-4">{goal.goalName}</p>
+            <p className="hidden md:block md:col-span-2">
+              {format(new Date(goal.createdAt), "MM/dd/yyyy")}
+            </p>
+            <p className="hidden md:block col-span-1 md:col-span-2">
+              {format(new Date(goal.targetDate), "MM/dd/yyyy")}
+            </p>
+            <p className="md:hidden col-span-1">
+              {format(new Date(goal.targetDate), "MM/dd")}
+            </p>
+            <p className="hidden md:block md:col-span-2">
+              {differenceInCalendarDays(new Date(goal.targetDate), new Date())}
+            </p>
+            <Link href={"/goals/" + goal.id}>
+              <PencilAltIcon className="w-5 h-5 cursor-pointer hover:text-yellow-600" />
+            </Link>
+          </div>
+        );
+      })}
+    </div>
   );
 }
