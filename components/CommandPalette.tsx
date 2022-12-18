99a93c5d35f2981249e577a120d70ee6fc63ae66
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-12-18 17:28:43 -0500

    Add a command palette

---
+++ components/CommandPalette.tsx
@@ -0,0 +1,185 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import CommandPalette from "react-command-palette";
+
+export default function SkillifyCommandPalette() {
+  const router = useRouter();
+
+  const commands = [
+    {
+      name: "Dashboard",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6 mr-4"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+        </svg>
+      ),
+      command() {
+        router.push("/studentPortal");
+      },
+    },
+    {
+      name: "Profile",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6 mr-4"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      ),
+      command() {
+        router.push("/profile");
+      },
+    },
+    {
+      name: "Goals",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6 mr-4"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.166 2.621v.858c-1.035.148-2.059.33-3.071.543a.75.75 0 00-.584.859 6.753 6.753 0 006.138 5.6 6.73 6.73 0 002.743 1.346A6.707 6.707 0 019.279 15H8.54c-1.036 0-1.875.84-1.875 1.875V19.5h-.75a2.25 2.25 0 00-2.25 2.25c0 .414.336.75.75.75h15a.75.75 0 00.75-.75 2.25 2.25 0 00-2.25-2.25h-.75v-2.625c0-1.036-.84-1.875-1.875-1.875h-.739a6.706 6.706 0 01-1.112-3.173 6.73 6.73 0 002.743-1.347 6.753 6.753 0 006.139-5.6.75.75 0 00-.585-.858 47.077 47.077 0 00-3.07-.543V2.62a.75.75 0 00-.658-.744 49.22 49.22 0 00-6.093-.377c-2.063 0-4.096.128-6.093.377a.75.75 0 00-.657.744zm0 2.629c0 1.196.312 2.32.857 3.294A5.266 5.266 0 013.16 5.337a45.6 45.6 0 012.006-.343v.256zm13.5 0v-.256c.674.1 1.343.214 2.006.343a5.265 5.265 0 01-2.863 3.207 6.72 6.72 0 00.857-3.294z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      ),
+      command() {
+        router.push("/goals");
+      },
+    },
+    {
+      name: "Coaches",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className={`w-6 h-6 mr-4`}
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+        </svg>
+      ),
+      command() {
+        router.push("/coaches");
+      },
+    },
+    {
+      name: "Workshops",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6 mr-4"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+        </svg>
+      ),
+      command() {
+        router.push("/workshops");
+      },
+    },
+    {
+      name: "Course: Coding Basics",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6 mr-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+          />
+        </svg>
+      ),
+      command() {
+        router.push("/studentPortal");
+      },
+    },
+    {
+      name: "Course: Web Development",
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-6 h-6 mr-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M8 13v-1m4 1v-3m4 3V8M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
+          />
+        </svg>
+      ),
+      command() {
+        router.push("/studentPortal/web");
+      },
+    },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+  // CommandPalette.jsx
+  useEffect(() => {
+    const onKeydown = (e) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setIsOpen(true);
+      }
+    };
+    window.addEventListener("keydown", onKeydown);
+    return () => {
+      window.removeEventListener("keydown", onKeydown);
+    };
+  }, []);
+
+  return (
+    <div>
+      {isOpen && (
+        <CommandPalette
+          open={isOpen}
+          resetInputOnOpen={true}
+          onRequestClose={() => {
+            setIsOpen(false);
+          }}
+          closeOnSelect={true}
+          commands={commands}
+          renderCommand={RenderCommand}
+        />
+      )}
+    </div>
+  );
+}
+
+function RenderCommand(suggestion) {
+  // A suggestion object will be passed to your custom component for each command
+  const { icon, name } = suggestion;
+  return (
+    <div className="flex">
+      <div>{icon}</div>
+      <p>{name}</p>
+    </div>
+  );
+}
