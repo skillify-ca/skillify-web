d938702444fbba5f4f460bca6d7ac578873708e4
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2022-12-19 16:57:01 -0500

    Coaching Dashboard Redux (#831)
    * Add name Lucky to list of names
    
    Added my preferred name Lucky to the array of names.
    
    * Store users in local state
    
    * Implement Redux Attempt
    
    * Implement Redux attempt 2
    
    * Dispatch Redux
    
    * skillify variable naming consistency
    
    * Revert "skillify variable naming consistency"
    
    This reverts commit b48aaba1391bf19098bf6a0ef1e1a4dce00331de.
    
    * Display userList redux state
    
    * skilifyUsersState fix
    
    * Display Profile cards
    
    * Store data using Redux
    
    Display Stored data using Redux + small JoinDate/BadgesEarned
    
    * .user selector
    
    * Remove alerts and comments
    
    * Name skillifyUserSelector
    
    * Pull userList from selector
    
    * Deconstructing userList object

---
+++ redux/skillifyUsersSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction, Slice } from "@reduxjs/toolkit";
+import SkillCard from "../components/math/stories/SkillCard";
+import { Users } from "../graphql/fetchUserProfileCard";
+import { RootState } from "./rootReducer";
+
+export type SkillifyUsersState = {
+  userList: Users[];
+};
+
+const initialState: SkillifyUsersState = { userList: [] };
+
+export const skillifyUsersSlice: Slice = createSlice({
+  name: "skillifyUsers",
+  initialState,
+  reducers: {
+    setUserList: (
+      state: SkillifyUsersState,
+      action: PayloadAction<Users[]>
+    ) => {
+      state.userList = action.payload;
+    },
+  },
+});
+
+export const { setUserList } = skillifyUsersSlice.actions;
+export default skillifyUsersSlice.reducer;
+export const skillifyUsersSelector = (state: RootState) =>
+  state.skillifyUsersState;
