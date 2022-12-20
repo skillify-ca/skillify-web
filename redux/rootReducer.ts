43f877d3ca5f7149a39edad407877da3f73f3c44
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2022-12-20 16:52:58 -0500

    Unit Test Coaching Dashboard (#837)
    * Add name Lucky to list of names
    
    Added my preferred name Lucky to the array of names.
    
    * Store users in local state
    
    * Implement Redux Attempt
    
    * Implement Redux attempt 2
    
    * Dispatch Redux
    
    * skillify variable naming consistency
    
    * Revert "skillify variable naming consistency"
    
    This reverts commit b48aaba1391bf19098bf6a0ef1e1a4dce00331de.
    
    * Display userList redux state
    
    * skilifyUsersState fix
    
    * Display Profile cards
    
    * Store data using Redux
    
    Display Stored data using Redux + small JoinDate/BadgesEarned
    
    * .user selector
    
    * Remove alerts and comments
    
    * Name skillifyUserSelector
    
    * Setup with Kari
    
    Setup unit testing with Kari, currently both test pass.
    
    * Remove console.logs
    
    * Setup Achievement component
    
    Setup achievement component inside profile/v2
    
    * Revert "Setup Achievement component"
    
    This reverts commit af1d9f883c01b18c8360e74388177acc95d44f30.
    
    * Change skilllifyusers to users
    
    * userState naming Consistency
    
    * Remove unnecessary imports
    
    * variable naming consistency
    
    * de-skillify names of variables and files
    
    * userSlice name change

--- redux/rootReducer.ts
+++ redux/rootReducer.ts
@@ -6,7 +6,7 @@ import evaluateExpressionSlice, {
 import profileSlice, { ProfileState } from "./profileSlice";
 import { QuizState, quizSlice } from "./quizSlice";
 import { SidebarState, sidebarSlice } from "./sidebarSlice";
-import skillifyUsersSlice, { SkillifyUsersState } from "./skillifyUsersSlice";
+import userSlice, { userState } from "./userSlice";
 import skillRatingsSlice, { SkillRatingsState } from "./skillRatingsSlice";
 import userGoalsSlice, { UserGoalsState } from "./userGoalsSlice";
 
@@ -18,7 +18,7 @@ type State = {
   skillRatingsState: SkillRatingsState;
   userGoalsState: UserGoalsState;
   profileState: ProfileState;
-  skillifyUsersState: SkillifyUsersState;
+  userState: userState;
 };
 const evaluateExpressionReducer = evaluateExpressionSlice;
 const quizReducer = quizSlice.reducer;
@@ -26,7 +26,7 @@ const sidebarReducer = sidebarSlice.reducer;
 const skillRatingsReducer = skillRatingsSlice;
 const userGoalsReducer = userGoalsSlice;
 const profileReducer = profileSlice;
-const skillifyUsersReducer = skillifyUsersSlice;
+const userReducer = userSlice;
 
 const rootReducer = combineReducers({
   evaluateExpressionHistory: evaluateExpressionReducer,
@@ -35,7 +35,7 @@ const rootReducer = combineReducers({
   skillRatingsState: skillRatingsReducer,
   userGoalsState: userGoalsReducer,
   profileState: profileReducer,
-  skillifyUsersState: skillifyUsersReducer,
+  userState: userReducer,
 });
 
 export type RootState = State;
