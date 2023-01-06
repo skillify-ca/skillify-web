c11b7c98a54d8d93f75c42cd45cb35fa6865edca
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-01-05 20:01:15 -0600

    used a filter and map function to bypass skillRow component
    1. used the query to get a unique list of unitNames.
    2.  created buttons to display the skills for each unit.
    3. changed the String to string in the types for the skillRatings
    4. WIP on the styling of the buttons

--- components/coding/SkillRatingsComponent.tsx
+++ components/coding/SkillRatingsComponent.tsx
@@ -1,4 +1,5 @@
 import { useQuery, useMutation } from "@apollo/client";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   FetchSkillsAndRatings,
@@ -15,6 +16,7 @@ import {
   skillRatingsSelector,
   setSkillRatings,
 } from "../../redux/skillRatingsSlice";
+import SkillRow from "../skillRatings/SkillRow";
 import SkillSection from "../skillRatings/SkillSection";
 import { Button } from "../ui/Button";
 import ExpandableContainer from "./ExpandableContainer";
@@ -23,6 +25,7 @@ export default function SkillRatingsComponent(props) {
   const dispatch = useDispatch();
   const { user } = useAuth();
   const { skillRatings } = useSelector(skillRatingsSelector);
+  const [activeTab, setActiveTab] = useState("");
 
   const {} = useQuery<FetchSkillsAndRatings>(FETCH_SKILLS_AND_RATINGS, {
     variables: {
@@ -42,11 +45,40 @@ export default function SkillRatingsComponent(props) {
     },
   });
 
+  const unitNames = skillRatings.map((skill) => skill.unitName);
+
+  const sections = unitNames.filter(
+    (unitName, index, array) => array.indexOf(unitName) === index
+  );
+
   return (
     <div className="flex flex-row overflow-auto-bg-scroll">
       <ExpandableContainer open={true} title={""}>
-        <div className="flex flex-col p-4 m-4">
-          {skillRatings && <SkillSection skillSection={skillRatings} />}
+        <div className="flex flex-row space-x-4">
+          {sections.map((it) => (
+            <button
+              className="flex flex-row space-between text-xl text-gray-500 w-20 h-12 cursor-pointer hover:text-black-500 hover:underline hover:hover:decoration-[0.20rem]"
+              onClick={() => setActiveTab(it)}
+            >
+              {it}
+            </button>
+          ))}
+          <div className={`${activeTab ? "flex flex-col p-4 m-4" : "hidden"} `}>
+            {skillRatings &&
+              skillRatings
+                .filter((skill) => skill.unitName === activeTab)
+                .map((it) => (
+                  <SkillRow
+                    skillRow={{
+                      userSkillId: it.userSkillId,
+                      skillId: it.skillId,
+                      skillName: it.skillName,
+                      unitName: it.unitName,
+                      studentRating: it.studentRating,
+                    }}
+                  />
+                ))}
+          </div>
         </div>
         <div className="flex float-right p-4 mr-8 mt-8">
           <Button
