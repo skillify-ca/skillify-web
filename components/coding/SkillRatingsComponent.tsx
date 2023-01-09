9ec24f459b97c0529fce2c487f2f41e425975237
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-01-08 18:51:43 -0600

    remove commented out code

--- components/coding/SkillRatingsComponent.tsx
+++ components/coding/SkillRatingsComponent.tsx
@@ -125,5 +125,3 @@ export default function SkillRatingsComponent(props) {
     </div>
   );
 }
-
-//"ml-8 justify-content-center text-2xl text-gray-500 w-36 py-2 h-12 cursor-pointer hover:text-black-500 hover:underline hover:hover:decoration-[0.18rem] hover:underline-offset-[16px]"
