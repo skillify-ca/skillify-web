0ddf8717293eae0e882fe8f404007d2a3e0b7c21
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-01-10 09:49:03 -0600

    removed unused import

--- components/coding/profileV2/AchievementComponent.tsx
+++ components/coding/profileV2/AchievementComponent.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import ExpandableContainer from "../ExpandableContainer";
 import { PencilAltIcon } from "@heroicons/react/outline";
 import { User } from "../../../graphql/fetchUserProfile";
 import { transformUserBadgeData } from "./AchievementTransformData";
