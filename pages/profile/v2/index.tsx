84d98e1beaf96415d247cf766ce63d6cc4dd7018
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-01-13 08:16:58 -0600

    cleaning up html

--- pages/profile/v2/index.tsx
+++ pages/profile/v2/index.tsx
@@ -31,14 +31,14 @@ export default function Profile(props) {
   );
 
   return (
-    <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll">
+    <div className="flex flex-col p-4 m-4 overflow-auto bg-scroll space-y-9">
       <ProfileHeaderComponent userId={user.uid} />
-      <div className="grid grid-cols-1 my-9">
+      <div>
         <ExpandableContainer open={true} title={"Assignments"}>
           <AssignmentSectionComponent />{" "}
         </ExpandableContainer>
       </div>
-      <div className="grid grid-cols-1 my-9">
+      <div className="grid">
         <ExpandableContainer open={true} title={"Goals"}>
           <GoalsSectionComponent
             inProfile={true}
@@ -48,12 +48,12 @@ export default function Profile(props) {
           />
         </ExpandableContainer>
       </div>
-      <div className="grid grid-cols-1 my-9">
+      <div className="grid">
         <ExpandableContainer open={true} title={"Skill Ratings"}>
           <SkillRatingsComponent />
         </ExpandableContainer>
       </div>
-      <div className="grid grid-cols-1 my-9">
+      <div className="grid">
         <ExpandableContainer open={true} title={"Achievements"}>
           <AchievementComponent user={user} />
         </ExpandableContainer>
