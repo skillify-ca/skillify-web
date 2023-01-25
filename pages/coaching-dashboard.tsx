38be3d5bd54e604f9b80a4ed08be8a20b48d369b
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-01-25 15:37:38 -0600

    added an index to the return of the map to fix the linter error

--- pages/coaching-dashboard.tsx
+++ pages/coaching-dashboard.tsx
@@ -27,25 +27,29 @@ const coachingDashboard = () => {
       <p className="mb-8 text-3xl">Coaching Dashboard</p>
       <h2 className="mb-4">Enrolled Students</h2>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
-        {userList.map((it) => (
-          <Link href={"profile/" + it.link}>
-            <div className="">
-              <ProfileDetailCard
-                avatar={
-                  it.profile_image == null
-                    ? "../../images/logo-2.png"
-                    : it.profile_image
-                }
-                name={it.name}
-                joinDate={it.created_at}
-                badges={it.badges_earned}
-                currentBadge={it.coding_badge}
-                nextGoal={""}
-                link={it.link}
-              />
+        {userList.map((it, index) => {
+          return (
+            <div key={index}>
+              <Link href={"profile/" + it.link}>
+                <div className="">
+                  <ProfileDetailCard
+                    avatar={
+                      it.profile_image == null
+                        ? "../../images/logo-2.png"
+                        : it.profile_image
+                    }
+                    name={it.name}
+                    joinDate={it.created_at}
+                    badges={it.badges_earned}
+                    currentBadge={it.coding_badge}
+                    nextGoal={""}
+                    link={it.link}
+                  />
+                </div>
+              </Link>
             </div>
-          </Link>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
