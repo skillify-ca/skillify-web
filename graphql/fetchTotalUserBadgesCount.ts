5bec79f7c6eaf2f74c2661bcb1c01b6a0aec0b50
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-01-26 20:32:20 -0600

    Fixed the distinct count!

--- graphql/fetchTotalUserBadgesCount.ts
+++ graphql/fetchTotalUserBadgesCount.ts
@@ -2,10 +2,10 @@ import { gql } from "@apollo/client";
 
 export const FETCH_TOTAL_USER_BADGES_COUNT = gql`
 query fetchTotalUserBadgesCount {
-    user_coding_badges_aggregate {
-      aggregate {
-        count(columns: badgeId)
-      }
+  user_coding_badges_aggregate {
+    aggregate {
+      count(columns: badgeId, distinct: true)
     }
   }
+}
 `;
