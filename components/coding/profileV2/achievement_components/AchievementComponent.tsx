735a58242f867cfa4e8bf585bf741cb9550daf2b
LuckyHariharan <lharihar@uwaterloo.ca>
2023-02-01 09:12:31 -0500

    Style Save Buttons
    Add slate-800 background to skill-ratings for consitency

--- components/coding/profileV2/achievement_components/AchievementComponent.tsx
+++ components/coding/profileV2/achievement_components/AchievementComponent.tsx
@@ -79,10 +79,6 @@ const AchievementComponent = ({
       <div className="flex justify-end w-full ">
         {isEditable && (
           <div className="mb-4">
-            <Button
-              label={"Save"}
-              onClick={() => handleOnSaveButtonClick()}
-            ></Button>
             <button
               onClick={() => setEditMode(!editMode)}
               className="w-5 h-5 cursor-pointer hover:text-yellow-600"
@@ -113,6 +109,13 @@ const AchievementComponent = ({
           })}
         </div>
       )}
+      <div className="p-4 md-8">
+        {" "}
+        <Button
+          label={"Save"}
+          onClick={() => handleOnSaveButtonClick()}
+        ></Button>
+      </div>
     </div>
   );
 };
