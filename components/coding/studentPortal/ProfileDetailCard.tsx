bf8a79d34e3a791123f2564456e5835e600f66f0
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-01-31 18:10:08 -0600

    added the circle badge and adjusted some styling

--- components/coding/studentPortal/ProfileDetailCard.tsx
+++ components/coding/studentPortal/ProfileDetailCard.tsx
@@ -32,7 +32,7 @@ function ProfileDetailCard({
   return (
     <div
       className="grid grid-cols-4 h-full w-full text-sm border-2 bg-slate-50 text-slate-800 border-slate-800 hover:bg-violet-100"
-      style={{ height: "320px", objectFit: "contain" }}
+      style={{ height: "340px", objectFit: "contain" }}
     >
       <div className="flex ml-3 place-items-center col-span-4 mt-4">
         <img
@@ -55,17 +55,21 @@ function ProfileDetailCard({
         </div>
       </div>
       <div className="ml-4 col-span-4 p-1 mb-4">
-        <p className="font-bold">Current Focus:</p>
+        <p className="ml-2 font-bold">Current Focus:</p>
         <div className="flex items-center p-2 ">
           <img
             src={currentBadge?.image}
-            className="col-span-1 flex items-center w-16 mr-4 rounded-full"
+            className="col-span-1 items-center w-16 mr-5 rounded-full"
           />
-          <p className="col-span-3 align-middle"> {currentBadge?.title}</p>
+          <p className="col-span-3 underline"> {currentBadge?.title}</p>
         </div>
-        <p className="font-bold">Last Goal:</p>
-        <div className="flex items-center p-1 pl-5 ">
-          <div className="col-span-1 mr-9">{completedDate} </div>
+        <p className="ml-2 font-bold">Last Goal:</p>
+        <div className="flex items-center p-2">
+          <div className="col-span-1 mr-5">
+            <div className="col-span-1 bg-slate-800 w-16 h-16 border-black rounded-full flex items-center justify-center">
+              <p className="text-center text-slate-100"> {completedDate}</p>
+            </div>
+          </div>
           <p className="col-span-3 underline"> {completedGoal}</p>
         </div>
       </div>
