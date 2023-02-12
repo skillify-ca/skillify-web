2f31feb29eedad2ef64c72ef6324c0afd417d478
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-02-12 13:15:24 -0600

    add padding and top margin to post layout

--- components/coding/blog/PostLayout.tsx
+++ components/coding/blog/PostLayout.tsx
@@ -7,7 +7,7 @@ export default function PostLayout({ children }: PostLayoutProps) {
   return (
     <div className="md:flex md:flex-col items-center bg-gray-100 heropattern-hideout-gray-300">
       <LandingNavbar />
-      <div className="md:flex md:flex-col items-center max-w-5xl gap-4 md:p-4 md:mx-16 md:my-8 bg-white shadow-lg">
+      <div className="md:flex md:flex-col items-center max-w-5xl gap-4 p-4 md:mx-16 my-8 bg-white shadow-lg">
         {children}
       </div>
     </div>
