70bccae1807148397cbff6a5f1ccbed64a0c0604
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-02-17 10:34:34 -0600

    updating imports for radix so only what we are using is imported.

--- pages/studentPortal/web/React/radix-ui.tsx
+++ pages/studentPortal/web/React/radix-ui.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import * as Progress from "@radix-ui/react-progress";
-import * as Tooltip from "@radix-ui/react-tooltip";
+import { Root, Indicator } from "@radix-ui/react-progress";
+import {
+  Provider,
+  Root as TooltipRoot,
+  Trigger,
+  Portal,
+  Content,
+  Arrow,
+} from "@radix-ui/react-tooltip";
 import { PlusCircleIcon } from "@heroicons/react/outline";
+import React, { useEffect, useState } from "react";
 
 const ProgressDemo = () => {
-  const [progress, setProgress] = React.useState(13);
+  const [progress, setProgress] = useState(13);
 
-  React.useEffect(() => {
+  useEffect(() => {
     const timer = setTimeout(() => setProgress(66), 500);
     return () => clearTimeout(timer);
   }, []);
@@ -15,38 +22,38 @@ const ProgressDemo = () => {
     <div className="space-y-10">
       <div className="items-center bg-gray-400">
         This is the progress bar
-        <Progress.Root
+        <Root
           className="relative overflow-hidden bg-blue-400 rounded-full w-[300px] h-[25px]"
           style={{
             transform: "translateZ(0)",
           }}
           value={66}
         >
-          <Progress.Indicator
+          <Indicator
             className="bg-white w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
             style={{ transform: `translateX(-${100 - progress}%)` }}
           />
-        </Progress.Root>
+        </Root>
       </div>
       <div>
-        <Tooltip.Provider>
-          <Tooltip.Root>
-            <Tooltip.Trigger asChild>
+        <Provider>
+          <TooltipRoot>
+            <Trigger asChild>
               <button className="text-blue-500 shadow-black-500 hover:bg-purple-200 inline-flex h-[35px] w-[35px] items-center justify-center rounded-full bg-white shadow-[0_2px_10px] outline-none focus:shadow-[0_0_0_2px] focus:shadow-black-300">
                 <PlusCircleIcon />
               </button>
-            </Tooltip.Trigger>
-            <Tooltip.Portal>
-              <Tooltip.Content
+            </Trigger>
+            <Portal>
+              <Content
                 className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-blue-500 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
                 sideOffset={5}
               >
                 Access Denied. Pony up the cash, please.
-                <Tooltip.Arrow className="fill-white" />
-              </Tooltip.Content>
-            </Tooltip.Portal>
-          </Tooltip.Root>
-        </Tooltip.Provider>
+                <Arrow className="fill-white" />
+              </Content>
+            </Portal>
+          </TooltipRoot>
+        </Provider>
       </div>
     </div>
   );
