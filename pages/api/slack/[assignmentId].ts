40add009852ec511f13255f1edf584ef4a939772
Raveen Reddy <raveenreddy89@gmail.com>
2023-02-21 10:55:23 -0500

    move slack request logic to next api route

---
+++ pages/api/slack/[assignmentId].ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const url = `https://hooks.slack.com/services/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`;
+
+  const { assignmentId } = req.query;
+
+  axios
+    .post(url, {
+      text: `New submission for assignment ${assignmentId}`,
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  try {
+    return res.status(200).json({
+      status: "OK",
+    });
+  } catch (err) {
+    return res.status(400).json({
+      status: err,
+    });
+  }
+};
