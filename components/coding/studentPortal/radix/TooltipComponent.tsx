777e824a35b8a2d0e3ebbb49bf8be5c26f2f5d4e
vithushan19 <vithushan19@gmail.com>
2023-02-23 14:44:14 -0500

    remove tooltip util

--- components/coding/studentPortal/radix/TooltipComponent.tsx
+++ components/coding/studentPortal/radix/TooltipComponent.tsx
@@ -7,7 +7,6 @@ import {
   Content,
   Arrow,
 } from "@radix-ui/react-tooltip";
-import { getTooltipClass } from "../../../../pages/api/radixClasses/getTooltipClass";
 
 interface TooltipComponentProps {
   message: string;
@@ -25,7 +24,7 @@ const TooltipComponent: React.FC<TooltipComponentProps> = ({
         <Trigger asChild>{children}</Trigger>
         <Portal>
           <Content
-            className={`${getTooltipClass()} `}
+            className={`data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-red-500 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]`}
             sideOffset={0}
             align="center"
             style={{ whiteSpace: "normal", width: "200px", lineHeight: "1.5" }}
