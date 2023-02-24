7a18c082be375b876a80403b30f629de2487c8c9
vithushan19 <vithushan19@gmail.com>
2023-02-24 12:55:05 -0500

    fix switcher button

--- components/coding/studentPortal/Layout.tsx
+++ components/coding/studentPortal/Layout.tsx
@@ -97,8 +97,8 @@ function Header({
   return (
     <div className="grid w-full h-16 grid-cols-3 border-b-2 bg-backgroundPrimary">
       {/* <div /> */}
-      <div onClick={handleMenuIconClick}>
-        <div className="cursor-pointer dark:text-white lg:hidden">
+      <div onClick={handleMenuIconClick} className="flex items-center pl-4">
+        <div className="cursor-pointer text-textPrimary lg:hidden">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="w-8 h-8"
@@ -120,7 +120,20 @@ function Header({
         onClick={handleToggleClick}
         className="flex items-center justify-end pr-4"
       >
-        <Button backgroundColor="primary" label="Theme" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6 text-textPrimary"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M7.5 21L3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+          />
+        </svg>
       </div>
     </div>
   );
