df029349cb70b38bb32b1aa36c944dc807bce8ab
Vithushan <vithushan19@users.noreply.github.com>
2023-02-24 10:10:47 -0500

    Fix: refactor the layout file to remove uneccessary divs (#889)
    * Extract header into its own component
    
    * fix opacity
    
    * fix layout

--- components/coding/studentPortal/Layout.tsx
+++ components/coding/studentPortal/Layout.tsx
@@ -43,37 +43,18 @@ export const Layout: React.FC = ({ children }) => {
           height: 100%;
         }
       `}</style>
-      <div className="fixed z-20 flex justify-between w-full h-16 col-span-12 p-4 bg-white border-b-2 dark:bg-gray-900">
-        <div onClick={() => setActive(!active)}>
-          <div className="cursor-pointer dark:text-white lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-8 h-8"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" />
-            </svg>
-          </div>
-        </div>
-        <img className="visible w-48 h-8 dark:hidden" src="/images/logo.svg" />
-        <img
-          className="hidden w-48 h-8 dark:visible"
-          src="/images/logo-dark.svg"
-        />
-        <div />
+
+      <div className="fixed z-20 w-full">
+        <Header handleMenuIconClick={() => setActive(!active)} />
       </div>
       <div className="flex">
         {/* Desktop Sidebar */}
-        <div className="hidden h-full max-h-screen grid-cols-1 overflow-auto w-96 lg:grid">
-          <div className="h-16" />
-
+        <div className="hidden h-full max-h-screen grid-cols-1 pt-16 overflow-auto border-r-2 w-96 lg:grid">
           <Sidebar />
         </div>
         <div
-          className={` dark:text-white overflow-auto w-full max-h-screen h-full transition-all transform duration-500 ease-in-out grid grid-cols-1 bg-white dark:bg-gray-800`}
+          className={` dark:text-white overflow-auto pt-16 w-full max-h-screen h-full transition-all transform duration-500 ease-in-out grid grid-cols-1 bg-white dark:bg-gray-800`}
         >
-          <div className="h-16" />
           <div className="min-h-screen">
             <div>{children}</div>
           </div>
@@ -84,12 +65,37 @@ export const Layout: React.FC = ({ children }) => {
       <div
         className={`fixed z-10 w-56 top-16 lg:hidden ${
           active ? "left-0" : "-left-56"
-        } h-full transition-all transform duration-500 ease-in-out`}
+        } h-full transition-all transform duration-500 ease-in-out bg-white dark:bg-gray-900 border-r-2`}
       >
         <Sidebar />
       </div>
     </div>
   );
 };
+
+function Header({ handleMenuIconClick }) {
+  return (
+    <div className="flex justify-between w-full h-16 p-4 bg-white border-b-2 dark:bg-gray-900">
+      <div onClick={handleMenuIconClick}>
+        <div className="cursor-pointer dark:text-white lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-8 h-8"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" />
+          </svg>
+        </div>
+      </div>
+      <img className="visible w-48 h-8 dark:hidden" src="/images/logo.svg" />
+      <img
+        className="hidden w-48 h-8 dark:visisble"
+        src="/images/logo-dark.svg"
+      />
+      <div />
+    </div>
+  );
+}
 
 export default Layout;
