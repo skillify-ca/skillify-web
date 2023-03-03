70494f19b59f8ea544abf37026bcda2803afea8e
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2023-03-03 17:05:11 -0500

    Update blog (#903)

--- pages/blog/making-the-leap-into-development.tsx
+++ pages/blog/making-the-leap-into-development.tsx
@@ -8,11 +8,13 @@ export default function Page() {
       <SEO
         title={"Making The Leap Into Development"}
         description={"An employees transition into programming."}
-        image={"https://melv1n.com/img/learn-to-code-how-to-start.png"}
+        image={"https://miro.medium.com/max/800/1*zSzbF3B9mi7gKTwBkw1O2w.png"}
       />
       <div className="flex flex-col gap-4 p-4">
         <img
-          src={"https://melv1n.com/img/learn-to-code-how-to-start.png"}
+          src={
+            "https://mlof74r28hae.i.optimole.com/w:1001/h:524/q:mauto/https://www.nadahogan.com/wp-content/uploads/2018/05/TakeTheLeap-BlogGraphic.jpg"
+          }
           className="object-cover h-48"
         />
         <h1 className="text-5xl font-bold">Making The Leap Into Development</h1>
@@ -22,7 +24,7 @@ export default function Page() {
           believing that I had missed the boat left me feeling stuck in my job.
           I graduated from the University of Waterloo in 2021, and later secured
           a full-time role as an actuarial analyst. I quickly realized that even
-          though it seemed like I was doing well, I was not happy in my field.{" "}
+          though it seemed like I was doing well, I was not happy in my field.
         </p>
         <p>
           {" "}
@@ -31,17 +33,25 @@ export default function Page() {
           programming. I remembered how I resonated with the course, and enjoyed
           working on each week’s assignment. I decided that enough was enough,
           and resolved to re-introduce the fun of programming into my life. So I
-          quit my job, and enrolled in a coding academy called Skillify.
+          quit my job, and enrolled in a coding academy called Skillify. Their
+          support has accelerated my growth by pointing me in the right
+          direction and providing key people and resources.
         </p>
-        For me, learning to code has not only meant investing in my technical
-        skills, but also my self-confidence, and my ability to embrace the
-        discomfort of solving challenging problems. The process has involved
-        constantly learning, and sharpening my ability to navigate uncertainty
-        effectively. Learning to better manage expectations has also been key.
-        Initially, I tended to rush into ambitious projects to ‘make-up’ for
-        lost time. I learned that if I took a step back and broke problems down
-        into bite-sized pieces, I could tackle challenges that may have felt
-        insurmountable.
+        <p>
+          For me, learning to code has not only meant investing in my
+          programming skills, but also my self-confidence, and my ability to
+          embrace the discomfort of solving challenging problems. The process
+          has involved constantly learning, and sharpening my ability to
+          navigate uncertainty effectively. With these skills, I've built a
+          personal landing page, features for the Skillify website, and I want
+          to explore building applications for mobile!
+        </p>
+        <p>
+          At Skillify I've scoped out and built a few games, and helped build a
+          coaching dashboard for Skillify coaches to more easily track a
+          student's progress. Building this feature included learning to use
+          GraphQL to talk to our backend, which I had never done before!
+        </p>
         <p>
           I know there is a lot left ahead of me on my journey, and I will keep
           my head down and keep learning! I cannot overstate how grateful I am
