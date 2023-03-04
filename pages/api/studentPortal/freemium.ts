6683a5256d12b45fe6e1069b79599ff28f540aa6
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-03-03 19:44:12 -0600

    updated to check for freemium

--- pages/api/studentPortal/freemium.ts
+++ pages/api/studentPortal/freemium.ts
@@ -4,7 +4,7 @@ import { UserRoleData } from '../../../graphql/studentPortal/users/fetchUserRole
 //used for freemium feature calculations
 //daysOld used as a variable to check users that are "x" days old
 export const trialDaysRemaining = (user: UserRoleData, trialLength, daysOld) => {
-  if (user.createdAt) {
+  if (user.userRole === "freemium") {
     const daysOldForUser = subDays(new Date(), daysOld);
     const daysFromUserCreation = addDays(new Date(user.createdAt), trialLength);
     if (isWithinInterval(new Date(user.createdAt), { start: daysOldForUser, end: new Date() })) {
@@ -16,12 +16,11 @@ export const trialDaysRemaining = (user: UserRoleData, trialLength, daysOld) => 
 };
 
 export const elapsedDays = (user: UserRoleData) => {
-  if (user.createdAt) {
+  if (user.userRole === "freemium") {
     const currentDate = new Date();
       const elapsedDays = differenceInCalendarDays(currentDate, new Date(user.createdAt));
       return elapsedDays;
     }
-  return null;
 };
 
 
