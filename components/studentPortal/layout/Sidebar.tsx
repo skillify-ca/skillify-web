339333e6e0d389397413130a34feb4c7d8aac925
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-03-04 19:25:02 -0600

    moved the coding basics and the web dev out of the sidebar items because they will not be grayed out for freemium users
    1.  added the userRole prop to the SidebarItem
    2.  Create an isDisabled state so that when the userRole is "freemium" it will be grayed out and the link inactive.

--- components/studentPortal/layout/Sidebar.tsx
+++ components/studentPortal/layout/Sidebar.tsx
@@ -22,6 +22,7 @@ interface SidebarItemProps {
   page: SidebarPage;
   icon: ReactElement;
   notifications?: boolean;
+  userRole: "freemium" | "student" | "coach";
 }
 const SidebarItem = ({
   name,
@@ -29,29 +30,35 @@ const SidebarItem = ({
   page,
   icon,
   notifications,
+  userRole,
 }: SidebarItemProps) => {
   const { activePage } = useSelector(activePageSelector);
+  const isDisabled = userRole === "freemium";
+  const href = isDisabled ? "" : link;
+  const onClick = isDisabled ? (e) => e.preventDefault() : undefined;
 
   return (
-    <Link href={link}>
-      <div
-        className={`flex flex-wrap items-center p-4 hover:border-l-4 ${
-          activePage === page
-            ? "border-l-4 border-brandPrimary text-brandPrimary cursor-default"
-            : "cursor-pointer hover:bg-backgroundSecondary"
-        } hover:border-brandPrimary hover:text-brandPrimary `}
-      >
-        <div>
-          {notifications ? (
-            <div className="relative left-6 top-1 ">
-              <div className="flex w-2 h-2 bg-red-500 rounded-full"></div>
-            </div>
-          ) : null}
-          {icon}
+    <>
+      <Link onClick={onClick} href={href}>
+        <div
+          className={`flex flex-wrap items-center p-4 cursor-pointer hover:border-l-4 ${
+            activePage === page ? "border-charmander text-charmander" : ""
+          } hover:border-charmander hover:text-charmander ${
+            isDisabled ? "border-gray-500 text-gray-500" : ""
+          }`}
+        >
+          <div>
+            {notifications ? (
+              <div className="relative left-6 top-1 ">
+                <div className="flex w-2 h-2 bg-red-500 rounded-full"></div>
+              </div>
+            ) : null}
+            {icon}
+          </div>
+          {name}
         </div>
-        {name}
-      </div>
-    </Link>
+      </Link>
+    </>
   );
 };
 
@@ -135,6 +142,7 @@ export const Sidebar: React.FC = () => {
               <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
             </svg>
           }
+          userRole={userRole}
         />
         <SidebarItem
           name={"Coaches"}
@@ -150,6 +158,7 @@ export const Sidebar: React.FC = () => {
               <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
             </svg>
           }
+          userRole={userRole}
         />
         <SidebarItem
           name={"Profile"}
@@ -165,6 +174,7 @@ export const Sidebar: React.FC = () => {
               <path d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" />
             </svg>
           }
+          userRole={userRole}
         />
         <SidebarItem
           name={"Goals"}
@@ -181,6 +191,7 @@ export const Sidebar: React.FC = () => {
               <path d="M5.166 2.621v.858c-1.035.148-2.059.33-3.071.543a.75.75 0 00-.584.859 6.753 6.753 0 006.138 5.6 6.73 6.73 0 002.743 1.346A6.707 6.707 0 019.279 15H8.54c-1.036 0-1.875.84-1.875 1.875V19.5h-.75a2.25 2.25 0 00-2.25 2.25c0 .414.336.75.75.75h15a.75.75 0 00.75-.75 2.25 2.25 0 00-2.25-2.25h-.75v-2.625c0-1.036-.84-1.875-1.875-1.875h-.739a6.706 6.706 0 01-1.112-3.173 6.73 6.73 0 002.743-1.347 6.753 6.753 0 006.139-5.6.75.75 0 00-.585-.858 47.077 47.077 0 00-3.07-.543V2.62a.75.75 0 00-.658-.744 49.22 49.22 0 00-6.093-.377c-2.063 0-4.096.128-6.093.377a.75.75 0 00-.657.744zm0 2.629c0 1.196.312 2.32.857 3.294A5.266 5.266 0 013.16 5.337a45.6 45.6 0 012.006-.343v.256zm13.5 0v-.256c.674.1 1.343.214 2.006.343a5.265 5.265 0 01-2.863 3.207 6.72 6.72 0 00.857-3.294z" />
             </svg>
           }
+          userRole={userRole}
         />
         <SidebarItem
           name={"Workshops"}
@@ -196,6 +207,7 @@ export const Sidebar: React.FC = () => {
               <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
             </svg>
           }
+          userRole={userRole}
         />
 
         {userRole === "coach" && (
@@ -217,6 +229,7 @@ export const Sidebar: React.FC = () => {
                 />
               </svg>
             }
+            userRole={userRole}
           />
         )}
 
@@ -225,44 +238,42 @@ export const Sidebar: React.FC = () => {
           <div className="flex items-center justify-between p-4 ">
             <p className="font-bold">Courses</p>
           </div>
-          <SidebarItem
-            name={"Coding Basics"}
-            link={"/studentPortal"}
-            page={"coding_basics"}
-            icon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 mr-4"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-                />
-              </svg>
-            }
-          />
-          <SidebarItem
-            name={"Web Development"}
-            link={"/studentPortal/web"}
-            page={"web"}
-            icon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 mr-4"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path d="M8 13v-1m4 1v-3m4 3V8M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
-              </svg>
-            }
-          />
+          <div className="overflow-auto h-36">
+            <Link href="/studentPortal">
+              <div className="flex p-4 shadow-sm cursor-pointer bg-backgroundPrimary hover:text-charmander hover:bg-backgroundHover">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                  />
+                </svg>
+                <p className="ml-3">Coding Basics</p>
+              </div>
+            </Link>
+            <Link href="/studentPortal/web">
+              <div className="flex p-4 cursor-pointer bg-backgroundPrimary hover:text-charmander hover:bg-backgroundHover">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path d="M8 13v-1m4 1v-3m4 3V8M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
+                </svg>
+                <p className="ml-3">Web Development</p>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
       <div
