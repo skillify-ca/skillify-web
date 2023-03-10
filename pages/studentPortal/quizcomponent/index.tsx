1af73368e75814f00dc7e3de4d438eca19426ccf
LuckyHariharan <lharihar@uwaterloo.ca>
2023-03-10 15:25:12 -0500

    Build Navbar

--- pages/studentPortal/quizcomponent/index.tsx
+++ pages/studentPortal/quizcomponent/index.tsx
@@ -1,13 +1,14 @@
-import React from "react";
 import ProgressBar from "./progressbar";
+import QuizNavbar from "./quiznavbar";
 import SkillSelection from "./skillselection";
 
 const QuizComponent = () => {
   return (
-    <div className="flex flex-col items-center justify-center place-content-center w-full px-4 py-8">
+    <div className="flex flex-col w-full px-4">
+      <QuizNavbar />
       <ProgressBar progress={50} />
 
-      <div className="p-4 flex flex-col items-center space-y-2 ">
+      <div className="p-4 flex flex-col items-center space-y-2 py-8">
         <h1 className="text-2xl font-bold text-center">
           What industries are you interested in working?
         </h1>
