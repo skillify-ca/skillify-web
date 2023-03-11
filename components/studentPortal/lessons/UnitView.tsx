1b8308d144939d6608ddd20b509163cd591b3d0d
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-03-11 00:09:52 -0600

    improve code readability and comment

--- components/studentPortal/lessons/UnitView.tsx
+++ components/studentPortal/lessons/UnitView.tsx
@@ -7,16 +7,25 @@ export type UnitViewProps = {
   userRole: "freemium" | "student" | "coach";
 };
 
+//These are hardcoded lists of the Units/Nodes that will be included with the Freemium version
+const freemiumUnits = ["Introduction", "Github", "TailwindCSS", "React"];
+const freemiumNodes = [
+  "Intro to Web Development",
+  "Deploying a Project on Github & Vercel",
+  "TailwindCSS - Grid & Flexbox",
+  "TailwindCSS - General Styling",
+  "Components",
+  "Props",
+  "Components and Props",
+  "Hooks - useState",
+];
 export const UnitView: React.FC<UnitViewProps> = ({
   data,
   userRole,
 }: UnitViewProps) => {
   return (
     <div className="grid grid-cols-1 gap-4">
-      {userRole === "freemium" &&
-      !["Introduction", "Github", "TailwindCSS", "React"].includes(
-        data.title
-      ) ? (
+      {userRole === "freemium" && !freemiumUnits.includes(data.title) ? (
         <p className="w-48 h-14 bg-gray-300 rounded-full"></p>
       ) : (
         <p className="w-48 p-4 text-center text-white bg-blue-900 rounded-full">
@@ -26,16 +35,9 @@ export const UnitView: React.FC<UnitViewProps> = ({
 
       <div>
         {data.nodes.map((node, index) => {
-          const grayedOut = ![
-            "Intro to Web Development",
-            "Deploying a Project on Github & Vercel",
-            "TailwindCSS - Grid & Flexbox",
-            "TailwindCSS - General Styling",
-            "Components",
-            "Props",
-            "Components and Props",
-            "Hooks - useState",
-          ].some((str) => node.description.includes(str));
+          const grayedOut = !freemiumNodes.some((str) =>
+            node.description.includes(str)
+          );
           const nodeProps = {
             hiddenLine: index === data.nodes.length - 1,
             completed: node.completed,
