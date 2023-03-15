5cbaad2705c7416634bb550e9735d7f8ab170753
LuckyHariharan <lharihar@uwaterloo.ca>
2023-03-15 19:43:53 -0400

    Finalize page styling
    and moved image to the image directory

--- pages/studentPortal/quizcomponent/career-results.tsx
+++ pages/studentPortal/quizcomponent/career-results.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 import { Button } from "../../../components/ui/Button";
 import QuizNavbar from "./quiznavbar";
-import SoftwareEngineer from "./software-engineer.png";
 
 const Career_Results = () => {
   return (
     <div className="flex flex-col w-full max-w-4xl mx-auto md:mr-12 text-center">
-      <QuizNavbar />
-      <div className="font-bold mt-4 text-2xl text-black-600">YOUR RESULTS</div>
+      <QuizNavbar></QuizNavbar>
+      <div className="font-bold mt-4 text-2xl ">YOUR RESULTS</div>
       <div className=" text-lg font-semibolds ">
         Here are jobs in tech you’re compatible with. Click on a career to learn
         more!
       </div>
-      <img src={SoftwareEngineer} alt="Software Engineer" className="mt-8" />
-      <div className="mt-4 mx-4 text-2xl font-semibold">
+      <img
+        src="/images/software-engineer.png"
+        alt="Software Engineer"
+        className=" p-4 rounded-[40px]"
+      />
+      <div className="mt-4 mx-4 text-2xl font-semibold ">
         Start your career with a Skillify coach today!
       </div>
       <div className="flex flex-col items-center space-y-4 py-4">
         <Button label="Book a call" />
         <Button label="Learn more" backgroundColor="blue" />
       </div>
-      <div>
+      <div className="mx-4">
         Skillify Coding Academy coaches university graduates to start a career
         in tech. Book a free call with one of our expert coaches to discuss the
         best strategy plan for you.
