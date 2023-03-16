7137fc41fd3478bd8cf4dc2b322463584be1936a
LuckyHariharan <lharihar@uwaterloo.ca>
2023-03-16 11:24:45 -0400

    refactor page

--- pages/studentPortal/quizcomponent/career-results.tsx
+++ pages/studentPortal/quizcomponent/career-results.tsx
@@ -4,29 +4,31 @@ import QuizNavbar from "./quiznavbar";
 
 const Career_Results = () => {
   return (
-    <div className="grid  w-full mx-auto text-center justify-items-center">
+    <div className="  w-full mx-auto ">
       <QuizNavbar></QuizNavbar>
-      <div className="font-bold mt-4 text-2xl ">YOUR RESULTS</div>
-      <div className=" text-lg font-semibolds ">
-        Here are jobs in tech you’re compatible with. Click on a career to learn
-        more!
-      </div>
-      <img
-        src="/images/software-engineer.png"
-        alt="Software Engineer"
-        className=" p-4 rounded-[40px]  md:h-60 md:w-80 "
-      />
-      <div className="mt-4 mx-4 text-2xl font-semibold ">
-        Start your career with a Skillify coach today!
-      </div>
-      <div className="flex flex-col items-center space-y-4 py-4">
-        <Button label="Book a call" />
-        <Button label="Learn more" backgroundColor="blue" />
-      </div>
-      <div className="mx-4">
-        Skillify Coding Academy coaches university graduates to start a career
-        in tech. Book a free call with one of our expert coaches to discuss the
-        best strategy plan for you.
+      <div className="grid text-center justify-items-center">
+        <div className="font-bold mt-4 text-2xl ">YOUR RESULTS</div>
+        <div className=" text-lg font-semibolds ">
+          Here are jobs in tech you’re compatible with. Click on a career to
+          learn more!
+        </div>
+        <img
+          src="/images/software-engineer.png"
+          alt="Software Engineer"
+          className=" p-4 rounded-[40px]  md:h-60 md:w-80 "
+        />
+        <div className="mt-4 mx-4 text-2xl font-semibold ">
+          Start your career with a Skillify coach today!
+        </div>
+        <div className="flex flex-col items-center space-y-4 py-4">
+          <Button label="Book a call" />
+          <Button label="Learn more" backgroundColor="blue" />
+        </div>
+        <div className="mx-4">
+          Skillify Coding Academy coaches university graduates to start a career
+          in tech. Book a free call with one of our expert coaches to discuss
+          the best strategy plan for you.
+        </div>
       </div>
     </div>
   );
