9de618ca60d50a8a26b669421eef8b19f44f81c9
vithushan19 <vithushan19@gmail.com>
2023-03-16 12:42:56 -0400

    Moved image into public folder

--- pages/studentPortal/quizcomponent/lang-results.tsx
+++ pages/studentPortal/quizcomponent/lang-results.tsx
@@ -1,19 +1,23 @@
 import { Button } from "../../../components/ui/Button";
 //import QuizNavbar from "./quiznavbar";
-import JS_Logo from "./javascript.jpg";
+import React from "react";
 
 const LangResults = () => {
   return (
-    <div className="flex flex-col w-full max-w-4xl mx-auto md:mr-12 text-center">
-      <div className="font-bold mt-4 text-2xl text-black-600">YOUR RESULTS</div>
-      <div className=" text-lg font-semibolds ">
+    <div className="flex flex-col w-full max-w-4xl mx-auto text-center md:mr-12">
+      <div className="mt-4 text-2xl font-bold text-black-600">YOUR RESULTS</div>
+      <div className="text-lg font-semibolds">
         The first coding language you should learn is...
       </div>
-      <img src={JS_Logo} alt="JavaScript" className="mt-8" />
-      <div className="mt-4 mx-4 text-2xl font-semibold">
+      <img
+        src={"/images/quiz/languages-quiz/javascript.png"}
+        alt="JavaScript"
+        className="w-64 mt-8"
+      />
+      <div className="mx-4 mt-4 text-2xl font-semibold">
         Start your career with a Skillify coach today!
       </div>
-      <div className="flex flex-col items-center space-y-4 py-4">
+      <div className="flex flex-col items-center py-4 space-y-4">
         <Button label="Book a call" />
         <Button label="Learn more" backgroundColor="blue" />
       </div>
