f49019725d54aa7bec74e271355c96aaf34fdafe
Jimi <jimi_p85@hotmail.com>
2023-03-16 16:48:24 -0400

    Updates to language results page

--- pages/studentPortal/quizcomponent/lang-results.tsx
+++ pages/studentPortal/quizcomponent/lang-results.tsx
@@ -1,27 +1,27 @@
 import { Button } from "../../../components/ui/Button";
 //import QuizNavbar from "./quiznavbar";
-import React from "react";
 
 const LangResults = () => {
   return (
     <div className="flex flex-col w-full max-w-4xl mx-auto text-center md:mr-12">
       <div className="mt-4 text-2xl font-bold text-black-600">YOUR RESULTS</div>
-      <div className="text-lg font-semibolds">
+      <div className="text-lg font-semibolds px-3">
         The first coding language you should learn is...
       </div>
       <img
         src={"/images/quiz/languages-quiz/javascript.png"}
         alt="JavaScript"
-        className="w-64 mt-8"
+        className=" scale-75"
       />
-      <div className="mx-4 mt-4 text-2xl font-semibold">
-        Start your career with a Skillify coach today!
+      <div className="text-3xl font-bold text-brandPrimary">JavaScript</div>
+      <div className="mx-4 mt-4 text-xl font-semibold">
+        Start learning to code with a Skillify coach today!
       </div>
-      <div className="flex flex-col items-center py-4 space-y-4">
+      <div className="flex flex-col items-center py-4 space-y-3 rounded-full">
         <Button label="Book a call" />
         <Button label="Learn more" backgroundColor="blue" />
       </div>
-      <div>
+      <div className="px-2">
         Skillify Coding Academy coaches university graduates to start a career
         in tech. Book a free call with one of our expert coaches to discuss the
         best strategy plan for you.
