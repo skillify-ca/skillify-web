799bba03e3fb4ec68426d1f243be82caa7fb562b
LuckyHariharan <lharihar@uwaterloo.ca>
2023-03-16 21:38:37 -0400

    Fix import

--- components/quizzes/careerQuiz/EduBackground.tsx
+++ components/quizzes/careerQuiz/EduBackground.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { Button } from "../../ui/Button";
-import ProgressBar from "../progressbar";
+import ProgressBar from "../ProgressBar";
 import SkillifyNavbar from "../SkillifyNavbar";
 
 type EduBackgroundProps = {
