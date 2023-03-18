0c693cc0883345268faf3dd2c2dea6231332762b
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-03-18 02:57:20 -0500

    fixed mobile responsiveness for 2nd slide

--- components/studentPortal/freemium/Modals/PremiumFeatures.tsx
+++ components/studentPortal/freemium/Modals/PremiumFeatures.tsx
@@ -2,44 +2,46 @@ import React from "react";
 
 export default function PremiumFeatures() {
   return (
-    <div>
-      <div className="flex flex-row space-x-4 p-8 justify-center">
+    <div className="flex flex-col items-center">
+      <div className="flex flex-row space-x-4 md:p-8 p-2 justify-center">
         <img src="/images/freemium/circleLock.svg" />
-        <h1 className="text-3xl font-bold text-white">
+        <h1 className="md:text-3xl text-md font-bold text-white">
           Premium Skillify Features
         </h1>
       </div>
-      <div className="grid grid-cols-3 gap-8 items-start">
-        <div className="flex flex-col items-center justify-center space-y-6">
-          <div className="bg-white rounded-full p-4 h-48 w-48">
+      <div className="grid grid-cols-1 md:grid-cols-3 md:gap-8 gap-4 items-start w-full md:max-w-6xl">
+        <div className="flex flex-col items-center justify-center space-y-2 md:space-y-6">
+          <div className="bg-white rounded-full md:p-4 p-1 md:w-3/4 w-1/4">
             <img className="" src="/images/freemium/buildRealProjects.svg" />
           </div>
-          <p className="text-charmander text-xl font-bold">
+          <p className="text-charmander md:text-xl text-md font-bold">
             Build Real Projects
           </p>
-          <p className="text-white">
+          <p className="text-white text-center md:text-left md:block hidden">
             You will build real products to add to your portfolio and resume,
             and work on an engineering team with product owners and designers.
           </p>
         </div>
-        <div className="flex flex-col items-center justify-center space-y-6">
-          <div className="bg-white rounded-full p-4 h-48 w-48">
+        <div className="flex flex-col items-center justify-center space-y-2 md:space-y-6">
+          <div className="bg-white rounded-full md:p-4 p-1 md:w-3/4 w-1/4">
             <img className="" src="/images/freemium/communityOfLearners.svg" />
           </div>
-          <p className="text-charmander text-xl font-bold">
+          <p className="text-charmander md:text-xl text-md font-bold">
             Community of Learners
           </p>
-          <p className="text-white">
+          <p className="text-white text-center md:text-left md:block hidden">
             Join a cohort-based course to learn with peers and keep each other
             accountable.
           </p>
         </div>
-        <div className="flex flex-col items-center justify-center space-y-6">
-          <div className="bg-white rounded-full p-4 h-48 w-48">
+        <div className="flex flex-col items-center justify-center space-y-2 md:space-y-6">
+          <div className="bg-white rounded-full md:p-4 p-1 md:w-3/4 w-1/4">
             <img className="" src="/images/freemium/industryExperts.svg" />
           </div>
-          <p className="text-charmander text-xl font-bold">Industry Experts</p>
-          <p className="text-white">
+          <p className="text-charmander md:text-xl text-md font-bold">
+            Industry Experts
+          </p>
+          <p className="text-white text-center md:text-left md:block hidden">
             Learn from the best with our coaches who are experts in the field.
           </p>
         </div>
--- components/studentPortal/freemium/Modals/Upgrade.tsx
+++ components/studentPortal/freemium/Modals/Upgrade.tsx
@@ -3,7 +3,7 @@ import { Button } from "../../../ui/Button";
 
 export default function Upgrade() {
   return (
-    <div className="flex flex-col space-y-4 text-white items-center justify-center">
+    <div className="flex flex-col gap-4 text-white items-center justify-center">
       <h1 className="flex font-bold text-base md:text-2xl max-w-xl text-center">
         Upgrade your plan at any time to unlock premium features.
       </h1>
@@ -13,7 +13,7 @@ export default function Upgrade() {
         easily transition into at any point during your free trial. Simply book
         a call to apply!
       </p>
-      <a className="" href="https://www.joinskillify.com/call">
+      <a className="p-8" href="https://www.joinskillify.com/call">
         <Button
           label="Apply Now!"
           onClick={(e) =>
