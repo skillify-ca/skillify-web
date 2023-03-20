c0e959f87eea0f7771d949fe40bb6a1924aec804
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-03-19 19:09:08 -0500

    modified animation and cleaned up FreemiumDialogComponent file

--- components/studentPortal/freemium/FreemiumDialogComponent.tsx
+++ components/studentPortal/freemium/FreemiumDialogComponent.tsx
@@ -2,13 +2,9 @@ import { XIcon } from "@heroicons/react/outline";
 import { Close, Content, Overlay, Portal, Root } from "@radix-ui/react-dialog";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { animated, useSpring } from "react-spring";
+import { getModalContent } from "../../../pages/api/studentPortal/freemium/GetModalContent";
 import { themeSelector } from "../../../redux/themeSlice";
-import OffYouGo from "./Modals/OffYouGo";
-import PremiumFeatures from "./Modals/PremiumFeatures";
-import Upgrade from "./Modals/Upgrade";
-import Welcome from "./Modals/Welcome";
-import WhereToStart from "./Modals/WhereToStart";
+import TrailAnimation from "../../ui/TrailAnimation";
 
 export enum ModalStage {
   ONE = 1,
@@ -22,7 +18,6 @@ const FreemiumDialogComponent: React.FC = ({ children }) => {
   const [currentStage, setCurrentStage] = useState(ModalStage.ONE); // using useState and ModalStage
   const { currentTheme } = useSelector(themeSelector);
   const [activeModal, setActiveModal] = useState(ModalStage.ONE);
-  const [springProps, set] = useSpring(() => ({ opacity: 1 }));
 
   const activeModalStyling = (currentStage: ModalStage) => {
     let styling = "md:w-6 md:h-6 w-4 h-4 rounded-full";
@@ -43,10 +38,6 @@ const FreemiumDialogComponent: React.FC = ({ children }) => {
         setActiveModal((currentStage) =>
           currentStage > ModalStage.ONE ? currentStage - 1 : ModalStage.ONE
         );
-        set({ opacity: 0 });
-        setTimeout(() => {
-          set({ opacity: 1 });
-        }, 500);
       } else if (event.code === "ArrowRight") {
         setCurrentStage((currentStage) =>
           currentStage < ModalStage.FIVE ? currentStage + 1 : ModalStage.FIVE
@@ -54,10 +45,6 @@ const FreemiumDialogComponent: React.FC = ({ children }) => {
         setActiveModal((currentStage) =>
           currentStage < ModalStage.FIVE ? currentStage + 1 : ModalStage.FIVE
         );
-        set({ opacity: 0 });
-        setTimeout(() => {
-          set({ opacity: 1 });
-        }, 500);
       }
     };
     document.addEventListener("keydown", handleKeyPress);
@@ -82,87 +69,31 @@ const FreemiumDialogComponent: React.FC = ({ children }) => {
                 <XIcon />
               </button>
             </Close>
-            {currentStage === ModalStage.ONE && (
-              <animated.div style={springProps}>
-                <Welcome />
-              </animated.div>
-            )}
-            {currentStage === ModalStage.TWO && (
-              <animated.div style={springProps}>
-                <PremiumFeatures />
-              </animated.div>
-            )}
-            {currentStage === ModalStage.THREE && (
-              <animated.div style={springProps}>
-                <Upgrade />
-              </animated.div>
-            )}
-            {currentStage === ModalStage.FOUR && (
-              <animated.div style={springProps}>
-                <WhereToStart />
-              </animated.div>
-            )}
-            {currentStage === ModalStage.FIVE && (
-              <animated.div style={springProps}>
-                <OffYouGo />
-              </animated.div>
+            {Object.values(ModalStage).map(
+              (stage) =>
+                currentStage === stage && (
+                  <TrailAnimation open={true}>
+                    {getModalContent(stage)}
+                  </TrailAnimation>
+                )
             )}
             <div className="flex flex-row justify-center space-x-2 absolute bottom-4 md:bottom-8 inset-x-0">
-              <div
-                className={activeModalStyling(ModalStage.ONE)}
-                onClick={() => {
-                  setCurrentStage(ModalStage.ONE);
-                  setActiveModal(ModalStage.ONE);
-                  set({ opacity: 0 });
-                  setTimeout(() => {
-                    set({ opacity: 1 });
-                  }, 500);
-                }}
-              ></div>
-              <div
-                className={activeModalStyling(ModalStage.TWO)}
-                onClick={() => {
-                  setCurrentStage(ModalStage.TWO);
-                  setActiveModal(ModalStage.TWO);
-                  set({ opacity: 0 });
-                  setTimeout(() => {
-                    set({ opacity: 1 });
-                  }, 500);
-                }}
-              ></div>
-              <div
-                className={activeModalStyling(ModalStage.THREE)}
-                onClick={() => {
-                  setCurrentStage(ModalStage.THREE);
-                  setActiveModal(ModalStage.THREE);
-                  set({ opacity: 0 });
-                  setTimeout(() => {
-                    set({ opacity: 1 });
-                  }, 500);
-                }}
-              ></div>
-              <div
-                className={activeModalStyling(ModalStage.FOUR)}
-                onClick={() => {
-                  setCurrentStage(ModalStage.FOUR);
-                  setActiveModal(ModalStage.FOUR);
-                  set({ opacity: 0 });
-                  setTimeout(() => {
-                    set({ opacity: 1 });
-                  }, 500);
-                }}
-              ></div>
-              <div
-                className={activeModalStyling(ModalStage.FIVE)}
-                onClick={() => {
-                  setCurrentStage(ModalStage.FIVE);
-                  setActiveModal(ModalStage.FIVE);
-                  set({ opacity: 0 });
-                  setTimeout(() => {
-                    set({ opacity: 1 });
-                  }, 500);
-                }}
-              ></div>
+              {[
+                ModalStage.ONE,
+                ModalStage.TWO,
+                ModalStage.THREE,
+                ModalStage.FOUR,
+                ModalStage.FIVE,
+              ].map((currentStage) => (
+                <div
+                  key={currentStage}
+                  className={activeModalStyling(currentStage)}
+                  onClick={() => {
+                    setCurrentStage(currentStage);
+                    setActiveModal(currentStage);
+                  }}
+                ></div>
+              ))}
             </div>
           </div>
         </Content>
---
+++ components/ui/TrailAnimation.tsx
@@ -0,0 +1,27 @@
+import { animated, useTrail } from "@react-spring/web";
+import React from "react";
+
+export const TrailAnimation: React.FC<{
+  open: boolean;
+  children: React.ReactNode;
+}> = ({ open, children }) => {
+  const items = React.Children.toArray(children);
+  const trail = useTrail(items.length, {
+    config: { mass: 5, tension: 2000, friction: 200, duration: 400 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 20,
+    height: open ? "auto" : 0,
+    from: { opacity: 0, y: 20, height: 0 },
+  });
+  return (
+    <div>
+      {trail.map(({ height, ...style }, index) => (
+        <animated.div key={index} style={style}>
+          <animated.div style={{ height }}>{items[index]}</animated.div>
+        </animated.div>
+      ))}
+    </div>
+  );
+};
+
+export default TrailAnimation;
