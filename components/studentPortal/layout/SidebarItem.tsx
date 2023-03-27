576f6f07fd43e462391f9e61af1b5cc3aab891ac
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-03-27 08:51:15 -0500

    created a freemium sidebar item so that it could be easily run/removed without changing the general student experience.

--- components/studentPortal/layout/SidebarItem.tsx
+++ components/studentPortal/layout/SidebarItem.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React, { ReactElement } from "react";
 import { useSelector } from "react-redux";
 import { activePageSelector, SidebarPage } from "../../../redux/sidebarSlice";
-import TooltipComponent from "../../ui/TooltipComponent";
+import FreemiumSidebarItem from "../freemium/FreemiumSidebarItem";
 
 interface SidebarItemProps {
   name: string;
@@ -27,40 +27,19 @@ const SidebarItem = ({
   return (
     <>
       {isDisabled ? (
-        <TooltipComponent
-          message={"This is a premium feature."}
-          icon="../../images/freemium/info.svg"
-        >
-          <div>
-            <Link onClick={onClick} href={href}>
-              <div
-                className={`flex flex-wrap items-center p-4 cursor-pointer hover:border-l-4 ${
-                  activePage === page ? "border-charmander text-charmander" : ""
-                } hover:border-charmander hover:text-charmander ${
-                  isDisabled ? "border-gray-500 text-gray-500" : ""
-                }`}
-              >
-                <div>
-                  {notifications ? (
-                    <div className="relative left-6 top-1 ">
-                      <div className="flex w-2 h-2 bg-red-500 rounded-full"></div>
-                    </div>
-                  ) : null}
-                  {icon}
-                </div>
-                {name}
-              </div>
-            </Link>
-          </div>
-        </TooltipComponent>
+        <FreemiumSidebarItem
+          name={name}
+          link={link}
+          page={page}
+          icon={icon}
+          isDisabled={isDisabled}
+        />
       ) : (
-        <Link onClick={onClick} href={href}>
+        <Link href={link}>
           <div
             className={`flex flex-wrap items-center p-4 cursor-pointer hover:border-l-4 ${
               activePage === page ? "border-charmander text-charmander" : ""
-            } hover:border-charmander hover:text-charmander ${
-              isDisabled ? "border-gray-500 text-gray-500" : ""
-            }`}
+            } hover:border-charmander hover:text-charmander`}
           >
             <div>
               {notifications ? (
