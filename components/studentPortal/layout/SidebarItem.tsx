d83a709caf87786a5e72fa2d370003d5042cb5df
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-03-27 08:15:42 -0500

    added tooltip to SidebarItem.tsx

--- components/studentPortal/layout/SidebarItem.tsx
+++ components/studentPortal/layout/SidebarItem.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React, { ReactElement } from "react";
 import { useSelector } from "react-redux";
 import { activePageSelector, SidebarPage } from "../../../redux/sidebarSlice";
+import TooltipComponent from "../../ui/TooltipComponent";
 
 interface SidebarItemProps {
   name: string;
@@ -25,25 +26,54 @@ const SidebarItem = ({
 
   return (
     <>
-      <Link onClick={onClick} href={href}>
-        <div
-          className={`flex flex-wrap items-center p-4 cursor-pointer hover:border-l-4 ${
-            activePage === page ? "border-charmander text-charmander" : ""
-          } hover:border-charmander hover:text-charmander ${
-            isDisabled ? "border-gray-500 text-gray-500" : ""
-          }`}
+      {isDisabled ? (
+        <TooltipComponent
+          message={"This is a premium feature."}
+          icon="../../images/freemium/info.svg"
         >
           <div>
-            {notifications ? (
-              <div className="relative left-6 top-1 ">
-                <div className="flex w-2 h-2 bg-red-500 rounded-full"></div>
+            <Link onClick={onClick} href={href}>
+              <div
+                className={`flex flex-wrap items-center p-4 cursor-pointer hover:border-l-4 ${
+                  activePage === page ? "border-charmander text-charmander" : ""
+                } hover:border-charmander hover:text-charmander ${
+                  isDisabled ? "border-gray-500 text-gray-500" : ""
+                }`}
+              >
+                <div>
+                  {notifications ? (
+                    <div className="relative left-6 top-1 ">
+                      <div className="flex w-2 h-2 bg-red-500 rounded-full"></div>
+                    </div>
+                  ) : null}
+                  {icon}
+                </div>
+                {name}
               </div>
-            ) : null}
-            {icon}
+            </Link>
           </div>
-          {name}
-        </div>
-      </Link>
+        </TooltipComponent>
+      ) : (
+        <Link onClick={onClick} href={href}>
+          <div
+            className={`flex flex-wrap items-center p-4 cursor-pointer hover:border-l-4 ${
+              activePage === page ? "border-charmander text-charmander" : ""
+            } hover:border-charmander hover:text-charmander ${
+              isDisabled ? "border-gray-500 text-gray-500" : ""
+            }`}
+          >
+            <div>
+              {notifications ? (
+                <div className="relative left-6 top-1 ">
+                  <div className="flex w-2 h-2 bg-red-500 rounded-full"></div>
+                </div>
+              ) : null}
+              {icon}
+            </div>
+            {name}
+          </div>
+        </Link>
+      )}
     </>
   );
 };
