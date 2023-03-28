df81f7b2486eb0eaeb83f6e8e10687f5bb86b314
Jimi <jimi_p85@hotmail.com>
2023-03-28 16:31:06 -0400

    routing logic w/ back & forth skillselection support

--- components/resources/quizzes/shared/SkillSelections.tsx
+++ components/resources/quizzes/shared/SkillSelections.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { Button } from "../../../ui/Button";
 import Progress from "./Progress";
 import SkillifyNavbar from "./SkillifyNavbar";
+
 type SkillSelectionsProps = {
   selections: string[];
+  selected: string[][];
+  setSelected: Dispatch<SetStateAction<string[][]>>;
+  setQuizResponses: Dispatch<SetStateAction<any[]>>;
+  currentStage: number;
   onNextClick: () => void;
   onBackClick: () => void;
   progress: number;
@@ -13,20 +18,49 @@ type SkillSelectionsProps = {
 
 const SkillSelections: React.FC<SkillSelectionsProps> = ({
   selections,
+  selected,
+  setSelected,
+  setQuizResponses,
+  currentStage,
   onNextClick,
   onBackClick,
   progress,
   title,
   body,
 }) => {
-  const [selected, setSelected] = useState<string[]>([]);
+  const currentSelections = selected[currentStage] || [];
+
   const handleSelection = (selection: string) => {
-    if (selected.includes(selection)) {
-      setSelected(selected.filter((s) => s !== selection));
+    const selectionIndex = currentSelections.indexOf(selection);
+    if (selectionIndex !== -1) {
+      setSelected((prev) => {
+        const updatedSelections = [...prev];
+        updatedSelections[currentStage] = currentSelections.filter(
+          (s, index) => index !== selectionIndex
+        );
+        return updatedSelections;
+      });
     } else {
-      setSelected([...selected, selection]);
+      setSelected((prev) => {
+        const updatedSelections = [...prev];
+        updatedSelections[currentStage] = [...currentSelections, selection];
+        return updatedSelections;
+      });
     }
   };
+
+  const handleStage = () => {
+    setQuizResponses((prev) => {
+      const updatedResponses = [...prev];
+      updatedResponses[currentStage - 1] = selected[currentStage];
+      return updatedResponses;
+    });
+    setSelected((prev) => {
+      const updatedSelections = [...prev];
+      return updatedSelections;
+    });
+  };
+
   return (
     <>
       <SkillifyNavbar hidden={false} onBackClick={onBackClick} />
@@ -37,22 +71,43 @@ const SkillSelections: React.FC<SkillSelectionsProps> = ({
         </div>
         <div className="text-lg font-semibolds px-3">{body}</div>
         <div className="flex flex-col w-full max-w-4xl mx-auto">
-          {selections.map((selection, index) => (
-            <div
-              key={index}
-              className={`flex items-start justify-start w-full px-4 py-2 my-2 cursor-pointer ${
-                selected.includes(selection)
-                  ? "bg-violet-300 text-black-500 border-2 border-black-500 rounded-xl"
-                  : "bg-white text-black-600 border-2 border-black-500 rounded-xl"
-              }`}
-              onClick={() => handleSelection(selection)}
-            >
-              {selections[index]}
-            </div>
-          ))}
+          {selections.map((selection, index) => {
+            const isSelected = currentSelections.includes(selection);
+            const className = [
+              "flex",
+              "items-start",
+              "justify-start",
+              "w-full",
+              "px-4",
+              "py-2",
+              "my-2",
+              "cursor-pointer",
+              "text-black-600",
+              "border-2",
+              "border-black-500",
+              "rounded-xl",
+              isSelected ? "bg-violet-300" : "bg-white",
+            ].join(" ");
+            return (
+              <div
+                key={index}
+                className={className}
+                onClick={() => handleSelection(selection)}
+              >
+                {selection}
+              </div>
+            );
+          })}
         </div>
         <div className="mt-4">
-          <Button label="Next" onClick={onNextClick} backgroundColor="yellow" />
+          <Button
+            label="Next"
+            onClick={() => {
+              handleStage();
+              onNextClick();
+            }}
+            backgroundColor="yellow"
+          />
         </div>
       </div>
     </>
---
+++ pages/studentPortal/quizcomponent/FirstProgrammingLanguageQuiz.tsx
@@ -0,0 +1,183 @@
+import React, { useState } from "react";
+import LangResults from "../../../components/resources/quizzes/langQuiz/LangResults";
+import BluePrint from "../../../components/resources/quizzes/shared/BluePrint";
+import SkillSelections from "../../../components/resources/quizzes/shared/SkillSelections";
+import StartQuiz from "../../../components/resources/quizzes/shared/StartQuiz";
+export enum Stage {
+  START,
+  LEARNCODING,
+  WORKFIELDS,
+  BUILDTARGET,
+  BLUEPRINT,
+  RESULTS,
+}
+
+type QuizSelection = {
+  name: string;
+  result?: string;
+  weight?: number;
+};
+
+const FirstProgrammingLanguageQuiz = () => {
+  const quiz: QuizSelection[][] = [
+    [
+      { name: "Learn an in-demand skill" },
+      { name: "Work in tech" },
+      { name: "Build programs for fun" },
+      { name: "I run a startup" },
+      { name: "Become a professional developer" },
+      { name: "Communicate better with technical coworkers" },
+      { name: "Start a side hustle" },
+    ],
+    [
+      { name: "Front end development", result: "JavaScript", weight: 1 },
+      { name: "Back end development", result: "JavaScript", weight: 1 },
+      { name: "Game development", result: "JavaScript", weight: 1 },
+      { name: "Mobile development", result: "JavaScript", weight: 1 },
+      { name: "Data analytics", result: "Python", weight: 1 },
+      { name: "Product management", result: "JavaScript", weight: 1 },
+      { name: "Digital marketing", result: "JavaScript", weight: 1 },
+      { name: "UX/UI design", result: "JavaScript", weight: 1 },
+      { name: "I don't know / Not sure yet", result: "JavaScript", weight: 1 },
+    ],
+    [
+      { name: "Websites", result: "JavaScript", weight: 1 },
+      { name: "Storefront", result: "HTML/CSS", weight: 1 },
+      { name: "Mobile apps", result: "JavaScript", weight: 1 },
+      { name: "Games", result: "JavaScript", weight: 1 },
+      { name: "Email", result: "HTML/CSS", weight: 1 },
+      { name: "Tools to automate your life", result: "JavaScript", weight: 1 },
+      { name: "I don't know / Not sure yet", result: "JavaScript", weight: 1 },
+    ],
+  ];
+
+  const pageOptions = quiz.map((pages) => {
+    return pages.map((options) => {
+      return options.name;
+    });
+  });
+
+  type QuizContext = {
+    title: string;
+    body: string;
+  };
+
+  const quizQuestions: QuizContext[] = [
+    {
+      title: "What Coding Language Should I Learn First?",
+      body: "Take this free quiz to find out what coding language you should learn first.",
+    },
+    {
+      title: "Why do you want to learn coding?",
+      body: "Select all that apply.",
+    },
+    {
+      title: "What field of work are you interested in?",
+      body: "Select 1-3 choices.",
+    },
+    {
+      title: "What are you interested in building?",
+      body: "Select all that apply.",
+    },
+  ];
+
+  const [quizResponses, setQuizResponses] = useState([]);
+  const [selectedSkill, setSelectedSkill] = useState<Array<Array<string>>>([]);
+
+  const [score, setScore] = useState({
+    JavaScript: 0,
+    HTMLCSS: 0,
+    Python: 0,
+  });
+
+  const [stage, setStage] = useState(Stage.START);
+  const handleNextClick = () => {
+    setStage((prevStage) => prevStage + 1);
+  };
+  const handleBackClick = () => {
+    setStage((prevStage) => {
+      const newStage = prevStage - 1;
+      setQuizResponses((prev) => {
+        const resetResponseIndex = [...prev];
+        console.log(`this is the value of stage: ${newStage}`);
+        return resetResponseIndex;
+      });
+      return newStage;
+    });
+  };
+
+  const renderStage = () => {
+    switch (stage) {
+      case Stage.START:
+        return (
+          <StartQuiz
+            onNextClick={handleNextClick}
+            title={quizQuestions[stage].title}
+            body={quizQuestions[stage].body}
+          />
+        );
+      case Stage.LEARNCODING:
+        return (
+          <SkillSelections
+            onNextClick={handleNextClick}
+            onBackClick={handleBackClick}
+            title={quizQuestions[stage].title}
+            body={quizQuestions[stage].body}
+            selected={selectedSkill}
+            setSelected={setSelectedSkill}
+            progress={20}
+            selections={pageOptions[stage - 1]}
+            setQuizResponses={setQuizResponses}
+            currentStage={stage}
+          />
+        );
+      case Stage.WORKFIELDS:
+        return (
+          <SkillSelections
+            onNextClick={handleNextClick}
+            onBackClick={handleBackClick}
+            title={quizQuestions[stage].title}
+            body={quizQuestions[stage].body}
+            selected={selectedSkill}
+            setSelected={setSelectedSkill}
+            progress={50}
+            selections={pageOptions[stage - 1]}
+            setQuizResponses={setQuizResponses}
+            currentStage={stage}
+          />
+        );
+      case Stage.BUILDTARGET:
+        return (
+          <SkillSelections
+            onNextClick={handleNextClick}
+            onBackClick={handleBackClick}
+            title={quizQuestions[stage].title}
+            body={quizQuestions[stage].body}
+            selected={selectedSkill}
+            setSelected={setSelectedSkill}
+            progress={80}
+            selections={pageOptions[stage - 1]}
+            setQuizResponses={setQuizResponses}
+            currentStage={stage}
+          />
+        );
+      case Stage.BLUEPRINT:
+        return (
+          <BluePrint
+            onNextClick={handleNextClick}
+            onBackClick={handleBackClick}
+          />
+        );
+      case Stage.RESULTS:
+        return <LangResults onBackClick={handleBackClick} />;
+      default:
+        return null;
+    }
+  };
+  return <div>{renderStage()}</div>;
+};
+
+export default FirstProgrammingLanguageQuiz;
+FirstProgrammingLanguageQuiz.getLayout = function getLayout(page) {
+  return <div>{page}</div>;
+};
