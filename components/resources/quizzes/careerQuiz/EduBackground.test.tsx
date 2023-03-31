ee1eb841a99a0d32e3b4dae6162d46d5a5ddb2b1
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2023-03-31 01:46:22 -0400

    New Education Background  (#921)
    * Setup New Education Background
    
    * Update EduBackground.tsx
    
    * Setup Education Tests
    
    * Uncomment code
    
    * Undo env change
    
    * Update EduBackground.tsx
    
    * Format Edu
    
    * Update EduBackground.tsx
    
    * Update EduBackground.tsx
    
    * reorganize components
    
    * Update career-quiz.tsx
    
    * Resolve errors

---
+++ components/resources/quizzes/careerQuiz/EduBackground.test.tsx
@@ -0,0 +1,66 @@
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import EduBackground, { EducationLevel } from "./EduBackground";
+
+describe("EducationBackground component", () => {
+  test("renders select element with EducationLevel options", () => {
+    const onNextClick = jest.fn();
+    const onBackClick = jest.fn();
+    render(
+      <EduBackground onNextClick={onNextClick} onBackClick={onBackClick} />
+    );
+    const selectElement = screen.getByLabelText("Education");
+    expect(selectElement).toBeInTheDocument();
+    expect(selectElement).toHaveValue("");
+    expect(screen.getByText("N/A")).toBeInTheDocument();
+    expect(screen.getByText("High School Diploma")).toBeInTheDocument();
+    expect(screen.getByText("GED")).toBeInTheDocument();
+    expect(screen.getByText("Undergraduate Degree")).toBeInTheDocument();
+    expect(screen.getByText("Postgraduate Degree")).toBeInTheDocument();
+    expect(screen.getByText("PHD")).toBeInTheDocument();
+  });
+
+  const testCases = [
+    { label: "N/A", value: EducationLevel.NA, shouldShow: true },
+    {
+      label: "High School Diploma",
+      value: EducationLevel.HighSchoolDiploma,
+      shouldShow: true,
+    },
+    { label: "GED", value: EducationLevel.GED, shouldShow: true },
+    {
+      label: "Undergraduate Degree",
+      value: EducationLevel.UndergraduateDegree,
+      shouldShow: false,
+    },
+    {
+      label: "Postgraduate Degree",
+      value: EducationLevel.PostgraduateDegree,
+      shouldShow: false,
+    },
+    { label: "PHD", value: EducationLevel.PHD, shouldShow: false },
+  ];
+
+  testCases.forEach((testCase) => {
+    test(`selecting ${testCase.value} ${
+      testCase.shouldShow ? "shows" : "does not show"
+    } experience dialog`, () => {
+      const onNextClick = jest.fn();
+      const onBackClick = jest.fn();
+      render(
+        <EduBackground onNextClick={onNextClick} onBackClick={onBackClick} />
+      );
+      const selectElement = screen.getByLabelText("Education");
+      fireEvent.change(selectElement, { target: { value: testCase.label } });
+      if (testCase.shouldShow) {
+        expect(
+          screen.getByText("Do you have experience coding?")
+        ).toBeInTheDocument();
+      } else {
+        expect(screen.getByText("Institution")).toBeInTheDocument();
+        expect(screen.getByText("Field of study")).toBeInTheDocument();
+      }
+    });
+  });
+});
--- components/resources/quizzes/shared/SkillifyNavbar.tsx
+++ components/resources/quizzes/shared/SkillifyNavbar.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 type SkillifyNavbarProps = {
   hidden: boolean;
   onBackClick: () => void;
