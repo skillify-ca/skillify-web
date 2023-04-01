37ec507f8566d39c5f076484cef27695f9b2cdb1
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-04-01 00:35:37 -0500

    simplified code

--- components/studentPortal/freemium/FreemiumDialogueComponent.tsx
+++ components/studentPortal/freemium/FreemiumDialogueComponent.tsx
@@ -4,53 +4,27 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { getModalContent } from "../../../pages/api/studentPortal/freemium/getModalContent";
 import { themeSelector } from "../../../redux/themeSlice";
+
 export enum ModalStage {
   ONE,
   TWO,
   THREE,
 }
 
-export const modalStageMappedArray = (
-  Object.keys(ModalStage).filter((stage) =>
-    isNaN(Number(stage))
-  ) as (keyof typeof ModalStage)[]
-).map((key, index) => {
-  return ModalStage[key];
-});
-
 const FreemiumDialogComponent: React.FC = () => {
-  // delete this log when ready to merge
-  console.log("freemium modal has rendered");
-  const [currentStage, setCurrentStage] = useState(ModalStage.ONE);
   const { currentTheme } = useSelector(themeSelector);
   const [activeModal, setActiveModal] = useState(ModalStage.ONE);
 
-  const activeModalStyling = (currentStage: ModalStage) => {
-    let styling = "md:w-3 md:h-3 w-2 h-2 rounded-full";
-    if (currentStage === activeModal) {
-      styling = styling + " bg-rattata";
-    } else {
-      styling = styling + " bg-gray-300";
-    }
-    return styling;
-  };
+  // Handler functions for navigating between modal stages
+  const handleClickBack = () => setActiveModal(activeModal - 1);
+  const handleClickNext = () => setActiveModal(activeModal + 1);
+  const handleClickModal = (stage: ModalStage) => setActiveModal(stage);
 
-  const handleClickBack = () => {
-    const previousStage = currentStage - 1;
-    setCurrentStage(previousStage);
-    setActiveModal(previousStage);
-  };
-
-  const handleClickNext = () => {
-    const nextStage = currentStage + 1;
-    setCurrentStage(nextStage);
-    setActiveModal(nextStage);
-  };
-
-  const handleClickModal = (stage) => {
-    setCurrentStage(stage);
-    setActiveModal(stage);
-  };
+  //creates an array of enum values, excluding the keys that are non-numeric so it doesn't duplicate
+  const stagesArray = Object.values(ModalStage).filter(
+    (stage) => !isNaN(Number(stage))
+  );
+  const lastStage = stagesArray[stagesArray.length - 1];
 
   return (
     <Root defaultOpen={true}>
@@ -59,7 +33,7 @@ const FreemiumDialogComponent: React.FC = () => {
         <Content className={`${currentTheme}`}>
           <div
             className={`fixed h-[450px] w-[300px] md:h-[600px] md:w-[900px] p-8 md:p-20 transform -translate-x-1/2 -translate-y-1/2 ${
-              currentStage === ModalStage.TWO ? "bg-[#18124C]" : "bg-white"
+              activeModal === ModalStage.TWO ? "bg-[#18124C]" : "bg-white"
             } rounded-lg left-1/2 top-1/2`}
           >
             <Close asChild>
@@ -67,11 +41,11 @@ const FreemiumDialogComponent: React.FC = () => {
                 <XIcon />
               </button>
             </Close>
-            {Object.values(ModalStage).map(
-              (stage) => currentStage === stage && <>{getModalContent(stage)}</>
-            )}
-            <div className="flex flex-row justify-center space-x-12 absolute bottom-4 md:bottom-8 inset-x-0">
-              {currentStage > ModalStage.ONE ? (
+            {/* content rendered based on enum */}
+            {getModalContent(activeModal)}
+            <div className="flex flex-row justify-center items-center space-x-12 absolute bottom-4 md:bottom-8 inset-x-0">
+              {/* no button/back button */}
+              {activeModal > ModalStage.ONE ? (
                 <img
                   src="../../images/freemium/back.svg"
                   onClick={handleClickBack}
@@ -79,19 +53,23 @@ const FreemiumDialogComponent: React.FC = () => {
               ) : (
                 <img
                   src="../../images/freemium/back.svg"
-                  style={{ visibility: "hidden" }}
-                />
+                  className="invisible"
+                ></img>
               )}
-              <div className="flex flex-row items-center space-x-1">
-                {modalStageMappedArray.map((stage) => (
+              {/* Map over enum values to render highlighted dots */}
+              <div className="flex flex-row items-center space-x-2">
+                {stagesArray.map((stage) => (
                   <div
                     key={stage}
-                    className={activeModalStyling(stage as ModalStage)}
+                    className={`md:w-3 md:h-3 w-2 h-2 rounded-full ${
+                      stage === activeModal ? "bg-rattata" : "bg-gray-300"
+                    }`}
                     onClick={() => handleClickModal(stage as ModalStage)}
                   ></div>
                 ))}
               </div>
-              {currentStage < ModalStage.THREE ? (
+              {/* Next/done button */}
+              {activeModal < lastStage ? (
                 <img
                   src="../../images/freemium/next.svg"
                   onClick={handleClickNext}
