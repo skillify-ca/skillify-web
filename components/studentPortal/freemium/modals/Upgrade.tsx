ffbcb46f832706faada7f6076c57438ae70bfda4
kwiedenhaupt <111075855+kwiedenhaupt@users.noreply.github.com>
2023-04-01 18:59:13 -0500

    Cleaner Freemium Modal Component (#929)
    * cycling through the enums for the modals
    
    * basic dialogue component pulled from my prior branch that I will adapt.
    
    * add modal shells
    
    * moving through the stages WIP
    
    1 added images for back, next and done
    2. added shell modal components
    3. began working on the logic to render the modals
    
    * added handleClicks
    
    * simplified code
    
    * removed duplicate
    
    * change back to react.
    
    * adding the close tag to close the dialog box with the done button
    
    ---------
    
    Co-authored-by: Vithushan <vithushan19@users.noreply.github.com>

---
+++ components/studentPortal/freemium/modals/Upgrade.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Upgrade() {
+  return <div className="md:space-y-10 space-y-2">Upgrade!</div>;
+}
---
+++ components/studentPortal/freemium/modals/WhereToStart.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function WhereToStart() {
+  return <div className="md:space-y-10 space-y-2">Start Here.</div>;
+}
