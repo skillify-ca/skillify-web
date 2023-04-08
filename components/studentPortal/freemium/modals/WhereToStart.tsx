f12efd3e115f29a65ef13a1af7ef71141d571da3
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-04-08 11:28:52 -0500

    modal modifications
    1. add extra padding to the FreemiumDialogComponent to help with the last modal
    2. made adjustments to the welcome modal for styling
    3. added where to start component
    4. added both mobile and courses svgs
    4. deleted old courses svg.

--- components/studentPortal/freemium/modals/WhereToStart.tsx
+++ components/studentPortal/freemium/modals/WhereToStart.tsx
@@ -1,4 +1,30 @@
 import React from "react";
+
 export default function WhereToStart() {
-  return <div className="md:space-y-10 space-y-2">Start Here.</div>;
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center space-x-8 text-start">
+      <div className="flex flex-col md:p-4 p-2 space-y-4 md:space-y-8 md:max-w-md w-full">
+        <h1 className="text-lg md:text-3xl font-bold">
+          Which Course Should You Start With?
+        </h1>
+        <img
+          src="/images/freemium/coursesMobile.svg"
+          className="block md:hidden"
+        />
+        <div className="bg-sky rounded-xl p-6 text-xs md:text-lg space-y-4 w-full">
+          <div className="flex flex-row md:flex-col">
+            <p>For beginners with little to no coding experience: </p>
+            <p className="font-bold">Coding Basics Course!</p>
+          </div>
+          <div className="flex flex-row md:flex-col">
+            <p>If you have intermediate or advanced coding experience: </p>
+            <p className="font-bold">Web Development Course!</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src="/images/freemium/courses.svg" className="hidden md:block" />
+      </div>
+    </div>
+  );
 }
