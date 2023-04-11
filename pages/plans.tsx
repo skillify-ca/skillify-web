b539bd39b59a222236d140b360038365883348e6
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-04-10 20:20:19 -0500

    updated for getServerSideProps

--- pages/plans.tsx
+++ pages/plans.tsx
@@ -1,95 +1,89 @@
+export const plansCardData = [
+  {
+    title: "Free 14-day Trial",
+    description: "No credit card required",
+    price: "$0",
+    buttonLabel: "Sign Up",
+    onClick: "/sign-up",
+    planRowData: [
+      {
+        icon: "../../images/freemium/greenCheck.svg",
+        description: "First Description",
+      },
+      {
+        icon: "../../images/freemium/greenCheck.svg",
+        description: "Second Description",
+      },
+      {
+        icon: "../../images/freemium/greenCheck.svg",
+        description: "Third Description",
+      },
+      {
+        icon: "../../images/freemium/redX.svg",
+        description: "Fourth Description",
+      },
+      {
+        icon: "../../images/freemium/redX.svg",
+        description: "Fifth Description",
+      },
+      {
+        icon: "../../images/freemium/redX.svg",
+        description:
+          "Sixth Description what happens if this is really long to do",
+      },
+    ],
+  },
+  {
+    title: "Premium",
+    description: "Contact us for pricing",
+    price: "Custom",
+    buttonLabel: "Book a Call",
+    onClick: "https://joinskillify.com/call",
+    planRowData: [
+      {
+        icon: "../../images/freemium/greenCheck.svg",
+        description: "FirstA Description",
+      },
+      {
+        icon: "../../images/freemium/greenCheck.svg",
+        description: "SecondA Description",
+      },
+      {
+        icon: "../../images/freemium/greenCheck.svg",
+        description: "ThirdA Description",
+      },
+      {
+        icon: "../../images/freemium/redX.svg",
+        description: "FourthA Description",
+      },
+      {
+        icon: "../../images/freemium/redX.svg",
+        description: "FifthA Description",
+      },
+      {
+        icon: "../../images/freemium/redX.svg",
+        description:
+          "SixthA Description what happens if this is really long to do",
+      },
+    ],
+  },
+];
+
 import router from "next/router";
-import React, { ReactNode, useState } from "react";
-import PlansCard, {
-  PlansCardProps,
-} from "../components/studentPortal/freemium/PlansCard";
+import React, { useState } from "react";
+import PlansCard from "../components/studentPortal/freemium/PlansCard";
 import SignInPage from "../components/welcomePage/SignInPage";
 
-export interface PlansProps {
-  children: ReactNode;
-}
-
-const Plans = ({ children }: PlansProps) => {
+const Plans = ({ plansData }) => {
   const [showSignInPage, setShowSignInPage] = useState(false);
 
   const handlePremium = () => {
-    router.push("joinskillify.com/call");
+    router.push(plansData[1].onClick);
   };
 
   const handleTrial = () => {
     setShowSignInPage(true);
   };
-
-  const plansCardData: PlansCardProps[] = [
-    {
-      title: "Free 14-day Trial",
-      description: "No credit card required",
-      price: "$0",
-      buttonLabel: "Sign Up",
-      onClick: handleTrial,
-      planRowData: [
-        {
-          icon: "../../images/freemium/greenCheck.svg",
-          description: "FirstA Description",
-        },
-        {
-          icon: "../../images/freemium/greenCheck.svg",
-          description: "SecondA Description",
-        },
-        {
-          icon: "../../images/freemium/greenCheck.svg",
-          description: "ThirdA Description",
-        },
-        {
-          icon: "../../images/freemium/redX.svg",
-          description: "FourthA Description",
-        },
-        {
-          icon: "../../images/freemium/redX.svg",
-          description: "FifthA Description",
-        },
-        {
-          icon: "../../images/freemium/redX.svg",
-          description:
-            "SixthA Description what happens if this is really long to do",
-        },
-      ],
-    },
-    {
-      title: "Premium",
-      description: "Contact us for pricing",
-      price: "Custom",
-      buttonLabel: "Book a Call",
-      onClick: handlePremium,
-      planRowData: [
-        {
-          icon: "../../images/freemium/greenCheck.svg",
-          description: "First Description",
-        },
-        {
-          icon: "../../images/freemium/greenCheck.svg",
-          description: "Second Description",
-        },
-        {
-          icon: "../../images/freemium/greenCheck.svg",
-          description: "Third Description",
-        },
-        {
-          icon: "../../images/freemium/redX.svg",
-          description: "Fourth Description",
-        },
-        {
-          icon: "../../images/freemium/redX.svg",
-          description: "Fifth Description",
-        },
-        {
-          icon: "../../images/freemium/redX.svg",
-          description:
-            "Sixth Description what happens if this is really long to do",
-        },
-      ],
-    },
-  ];
 
   return (
     <div>
@@ -104,14 +98,16 @@ const Plans = ({ children }: PlansProps) => {
             <p>Reserve your spot today!</p>
           </div>
           <div className="flex md:flex-row flex-col md:space-x-10 space-x-0 items-center justify-center">
-            {plansCardData.map((card) => (
+            {plansData.map((card) => (
               <PlansCard
                 key={card.title}
                 title={card.title}
                 description={card.description}
                 price={card.price}
                 buttonLabel={card.buttonLabel}
-                onClick={card.onClick}
+                onClick={
+                  card.onClick === "/sign-up" ? handleTrial : handlePremium
+                }
                 planRowData={card.planRowData}
               ></PlansCard>
             ))}
@@ -121,5 +117,13 @@ const Plans = ({ children }: PlansProps) => {
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      plansData: plansCardData,
+    },
+  };
+}
 
 export default Plans;
