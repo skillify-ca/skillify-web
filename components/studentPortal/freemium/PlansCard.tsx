8837d4782e881e96dfeab804c77dab3bbc736e67
kwiedenhaupt <111075855+kwiedenhaupt@users.noreply.github.com>
2023-04-19 08:15:36 -0500

    plans component (#937)
    * WIP infrastructure for the plans page
    
    1. created a Plans page for the sign-up experience
    2. created a PlansCard component that will take in the PlansRow information
    3. created a PlansRow component that will be used to give the description of the items in the plan card.
    added two icons, the green check and the red x
    
    * created plan cards
    
    1. added grid spacing to the plan rows to make it fill the container and wrap correctly
    2. adjusted spacing and added an onClick to the to the Button
    3. refactored the index page to make it cleaner.
    
    * adjusted styling for mobile view
    
    * removed some of the content and routing to make the PR easier to review
    
    * updated data and location of file
    
    * updated for getServerSideProps
    
    * styling changes
    
    * change variable names
    
    * updated styling
    
    * remove optional message to make them equal
    
    * moved data to put it inside the server side props

---
+++ components/studentPortal/freemium/PlansCard.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Button } from "../../ui/Button";
+import PlanRow, { PlanRowProps } from "./PlanRow";
+
+export type PlansCardProps = {
+  title: string;
+  price: string;
+  description: string;
+  buttonLabel: string;
+  onClick: () => void;
+  planRowData: PlanRowProps[];
+};
+
+const PlansCard = ({
+  price,
+  description,
+  buttonLabel,
+  title,
+  onClick,
+  planRowData,
+}: PlansCardProps) => {
+  return (
+    <div className="flex flex-col w-[325px] md:w-[400px] space-y-4 transition-all transform border-t-2 text-textPrimary bg-backgroundPrimary shadow-lg cursor-pointer rounded-xl hover:scale-105 mb-4">
+      <div className="flex items-center justify-center bg-rattata p-6 text-white font-bold text-2xl rounded-xl">
+        {title}
+      </div>
+      <div className="flex flex-col items-center w-full">
+        <p className="mb-4 font-bold text-2xl">{price}</p>
+        <p>{description}</p>
+        <p className="font-bold text-center p-4">WHAT'S INCLUDED</p>
+        {planRowData.map((item, index) => {
+          return (
+            <PlanRow
+              key={index}
+              icon={item.icon}
+              description={item.description}
+            />
+          );
+        })}
+        <div className="my-4">
+          <Button onClick={onClick} label={buttonLabel} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlansCard;
