0f46b3e2e70237eb7fcca200cfb5ea7c31ef6269
Raveen Reddy <raveenreddy89@gmail.com>
2023-04-20 11:41:07 -0400

    implement quiz template

--- pages/resources/quizzes/hirabilityQuiz/index.tsx
+++ pages/resources/quizzes/hirabilityQuiz/index.tsx
@@ -1,20 +1,134 @@
-import React from "react";
-import LandingNavbar from "../../../../components/landingPage/LandingNavbar";
-import HirabilityQuizComponent from "../../../../components/resources/quizzes/hirabilityQuiz/HirabilityQuizComponent";
+/* eslint-disable react/react-in-jsx-scope */
+import { useState } from "react";
+import LanguageResults from "../../../../components/resources/quizzes/firstProgrammingLanguageQuiz/LanguageResults";
+import { hirabilityQuizData } from "../../../../components/resources/quizzes/hirabilityQuiz/quizData";
+import BluePrint from "../../../../components/resources/quizzes/shared/BluePrint";
+import SkillSelections from "../../../../components/resources/quizzes/shared/SkillSelections";
+import StartQuiz from "../../../../components/resources/quizzes/shared/StartQuiz";
+import {
+  QuizOptionViewState,
+  QuizViewState,
+} from "../../../../components/resources/quizzes/shared/types";
 
-export default function HirabilityQuizPage() {
-  return (
-    <div>
-      <HirabilityQuizComponent />
-    </div>
-  );
+enum Stage {
+  START,
+  QUESTIONS,
+  ROLE,
+  SKILL_RATING,
+  RESULT,
 }
 
-HirabilityQuizPage.getLayout = function getLayout(page) {
-  return (
-    <div className="theme-default">
-      <LandingNavbar />
-      {page}
-    </div>
-  );
+const quizData = hirabilityQuizData;
+
+const initializeQuizViewState = {
+  title: quizData.title,
+  body: quizData.body,
+  questions: quizData.questions.map((question) => {
+    return {
+      title: question.title,
+      body: question.body,
+      options: question.options.map((option) => {
+        return { ...option, isSelected: false };
+      }),
+    };
+  }),
+  currentQuestion: 0,
+  progress: 0,
 };
+
+export default function HirabilityQuizPage() {
+  const [stage, setStage] = useState(Stage.START);
+  const [quizViewState, setQuizViewState] = useState<QuizViewState>(
+    initializeQuizViewState
+  );
+
+  const handleNextClick = () => {
+    if (
+      stage == Stage.QUESTIONS &&
+      quizViewState.currentQuestion < quizData.questions.length - 1
+    ) {
+      setQuizViewState({
+        ...quizViewState,
+        currentQuestion: quizViewState.currentQuestion + 1,
+      });
+    } else setStage((prevStage) => prevStage + 1);
+  };
+
+  const handleBackClick = () => {
+    if (stage == Stage.QUESTIONS && quizViewState.currentQuestion > 0) {
+      setQuizViewState({
+        ...quizViewState,
+        currentQuestion: quizViewState.currentQuestion - 1,
+      });
+    } else setStage((prevStage) => prevStage - 1);
+  };
+
+  const handleOptionClick = (option: QuizOptionViewState) => {
+    const selectedQuizOption = quizViewState.questions.map((question) => ({
+      ...question,
+      options: question.options.map((questionOption) =>
+        questionOption.name === option.name
+          ? {
+              ...questionOption,
+              isSelected: questionOption.isSelected ? false : true,
+            }
+          : questionOption
+      ),
+    }));
+
+    const updatedQuizViewState = {
+      ...quizViewState,
+      questions: selectedQuizOption,
+    };
+    setQuizViewState(updatedQuizViewState);
+  };
+
+  const renderStage = () => {
+    switch (stage) {
+      case Stage.START:
+        return (
+          <StartQuiz
+            onNextClick={handleNextClick}
+            title={quizData.title}
+            body={quizData.body}
+          />
+        );
+      case Stage.QUESTIONS:
+        return (
+          <SkillSelections
+            onNextClick={handleNextClick}
+            onBackClick={handleBackClick}
+            handleOptionClick={handleOptionClick}
+            quizViewState={quizViewState}
+          />
+        );
+      case Stage.ROLE:
+        return (
+          <SkillSelections
+            onNextClick={handleNextClick}
+            onBackClick={handleBackClick}
+            handleOptionClick={handleOptionClick}
+            quizViewState={quizViewState}
+          />
+        );
+      case Stage.SKILL_RATING:
+        return (
+          <BluePrint
+            onNextClick={handleNextClick}
+            onBackClick={handleBackClick}
+          />
+        );
+      case Stage.RESULT:
+        return (
+          <LanguageResults
+            onBackClick={handleBackClick}
+            quizViewState={quizViewState}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
+  return <div>{renderStage()}</div>;
+}
