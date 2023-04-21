89e32706a2754f35d14f23f36b9698eb3266dbe7
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-04-20 19:57:10 -0500

    made additional changes as functions were previously updated

--- pages/api/studentPortal/freemium/helpers.test.ts
+++ pages/api/studentPortal/freemium/helpers.test.ts
@@ -5,21 +5,19 @@ test("Test elapsedDays function - within trial period", async () => {
   //set Mock date using MockDate package
   MockDate.set("2023-03-06");
 
-    const createdAt = new Date("2023-02-22")
-    const daysRemaining = 12
-
+    const createdAt = new Date("2023-03-04")
   // Act
   const result = elapsedDays(createdAt);
 
   // Assert
-  expect(result).toBe(daysRemaining);
+  expect(result).toBe(2);
 });
 
 test("Test elapsedDays function - expired", async () => {
     //set Mock date using MockDate package
-    MockDate.set("2023-03-06");
+    MockDate.set("2023-01-16");
   
-      const createdAt = new Date("2023-01-22")
+      const createdAt = new Date("2023-01-01")
   
     // Act
     const result = elapsedDays(createdAt);
@@ -28,26 +26,12 @@ test("Test elapsedDays function - expired", async () => {
     expect(result).toBe(TOTAL_TRIAL_DAYS);
   });
 
-  test("Test elapsedDays function - createdAt date is after system date", async () => {
-    //this use case is in case the database accidentially gets changed -- I think it should in this case go to 0 so we know there's an error.
-    //set Mock date using MockDate package
-    MockDate.set("2023-03-06");
-  
-      const createdAt = new Date("2024-01-22")
-  
-    // Act
-    const result = elapsedDays(createdAt);
-  
-    // Assert
-    expect(result).toBe(0);
-  });
-
 test("Test calcualteRemainingTrialDays function - within trial period", async () => {
   //set Mock date using MockDate package
   MockDate.set("2023-03-06");
 
     const createdAt = new Date("2023-02-22")
-    const daysRemaining = 9
+    const daysRemaining = 2
 
   // Act
   const result = calculateRemainingTrialDays(createdAt);
