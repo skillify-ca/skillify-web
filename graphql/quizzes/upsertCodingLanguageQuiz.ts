84ea6a89fa807e50e4f8592b71f88a7f8a8248d9
Raveen Reddy <raveenreddy89@gmail.com>
2023-04-21 13:02:30 -0400

    fix gql query

--- graphql/quizzes/upsertCodingLanguageQuiz.ts
+++ graphql/quizzes/upsertCodingLanguageQuiz.ts
@@ -1,11 +1,22 @@
 import { gql } from "@apollo/client";
 
 export const UPSERT_CODING_LANGUAGE_QUIZ_RESPONSE = gql`
-mutation upsertCodingLanguageQuizResponse($objects: [coding_language_quiz_insert_input!]!) {
-  insert_coding_language_quiz(objects: $objects, on_conflict: {constraint: coding_language_quiz_pkey, update_columns: [name, email, reasons, fields, interests, result]}) {
-    affected_rows
-    returning{
-      name
-      result
-      email
-    }`;
+  mutation upsertCodingLanguageQuizResponse(
+    $objects: [coding_language_quiz_insert_input!]!
+  ) {
+    insert_coding_language_quiz(
+      objects: $objects
+      on_conflict: {
+        constraint: coding_language_quiz_pkey
+        update_columns: [name, email, reasons, fields, interests, result]
+      }
+    ) {
+      affected_rows
+      returning {
+        name
+        result
+        email
+      }
+    }
+  }
+`;
