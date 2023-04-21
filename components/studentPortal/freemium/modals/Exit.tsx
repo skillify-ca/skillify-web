56cff16589148e17eeda86f88b314db263827143
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-04-20 20:13:47 -0500

    modified FreemiumExitComponent
    1. modified this component to cut down to 1 modal.
    2.  populated the content in the Exit.tsx

---
+++ components/studentPortal/freemium/modals/Exit.tsx
@@ -0,0 +1,21 @@
+import router from "next/router";
+import React from "react";
+import { Button } from "../../../ui/Button";
+
+const handlePlansSignUp = () => {
+  router.push("/plans");
+};
+
+export default function Exit() {
+  return (
+    <div className="flex flex-col justify-center items-center space-y-8 h-full">
+      <div className="text-white text-center text-lg md:text-3xl font-bold my-10 md:my-0">
+        <h1>Sorry, your free trial has ended.</h1>
+        <h1>Upgrade your plan to continue learning!</h1>
+      </div>
+      <div className="flex justify-center items-center">
+        <Button onClick={handlePlansSignUp} label={"View Plans"} />
+      </div>
+    </div>
+  );
+}
