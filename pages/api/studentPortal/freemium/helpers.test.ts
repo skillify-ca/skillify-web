1e0ae1666ce68e1db5c92c4fceb0146de53264ba
kwiedenhaupt <kwiedenhaupt@yahoo.com>
2023-04-20 19:40:52 -0500

    making adjustments on unit tests because of the function changes

--- pages/api/studentPortal/freemium/helpers.test.ts
+++ pages/api/studentPortal/freemium/helpers.test.ts
@@ -1,16 +1,15 @@
 import MockDate from "mockdate";
-import { calculateRemainingTrialDays, elapsedDays } from "./helpers";
+import { TOTAL_TRIAL_DAYS, calculateRemainingTrialDays, elapsedDays } from "./helpers";
 
 test("Test elapsedDays function - within trial period", async () => {
   //set Mock date using MockDate package
   MockDate.set("2023-03-06");
 
     const createdAt = new Date("2023-02-22")
-    const trialLength = 30
     const daysRemaining = 12
 
   // Act
-  const result = elapsedDays(createdAt, trialLength);
+  const result = elapsedDays(createdAt);
 
   // Assert
   expect(result).toBe(daysRemaining);
@@ -21,13 +20,12 @@ test("Test elapsedDays function - expired", async () => {
     MockDate.set("2023-03-06");
   
       const createdAt = new Date("2023-01-22")
-      const trialLength = 30
   
     // Act
-    const result = elapsedDays(createdAt, trialLength);
+    const result = elapsedDays(createdAt);
   
     // Assert
-    expect(result).toBe(trialLength);
+    expect(result).toBe(TOTAL_TRIAL_DAYS);
   });
 
   test("Test elapsedDays function - createdAt date is after system date", async () => {
@@ -36,10 +34,9 @@ test("Test elapsedDays function - expired", async () => {
     MockDate.set("2023-03-06");
   
       const createdAt = new Date("2024-01-22")
-      const trialLength = 30
   
     // Act
-    const result = elapsedDays(createdAt, trialLength);
+    const result = elapsedDays(createdAt);
   
     // Assert
     expect(result).toBe(0);
@@ -50,7 +47,7 @@ test("Test calcualteRemainingTrialDays function - within trial period", async ()
   MockDate.set("2023-03-06");
 
     const createdAt = new Date("2023-02-22")
-    const daysRemaining = 18
+    const daysRemaining = 9
 
   // Act
   const result = calculateRemainingTrialDays(createdAt);
@@ -64,20 +61,6 @@ test("Test calculateRemainingTrialDays function - expired", async () => {
     MockDate.set("2023-03-06");
   
       const createdAt = new Date("2023-01-22")
-  
-    // Act
-    const result = calculateRemainingTrialDays(createdAt);
-  
-    // Assert
-    expect(result).toBe(0);
-  });
-
-  test("Test calculateRemainingTrialDaysRemaining function - createdAt date is after system date", async () => {
-    //this use case is in case the database accidentially gets changed -- I think it should in this case go to 0 so we know there's an error.
-    //set Mock date using MockDate package
-    MockDate.set("2023-03-06");
-  
-      const createdAt = new Date("2024-01-22")
   
     // Act
     const result = calculateRemainingTrialDays(createdAt);
