2e7626cac88c64cbf22b054ba621f0a314c43371
Raveen Reddy <raveenreddy89@gmail.com>
2023-04-25 15:08:59 -0400

    sign up flow

--- pages/plans.tsx
+++ pages/plans.tsx
@@ -1,63 +1,47 @@
-import router from "next/router";
-import React, { useState } from "react";
 import LandingNavbar from "../components/landingPage/LandingNavbar";
 import PlansCard from "../components/studentPortal/freemium/PlansCard";
-import SignInPage from "../components/welcomePage/SignInPage";
+import { useAuth } from "../lib/authContext";
 
-const Plans = ({ plansCardData }) => {
-  const [showSignInPage, setShowSignInPage] = useState(false);
+export type PlanCard = {
+  planName: "freeTrial" | "premium";
+  title: string;
+  description: string;
+  price: string;
+  buttonLabel: string;
+  planCardRow: Array<{ icon: string; description: string }>;
+};
 
-  const handlePremium = () => {
-    router.push(plansCardData[1].onClick);
-  };
-
-  const handleTrial = () => {
-    setShowSignInPage(true);
-  };
+const Plans = (props: { planCardData: PlanCard[] }) => {
+  const { planCardData } = props;
+  const { signIn } = useAuth();
 
   return (
     <div>
-      {showSignInPage ? (
-        <SignInPage />
-      ) : (
-        <div>
-          <LandingNavbar />
-          <div className="flex flex-col items-center justify-center space-y-2 mb-8">
-            <h1 className="text-charmander text-3xl font-bold text-center p-4">
-              Pick the Plan That's Right For You
-            </h1>
-            <p>Reserve your spot today!</p>
-          </div>
-          <div className="flex flex-wrap justify-center align-items-stretch md:space-x-10 space-x-0">
-            {plansCardData.map((card) => (
-              <PlansCard
-                key={card.title}
-                title={card.title}
-                description={card.description}
-                price={card.price}
-                buttonLabel={card.buttonLabel}
-                onClick={
-                  card.onClick === "/sign-up" ? handleTrial : handlePremium
-                }
-                planRowData={card.planRowData}
-              />
-            ))}
-          </div>
-        </div>
-      )}
+      <LandingNavbar />
+      <div className="flex flex-col items-center justify-center space-y-2 mb-8">
+        <h1 className="text-charmander text-3xl font-bold text-center p-4">
+          Pick the plan that is right for you
+        </h1>
+        <p>Reserve your spot today!</p>
+      </div>
+      <div className="flex flex-wrap justify-center align-items-stretch md:space-x-10 space-x-0">
+        {planCardData.map((card, index) => (
+          <PlansCard key={index} planCard={card} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export async function getServerSideProps() {
-  const plansCardData = [
+  const planCardData: PlanCard[] = [
     {
+      planName: "freeTrial",
       title: "Free 14-day Trial",
       description: "No credit card required",
       price: "$0",
       buttonLabel: "Sign Up",
-      onClick: "/sign-up",
-      planRowData: [
+      planCardRow: [
         {
           icon: "../../images/freemium/greenCheck.svg",
           description: "Two weeks access to our Coding Basics course",
@@ -86,12 +70,12 @@ export async function getServerSideProps() {
       ],
     },
     {
+      planName: "premium",
       title: "Premium",
       description: "Contact us for pricing",
       price: "Custom",
       buttonLabel: "Book a Call",
-      onClick: "https://joinskillify.com/call",
-      planRowData: [
+      planCardRow: [
         {
           icon: "../../images/freemium/greenCheck.svg",
           description: "Unlimited access to our Coding Basics course",
@@ -122,7 +106,7 @@ export async function getServerSideProps() {
   ];
   return {
     props: {
-      plansCardData: plansCardData,
+      planCardData: planCardData,
     },
   };
 }
