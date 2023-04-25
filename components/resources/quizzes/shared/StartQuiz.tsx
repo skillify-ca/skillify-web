96dae2d040f5bd522063f60c397a48ef0573f49e
LuckyHariharan <lharihar@uwaterloo.ca>
2023-04-25 03:34:19 -0400

    Update StartQuiz.tsx

--- components/resources/quizzes/shared/StartQuiz.tsx
+++ components/resources/quizzes/shared/StartQuiz.tsx
@@ -1,18 +1,38 @@
 import React from "react";
 import { Button } from "../../../ui/Button";
-
 import SkillifyNavbar from "./SkillifyNavbar";
+
 type StartQuizProps = {
   onNextClick: () => void;
   title: string;
   body: string;
+  userInput: {
+    name: string;
+    email: string;
+  };
+  setUserInput: (userInput: { name: string; email: string }) => void;
+  handleUserInputMutations: (userInput: {
+    name: string;
+    email: string;
+  }) => void;
 };
 
-const StartQuiz = ({ onNextClick, body, title }: StartQuizProps) => {
+const StartQuiz = ({
+  onNextClick,
+  body,
+  title,
+  userInput,
+  setUserInput,
+  handleUserInputMutations,
+}: StartQuizProps) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUserInput({ ...userInput, [name]: value });
+  };
+
   return (
     <div className="w-full space-y-4 ">
       <SkillifyNavbar hidden={true} onBackClick={() => ""} />
-
       <div className="flex justify-center p-4 ml-16 "></div>
       <div className="flex flex-col items-center px-4 space-y-6 text-center">
         <h1 className="text-3xl font-semibold">{title}</h1>
@@ -22,19 +42,27 @@ const StartQuiz = ({ onNextClick, body, title }: StartQuizProps) => {
           <input
             type="text"
             name="name"
+            value={userInput.name}
+            onChange={handleInputChange}
             className="w-4/5 ml-8 border border-gray-500 rounded-lg shadow appearance-none"
           ></input>
           <h3 className="ml-8">Email Address</h3>{" "}
           <input
             type="email"
             name="email"
+            value={userInput.email}
+            onChange={handleInputChange}
             className="w-4/5 ml-8 border border-gray-500 rounded-lg shadow appearance-none"
           ></input>
         </div>
         <Button
           backgroundColor="yellow"
           label="Start Quiz"
-          onClick={onNextClick}
+          disabled={userInput.name.length == 0 && userInput.email.length == 0}
+          onClick={() => {
+            onNextClick();
+            handleUserInputMutations(userInput);
+          }}
         />
       </div>
     </div>
