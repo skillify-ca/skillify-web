96b050a10de56a09ad7623e377d967a75fe1c2aa
Jimi <jimi_p85@hotmail.com>
2023-04-25 15:12:36 -0400

    removed practice file

--- pages/gql.tsx
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@apollo/client";
-import { FETCH_COACHES } from "../graphql/studentPortal/coaches/fetchCoaches";
-
-const gqlPracticePage = () => {
-  const { data } = useQuery(FETCH_COACHES);
-  return <div>{JSON.stringify(data)}</div>;
-};
-
-export default gqlPracticePage;
