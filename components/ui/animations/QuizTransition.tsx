f00dea89f30cffc1039b0a33be500c9f348a5917
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2023-04-27 15:54:31 -0400

    Quiz Styling Fixes (#959)
    * Fix blueprint responsive design
    
    * Remove initial animation
    
    * Fix responsive design
    
    * Update EduBackground.tsx
    
    * slightly speed up animation
    
    * Update EduBackground.tsx
    
    * Responsive design
    
    * Responsive design fix
    
    * Update EduBackground.tsx
    
    * div width on education background stage
    
    ---------
    
    Co-authored-by: Raveen Reddy <raveenreddy89@gmail.com>

--- components/ui/animations/QuizTransition.tsx
+++ components/ui/animations/QuizTransition.tsx
@@ -11,12 +11,10 @@ export const QuizTransition: React.FC<{
       mass: 1,
       tension: 120,
       friction: 14,
-      duration: 300,
+      duration: 200,
       ease: "easeInOut",
     },
     opacity: triggerAnimation ? 1 : 0,
-    transform: triggerAnimation ? "translateX(0)" : "translateX(-100%)",
-    from: { opacity: 0, transform: "translateX(-100%)" },
   });
   return (
     <div>
