11ba27b3e0b764dacdf8dbef9fcd07f186e2353d
Jimi <122390267+jimip85@users.noreply.github.com>
2023-05-25 13:09:28 -0400

    migrating props lesson to shared components

--- pages/api/studentPortal/lessons/web/react/index.ts
+++ pages/api/studentPortal/lessons/web/react/index.ts
@@ -1,11 +1,14 @@
 import { getDataForComponentsLesson } from "./components";
 import { getGithubLessonData } from "./github";
+import { getPropsLessonData } from "./props";
 
 export function getLessonForReactCourse(lessonId: string) {
   if (lessonId === "components") {
     return getDataForComponentsLesson();
   } else if (lessonId === "github") {
     return getGithubLessonData();
+  } else if (lessonId === "props") {
+    return getPropsLessonData();
   }
 
   throw new Error(`Could not find lessons`);
