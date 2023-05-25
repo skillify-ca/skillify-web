277410d6416de93bca2f905c7163a54e91ee6276
Jimi <122390267+jimip85@users.noreply.github.com>
2023-05-25 12:56:48 -0400

    migrating github to shared endpoint

--- pages/api/studentPortal/lessons/web/react/index.ts
+++ pages/api/studentPortal/lessons/web/react/index.ts
@@ -1,8 +1,11 @@
 import { getDataForComponentsLesson } from "./components";
+import { getGithubLessonData } from "./github";
 
 export function getLessonForReactCourse(lessonId: string) {
   if (lessonId === "components") {
     return getDataForComponentsLesson();
+  } else if (lessonId === "github") {
+    return getGithubLessonData();
   }
 
   throw new Error(`Could not find lessons`);
