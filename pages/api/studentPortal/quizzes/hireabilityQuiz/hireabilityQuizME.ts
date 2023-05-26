fae2459cc8a8ffdb55299f7dffe5693a709860ee
LuckyHariharan <lharihar@uwaterloo.ca>
2023-05-26 05:42:45 -0400

    Initialize four track data

---
+++ pages/api/studentPortal/quizzes/hireabilityQuiz/hireabilityQuizME.ts
@@ -0,0 +1,191 @@
+import { QuizViewState } from "../../../../../components/resources/quizzes/shared/types";
+
+export const quizData: QuizViewState = {
+  title: "How hireable are you?",
+  body: "Take this free quiz to reveal your hireability score for software engineering, product, or design roles.",
+  questions: [
+    {
+      title: "What role are you interested in?",
+      body: "Select the most relevant role.",
+      options: [
+        {
+          name: "Frontend Engineer",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Backend Engineer",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Game Engineer",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Mobile Engineer",
+          weight: 0,
+          isSelected: false,
+        },
+      ],
+    },
+    {
+      title: "ME Question 1",
+      body: "Select the most relevant options for this question",
+      options: [
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+      ],
+    },
+    {
+      title: "ME Question 2",
+      body: "Select the most relevant options for this question",
+      options: [
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+      ],
+    },
+    {
+      title: "ME Question 3",
+      body: "Select the most relevant options for this question",
+      options: [
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+      ],
+    },
+    {
+      title: "ME Question 4",
+      body: "Select the most relevant options for this question",
+      options: [
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+      ],
+    },
+    {
+      title: "ME Question 5",
+      body: "Select the most relevant options for this question",
+      options: [
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+        {
+          name: "Option",
+          weight: 0,
+          isSelected: false,
+        },
+      ],
+    },
+  ],
+  currentQuestion: 0,
+  progress: 0,
+};
