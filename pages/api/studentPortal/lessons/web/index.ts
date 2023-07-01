198e1c1d4866ee0f987f4eff9e4379abc4b1f842
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2023-07-01 10:18:49 -0400

    fix react course

--- pages/api/studentPortal/lessons/web/index.ts
+++ pages/api/studentPortal/lessons/web/index.ts
@@ -1,8 +1,12 @@
 import { getLessonForReactCourse } from "./react";
 
 export function getCourseForWebSection(courseId: string, lessonId: string) {
-  if (courseId === "react") {
+  console.log("courseId");
+  console.log(courseId);
+console.log(lessonId);
+
+  if (courseId.toLowerCase() === "react") {
     return getLessonForReactCourse(lessonId);
   }
-  throw new Error(`Could not find lessons`);
+  throw new Error(`Could not find lessons for ${courseId} ${lessonId}`);
 }
