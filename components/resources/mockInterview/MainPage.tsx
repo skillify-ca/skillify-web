c6bae0927057eb3603116fdda3866f222fb19426
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2023-08-01 22:07:18 -0400

    Update MainPage.tsx

--- components/resources/mockInterview/MainPage.tsx
+++ components/resources/mockInterview/MainPage.tsx
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const MainPage = ({ onNext }) => {
-  const boxStyle = {
-    backgroundColor: 'cyan',
-    borderRadius: '0.5rem',
-    padding: '1rem',
-    cursor: 'pointer',
-  };
+  const [selectedMode, setSelectedMode] = useState(
+    "Technical Recruiter Screen"
+  );
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-200">
-      <div className="grid grid-cols-3 gap-4">
-        <div
-          className="flex items-center justify-center"
-          style={boxStyle}
-          onClick={onNext}
-        >
-          Next
-        </div>
-        <div
-          className="flex items-center justify-center"
-          style={boxStyle}
-          onClick={onNext}
-        >
-          Next
-        </div>
-        <div
-          className="flex items-center justify-center"
-          style={boxStyle}
-          onClick={onNext}
-        >
-          Next
-        </div>
-      </div>
+    <div className="flex flex-col items-center justify-center h-screen gap-8 bg-gray-200">
+      <h1 className="text-2xl">Welcome to Mock Interview Prep. </h1>
+      <p>
+        Use this tool to record yourself answering questions to the most common
+        interview questions.
+      </p>
+      <select
+        value={selectedMode}
+        onChange={(e) => setSelectedMode(e.target.value)}
+        className="border-2 border-gray-400 rounded-lg"
+      >
+        {[
+          "Technical Recruiter Screen",
+          "Technical Phone Screen",
+          "Hiring Manager Screen",
+          "Onsite Interview",
+        ].map((it, index) => (
+          <option key={index}>{it}</option>
+        ))}
+      </select>
+      <ModeButton
+        disabled={selectedMode !== "Technical Recruiter Screen"}
+        text="Start"
+        onClick={() => {
+          if (selectedMode === "Technical Recruiter Screen") {
+            onNext();
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+const ModeButton = ({ disabled, text, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`${
+        disabled
+          ? "bg-gray-400"
+          : "bg-orange-400 cursor-pointer hover:bg-orange-500"
+      } px-4 py-2 rounded-lg font-bold text-white`}
+    >
+      {text}
     </div>
   );
 };
