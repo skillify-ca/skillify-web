371626700ea3cfbc16c8c4dffb2a2656d9a1ce31
vithushan19@users.noreply.github.com <vithushan19@gmail.com>
2023-08-02 17:44:46 -0400

    swap icon

--- pages/resources/interviewPrep/tools.tsx
+++ pages/resources/interviewPrep/tools.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { InterviewPrepCourse } from '.';
-import { FaBars } from 'react-icons/fa';
 import LandingNavbar from '../../../components/landingPage/LandingNavbar';
 
 export default function Dictionary() {
@@ -242,7 +241,18 @@ const DictionaryContent = () => {
                 className="float-right cursor-pointer"
                 onClick={toggleSidebar}
               >
-                <FaBars />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               </span>
             </div>
             {jobTerms.map((job) => (
