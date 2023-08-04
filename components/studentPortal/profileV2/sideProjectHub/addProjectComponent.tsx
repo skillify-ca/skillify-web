55f0060d2416624c3e8c47be3dd9de963d229e96
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-08-04 00:24:55 -0400

    Add placeholders for inputs on Add New Project page

--- components/studentPortal/profileV2/sideProjectHub/addProjectComponent.tsx
+++ components/studentPortal/profileV2/sideProjectHub/addProjectComponent.tsx
@@ -12,19 +12,35 @@ export default function AddProjectComponent() {
       <h2 className="text-3xl font-bold">Add New Project</h2>
 
       <h3 className="my-4 text-xl font-bold">Project Name</h3>
-      <Input value={projectName} setValue={setProjectName} />
+      <Input
+        value={projectName}
+        setValue={setProjectName}
+        placeholder="Your project name"
+      />
 
       <h3 className="my-4 text-xl font-bold">GitHub Link</h3>
-      <Input value={githubLink} setValue={setGithubLink} />
+      <Input
+        value={githubLink}
+        setValue={setGithubLink}
+        placeholder="https://github.com/..."
+      />
 
       <h3 className="my-4 text-xl font-bold">Project Link</h3>
-      <Input value={projectLink} setValue={setProjectLink} />
+      <Input
+        value={projectLink}
+        setValue={setProjectLink}
+        placeholder="https://xyz.vercel.app"
+      />
 
       <h3 className="my-4 text-xl font-bold">Image</h3>
       <div className="mb-4 p-2 flex flex-col rounded bg-backgroundSecondary">
         <div className="mt-2 mb-2">
           <h4 className="font-bold">Image link</h4>
-          <Input value={githubLink} setValue={setGithubLink} />
+          <Input
+            value={githubLink}
+            setValue={setGithubLink}
+            placeholder="https://xyz.vercel.app/image.jpeg"
+          />
         </div>
         <p>or</p>
         <div className="mb-2">
