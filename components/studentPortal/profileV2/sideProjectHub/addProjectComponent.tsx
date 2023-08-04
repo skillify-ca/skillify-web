a7bb5a0d4487102a42278e0ccb238ad1a6c9fd43
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-08-04 01:58:30 -0400

    Move headers and related input fields closer together

--- components/studentPortal/profileV2/sideProjectHub/addProjectComponent.tsx
+++ components/studentPortal/profileV2/sideProjectHub/addProjectComponent.tsx
@@ -12,30 +12,30 @@ export default function AddProjectComponent() {
     <div className="p-4 flex flex-col">
       <h2 className="text-3xl font-bold">Add New Project</h2>
 
-      <h3 className="my-4 text-xl font-bold">Project Name</h3>
+      <h3 className="mt-4 mb-2 text-xl font-bold">Project Name</h3>
       <Input
         value={projectName}
         setValue={setProjectName}
         placeholder="Your project name"
       />
 
-      <h3 className="my-4 text-xl font-bold">GitHub Link</h3>
+      <h3 className="mt-4 mb-2 text-xl font-bold">GitHub Link</h3>
       <Input
         value={githubLink}
         setValue={setGithubLink}
         placeholder="https://github.com/..."
       />
 
-      <h3 className="my-4 text-xl font-bold">Project Link</h3>
+      <h3 className="mt-4 mb-2 text-xl font-bold">Project Link</h3>
       <Input
         value={projectLink}
         setValue={setProjectLink}
         placeholder="https://xyz.vercel.app"
       />
 
-      <h3 className="my-4 text-xl font-bold">Image</h3>
+      <h3 className="mt-4 mb-2 text-xl font-bold">Image</h3>
       <div className="mb-4 p-2 flex flex-col rounded bg-backgroundSecondary">
-        <div className="mt-2 mb-2">
+        <div className="mt-2">
           <h4 className="font-bold">Image link</h4>
           <Input
             value={imageLink}
@@ -43,7 +43,7 @@ export default function AddProjectComponent() {
             placeholder="https://xyz.vercel.app/image.jpeg"
           />
         </div>
-        <p>or</p>
+        <p className="my-2">or</p>
         <div className="mb-2">
           <h4 className="font-bold">Upload file</h4>
           <input type="file" />
