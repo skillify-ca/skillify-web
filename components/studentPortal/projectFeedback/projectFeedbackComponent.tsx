e546e88ee37a7d260791c18f076750017588e981
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-08-04 09:29:16 -0400

    Add space between Mock data and Clear data buttons

--- components/studentPortal/projectFeedback/projectFeedbackComponent.tsx
+++ components/studentPortal/projectFeedback/projectFeedbackComponent.tsx
@@ -68,6 +68,7 @@ export default function ProjectFeedbackComponent() {
         backgroundColor="blue"
       />
       <br />
+      <br />
       <Button
         label="Clear data"
         onClick={() => setFeedbackRequests([])}
