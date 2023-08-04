412fdb5d3c4aa51829957408c526a93f95cffe78
Nils Emmenegger <61921611+nils-emmenegger@users.noreply.github.com>
2023-08-04 01:20:55 -0400

    Fix image link input + add placeholders on Edit Project page

--- components/studentPortal/profileV2/sideProjectHub/editProjectComponent.tsx
+++ components/studentPortal/profileV2/sideProjectHub/editProjectComponent.tsx
@@ -12,25 +12,42 @@ export default function EditProjectComponent({
   const [projectName, setProjectName] = useState("");
   const [githubLink, setGithubLink] = useState("");
   const [projectLink, setProjectLink] = useState("");
+  const [imageLink, setImageLink] = useState("");
 
   return (
     <div className="p-4 flex flex-col">
       <h2 className="text-3xl font-bold">Edit Project</h2>
 
       <h3 className="my-4 text-xl font-bold">Project Name</h3>
-      <Input value={projectName} setValue={setProjectName} />
+      <Input
+        value={projectName}
+        setValue={setProjectName}
+        placeholder="Your project name"
+      />
 
       <h3 className="my-4 text-xl font-bold">GitHub Link</h3>
-      <Input value={githubLink} setValue={setGithubLink} />
+      <Input
+        value={githubLink}
+        setValue={setGithubLink}
+        placeholder="https://github.com/..."
+      />
 
       <h3 className="my-4 text-xl font-bold">Project Link</h3>
-      <Input value={projectLink} setValue={setProjectLink} />
+      <Input
+        value={projectLink}
+        setValue={setProjectLink}
+        placeholder="https://xyz.vercel.app"
+      />
 
       <h3 className="my-4 text-xl font-bold">Image</h3>
       <div className="mb-4 p-2 flex flex-col rounded bg-backgroundSecondary">
         <div className="mt-2 mb-2">
           <h4 className="font-bold">Image link</h4>
-          <Input value={githubLink} setValue={setGithubLink} />
+          <Input
+            value={imageLink}
+            setValue={setImageLink}
+            placeholder="https://xyz.vercel.app/image.jpeg"
+          />
         </div>
         <p>or</p>
         <div className="mb-2">
