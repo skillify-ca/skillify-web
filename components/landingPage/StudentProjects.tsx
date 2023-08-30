6d5a63825db9cb56aa97fc0324a939b856714de6
Gawshan987 <gawshan.sri@gmail.com>
2023-08-30 15:48:45 -0400

    Got the images to overlap the cards

--- components/landingPage/StudentProjects.tsx
+++ components/landingPage/StudentProjects.tsx
@@ -1,35 +1,49 @@
 import React from "react";
+
 export default function StudentProjects() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-blue-800 text-center text-3xl mb-10">
+      <h1 className="text-blue-800 text-center text-3xl mb-10 font-bold">
         Student Projects
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-        <div className="flex flex-col items-center">
-          <img
-            className="rounded-full h-16 w-16"
-            src="/images/landingPage/raveen.jpg"
-            alt="Raveen's Profile"
-          />
+        <div className="flex flex-col items-center relative">
           <div className="bg-zinc-400 h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-lg">
             <span className="font-bold italic">Stock</span>
             <span className="font-bold bg-black-500 px-2 rounded text-white">
               News
             </span>
           </div>
-          <h1 className="mt-2 text-center">Stock News</h1>
+          <img
+            className="rounded-full h-16 w-16 -mt-8 absolute z-10"
+            src="/images/landingPage/raveen.jpg"
+            alt="Raveen's Profile"
+          />
+          <h1 className="mt-2 text-center text-xl text-charmander">
+            Stock News
+          </h1>
+          <h2>Raveen R.</h2>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center relative">
           <div className="bg-charmander h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-md">
             <div>Project Feedback</div>
           </div>
+          <img
+            className="rounded-full h-16 w-16 -mt-8 absolute z-10"
+            src="/images/landingPage/raveen.jpg"
+            alt="Raveen's Profile"
+          />
           <h1 className="mt-2 text-center">Project Feedback</h1>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center relative">
           <div className="bg-black-500 h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-xl text-white">
             <div>Interview</div>
           </div>
+          <img
+            className="rounded-full h-16 w-16 -mt-8 absolute z-10"
+            src="/images/landingPage/raveen.jpg"
+            alt="Raveen's Profile"
+          />
           <h1 className="mt-2 text-center">Interview</h1>
         </div>
       </div>
