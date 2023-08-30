eec751d48aeabb0b865ca4a1e09d4f3ec80d08e3
Gawshan987 <gawshan.sri@gmail.com>
2023-08-30 15:00:46 -0400

    Fixed start of subtitles

--- components/landingPage/StudentProjects.tsx
+++ components/landingPage/StudentProjects.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 export default function StudentProjects() {
   return (
     <div className="flex flex-col items-center">
@@ -13,27 +12,25 @@ export default function StudentProjects() {
             src="/images/landingPage/raveen.jpg"
             alt="Raveen's Profile"
           />
-          <div>
-            <div className="bg-zinc-400 h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-lg">
-              <span className="font-bold italic">Stock</span>
-              <span className="font-bold bg-black-500 px-2 rounded text-white">
-                News
-              </span>
-            </div>
-            <h1 className="mt-2 text-center">Stock News</h1>
+          <div className="bg-zinc-400 h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-lg">
+            <span className="font-bold italic">Stock</span>
+            <span className="font-bold bg-black-500 px-2 rounded text-white">
+              News
+            </span>
           </div>
+          <h1 className="mt-2 text-center">Stock News</h1>
         </div>
         <div className="flex flex-col items-center">
           <div className="bg-charmander h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-md">
             <div>Project Feedback</div>
-            <h1 className="mt-2 text-center">Project Feedback</h1>
           </div>
+          <h1 className="mt-2 text-center">Project Feedback</h1>
         </div>
         <div className="flex flex-col items-center">
           <div className="bg-black-500 h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-xl text-white">
             <div>Interview</div>
-            <h1 className="mt-2 text-center">Interview</h1>
           </div>
+          <h1 className="mt-2 text-center">Interview</h1>
         </div>
       </div>
     </div>
