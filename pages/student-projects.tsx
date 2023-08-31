6367c449a0cdcee8c3109a309143edfcd28c5321
Gawshan987 <gawshan.sri@gmail.com>
2023-08-31 19:42:48 -0400

    Added second row of cards

--- pages/student-projects.tsx
+++ pages/student-projects.tsx
@@ -10,38 +10,24 @@ const StudentProjectsPage = () => {
           Student Projects
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
-          {/* Existing Cards */}
-          {/* Card 1 */}
-          <div className="flex flex-col items-center relative">
-            {/* Card Content */}
-          </div>
-          {/* Card 2 */}
-          <div className="flex flex-col items-center relative">
-            {/* Card Content */}
-          </div>
-          {/* Card 3 */}
-          <div className="flex flex-col items-center relative">
-            {/* Card Content */}
-          </div>
+          <div className="flex flex-col items-center relative"></div>
+          <div className="flex flex-col items-center relative"></div>
+          <div className="flex flex-col items-center relative"></div>
 
-          {/* New Cards */}
-          {/* Card 4 */}
           <div className="flex flex-col items-center relative">
             <div className="bg-zinc-400 h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-lg">
-              <h1>Place Holder 4</h1>
+              <h1>Stock News</h1>
             </div>
             <img
               className="rounded-full h-16 w-16 -mt-8 absolute z-10"
               src="/images/landingPage/raveen.jpg"
-              alt="Raveen's Profile"
             />
             <h1 className="mt-2 text-center text-xl text-charmander">
-              Stock News 2
+              Stock News
             </h1>
             <h1>Raveen R.</h1>
           </div>
 
-          {/* Card 5 */}
           <div className="flex flex-col items-center relative">
             <div
               className="bg-charmander h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-md"
@@ -52,33 +38,75 @@ const StudentProjectsPage = () => {
                 backgroundPosition: "center",
               }}
             >
-              <div>Project Feedback 2</div>
+              <div>Project Feedback </div>
             </div>
             <img
               className="rounded-full h-16 w-16 -mt-8 absolute z-10"
               src="/images/landingPage/raveen.jpg"
-              alt="Raveen's Profile"
             />
             <h1 className="mt-2 text-center text-xl text-charmander">
-              PlaceHolder 5
+              PlaceHolder
             </h1>
             <h1>Place Holder</h1>
           </div>
 
-          {/* Card 6 */}
           <div className="flex flex-col items-center relative">
             <div className="bg-black-500 h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-xl text-white">
-              <div>Interview 2</div>
+              <div>Interview </div>
             </div>
             <img
               className="rounded-full h-16 w-16 -mt-8 absolute z-10"
               src="/images/landingPage/raveen.jpg"
-              alt="Raveen's Profile"
             />
             <h1 className="mt-2 text-center text-xl text-charmander">
-              Interview 2
+              PlaceHolder
             </h1>
-            <h1>Jane H.</h1>
+            <h1>PlaceHolder</h1>
+          </div>
+          <div>
+            <div className="flex flex-col items-center relative">
+              <div className="bg-yellow-500 h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-xl text-white">
+                <div>PlaceHolder</div>
+              </div>
+              <img
+                className="rounded-full h-16 w-16 -mt-8 absolute z-10"
+                src="/images/landingPage/raveen.jpg"
+              />
+              <h1 className="mt-2 text-center text-xl text-charmander">
+                PlaceHolder
+              </h1>
+              <h1>PlaceHolder</h1>
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-col items-center relative">
+              <div className="bg-blue-800 h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-xl text-white">
+                <div>PlaceHolder</div>
+              </div>
+              <img
+                className="rounded-full h-16 w-16 -mt-8 absolute z-10"
+                src="/images/landingPage/raveen.jpg"
+              />
+              <h1 className="mt-2 text-center text-xl text-charmander">
+                PlaceHolder
+              </h1>
+              <h1>PlaceHolder</h1>
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-col items-center relative">
+              <div className="bg-red-500 h-80 w-72 flex flex-col justify-center items-center rounded-lg shadow-xl text-white">
+                <div>PlaceHolder</div>
+              </div>
+              <img
+                className="rounded-full h-16 w-16 -mt-8 absolute z-10"
+                src="/images/landingPage/raveen.jpg"
+              />
+              <h1 className="mt-2 text-center text-xl text-charmander">
+                PlaceHolder
+              </h1>
+              <h1>PlaceHolder</h1>
+            </div>
           </div>
         </div>
       </div>
