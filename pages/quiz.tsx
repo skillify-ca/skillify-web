15310986aa636b00375b2ecb203eb88bf51d04fb
LuckyHariharan <lharihar@uwaterloo.ca>
2023-09-27 20:11:20 -0400

    Complete Quizzes Dashboard

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -1,39 +1,81 @@
+import Head from "next/head";
 import React from "react";
-import SEO from "../components/SEO";
+import SEO from "../components/SEO"; // Assuming this is for the quiz page
+import Card from "../components/blog/BlogCard";
+import LandingNavbar from "../components/landingPage/LandingNavbar";
 
-export default function Page() {
+export default function Quiz() {
   return (
     <div>
+      <Head>
+        <title>{"Quizzes"}</title>
+        <meta
+          name="description"
+          content={"Articles and resources about Toronto coding bootcamps"}
+        />
+        <meta property="og:title" content={"Skillify Quiz"} />
+        <meta
+          property="og:image"
+          content={"https://www.skillify.ca/images/images/logo.png"}
+        />
+        <meta
+          property="og:description"
+          content={"Articles and resources about Toronto coding bootcamps"}
+        />
+        <meta property="og:url" content="https://skillify.ca/" />
+        <meta property="og:type" content="website" />
+      </Head>
+
+      {/* Assuming this is the SEO component for the quiz page */}
       <SEO
         title={"Breaking into Tech Career Personality Quiz"}
         description={
-          "This quiz tells you best path to starting a career in tech"
+          "This quiz tells you the best path to starting a career in tech"
         }
         image={"https://melv1n.com/img/learn-to-code-how-to-start.png"}
       />
-      <div className="flex flex-col bg-white">
-        <div className="flex flex-col items-center py-4 text-white bg-murkrow">
-          <img src="/images/logo-dark.svg" className="w-36" />
-          <h1 className="mt-4 text-3xl font-bold text-center">
-            Career in Tech Personality Quiz
-          </h1>
-          <p className="text-center">
-            This free quiz will help you figure out what job in tech is the best
-            fit for you.
-          </p>
-        </div>
 
-        <div className="bg-gray-200">
-          <iframe
-            src="https://ly0na3vldi7.typeform.com/to/m5Omy5la"
-            className="w-full h-screen"
-          ></iframe>
+      <LandingNavbar />
+      <h1 className="w-full p-4 text-5xl font-bold text-center ">Quizzes</h1>
+
+      <div className="absolute w-full ">
+        <div className="absolute top-0 w-full h-full p-8 md:p-16 opacity-5"></div>
+
+        <div className="grid items-center w-full grid-cols-1 gap-16 p-4 sm:grid-cols-2 lg:grid-cols-3">
+          <div>
+            <Card
+              date={"March 3, 2023"}
+              title={"Which Programming Language should I start with?"}
+              image={
+                "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
+              }
+              description={
+                "A personality quiz to help chose a programming language to start with."
+              }
+              link={"/resources/quizzes/firstProgrammingLanguage"}
+              color={3}
+            />
+          </div>
+          <div>
+            <Card
+              date={"March 2, 2023"}
+              title={"Which Career in tech is Meant for You?"}
+              image={
+                "https://images.unsplash.com/photo-1459180129673-eefb56f79b45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2073&q=80"
+              }
+              description={
+                "A Skillify Student's Account of Switching Careers from Actuarial Science to Software Engineering"
+              }
+              link={"/resources/quizzes/careerQuiz"}
+              color={0}
+            />
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-Page.getLayout = function getLayout(page) {
+Quiz.getLayout = function getLayout(page) {
   return <div>{page}</div>;
 };
