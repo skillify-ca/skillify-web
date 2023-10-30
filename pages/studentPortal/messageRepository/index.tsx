b866b259f1bacc30dd3f792a1826726c28ff45d6
Gawshan987 <114423326+Gawshan987@users.noreply.github.com>
2023-10-29 21:03:24 -0400

    Message Repository UI (#998)
    * save starting point
    
    starting project to hand over to Gawshan
    
    * Add starting messgae repo
    
    * Add Message Repo UI
    
    * Fix falling pull request
    
    * Update index.tsx
    
    * Add mutation for message repo
    
    * Refractor Message and Date
    
    * Add MessaegFeed and insertMessage query
    
    * fix broken insert
    
    * Revert "fix broken insert"
    
    This reverts commit badea4fefcb6169e305c51845ecc100a498c9cd6.
    
    * Revert "Add MessaegFeed and insertMessage query"
    
    This reverts commit 554356bcb4ff31e50338c8dcfa36e0c2fd43a041.
    
    * fix insert
    
    ---------
    
    Co-authored-by: LuckyHariharan <lharihar@uwaterloo.ca>

---
+++ pages/studentPortal/messageRepository/index.tsx
@@ -0,0 +1,76 @@
+import { useMutation } from "@apollo/client";
+import { format } from "date-fns";
+import React, { useState } from "react";
+import { Button } from "../../../components/ui/Button";
+import { insert_Message } from "../../../graphql/studentPortal/messageRepository/insertMessage";
+import { useAuth } from "../../../lib/authContext";
+type Message = {
+  message: string;
+  date: string;
+};
+function MessageRepository() {
+  // Initialize state for the message input.
+  const [message, setMessage] = useState("");
+  const { user } = useAuth();
+
+  //date for state var
+  const [date, setDate] = useState(new Date());
+
+  const [targetDate, setTargetDate] = useState({});
+
+  // route back to goals overview page on complete
+  const [saveNewMessage] = useMutation(insert_Message);
+
+  // Function to update the 'message' state when the input changes
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  // Function to handle form submission.
+  const handleSubmit = (e) => {
+    //prevent default
+    e.preventDefault();
+    //log the current message
+    // Clear the input after submission.
+    saveNewMessage({
+      variables: {
+        date: date,
+        message: message,
+      },
+    });
+    setMessage("");
+  };
+  // Render a form with an input field and a submit button.
+  return (
+    <div
+      onSubmit={handleSubmit}
+      className="ml-4 p-2 rounded-lg space-y-6 flex flex-col"
+    >
+      <div className="pt-6">
+        <h1 className="font-bold text-4xl">Message Repo</h1>
+        <h1 className="font-bold text-xl pt-12">Reachout Message</h1>
+      </div>
+
+      <textarea
+        className="text-left p-2 border rounded-md shadow-md w-1/2 dark:text-murkrow"
+        placeholder="Enter your reachout message..."
+        value={message}
+        onChange={handleMessageChange}
+      />
+      <h1 className="font-bold text-xl">Target Completion Date</h1>
+      <input
+        type="date"
+        className={`text-left p-2 border rounded-md w-1/2 dark:text-murkrow shadow-lg `}
+        value={format(date, "yyyy-MM-dd")}
+        onChange={(e) => {
+          setDate(new Date(e.target.value));
+        }}
+      />
+      <Button label="Add Goal" onClick={handleSubmit}>
+        Add Goal
+      </Button>
+    </div>
+  );
+}
+
+export default MessageRepository;
