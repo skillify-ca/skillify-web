d5339513767ab8cb6df9565d0ca8fd27ede0c679
Vithushan <vithushan19@users.noreply.github.com>
2023-10-31 14:16:54 -0400

    Allow user to upload a new picture for their profile to aws s3 (#1001)
    * Create drag and drop UI
    
    * Update edit.tsx
    
    * upload to aws on save click

--- pages/api/studentPortal/profile/profilePicturesClient.ts
+++ pages/api/studentPortal/profile/profilePicturesClient.ts
@@ -1,6 +1,8 @@
 import {
   GetObjectCommand,
   ListObjectsV2Command,
+  PutObjectCommand,
+  PutObjectCommandOutput,
   S3Client,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
@@ -25,8 +27,18 @@ export const fetchProfilePicture = async (
     .send(new ListObjectsV2Command(params))
     .then(async (res) => {
       if (res.Contents && res.Contents.length > 0) {
-        const firstImage = res.Contents.filter((x) => !x.Key?.endsWith("/"))[0]
-          .Key;
+        const bucketObjects = res.Contents.filter((x) => !x.Key?.endsWith("/"));
+
+        // sort by last modified date
+        bucketObjects.sort((a, b) => {
+          if (a.LastModified && b.LastModified) {
+            return b.LastModified.getTime() - a.LastModified.getTime();
+          }
+          return 0;
+        });
+
+        const firstImage = bucketObjects[0].Key;
+
         const imageUrl = await getSignedUrl(
           s3,
           new GetObjectCommand({
@@ -43,4 +55,27 @@ export const fetchProfilePicture = async (
       console.log("Error fetching image from bucket", err);
       return null;
     });
+};
+
+export const uploadProfilePicture = async (
+  userId: string,
+  file: File
+): Promise<PutObjectCommandOutput> => {
+  const s3 = new S3Client({
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID || "",
+      secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY || "",
+    },
+  });
+
+  const uploadParams = {
+    Bucket: "student-profile-pictures",
+    Key: `${userId}/${file.name}`,
+    Body: file,
+  };
+
+  return s3.send(new PutObjectCommand(uploadParams)).then(async (res) => {
+    return res;
+  });
 };
