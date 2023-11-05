c9dc17cc01af1d426bf9e9d52bcc4c06734d435e
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2023-11-05 06:59:09 -0500

    use open ai functions

--- pages/api/studentPortal/games/matching/generateQuestionAnswerPairs.ts
+++ pages/api/studentPortal/games/matching/generateQuestionAnswerPairs.ts
@@ -14,7 +14,7 @@ export default async function generateQuestionAnswerPairs(
 
   // generate from open ai api
   const data = await runConversation(userPrompt as string);
-  res.status(200).json({ data: JSON.parse(data) });
+  res.status(200).json({ data });
 }
 
 async function runConversation(userPrompt: string) {
@@ -25,15 +25,50 @@ async function runConversation(userPrompt: string) {
         "Generate a list of 10 question and answer pairs. The questions should be simple definitions.\n" +
         "The answers should be the term that matches to its definition.\n" +
         `The questions should be about ${userPrompt}.\n` +
-        "Do not use the term itself inside of the definition.\n" +
-        "Output the list as a JSON array.",
+        "Do not use the term itself inside of the definition.\n",
+    },
+  ];
+
+  const functions = [
+    {
+      name: "generate_question_answer_pairs",
+      description:
+        "Generate question and answer pairs for an educational matching game.",
+      parameters: {
+        type: "object",
+        properties: {
+          data: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                question: {
+                  type: "string",
+                },
+                answer: {
+                  type: "string",
+                },
+              },
+              required: ["question", "answer"],
+            },
+          },
+        },
+      },
     },
   ];
 
   const response: ChatCompletion = await openai.chat.completions.create({
     model: "gpt-4",
     messages: messages,
+    functions: functions,
   });
 
-  return response.choices[0].message.content;
+  const responseMessage = response.choices[0].message;
+  if (responseMessage.function_call) {
+    const result = JSON.parse(responseMessage.function_call.arguments).data;
+    console.log(result);
+    return result;
+  } else {
+    return null;
+  }
 }
