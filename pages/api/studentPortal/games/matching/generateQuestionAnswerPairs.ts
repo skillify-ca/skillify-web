886b66c1190e3605b1fb1eb8119afc4a7701b4e6
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2023-11-06 15:40:54 -0500

    Update generateQuestionAnswerPairs.ts

--- pages/api/studentPortal/games/matching/generateQuestionAnswerPairs.ts
+++ pages/api/studentPortal/games/matching/generateQuestionAnswerPairs.ts
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 import { ChatCompletion, ChatCompletionMessageParam } from "openai/resources";
 
 const openai = new OpenAI({
-  apiKey: process.env["OPENAI_API_KEY"],
+  apiKey: process.env["NEXT_PUBLIC_OPENAI_API_KEY"],
 });
 
 export default async function generateQuestionAnswerPairs(
