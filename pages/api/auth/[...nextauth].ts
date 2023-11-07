83437bc9f4456aa204fc4cb8db83d4a4c157f957
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2023-11-06 20:57:17 -0500

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,8 +6,8 @@ import { FETCH_USER_BY_EMAIL } from "../../../graphql/studentPortal/users/fetchU
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? "",
       authorization: {
         params: {
           prompt: "consent",
