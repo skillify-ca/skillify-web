c3485803581ba80dc1efa19931d387fb4a9d04b9
Gawshan987 <gawshan.sri@gmail.com>
2023-11-17 14:08:47 -0500

    Start Vercel Lesson

---
+++ pages/studentPortal/lessons/github/index.tsx
@@ -0,0 +1,13 @@
+const index = () => {
+  return (
+    <div>
+      Vercel Lesson
+      <h1>Intro</h1>
+      <h1>What is vercel?</h1>
+      <h1>Getting Started with Vercel</h1>
+      <h1>Deploying your website</h1>
+    </div>
+  );
+};
+
+export default index;
