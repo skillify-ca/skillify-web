bb9cd22ec9a715174d7a7e8e1bed5b3f1129a745
Gawshan987 <gawshan.sri@gmail.com>
2023-11-19 17:33:44 -0500

    started Vercel lesson intro

--- pages/studentPortal/lessons/github/index.tsx
+++ pages/studentPortal/lessons/github/index.tsx
@@ -1,13 +1,19 @@
-const index = () => {
+export default function index() {
   return (
-    <div>
-      Vercel Lesson
+    <div className="grid grid-cols-1 px-10">
+      <h1 className="font-bold text-4xl ">Deploying a Website using Vercel</h1>
       <h1>Intro</h1>
-      <h1>What is vercel?</h1>
+      <p>
+        Let's say you've created a protfolio website that display's your
+        projects and skills. To be able for everyone wanting to check out your
+        projects you would have to have a link to your website, that you can do
+        by using Vercel.
+      </p>
       <h1>Getting Started with Vercel</h1>
+      <p>Step 1: First create a account in Vercel</p>
+      <p>Step 2: Then your going to press the add new button image here</p>
+      <img src="/images/lessons/addnew.png" width={500} height={500} />
       <h1>Deploying your website</h1>
     </div>
   );
-};
-
-export default index;
+}
