83ab270ba3bba4df3bcd52de84b3c27f62ee692f
vithushan19@users.noreply.github.com <vithushan19@gmail.com>
2023-12-01 01:08:48 -0500

    Update index.tsx

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,4 +1,4 @@
-import { Head } from "next/document";
+import Head from "next/head";
 import React from "react";
 import BlogCard from "../../components/blog/BlogCard";
 import BlogPost from "../../components/blog/BlogPost";
