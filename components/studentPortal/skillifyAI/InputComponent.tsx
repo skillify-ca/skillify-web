9347ae8edecf4ae2de0d774f2b7bcaba8a6580f7
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2023-12-14 13:02:10 -0500

    add clear button

--- components/studentPortal/skillifyAI/InputComponent.tsx
+++ components/studentPortal/skillifyAI/InputComponent.tsx
@@ -17,14 +17,19 @@ const InputComponent = () => {
     <div className="relative bottom-0 md:left-20">
       <div className="flex lg:mr-44 mr-4 md:mt-10 mt-20">
         <textarea
-          className="md:w-2/3 w-4/5  mr-4"
+          className="md:w-2/3 w-4/5 md:h-32  mr-4 rounded-xl"
           style={{ resize: "none" }}
           onChange={handleTextareaChange}
           value={prompt}
         ></textarea>
-        <Button label="submit" onClick={handleSubmit}>
-          {" "}
-        </Button>
+        <div className="flex flex-col space-y-4 mt-2">
+          <Button label="Submit" onClick={handleSubmit}>
+            {" "}
+          </Button>
+          <Button label="Clear" backgroundColor="blue" onClick={handleSubmit}>
+            {" "}
+          </Button>
+        </div>
       </div>
     </div>
   );
