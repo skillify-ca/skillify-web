e0bd216198e8a6fa3c80dc7510726b9bc87d1361
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2023-12-13 19:16:25 -0500

    add input state

--- components/studentPortal/skillifyAI/InputComponent.tsx
+++ components/studentPortal/skillifyAI/InputComponent.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../../ui/Button";
 
 const InputComponent = () => {
+  const [prompt, setPrompt] = useState("");
+
+  const handleTextareaChange = (event) => {
+    setPrompt(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Do something with the prompt, e.g., send it to an API or process it in some way
+    console.log("Prompt:", prompt);
+  };
+
   return (
-    <div className="relative bottom-0 left-20  ">
-      <div className="flex  lg:mr-44">
-        <textarea className="w-2/3 mr-6 " style={{ resize: "none" }}></textarea>
-        <Button label="submit "> </Button>
+    <div className="relative bottom-0 left-20">
+      <div className="flex lg:mr-44">
+        <textarea
+          className="w-2/3 mr-6"
+          style={{ resize: "none" }}
+          onChange={handleTextareaChange}
+          value={prompt}
+        ></textarea>
+        <Button label="submit" onClick={handleSubmit}>
+          {" "}
+        </Button>
       </div>
     </div>
   );
