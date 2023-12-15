031ac4dbcf5090aeb3595c68f8b02dfe4ed295a7
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2023-12-14 23:51:27 -0500

    Skillify AI - Frontend MVP  (#1010)
    * Create index page
    
    * add react import
    
    * create components
    
    * create conversation screen
    
    * fix chat area
    
    * style input
    
    * Update Input
    
    * add input state
    
    * update responsive design
    
    * increase text input
    
    * add clear button
    
    * swap buttons
    
    * Update InputComponent.tsx
    
    * add user image
    
    * adjust image styling
    
    and responsive desgin
    
    * insert messages
    
    * create autoscroll
    
    * fix styling
    
    * max conversation screen size
    
    * complete responsive design
    
    * Update index.tsx
    
    * modify styling
    
    * update mobile styling

---
+++ components/studentPortal/skillifyAI/MessageComponent.tsx
@@ -0,0 +1,34 @@
+// MessageComponent.tsx
+import React from "react";
+
+interface MessageComponentProps {
+  name: string;
+  message: string;
+  image: string;
+}
+
+const MessageComponent: React.FC<MessageComponentProps> = ({
+  name,
+  message,
+  image,
+}) => {
+  return (
+    <div className="px-8 md:text-lg text-md border-2 rounded bg-backgroundSecondary">
+      <div>
+        <div className="px-2 mt-12 flex items-center space-x-8 border-2 rounded-full hover:bg-charmander">
+          <img
+            src={image}
+            className="rounded-full h-12 w-12"
+            alt="User Avatar"
+          />
+          <div>
+            <p className="font-bold">{name}</p>
+            <p>{message}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MessageComponent;
