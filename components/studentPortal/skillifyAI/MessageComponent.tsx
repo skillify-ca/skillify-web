9837c5327c4af7017c2e302bf7464b4edf93eeed
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2023-12-15 16:13:19 -0500

    update message styling
    so they look fuler, and closer spaced

--- components/studentPortal/skillifyAI/MessageComponent.tsx
+++ components/studentPortal/skillifyAI/MessageComponent.tsx
@@ -15,7 +15,7 @@ const MessageComponent: React.FC<MessageComponentProps> = ({
   return (
     <div className="px-8 md:text-lg text-md border-2 rounded bg-backgroundSecondary">
       <div>
-        <div className="px-2 mt-12 flex items-center space-x-8 border-2 rounded-full hover:bg-charmander">
+        <div className="px-2 mt-12 flex items-center space-x-8 border-2 rounded-md min-h-full space-y-4 hover:bg-charmander">
           <img
             src={image}
             className="rounded-full h-12 w-12"
