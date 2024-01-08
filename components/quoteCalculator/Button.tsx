e855aef3d58c8210a675cbf2efc6d1041b181ada
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2024-01-08 11:33:04 -0500

    create skill selections

--- components/quoteCalculator/Button.tsx
+++ components/quoteCalculator/Button.tsx
@@ -4,7 +4,7 @@ const Button = () => {
   return (
     <button
       type="button"
-      className={` px-4 border-white border-2 font w-32 h-16 rounded-full "text-slate-900
+      className={` px-4 hover:bg-white hover:text-black-500 border-white border-2 font w-32 h-16 rounded-full "text-slate-900
 
     `}
     >
