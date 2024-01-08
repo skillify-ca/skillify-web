b34ec896cd20877d9eb8744a4f467b9a4e912302
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2024-01-08 10:47:01 -0500

    create initial screen

---
+++ components/quoteCalculator/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button
+      type="button"
+      className={` px-4 border-white border-2 font w-32 h-16 rounded-full "text-slate-900
+
+    `}
+    >
+      <p className={""}>Start</p>
+    </button>
+  );
+};
+
+export default Button;
--- pages/quoteCalculator/index.tsx
+++ pages/quoteCalculator/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import React from "react";
 import LandingNavbar from "../../components/landingPage/LandingNavbar";
+import Button from "../../components/quoteCalculator/Button";
+
 export default function Quote() {
   return (
     <div className="bg-charmander h-full">
@@ -9,18 +11,23 @@ export default function Quote() {
         {/* Add meta tags for SEO if needed */}
       </Head>
       <LandingNavbar />
-      <div className="w-full mx-auto bg-charmander text-white text-center h-screen max-w-7xl sm:p-8 flex flex-col space-y-4">
+      <div className="w-full mx-auto bg-charmander text-white text-center h-screen max-w-7xl sm:p-8 flex flex-col space-y-8">
         <img
           src="/images/quoteCalculator/person-studying-online.png"
           className="py-4 md:h-1/2 md:w-1/2 self-center"
+          alt="Studying Person"
         ></img>
-        <h1 className="text-2xl font-bold">
+        <h1 className="text-4xl font-bold">
           {" "}
           We're thrilled you want to learn with Skillify
         </h1>
-        <p> Complete our brief questionnaire to receive a quote.</p>
-        <button>Start</button>
-      </div>{" "}
+        <p className="text-xl">
+          Complete our brief questionnaire to receive a quote.
+        </p>
+        <div className="">
+          <Button>Start</Button>
+        </div>
+      </div>
       <p className="bottom-0 text-white text-center">
         Due to the personalized nature of our program, please note that the
         prices generated are an estimate.
