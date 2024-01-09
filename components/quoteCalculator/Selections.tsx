50e057f69b53410c6772e97bb9a332a9d9af3024
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2024-01-09 04:49:28 -0500

    Update Selections.tsx

--- components/quoteCalculator/Selections.tsx
+++ components/quoteCalculator/Selections.tsx
@@ -2,44 +2,51 @@ import React, { useState } from "react";
 import LandingNavbar from "../landingPage/LandingNavbar";
 import Progress from "../resources/quizzes/shared/Progress";
 import { Button } from "../ui/Button";
+
 type ContactProps = {
   handleClick: () => void;
 };
+
 const Contact: React.FC<ContactProps> = ({ handleClick }) => {
   const [progress] = useState(55);
 
+  const options = [
+    "Learn foundational programming concepts and build simple projects",
+    "Further my skills in programming and build more complex projects",
+    "Fully transition into a tech career",
+    "Other (Please specify)",
+  ];
+
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+  };
+
   return (
-    <div className="flex flex-col bg-slate-50  h-screen my-auto p-8  md:space-y-12 space-y-8 items-center">
-      {" "}
+    <div className="flex flex-col bg-slate-50 h-screen my-auto p-8 md:space-y-12 space-y-8 items-center">
       <LandingNavbar />
       <div className="w-full items-center flex flex-col space-y-2 md:space-y-6">
         <Progress progress={progress} />
         <div className="text-sm">2/4</div>
-      </div>{" "}
+      </div>
       <div className="mt-8 text-xl font-bold">
-        Please enter your name and email:
+        What are your learning goals?
       </div>
       <div className="flex flex-col items-center mt-4">
-        <label className="text-md font-bold" htmlFor="name">
-          Name:
-        </label>
-        <input
-          type="text"
-          id="name"
-          placeholder="Enter name"
-          className="w-80 px-2 h-12  py-1 mt-2 border border-gray-300 rounded-lg"
-        />
-      </div>
-      <div className="flex flex-col items-center mt-4">
-        <label className="text-md font-bold" htmlFor="email">
-          Email:
-        </label>
-        <input
-          type="text"
-          id="email"
-          placeholder="Enter email address"
-          className="w-80 px-2 h-12  py-1 mt-2 border border-gray-300 rounded-lg"
-        />
+        {options.map((option, index) => (
+          <div
+            key={index}
+            className={`w-80 px-4 py-2 mt-2 border rounded-full cursor-pointer ${
+              selectedOption === option
+                ? "bg-charmander text-white"
+                : "bg-white text-black-600 border-black-300"
+            }`}
+            onClick={() => handleOptionClick(option)}
+          >
+            {option}
+          </div>
+        ))}
       </div>
       <button onClick={handleClick} className="space-x-12 mt-8">
         <Button label="Next" />
