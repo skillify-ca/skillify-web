580bd6094d40a48892b413f634e3e99b221b3d25
LuckyHariharan <99484575+LuckyHariharan@users.noreply.github.com>
2024-01-09 04:33:37 -0500

    modify frontend to meet design

--- components/quoteCalculator/Selections.tsx
+++ components/quoteCalculator/Selections.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import LandingNavbar from "../landingPage/LandingNavbar";
 import Progress from "../resources/quizzes/shared/Progress";
 import { Button } from "../ui/Button";
 type ContactProps = {
@@ -8,15 +9,15 @@ const Contact: React.FC<ContactProps> = ({ handleClick }) => {
   const [progress] = useState(55);
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col my-auto p-8 space-y-12 items-center">
+      {" "}
+      <LandingNavbar />
       <Progress progress={progress} />
-
       <div className="mt-8 text-xl font-bold">
         Please enter your name and email:
       </div>
-
       <div className="flex flex-col items-center mt-4">
-        <label className="text-lg font-bold" htmlFor="name">
+        <label className="text-md font-bold" htmlFor="name">
           Name:
         </label>
         <input
@@ -26,9 +27,8 @@ const Contact: React.FC<ContactProps> = ({ handleClick }) => {
           className="w-40 px-2 py-1 mt-2 border border-gray-300 rounded"
         />
       </div>
-
       <div className="flex flex-col items-center mt-4">
-        <label className="text-lg font-bold" htmlFor="email">
+        <label className="text-md font-bold" htmlFor="email">
           Email:
         </label>
         <input
@@ -38,9 +38,9 @@ const Contact: React.FC<ContactProps> = ({ handleClick }) => {
           className="w-40 px-2 py-1 mt-2 border border-gray-300 rounded"
         />
       </div>
-
-      <button onClick={handleClick} className="mt-8">
+      <button onClick={handleClick} className="space-x-12 mt-8">
         <Button label="Next" />
+        <Button label="Back" backgroundColor="white" textColor="text-black" />
       </button>
     </div>
   );
