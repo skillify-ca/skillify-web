af6bf622e187be2c7d6d6009d40c5ac7e1b6cbd9
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2024-02-19 15:57:44 -0500

    Update ProfileGoalBadge.tsx

--- components/studentPortal/profileV2/ProfileGoalBadge.tsx
+++ components/studentPortal/profileV2/ProfileGoalBadge.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { userGoalsSelector } from "../../../redux/userGoalsSlice";
 
-export type ProfileGoalBadgeProps = {};
-
-export default function ProfileGoalBadge({}: ProfileGoalBadgeProps) {
-  const { userGoals, goalsSections } = useSelector(userGoalsSelector);
-  return (
+export default function ProfileGoalBadge() {
+  const { userGoals } = useSelector(userGoalsSelector);
+  return userGoals.filter(
+    (it) => !it.isComplete && new Date(it.targetDate) < new Date()
+  ).length > 0 ? (
     <div className="flex items-center justify-center gap-2 p-2 bg-red-300 rounded-full">
       <HandIcon className="w-4 h-4 text-red-700 border-2 border-red-700 rounded-full" />
       <p className="text-sm font-bold text-red-800">
@@ -19,5 +19,5 @@ export default function ProfileGoalBadge({}: ProfileGoalBadgeProps) {
         Overdue
       </p>
     </div>
-  );
+  ) : null;
 }
