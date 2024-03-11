410e0ba414bb8ee9696d19e45460922210379053
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2024-03-11 02:47:32 -0400

    fix

--- graphql/studentPortal/users/initializeUser.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.upsertUserQuery = void 0;
-exports.upsertUserQuery = "\nmutation initializeUser ($userId: String!, $name:String, $email:String){\n  insert_users(objects: [{ id: $userId, name:$name, email:$email  }], on_conflict: { constraint: users_pkey, update_columns: [] }) {\n    affected_rows\n}\n\n}\n\n\n\n";
