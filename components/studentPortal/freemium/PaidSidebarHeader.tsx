4d18f9c4cfad0047795a08acbafe8ee2408dd2bd
vithushan19@users.noreply.github.com <vithushan19@users.noreply.github.com>
2024-10-22 13:35:39 -0400

    use avatar fallback for sidebar

--- components/studentPortal/freemium/PaidSidebarHeader.tsx
+++ components/studentPortal/freemium/PaidSidebarHeader.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { useAuth } from "../../../lib/authContext";
+import { Avatar, AvatarFallback, AvatarImage } from "../../ui/Avatar";
 
 export default function PaidSidebarHeader({ userProfileImage, userRole }) {
   const { user } = useAuth();
   return (
     <div className="flex p-4">
-      {user && (
-        <img className="w-16 h-16 rounded-full" src={userProfileImage} alt="" />
-      )}
-      {user && (
-        <div className="w-full">
-          <p className="w-full ml-4 font-bold">{user.displayName}</p>
-          <p className="ml-4 font-medium capitalize">{userRole}</p>
+      <div className="flex flex-col w-full border-b-2 bg-backgroundPrimary text-textPrimary">
+        <div className="grid">
+          {user && (
+            <div className="flex flex-col items-center w-full gap-4 p-4">
+              <Avatar className="bg-slate-200">
+                <AvatarImage src={user.photoURL} alt="user avatar" />
+                <AvatarFallback>{user.displayName.charAt(0)}</AvatarFallback>
+              </Avatar>
+              <div className="w-full ml-4">
+                <p className="text-lg font-bold">{user.displayName}</p>
+                <p className="font-medium text-gray-500 capitalize">
+                  {userRole}
+                </p>
+              </div>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 }
