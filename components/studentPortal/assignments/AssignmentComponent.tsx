37323cf06e46db245a3771c96b50b2acf9034da9
vithushan19@users.noreply.github.com <vithushan19@gmail.com>
2024-10-24 13:09:04 -0400

    apply next image codemod

--- components/studentPortal/assignments/AssignmentComponent.tsx
+++ components/studentPortal/assignments/AssignmentComponent.tsx
@@ -1,5 +1,5 @@
 import { Sandpack } from "@codesandbox/sandpack-react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import React from "react";
 import ExpandableContainer from "../../ui/ExpandableContainer";
 import AssignmentInputBox from "./AssignmentInputBox";
