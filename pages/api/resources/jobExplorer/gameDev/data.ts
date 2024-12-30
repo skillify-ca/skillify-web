004bcce5ef91be0b67161e9d43ba9d032fa86179
Curtoista <145144842+Curtoista@users.noreply.github.com>
2024-12-30 16:33:38 -0500

    Update job explorer (#1018)
    * Fixed broken images to unique images for each career path
    
    * add link for game developer to link to career path page
    
    * Added mandatory skills to game dev page
    
    * Added additional skills for game dev
    
    * added disabled Jobs array
    
    * Change Config back to React
    
    * Edit Image names, refactor jobListComponent

--- pages/api/resources/jobExplorer/gameDev/data.ts
+++ pages/api/resources/jobExplorer/gameDev/data.ts
@@ -1,12 +1,113 @@
+
+
 export const title = "Game Developer";
 
 export const description =
   "A game developer is a creative and skilled professional responsible for designing, developing, and producing video games for various platforms, including consoles, PCs, mobile devices, and virtual reality. Game developers work collaboratively in teams, combining technical expertise with artistic creativity to create immersive and engaging gaming experiences for players around the world.";
 
+
 export const mandatorySkills = [
+  {
+    name: "Unreal Engine (C++)",
+    link: "https://academy.unrealengine.com/totara/dashboard/index.php?id=10",
+  },
+  {
+    name: "Unity (C#)",
+    link: "https://learn.unity.com/",
+  },
+  
+  {
+    name: "Godot (GDScript, C#, or C++)",
+    link: "https://docs.godotengine.org/en/stable/getting_started/introduction/introduction_to_godot.html",
+  },
+  
+  {
+    name: "C++",
+    link: "https://www.w3schools.com/cpp/",
+  },
+
+  {
+    name: "C#",
+    link: "https://www.w3schools.com/cs/index.php",
+  },
+
+  {
+    name: "Python",
+    link: "https://www.python.org/",
+  },
+
+  {
+    name: "Version Control/Git",
+    link: "https://www.atlassian.com/git/tutorials/what-is-git",
+  },
+
+  {
+    name: "Strong Foundation in math and science (linear algebra, geometry, physics)",
+    link: "https://www.khanacademy.org/",
+  },
+
+  {
+    name: "Data structures and Algorithms",
+    link: "https://www.w3schools.com/dsa/",
+  },
+
+  {
+    name: "Gameplay programming and scripting",
+    link: "https://learn.unity.com/project/beginner-gameplay-scripting",
+  },
+
+  {
+    name: "Testing and Debugging",
+    link: "https://jestjs.io/docs/getting-started",
+  },
+
+  {
+    name: "Asset integration",
+    link: "https://www.diversion.dev/blog/the-key-to-proper-asset-management-in-game-development"
+  },
+
+  
 ];
 
 export const additionalSkills = [
+  {
+    name: "Shader Programming",
+    link: "https://learnopengl.com/",
+  },
+  {
+    name: "Networking",
+    link: "https://gamedev.net/",
+  },
+  {
+    name: "Backend Development  ",
+    link: "https://www.boot.dev/",
+  },
+  {
+    name: "Java",
+    link: "https://www.w3schools.com/java/java_intro.asp",
+  },
+  
+  {
+    name: "Javascript",
+    link: "https://www.w3schools.com/js/js_intro.asp",
+  },
+  {
+    name: "Gaming AI",
+    link: "https://www.codingame.com/start/",
+  },
+  {
+    name: "Augmented and Virtual reality",
+    link: "https://gamedevacademy.org/category/vr-ar-tutorials/",
+  },
+  {
+    name: "Game art design",
+    link: "https://www.hyperpad.com/blog/free-art-tools-for-game-design",
+  },
+  {
+    name: "GDScript",
+    link: "https://www.gdquest.com/tutorial/godot/gdscript/intro/",
+  },
+ 
 ];
 
 export const project = {
