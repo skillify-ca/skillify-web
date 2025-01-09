07fdc5d32ebf444cf805ff81aec035af296503c0
LuckyHariharan <lharihar@uwaterloo.ca>
2025-01-09 09:41:40 -0500

    Create LuckyInterviewData.ts

---
+++ pages/api/studentPortal/interviews/LuckyInterviewData.ts
@@ -0,0 +1,150 @@
+import { Offer } from "../../../../components/resources/jobTracker/JobTrackerComponent";
+
+// dummyInterviewData.json
+export const LuckyInterviewData: Offer[]=[
+    {
+      "Company": "Robinhood",
+      "companyUrl": "https://www.robinhood.com/",
+      "Location": "Remote",
+      "Role": "Frontend Developer",
+      "Time Spent": "March 2024",
+      "timeSpentHours": 10,
+      "Result": "Failed First Technical Round",
+      "Base": 0,
+      "opportunitySource": "Livecoding - List of instruments with filtering",
+      "industry": "Finance"
+    },
+    {
+      "Company": "Ramp",
+      "companyUrl": "https://www.ramp.com/",
+      "Location": "Remote",
+      "Role": "Frontend Developer",
+      "Time Spent": "March 2024",
+      "timeSpentHours": 20,
+      "Result": "Failed Final Technical Round",
+      "Base": 0,
+      "opportunitySource": "Takehome and Livecoding - Tic Tac Toe App",
+      "industry": "Finance"
+    },
+    {
+      "Company": "KonradGroup",
+      "companyUrl": "https://www.konrad.com/",
+      "Location": "Toronto",
+      "Role": "Fullstack Developer",
+      "Time Spent": "June/July 2024",
+      "timeSpentHours": 15,
+      "Result": "Failed Final Technical Round",
+      "Base": 0,
+      "opportunitySource": "In-Person Interview and Takehome Challenge",
+      "industry": "Tech"
+    },
+    {
+      "Company": "NetGuru",
+      "companyUrl": "https://www.netguru.com/",
+      "Location": "Remote",
+      "Role": "Frontend Developer",
+      "Time Spent": "June/July 2024",
+      "timeSpentHours": 18,
+      "Result": "Passed Final Round, No Offer (Recruitment Paused)",
+      "Base": 0,
+      "opportunitySource": "Completed Takehome and Livecoding",
+      "industry": "Tech Consulting"
+    },
+    {
+      "Company": "Finofo",
+      "companyUrl": "https://www.finofo.com/",
+      "Location": "Remote",
+      "Role": "Frontend Developer",
+      "Time Spent": "June/July 2024",
+      "timeSpentHours": 10,
+      "Result": "Failed Takehome",
+      "Base": 0,
+      "opportunitySource": "Takehome - Fruity Selector App",
+      "industry": "Finance"
+    },
+    {
+      "Company": "NDAX",
+      "companyUrl": "https://www.ndax.io/",
+      "Location": "Remote",
+      "Role": "Frontend Developer",
+      "Time Spent": "June/July 2024",
+      "timeSpentHours": 8,
+      "Result": "Failed First Technical Round",
+      "Base": 0,
+      "opportunitySource": "Technical Interview with HR Misalignment",
+      "industry": "Crypto"
+    },
+    {
+      "Company": "Toast",
+      "companyUrl": "https://pos.toasttab.com/",
+      "Location": "Remote",
+      "Role": "Fullstack Developer",
+      "Time Spent": "September 2024",
+      "timeSpentHours": 15,
+      "Result": "Failed Final Technical Round",
+      "Base": 0,
+      "opportunitySource": "Livecoding and In-Person Interviews",
+      "industry": "Tech"
+    },
+    {
+      "Company": "Shopify",
+      "companyUrl": "https://www.shopify.com/",
+      "Location": "Remote",
+      "Role": "Fullstack Developer",
+      "Time Spent": "September 2024",
+      "timeSpentHours": 20,
+      "Result": "Failed First Technical Round",
+      "Base": 0,
+      "opportunitySource": "Prescreen, Livecoding, and Assignment",
+      "industry": "E-Commerce"
+    },
+    {
+      "Company": "Instacart",
+      "companyUrl": "https://www.instacart.com/",
+      "Location": "Remote",
+      "Role": "Fullstack Developer",
+      "Time Spent": "September 2024",
+      "timeSpentHours": 15,
+      "Result": "Failed First Technical Round",
+      "Base": 0,
+      "opportunitySource": "Prescreen, Livecoding, and Algorithm Challenges",
+      "industry": "Tech"
+    },
+    {
+      "Company": "Be Think Labs",
+      "companyUrl": "https://www.bethinklabs.com/",
+      "Location": "Remote",
+      "Role": "Java Developer",
+      "Time Spent": "September 2024",
+      "timeSpentHours": 10,
+      "Result": "Failed First Technical Round",
+      "Base": 0,
+      "opportunitySource": "TDD and Java Data Structure Practice",
+      "industry": "Tech"
+    },
+    {
+      "Company": "Robinhood R2",
+      "companyUrl": "https://www.robinhood.com/",
+      "Location": "Remote",
+      "Role": "Android Developer",
+      "Time Spent": "Upcoming Interview",
+      "timeSpentHours": 0,
+      "Result": "Upcoming Interview",
+      "Base": 0,
+      "opportunitySource": "Financial Instrument, Tic Tac Toe, Android Studio",
+      "industry": "Finance"
+    },
+    {
+      "Company": "Other",
+      "companyUrl": "https://www.example.com/",
+      "Location": "Remote",
+      "Role": "Various Roles",
+      "Time Spent": "Ongoing",
+      "timeSpentHours": 50,
+      "Result": "Failed Multiple Technical Rounds",
+      "Base": 0,
+      "opportunitySource": "LeetCode and Blockchain Practice",
+      "industry": "Tech"
+    }
+  ]
+  
