1097ed84f66130f4273d7ab993ca7d228a84f0ac
LuckyHariharan <lharihar@uwaterloo.ca>
2025-01-14 10:41:00 -0500

    Build Product Manager & Project pages

---
+++ pages/resources/jobExplorer/productManagerProject/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductManagerProject from '../../../../components/resources/jobExplorer/productManager/ProductManagerProject'
+
+const ProductManagerPage = () => {
+  return (
+    <ProductManagerProject/>
+  )
+}
+
+export default ProductManagerPage
