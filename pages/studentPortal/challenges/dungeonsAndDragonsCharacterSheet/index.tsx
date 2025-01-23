3b8889f66fe10c334d2ab845ec706cbd3ca62161
vithushan19@users.noreply.github.com <vithushan19@gmail.com>
2025-01-23 18:41:31 -0500

    update text and show progress

--- pages/studentPortal/challenges/dungeonsAndDragonsCharacterSheet/index.tsx
+++ pages/studentPortal/challenges/dungeonsAndDragonsCharacterSheet/index.tsx
@@ -53,7 +53,7 @@ function DungeonsAndDragonsCharacterSheet() {
     {
       5: (
         <p>
-          5. Implement skills. See `SKILL_LIST` for the list of all skills and
+          Implement skills. See `SKILL_LIST` for the list of all skills and
           their attribute modifier - Characters have 10 + (4 * Intelligence
           Modifier) points to spend between skills. - There is a minimum of 0,
           but no maximum aside from the total points available to spend. - for
@@ -73,12 +73,12 @@ function DungeonsAndDragonsCharacterSheet() {
     {
       6: (
         <p>
-          6. Save the character(s) to an API so they can be retrieved when the
-          app starts next time. - Make a post request with a JSON payload to
-          %POST URL% to save data, and a get request to %GET URL% to retrieve
-          the data. It will accept any valid JSON blob and return the most
-          recent version - for example, if your github username is vithushan19,
-          you would use %URL% (include the curly braces) - you must include a
+          Save the character(s) to an API so they can be retrieved when the app
+          starts next time. - Make a post request with a JSON payload to %POST
+          URL% to save data, and a get request to %GET URL% to retrieve the
+          data. It will accept any valid JSON blob and return the most recent
+          version - for example, if your github username is vithushan19, you
+          would use %URL% (include the curly braces) - you must include a
           content-type header of application/json for the post to be accepted{" "}
         </p>
       ),
@@ -86,7 +86,7 @@ function DungeonsAndDragonsCharacterSheet() {
     {
       7: (
         <p>
-          7. Implement a maximum on all attributes of 70. For example, if a
+          Implement a maximum on all attributes of 70. For example, if a
           character has 20 strength and 10 for all other attributes, they must
           decrease one before they can increase another
         </p>
@@ -95,7 +95,7 @@ function DungeonsAndDragonsCharacterSheet() {
     {
       8: (
         <p>
-          8. Add the ability to edit multiple characters simultaneously with the
+          Add the ability to edit multiple characters simultaneously with the
           same rules above
         </p>
       ),
@@ -103,7 +103,7 @@ function DungeonsAndDragonsCharacterSheet() {
     {
       9: (
         <p>
-          9. Add a Skill Check section for each character. This represents a
+          Add a Skill Check section for each character. This represents a
           character's attempt to perform an action - the character is successful
           if they meet or exceed the DC (see below) of the skill check. Add the
           total skill to a random number between 1 and 20 inclusive, if this
@@ -121,10 +121,9 @@ function DungeonsAndDragonsCharacterSheet() {
     {
       10: (
         <p>
-          10. Add a party skill check section. This is the same as the above,
-          except we should use the character with the highest skill total to
-          attempt the action - Show which character was selected to attempt the
-          action
+          Add a party skill check section. This is the same as the above, except
+          we should use the character with the highest skill total to attempt
+          the action - Show which character was selected to attempt the action
         </p>
       ),
     },
@@ -173,6 +172,22 @@ function DungeonsAndDragonsCharacterSheet() {
         </ul>
       </div>
       <div>
+        <h2 className="text-lg font-bold">Current Progress</h2>
+        <div className="p-4 bg-backgroundSecondary">
+          Github PR Links:
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            {pullRequestLinks.map((link, index) => (
+              <div key={index}>
+                Level {index + 1}:{" "}
+                <a href={link} target="_blank" rel="noreferrer noopener">
+                  {link}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div>
         <h2 className="text-lg font-bold">Requirements</h2>
         <p>
           You will be building this app in levels. Each level will build on the
@@ -184,26 +199,51 @@ function DungeonsAndDragonsCharacterSheet() {
           Current Level: Level {currentLevel}
         </h2>
 
-        {levelDescriptions[currentLevel - 1][currentLevel]}
-
-        <Input
-          placeholder="Enter your Github PR link here"
-          setValue={(e) =>
-            setPullRequestLinks(
-              pullRequestLinks.map((link, index) =>
-                index === currentLevel - 1 ? e.target.value : link
+        <div className="h-64 p-4 overflow-y-auto rounded shadow bg-backgroundSecondary">
+          {levelDescriptions[currentLevel - 1][currentLevel]}
+        </div>
+      </div>
+      <div>
+        <div className="flex items-center gap-4">
+          <Input
+            placeholder="Enter your Github PR link here"
+            setValue={(e) =>
+              setPullRequestLinks(
+                pullRequestLinks.map((link, index) =>
+                  index === currentLevel - 1 ? e.target.value : link
+                )
               )
-            )
-          }
-          value={pullRequestLinks[currentLevel - 1]}
-        />
+            }
+            value={pullRequestLinks[currentLevel - 1]}
+          />
+          <Button
+            label="Submit"
+            onClick={() => {
+              console.log(
+                "Submitted PR link: ",
+                pullRequestLinks[currentLevel - 1]
+              );
+            }}
+          />
+        </div>
+      </div>
+      <div className="flex gap-4">
+        {/* prev button */}
         <Button
-          label="Submit"
+          label="Previous"
           onClick={() => {
-            console.log(
-              "Submitted PR link: ",
-              pullRequestLinks[currentLevel - 1]
-            );
+            if (currentLevel > 1) {
+              setCurrentLevel(currentLevel - 1);
+            }
+          }}
+        />
+        {/* next button */}
+        <Button
+          label="Next"
+          onClick={() => {
+            if (currentLevel < MAX_LEVEL) {
+              setCurrentLevel(currentLevel + 1);
+            }
           }}
         />
       </div>
