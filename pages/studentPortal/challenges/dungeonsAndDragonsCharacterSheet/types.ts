f3add1c48fa44a41360ee66627e8c8187c6707e8
Vithushan <vithushan19@users.noreply.github.com>
2025-01-28 11:25:53 -0500

    feat: create a dnd interview challenge (#1029)
    * create dnd challenge
    
    * update text and show progress

---
+++ pages/studentPortal/challenges/dungeonsAndDragonsCharacterSheet/types.ts
@@ -0,0 +1,10 @@
+export type Attributes = {
+  Strength: number;
+  Dexterity: number;
+  Constitution: number;
+  Intelligence: number;
+  Wisdom: number;
+  Charisma: number;
+};
+
+export type Class = "Barbarian" | "Wizard" | "Bard";
