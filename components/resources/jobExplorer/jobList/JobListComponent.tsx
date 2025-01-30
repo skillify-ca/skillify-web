ed9330238bde6b608870f72cc38c854a2e8bd972
vithushan19@users.noreply.github.com <vithushan19@gmail.com>
2025-01-30 13:43:52 -0500

    create react native job page

--- components/resources/jobExplorer/jobList/JobListComponent.tsx
+++ components/resources/jobExplorer/jobList/JobListComponent.tsx
@@ -1,14 +1,12 @@
 import Image from "next/legacy/image";
 import React from "react";
 
-
-const disabledJobs = [
-];
+const disabledJobs = [];
 
 const JobGroup = ({ subheading, jobs }) => (
   <div className="mb-12">
-    <h2 className="text-2xl font-bold mb-4">{subheading}</h2>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+    <h2 className="mb-4 text-2xl font-bold">{subheading}</h2>
+    <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
       {jobs.map((job) => {
         const isDisabled = disabledJobs.includes(job.title); // Check if job.title exists in disabledJobs array
 
@@ -21,7 +19,7 @@ const JobGroup = ({ subheading, jobs }) => (
                 : "bg-orange-500 text-white hover:bg-orange-600 hover:scale-105"
             } cursor-pointer transition duration-300`}
           >
-            <h2 className="text-lg font-semibold mb-4">
+            <h2 className="mb-4 text-lg font-semibold">
               <a href={job.link} className="text-black hover:underline">
                 {job.title}
               </a>
@@ -44,8 +42,6 @@ const JobGroup = ({ subheading, jobs }) => (
     </div>
   </div>
 );
-
-
 
 const JobListComponent = () => {
   const jobs = [
@@ -99,18 +95,37 @@ const JobListComponent = () => {
       link: "././cyberSecurityAnalyst",
     },
     {
-      title: "Mobile Developer",
-      image: "/images/resources/jobExplorer/MobileDev.jpg",
+      title: "React Native Developer",
+      image: "/images/resources/jobExplorer/reactNative.png",
       width: 100,
       height: 100,
-      link: "././mobileAppDev",
+      link: "././reactNativeDeveloper",
+      group: "mobile",
+    },
+    {
+      // android developer
+      title: "Android Developer",
+      image: "/images/resources/jobExplorer/android.png",
+      width: 100,
+      height: 100,
+      link: "././androidAppDev",
+      group: "mobile",
+    },
+    {
+      // ios developer
+      title: "iOS Developer",
+      image: "/images/resources/jobExplorer/ios.png",
+      width: 100,
+      height: 100,
+      link: "././iosAppDev",
+      group: "mobile",
     },
     {
       title: "Game Developer",
       image: "/images/resources/jobExplorer/GameDev.jpg",
       width: 100,
       height: 100,
-      link: "././gameDev"
+      link: "././gameDev",
     },
     {
       title: "UX Designer",
@@ -135,18 +150,41 @@ const JobListComponent = () => {
     },
   ];
 
-  const beginnerJobs = jobs.filter((job) => job.title.includes("Front End Developer") || job.title.includes("Back End Developer") || job.title.includes("Data Scientist"));
-  const intermediateJobs = jobs.filter((job) => job.title.includes("Fullstack Developer") || job.title.includes("Mobile Developer") || job.title.includes("Game Developer"));
-  const advancedJobs = jobs.filter((job) => job.title.includes("AI Engineer") || job.title.includes("Cyber Security Analyst") || job.title.includes("DevOps Engineer"));
-  const nonTechnicalJobs = jobs.filter((job) => job.title.includes("UX Designer") || job.title.includes("Product Manager") || job.title.includes("Digital Marketer"));
+  const beginnerJobs = jobs.filter(
+    (job) =>
+      job.title.includes("Front End Developer") ||
+      job.title.includes("Back End Developer") ||
+      job.title.includes("Data Scientist")
+  );
+  const intermediateJobs = jobs.filter(
+    (job) =>
+      job.title.includes("Fullstack Developer") ||
+      job.title.includes("Game Developer") ||
+      job.group === "mobile"
+  );
+  const advancedJobs = jobs.filter(
+    (job) =>
+      job.title.includes("AI Engineer") ||
+      job.title.includes("Cyber Security Analyst") ||
+      job.title.includes("DevOps Engineer")
+  );
+  const nonTechnicalJobs = jobs.filter(
+    (job) =>
+      job.title.includes("UX Designer") ||
+      job.title.includes("Product Manager") ||
+      job.title.includes("Digital Marketer")
+  );
 
   return (
-    <div className="bg-gray-100 min-h-screen py-8">
+    <div className="min-h-screen py-8 bg-gray-100">
       <div className="flex flex-col items-center justify-center max-w-6xl mx-auto">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">Job Explorer</h1>
+        <h1 className="mb-4 text-4xl font-bold text-blue-600">Job Explorer</h1>
 
-        <p className="text-lg text-gray-700 mb-8 text-center w-3/4 mx-auto">
-          Job Explorer is a helpful tool designed to assist students in learning about the requirements and workings of popular jobs in the tech industry. Explore various job roles, their responsibilities, and the skills needed to excel in these careers.
+        <p className="w-3/4 mx-auto mb-8 text-lg text-center text-gray-700">
+          Job Explorer is a helpful tool designed to assist students in learning
+          about the requirements and workings of popular jobs in the tech
+          industry. Explore various job roles, their responsibilities, and the
+          skills needed to excel in these careers.
         </p>
 
         <div className="grid grid-cols-1 gap-8">
---
+++ pages/resources/jobExplorer/reactNativeDeveloper.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import LandingNavbar from "../../../components/landingPage/LandingNavbar";
+import JobExplorerJobComponent from "../../../components/resources/jobExplorer/JobExplorerJobComponent";
+import { REACT_NATIVE_DEVELOPER_DATA } from "../../api/resources/jobExplorer/reactNativeDeveloper";
+
+export default function MobileAppDevPage() {
+  const { title, description, mandatorySkills, additionalSkills, project } =
+    REACT_NATIVE_DEVELOPER_DATA;
+  return (
+    <div>
+      <JobExplorerJobComponent
+        title={title}
+        description={description}
+        mandatorySkills={mandatorySkills}
+        additionalSkills={additionalSkills}
+        project={project}
+      />
+    </div>
+  );
+}
+
+MobileAppDevPage.getLayout = function getLayout(page) {
+  return (
+    <div className="theme-default">
+      <LandingNavbar />
+      {page}
+    </div>
+  );
+};
