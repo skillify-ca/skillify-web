09b07b865fe7270b08ffad819f6cec62e7a5de90
Curtis-F <fullman1188@gmail.com>
2025-01-31 16:00:35 -0500

    - refactored data for beginner jobs into one object -refactored beginner jobs to render using slug file and new data object -deleted old beginner job pages

--- pages/api/resources/jobExplorer/fetchJobDetailsData.ts
+++ pages/api/resources/jobExplorer/fetchJobDetailsData.ts
@@ -1,11 +1,16 @@
 import { ANDROID_DEVELOPER_DATA } from "./androidDeveloper";
 import { IOS_DEVELOPER_DATA } from "./iosDeveloper";
 import { REACT_NATIVE_DEVELOPER_DATA } from "./reactNativeDeveloper";
-
+import { BACK_END_DEVELOPER_DATA } from "./backEndDev/data";
+import { FRONT_END_DEVELOPER_DATA } from "./frontEndDev/data";
+import { DATA_SCIENTIST_DATA } from "./dataScientist/data";
 const jobDetailsMap = {
   reactNativeDeveloper: REACT_NATIVE_DEVELOPER_DATA,
   iosDeveloper: IOS_DEVELOPER_DATA,
   androidDeveloper: ANDROID_DEVELOPER_DATA,
+  backEndDev: BACK_END_DEVELOPER_DATA,
+  frontEndDev: FRONT_END_DEVELOPER_DATA,
+  dataScientist: DATA_SCIENTIST_DATA,
 };
 
 export const fetchJobDetailsData = async (jobTitle) => {
