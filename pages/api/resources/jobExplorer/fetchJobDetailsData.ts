0ae795e6e2f64d23642c2846cb3ef9e084e55b58
Vithushan <vithushan19@users.noreply.github.com>
2025-01-31 22:59:40 -0500

    remove ai engineer components in favour of the re-usable one (#1035)

--- pages/api/resources/jobExplorer/fetchJobDetailsData.ts
+++ pages/api/resources/jobExplorer/fetchJobDetailsData.ts
@@ -1,3 +1,4 @@
+import { AI_ENGINEER_JOB_DATA } from "./aiEngineer/data";
 import { ANDROID_DEVELOPER_DATA } from "./androidDeveloper";
 import { IOS_DEVELOPER_DATA } from "./iosDeveloper";
 import { REACT_NATIVE_DEVELOPER_DATA } from "./reactNativeDeveloper";
@@ -6,6 +7,7 @@ const jobDetailsMap = {
   reactNativeDeveloper: REACT_NATIVE_DEVELOPER_DATA,
   iosDeveloper: IOS_DEVELOPER_DATA,
   androidDeveloper: ANDROID_DEVELOPER_DATA,
+  aiEngineer: AI_ENGINEER_JOB_DATA,
 };
 
 export const fetchJobDetailsData = async (jobTitle) => {
