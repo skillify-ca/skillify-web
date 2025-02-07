b531415a7c3aae151ae26a81dc6ecad3b0f82a21
Curtis-F <fullman1188@gmail.com>
2025-02-07 18:03:39 -0500

    -Moved all mobile dev dev data files into their respective folder -Deleted old data files -Imported new data file outlines into the job explorer slug -created files for the mobile dev project data

---
+++ pages/api/resources/jobExplorer/androidDev/data.ts
@@ -0,0 +1,26 @@
+export const ANDROID_DEVELOPER_DATA = {
+    title: "Android Developer",
+    description: "description",
+    mandatorySkills: [
+      {
+        name: "skill1",
+        link: "link",
+      },
+      {
+        name: "skill2",
+        link: "link",
+      },
+    ],
+    additionalSkills: [
+      {
+        name: "skill1",
+        link: "link",
+      },
+      {
+        name: "skill2",
+        link: "",
+      },
+    ],
+    project: { name: "project", link: "link" },
+  };
+  
--- pages/api/resources/jobExplorer/fetchJobDetailsData.ts
+++ pages/api/resources/jobExplorer/fetchJobDetailsData.ts
@@ -1,12 +1,12 @@
 import { AI_ENGINEER_JOB_DATA } from "./aiEngineer/data";
-import { ANDROID_DEVELOPER_DATA } from "./androidDeveloper";
-import { IOS_DEVELOPER_DATA } from "./iosDeveloper";
 import { REACT_NATIVE_DEVELOPER_DATA } from "./reactNativeDeveloper";
 import { BACK_END_DEVELOPER_DATA } from "./backEndDev/data";
 import { FRONT_END_DEVELOPER_DATA } from "./frontEndDev/data";
 import { DATA_SCIENTIST_DATA } from "./dataScientist/data";
 import { FULLSTACK_DEVELOPER_DATA } from "./fullstackDev/data";
 import { GAME_DEVELOPER_DATA } from "./gameDev/data";
+import { ANDROID_DEVELOPER_DATA } from "./androidDev/data";
+import { IOS_DEVELOPER_DATA } from "./iosDev/data";
 
 const jobDetailsMap = {
   reactNativeDeveloper: REACT_NATIVE_DEVELOPER_DATA,
