6c14df0d1f438b6dca39b45385a1cfa643c6b100
RAWjasekaram <rishna.rajasekaram@gmail.com>
2025-02-19 12:43:22 -0500

    add description + 6 links
    updated description, and added a few links

--- pages/api/resources/jobExplorer/reactNativeDeveloper.ts
+++ pages/api/resources/jobExplorer/reactNativeDeveloper.ts
@@ -1,24 +1,33 @@
 export const REACT_NATIVE_DEVELOPER_DATA = {
   title: "React Native Developer",
-  description: "description",
+  description: "A React Native Developer builds mobile applications for iOS and Android platforms using React Native, a JavaScript framework. Their main responsibility is to write efficient, reusable, and maintainable code that creates smooth, user-friendly mobile apps. They collaborate with designers and back-end developers to implement mobile features, optimize performance, and ensure cross-platform functionality. React Native developers also handle debugging, testing, and deployment of mobile applications, while staying up-to-date with the latest mobile development trends and best practices. Their goal is to deliver high-quality, scalable apps that offer a seamless experience across devices.",
   mandatorySkills: [
     {
       name: "Wiki Page, general info",
       link: "https://en.wikipedia.org/wiki/React_Native",
     },
     {
-      name: "skill2",
-      link: "link",
+      name: "React",
+      link: "https://react.dev/",
     },
+    {
+      name: "Javascript",
+      link: "https://www.codecademy.com/learn/introduction-to-javascript",
+    },
+    {
+      name: "CSS",
+      link: "https://www.codecademy.com/learn/learn-css",
+    },
+  
   ],
   additionalSkills: [
     {
-      name: "skill1",
-      link: "link",
+      name: "Debugging",
+      link: "https://reactnative.dev/docs/debugging",
     },
     {
-      name: "skill2",
-      link: "",
+      name: "Mobile Development",
+      link: "https://reactnative.dev/docs/intro-react-native-components#views-and-mobile-development",
     },
   ],
   project: { name: "project", link: "link" },
