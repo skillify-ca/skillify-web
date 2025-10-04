5506753a29e8c34a81f799dfe23669f210489b11
Vithushan <vithushan19@gmail.com>
2025-10-04 08:26:30 -0400

    Update [...slug].tsx

--- pages/studentPortal/courses/vibeCoding/[...slug].tsx
+++ pages/studentPortal/courses/vibeCoding/[...slug].tsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import React from "react";
 import { LessonComponentData } from "../../../../components/studentPortal/lessons/LessonComponent";
 import ProgressBar from "../../../../components/ui/ProgressBar";
 import { getLessonForVibeCodingCourse } from "../../../api/studentPortal/courses/vibeCoding";
