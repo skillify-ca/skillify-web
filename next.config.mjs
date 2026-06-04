7e41280c20e04105e805274bdb9bd790b9477746
Vithushan <vithushan19@gmail.com>
2026-06-04 14:17:39 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,15 @@ export default withMDX({
           { key: "Content-Type", value: "application/javascript" },
         ],
       },
+      {
+        source: "/api/curtis",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "false" },
+          { key: "Access-Control-Allow-Origin", value: 'http://localhost:5174' },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+        ]
+      }
     ];
   },
 });
